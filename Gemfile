ce687fd2104e78b4cd77612dc22ed9ed261780ea
Fernando Espinosa <fer@ferdev.com>
2012-05-16 17:13:00 +0200

    First version

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+gem 'rails', '~> 3.2.3'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gfw::Application.config.secret_token = '8418d1a1a1b9a0c154e47a270825bfee73035ed72b269232fdb1ddfc03a29cb92b4e44f7c64e7d1447863c132f95e4d06c8058afd7ad2fcdf4d13a24d912e058'
