a900f4521ded227205deda1ef66dc835c186edab
Javier Arce <javierarce@gmail.com>
2012-05-18 16:44:51 +0200

    Adds demo controller to showcase html elements

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,5 @@
+#encoding: UTF-8
+class DemoController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Gfw::Application.routes.draw do
   resources :countries
+  resources :demo
+
   resources :areas, :path => :map
   root :to => 'home#index'
 end
