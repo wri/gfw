306a69fc36c9118e4fe7eddefe80c8f4a88a6c9c
Fernando Espinosa <fer@ferdev.com>
2012-06-04 13:54:57 +0200

    Disables database calls in assets precompile task

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,9 @@ Gfw::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  # Disables database calls in precompile
+  config.assets.initialize_on_precompile=false
+
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
 
