4751f39c32555dba26a06d6cdce5fbf81e061dcd
Javier de la Torre <jatorre@gmail.com>
2012-06-13 21:10:27 -0400

    changes on blog

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,14 @@
 #encoding: UTF-8
 class PostsController < ApplicationController
   def show
-    @post = OpenStruct.new({
-      name: 'Central African Republic',
-      alerts_count: 2931,
-      description: (<<-EOF
-        The Central African Republic (CAR) (French: République centrafricaine, pronounced: [ʁepyblik sɑ̃tʁafʁikɛn], or Centrafrique [sɑ̃tʀafʀik]; Sango Ködörösêse tî Bêafrîka), is a landlocked country in Central Africa. It borders Chad in the north, Sudan in the northeast, South Sudan in the east, the Democratic Republic of the Congo and Congo in the south, and Cameroon in the west. The CAR covers a land area of about 240,000 square miles (620,000 km2), and has an estimated population of about 4.4 million as of 2008. Bangui is the capital city.
-      EOF
-      ),
-      last_alerts: [{:date => DateTime.new(2012, 5, 5, 16, 32), :count => 12345}] * 5
-    })
+    if params[:post]=='1'
+      render 'post1'
+    end
+    if params[:post]=='2'
+      render 'post2'
+    end
+    if params[:post]=='3'
+      render 'post3'
+    end
   end
 end
