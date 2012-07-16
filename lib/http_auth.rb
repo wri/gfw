7b770fb42f4cc62835881ccce402c3e124d44a42
Javier Arce <javierarce@gmail.com>
2012-07-16 17:14:25 +0200

    Temporarily removes the auth

--- lib/http_auth.rb
+++ lib/http_auth.rb
@@ -1,20 +1,20 @@
 module Gfw
 
-  module HttpAuth
-    DEMO_USER = {'gfw' => 'gfw'}
+  #module HttpAuth
+    #DEMO_USER = {'gfw' => 'gfw'}
 
-    def self.included(base)
-      base.before_filter :authentication_check
-    end
+    #def self.included(base)
+      #base.before_filter :authentication_check
+    #end
 
-    def authentication_check
-      return unless Rails.env.production?
+    #def authentication_check
+      #return unless Rails.env.production?
 
-      authenticate_or_request_with_http_basic do |user, password|
-        DEMO_USER[user] == password
-      end
-    end
+      #authenticate_or_request_with_http_basic do |user, password|
+        #DEMO_USER[user] == password
+      #end
+    #end
 
-  end
+  #end
 
 end
