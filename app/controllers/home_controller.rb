676cb0626e1450c2be09426c6caeaad1f4aa9534
Fernando Espinosa <fer@ferdev.com>
2012-12-04 18:13:52 +0100

    Adds real numbers for alerts amounts through the app

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
+
   def index
     #@area = Area.new
     @featured = Story.where(:featured => true).order('cartodb_id ASC')
+
+    @alerts_count = Alert.ammount_in_the_last_six_months
   end
+
 end
