a75aac4fc0d5e9986ec8c0ea520c1071709e7ab5
dcano <dcano@vizzuality.com>
2012-12-10 16:27:52 +0100

    Now we only show 3 case studies in the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def index
     #@area = Area.new
-    @featured = Story.where(:featured => true).order('cartodb_id ASC')
+    @featured = Story.where(:featured => true).order('cartodb_id DESC').first(3)
 
     @alerts_count = Alert.ammount_in_the_last_six_months
   end
