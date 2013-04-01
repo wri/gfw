cbf8e93798f8626ab3a2c114c090ee260956bf69
Fernando Espinosa <fer@ferdev.com>
2013-04-01 19:14:29 +0200

    More speed improvements in the homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def index
     #@area = Area.new
-    @featured = Story.where(:featured => true).order('cartodb_id DESC').per_page(3).all
+    @featured = Story.first_three_featured
 
     #require 'debugger'; debugger
     @alerts_count = Alert.ammount_in_the_last_year
