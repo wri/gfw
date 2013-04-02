3c406f94cb9f9a4707c98f88918eb6d454553266
Javier Arce <javierarce@gmail.com>
2013-04-02 16:23:01 +0200

    Bumpt to v1.1

--- config/application.rb
+++ config/application.rb
@@ -60,7 +60,7 @@ module Gfw
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
 
     # Disables database calls in precompile
     config.assets.initialize_on_precompile=false
