176ef535d9ad9510b97ced33b20b42922c762159
Javier Arce <javierarce@gmail.com>
2013-04-03 19:10:52 +0200

    bump version to 13

--- app/assets/javascripts/gfw_lib.js
+++ app/assets/javascripts/gfw_lib.js
@@ -43,7 +43,7 @@ GFW.modules.app = function(gfw) {
       this._precision = 2;
       this._layers = [];
       this._cloudfront_url = "dyynnn89u7nkm.cloudfront.net";
-      this._global_version = 11;
+      this._global_version = 12;
 
       gfw.log.enabled = options ? options.logging: false;
 
