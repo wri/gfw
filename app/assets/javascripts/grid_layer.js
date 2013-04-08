157963563a3e687da75ff87f2a2afbef9693db4a
Javier Arce <javierarce@gmail.com>
2013-04-08 19:43:26 +0200

    Fixes month issues

--- app/assets/javascripts/grid_layer.js
+++ app/assets/javascripts/grid_layer.js
@@ -187,7 +187,7 @@ TimePlayer.prototype.get_time_data = function(tile, coord, zoom) {
 
 TimePlayer.prototype.render_time = function(tile, coord, zoom) {
   var self = this;
-  var month = -BASE_MONTH + 1 + this.time>>0;
+  var month = -BASE_MONTH + this.time>>0;
   var w = tile.canvas.width;
   var h = tile.canvas.height;
   var ctx = tile.ctx;
