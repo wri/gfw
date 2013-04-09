58cdd49c8ad246945cdc6307aebf7082ae883c86
Fernando Espinosa <fer@ferdev.com>
2013-04-09 10:01:03 +0200

    Updates app host in mails links

--- config/application.rb
+++ config/application.rb
@@ -66,7 +66,7 @@ module Gfw
     config.assets.initialize_on_precompile=false
 
     config.action_mailer.delivery_method = :smtp
-    config.action_mailer.default_url_options = { :host => "gfw-staging.herokuapp.com" }
+    config.action_mailer.default_url_options = { :host => "www.gfw-beta.org" }
     config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
       :port                 => 587,
