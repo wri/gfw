9c606638c4e8f5db5456a7ec9c6b88f1d6d27d48
Javier Arce <javierarce@gmail.com>
2013-06-28 11:04:53 +0200

    Refreshes cache

--- app/assets/javascripts/gfw_lib.js
+++ app/assets/javascripts/gfw_lib.js
@@ -45,7 +45,7 @@ GFW.modules.app = function(gfw) {
       this._precision = 2;
       this._layers = [];
       this._cloudfront_url = "dyynnn89u7nkm.cloudfront.net";
-      this._global_version = 14;
+      this._global_version = 20;
 
       gfw.log.enabled = options ? options.logging: false;
 
