416c72563ca915f95201b85ee25bc00c68debec6
Carlos Matallín <matallo@vizzuality.com>
2013-10-02 17:36:48 +0200

    replace mail

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -1,5 +1,5 @@
 class Notifications < ActionMailer::Base
-  default from: "gfw2-website@wri.org"
+  default from: "gfw@wri.org"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -9,14 +9,14 @@ class Notifications < ActionMailer::Base
   def new_story(story)
     @story = story
 
-    mail to: "janderson@wri.org, gfw2@wri.org, sminnemeyer@wri.org, aleach@wri.org"
+    mail to: "janderson@wri.org, gfw@wri.org, sminnemeyer@wri.org, aleach@wri.org"
     #mail to: "dcano@vizzuality.com, ferdev@vizzuality.com"
   end
 
   def new_user(user_email)
     @reply_to_email = user_email
 
-    mail to: "gfw2@wri.org", reply_to: user_email, subject: 'A new user requested access to Global Forest Watch 2.0'
+    mail to: "gfw@wri.org", reply_to: user_email, subject: 'A new user requested access to Global Forest Watch 2.0'
   end
 
 end
