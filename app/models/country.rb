52134b69a4d674bf51bb0f339b0583096a85c8e0
Carlos Matallín <matallo@vizzuality.com>
2013-11-06 17:23:10 +0100

    countries refactor

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,5 +1,36 @@
 class Country
 
+  def self.api_attributes
+    [
+      'name',
+      'iso',
+      'forest_extent',
+      'gross_value',
+      'gdp_percent_fixed',
+      'employment',
+      'national_policy_link',
+      'national_policy_title',
+      'carbon_stocks',
+      'emissions_land',
+      'emissions_noland',
+      'ministry_link',
+      'dataset_link',
+      'lat',
+      'lon',
+      'map_coord',
+      'convention_cbd',
+      'convention_unfccc',
+      'convention_kyoto',
+      'convention_unccd',
+      'convention_itta',
+      'convention_cites',
+      'convention_ramsar',
+      'convention_world_heritage',
+      'convention_nlbi',
+      'convention_ilo'
+     ]
+  end
+
   def self.countries_info
     CartoDB::Connection.query("SELECT name, iso, enabled FROM gfw2_countries ORDER BY name;")[:rows]
   end
@@ -10,43 +41,23 @@ class Country
              c.name as name,
              c.enabled as enabled,
              (SELECT COALESCE(sum(alerts), 0) as sum
-              FROM gfw2_forma_graphs as d
-              WHERE d.iso = c.iso AND date >= (SELECT MAX(n) FROM gfw2_forma_datecode WHERE date < now() - INTERVAL '12 MONTHS')
+                FROM gfw2_forma_graphs as d
+                WHERE d.iso = c.iso AND date >= (SELECT MAX(n) FROM gfw2_forma_datecode WHERE date < now() - INTERVAL '12 MONTHS')
              ) as sum_alerts
       FROM gfw2_countries as c
       ORDER BY name ASC;")[:rows]
   end
 
   def self.country_info(name = '')
-    CartoDB::Connection.query("
-      SELECT name,
-             iso,
-             forest_extent,
-             gross_value,
-             gdp_percent_fixed,
-             employment,
-             national_policy_link,
-             national_policy_title,
-             carbon_stocks,
-             emissions_land,
-             emissions_noland,
-             ministry_link,
-             dataset_link,
-             lat,
-             lon,
-             map_coord,
-             convention_cbd,
-             convention_unfccc,
-             convention_kyoto,
-             convention_unccd,
-             convention_itta,
-             convention_cites,
-             convention_ramsar,
-             convention_world_heritage,
-             convention_nlbi,
-             convention_ilo
-      FROM gfw2_countries
-      WHERE name = '"+name.humanize.titleize+"';")[:rows]
+    query = "SELECT "
+
+    self.api_attributes.each do |attr|
+      query << "#{attr}, "
+    end
+
+    CartoDB::Connection.query(
+      query.gsub(/\, $/, '') << " FROM gfw2_countries WHERE name = '"+name.humanize.titleize+"'"
+    )[:rows]
   end
 
 end
