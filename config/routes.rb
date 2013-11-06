ba729829524a98b13cb17cdab20d30cf5b1d2e0e
Carlos Matallín <matallo@vizzuality.com>
2013-11-06 19:10:43 +0100

    refactor views

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,23 @@
 Gfw::Application.routes.draw do
   resources :countries
   resources :areas
-  resources :posts
-  resources :sources
   resources :stories
   resources :media
 
+  # static
   match 'about' => 'static#about'
   match 'old' => 'static#old'
+  match 'sources' => 'static#sources'
 
+  # map
   match 'map' => 'home#index'
   match 'map/:zoom/:lat/:lng(/:iso)' => 'home#index', :lat => /[^\/]+/, :lng => /[^\/]+/
   match 'map/:zoom/:lat/:lng/:iso(/:filters)' => 'home#index', :lat => /[^\/]+/, :lng => /[^\/]+/
 
+  # countries
   match 'country/:id' => 'countries#show'
 
+  # home
   root :to => 'home#index'
   post '/register' => 'home#register'
 end
