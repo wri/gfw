147ed802bcab6f454d83455670844e5fec88a005
Carlos Matallín <matallo@vizzuality.com>
2014-01-07 11:11:38 +0100

    views: sources: change layout

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => ENV['S3_KEY_ID'],
-    :aws_secret_access_key  => ENV['S3_KEY_SECRET']
+    :aws_access_key_id      => 'AKIAJAGNXHWUDQVKXTZQ',
+    :aws_secret_access_key  => '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'
   }
   config.fog_directory       = 'gfw.stories'
   config.fog_attributes      = {'Cache-Control' => 'max-age = 315576000'}
