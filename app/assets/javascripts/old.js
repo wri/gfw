3a03c6f24fc760b794f2fc6e3f52cb33347b2185
Carlos Matallín <matallo@vizzuality.com>
2014-02-15 18:16:49 +0100

    static: terms: double check

--- app/assets/javascripts/old.js
+++ app/assets/javascripts/old.js
@@ -1,7 +1,32 @@
-$(function() {
-  var map = new google.maps.Map(document.getElementById('map'), config.MAPOPTIONS);
+//= require gfw/ui/widget
+//= require gfw/ui/sourcewindow
 
-  var styledMap = new google.maps.StyledMapType(config.BASE_MAP_STYLE, { name: 'terrain_style' });
-  map.mapTypes.set('terrain_style', styledMap);
-  map.setMapTypeId('terrain_style');
+
+var map;
+
+gfw.ui.view.Terms = cdb.core.View.extend({
+  el: document.body,
+
+  events: {
+    'click .continue': '_onClickContinue'
+  },
+
+  initialize: function() {
+    this.sourceWindow  = new gfw.ui.view.SourceWindow();
+    this.$el.append(this.sourceWindow.render());
+
+    map = new google.maps.Map(document.getElementById('map'), config.MAPOPTIONS);
+
+    var styledMap = new google.maps.StyledMapType(config.BASE_MAP_STYLE, { name: 'terrain_style' });
+    map.mapTypes.set('terrain_style', styledMap);
+    map.setMapTypeId('terrain_style');
+  },
+
+  _onClickContinue: function(e) {
+    e.preventDefault();
+
+    var source = $(e.target).closest('.continue').attr('data-source');
+
+    this.sourceWindow.show(source).addScroll();
+  }
 });
