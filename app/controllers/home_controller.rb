dca32a2a1a1e0cdf3b9a7a372fc8f953238374e8
Carlos Matallín <matallo@vizzuality.com>
2014-02-26 13:09:10 +0100

    home: index: de-hardcode circles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,10 @@ class HomeController < ApplicationController
   skip_before_filter :check_terms, :only => [:accept_and_redirect]
   before_filter :load_stories, :load_alerts, :only => [:index]
 
+  def index
+    @circles = HTTParty.get("https://wri-01.cartodb.com/api/v2/sql?q=WITH%20loss%20as%20(SELECT%20sum(loss_gt_0)%20as%20sum_loss,%20(SELECT%20sum(loss_gt_0)%20FROM%20umd%20WHERE%20year%20=%202012)%20as%20loss_2012%20FROM%20umd),%20gain%20as%20(SELECT%20sum(umd.gain)%20last_gain%20FROM%20(SELECT%20DISTINCT%20iso,%20gain%20FROM%20umd)%20umd),%20forma%20as%20(SELECT%20count(cartodb_id)%20FROM%20forma_api%20WHERE%20date%20%3E=%20(SELECT%20max(date)%20FROM%20forma_api))%20SELECT%20*%20from%20loss,%20gain,%20forma")['rows'][0]
+  end
+
   def accept_and_redirect
     cookies.permanent[ENV['TERMS_COOKIE'].to_sym] = true
 
