509cc1fd22cefe71858a8bf39ed2f63900843f58
Carlos Matallín <matallo@vizzuality.com>
2014-03-10 19:54:50 +0100

    api: remove blog

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -7,8 +7,8 @@ class CountriesController < ApplicationController
 
     not_found unless country.present?
 
-    blog_story = Api::Blog.find_post_by_country(country['name'].downcase.gsub(" ", "_"))
-    @blog_story = blog_story.present? ? blog_story.first : nil
+    blog_story = HTTParty.get("#{ENV['BLOG_HOST']}/", query: { tag: country['name'].downcase.gsub(" ", "_"), feed: "rss2" })
+    @blog_story = blog_story['rss']['channel']['item'].present? ? blog_story['rss']['channel']['item'].first : nil
 
     response = Typhoeus.get("https://wri-01.cartodb.com/api/v2/sql?q=SELECT%20*%20FROM%20mongabaydb%20WHERE%20position('#{I18n.transliterate(country['name']).downcase.gsub(" ", "_")}'%20in%20keywords)%20%3C%3E%200", headers: { "Accept" => "application/json" })
     @mongabay_story = response.success? ? JSON.parse(response.body)['rows'][0] : nil
