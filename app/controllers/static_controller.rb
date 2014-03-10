c8f76e49b233a4d07186e2a00ab09c26d3555945
Carlos Matallín <matallo@vizzuality.com>
2014-03-10 11:23:18 +0100

    terms: add accept terms for data

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,5 @@
 class StaticController < ApplicationController
-  skip_before_filter :check_terms
+  skip_before_filter :check_terms, :except => [:data]
 
   layout 'old', :only => [:old, :terms, :accept_terms]
 end
