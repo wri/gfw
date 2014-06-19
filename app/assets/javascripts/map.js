927bb86051498be097d4dade2ca20bf7812404b3
pedro <pdgago@gmail.com>
2014-06-19 23:57:13 +0200

    new filter layer

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -4,16 +4,19 @@ require([
   'router',
   'presenter',
   'mediator',
+  'views/layersMenu',
   'backbone',
   'analysis',
   'mps',
-  'jasmine'
-], function (app, router, presenter, mediator, Backbone, analysis, mps, jasmine) {
+  'jasmine',
+], function (app, router, presenter, mediator, layersMenu, Backbone, analysis, mps, jasmine) {
   console.log('Main entry point...', app);
+
   if (!Backbone.History.started) {
     console.log('Backbone.history.start');
     Backbone.history.start({pushState: true});
   }
+
   // For dev
   window.analysis = analysis;
   window.mps = mps;
