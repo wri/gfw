b3c206fcc8a166349b877a2211e30ddc584836cc
Adrián Pérez <adrian@simbiotica.es>
2014-06-25 13:26:09 +0200

    Revert "Merge branch 'feature/new-country-page' of github.com:Vizzuality/gfw into staging"
    This reverts commit ba50f9de1bb3daa354b22d0f6d940134f9e1f2c4, reversing
    changes made to 22153e0cff29681e649b88a5dc8c8fd706dc7f82.

--- app/helpers/countries_helper.rb
+++ app/helpers/countries_helper.rb
@@ -4,25 +4,16 @@ module CountriesHelper
     "#{map_path}/5/#{@country['lat'].round(2)}/#{@country['lng'].round(2)}/#{@country['iso']}"
   end
 
-  def extent_to_human(extent, options = {})
-    options = {format: '%n %u', shortUnit: false}.merge(options)
-
+  def extent_to_human(extent)
     if extent < 1000
-      number = "#{number_with_delimiter(extent.round(0))}"
-      unit = options[:shortUnit] ? "Ha" : ''
+      result = "#{number_with_delimiter(extent.round(1))}"
     elsif extent >= 1000 && extent < 1000000
-      number = "#{number_with_delimiter((extent/1000).round(1))}"
-      unit = options[:shortUnit] ? "KHa" : 'thousand'
+      result = "#{number_with_delimiter((extent/1000).round(1))} thousand"
     else
-      number = "#{number_with_delimiter((extent/1000000).round(1))}"
-      unit = options[:shortUnit] ? "MHa" : 'million'
+      result = "#{number_with_delimiter((extent/1000000).round(1))} million"
     end
 
-    string = options[:format]
-    string.gsub!('%n', number)
-    string.gsub!('%u', unit)
-
-    return string
+    return result
   end
 
   def gva_to_human(gva)
