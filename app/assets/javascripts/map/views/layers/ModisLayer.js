d81f356dd49ae55fce9571c4134b43dc854504d7
Adrián Pérez <adrian@simbiotica.es>
2014-07-14 13:38:16 +0200

    filter modis

--- app/assets/javascripts/map/views/layers/ModisLayer.js
+++ app/assets/javascripts/map/views/layers/ModisLayer.js
@@ -13,7 +13,7 @@ define([
   var ModisLayer = CartoDBLayerClass.extend({
 
     options: {
-      sql: 'SELECT cartodb_id, the_geom_webmercator, \'{tableName}\' AS layer FROM {tableName}',
+      sql: 'SELECT cartodb_id, the_geom_webmercator, \'{tableName}\' AS layer FROM {tableName} WHERE EXTRACT(YEAR FROM date) = 2014 AND EXTRACT(MONTH FROM date) = 03 AND ST_Y(the_geom) < 37',
       cartocss: modisCartoCSS,
       interactivity: 'cartodb_id'
     }
