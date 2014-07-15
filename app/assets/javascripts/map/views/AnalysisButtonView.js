d0c5cd8faf4818041aecf493744da4f59a8e9cc7
Adrián Pérez <adrian@simbiotica.es>
2014-07-15 12:47:37 +0200

    trigger drawing manager

--- app/assets/javascripts/map/views/AnalysisButtonView.js
+++ app/assets/javascripts/map/views/AnalysisButtonView.js
@@ -80,8 +80,87 @@ define([
     showHelperBar: function() {
       var self = this;
       $('.timeline').fadeOut(function(){
-        self.$el.find('.helper_bar').fadeIn()
+        self.$el.find('.helper_bar').fadeIn();
+        self.startDrawingManager()
       })
+    },
+
+    startDrawingManager: function() {
+      var self = this,
+          map = new google.maps.Map(document.getElementById('map'));
+
+      var style = {
+        strokeWeight: 2,
+        fillOpacity: 0.45,
+        fillColor: "#FFF",
+        strokeColor: "#A2BC28",
+        editable: true,
+        icon: new google.maps.MarkerImage(
+          '/assets/icons/marker_exclamation.png',
+          new google.maps.Size(36, 36), // size
+          new google.maps.Point(0, 0), // offset
+          new google.maps.Point(18, 18) // anchor
+        )
+      };
+
+      var options = {
+        drawingModes: [ google.maps.drawing.OverlayType.POLYGON ],
+        drawingControl: false,
+        polygonOptions: style,
+        panControl: false,
+        map: map
+      };
+
+      // Create the drawing manager
+      this.drawingManager = new google.maps.drawing.DrawingManager(options);
+
+      // Start drawing right away
+      this.drawingManager.setDrawingMode(google.maps.drawing.OverlayType.POLYGON);
+
+      // Event binding
+    console.log(google.maps.event)
+      google.maps.event.addListener(this.drawingManager, 'overlaycomplete', this._onOverlayComplete());
+    },
+
+    _onOverlayComplete: function(e) {
+      var polygon = {};
+      var c0 = [];
+      var area = null;
+
+      this.drawingManager.setDrawingMode(null);
+      this.drawingManager.path = e.overlay.getPath().getArray();
+      this.drawingManager.setOptions({ drawingControl: false });
+      //this._enableDoneButton();
+
+      var newShape = e.overlay;
+      newShape.type = e.type;
+
+      this._setSelection(newShape);
+
+      polygon = {
+        'type': 'Polygon',
+        'coordinates': [
+          $.map(this.drawingManager.path, function(latlong, index) {
+            return [[latlong.lng(), latlong.lat()]];
+          })
+        ]
+      };
+
+      if (typeof polygon === 'undefined' || polygon === '' || polygon.coordinates[0].length < 3) {
+        this.$('.cancel').click();
+
+        return;
+      }
+
+      // Close the polygon:
+      c0 = polygon.coordinates[0][0];
+      polygon.coordinates[0].push(c0);
+
+      //area = formatNumber(Math.ceil((google.maps.geometry.spherical.computeArea(this.drawingManager.path)/10000) * 10) / 10, true)
+
+      // this.model.set('area', JSON.stringify(polygon));
+      // this.info.model.set('ha', formatNumber(this._calcAreaPolygon(polygon), true));
+      // this.info.model.set('title', this.info.model.defaults.title);
     },
   });
 
