02629d1251aa8bd504fefde49477768764882e36
Pedro Garcia <pedro@simbiotica.es>
2014-07-25 17:19:51 +0200

    working on it

--- app/assets/javascripts/map/presenters/LegendPresenter.js
+++ app/assets/javascripts/map/presenters/LegendPresenter.js
@@ -6,8 +6,9 @@
 define([
   'Class',
   'underscore',
-  'mps'
-], function(Class, _, mps) {
+  'mps',
+  'services/LayerSpecService'
+], function(Class, _, mps, layerSpecService) {
 
   'use strict';
 
@@ -28,6 +29,8 @@ define([
      */
     _subscribe: function() {
       mps.subscribe('LayerNav/change', _.bind(function(layerSpec) {
+        console.log(layerSpec);
+        // toggle selected layers
         this._updateLegend(layerSpec);
       }, this));
 
@@ -47,7 +50,23 @@ define([
 
     _updateLegend: function(layerSpec) {
       this.view.update(layerSpec.getLayersByCategory());
+    },
+
+    /**
+     * Publish a a Map/toggle-layer.
+     *
+     * @param  {string} layerSlug
+     */
+    toggleLayer: function(layerSlug) {
+      var where = [{slug: layerSlug}];
+
+      layerSpecService.toggle(where, {},
+        _.bind(function(layerSpec) {
+          mps.publish('LayerNav/change', [layerSpec]);
+          mps.publish('Place/update', [{go: false}]);
+        }, this));
     }
+
   });
 
   return LegendPresenter;
--- app/assets/javascripts/map/views/LegendView.js
+++ app/assets/javascripts/map/views/LegendView.js
@@ -34,6 +34,7 @@ define([
     events: function(){
       return _.extend({}, LegendView.__super__.events, {
         'click .widget-closed': '_toggleBoxClosed',
+        'click .layer-sublayer': '_toggleLayer'
       });
     },
 
@@ -41,10 +42,6 @@ define([
       _.bindAll(this, 'update');
       this.presenter = new Presenter(this);
       LegendView.__super__.initialize.apply(this);
-    },
-
-    _setHidden: function() {
-      LegendView.__super__._setHidden.apply(this);
     },
 
     /**
@@ -85,6 +82,17 @@ define([
         this.model.set({'hidden': false, 'boxClosed': false});
         this._renderLegend(layers);
       }
+    },
+
+    /**
+     * Handles a toggle layer change UI event by dispatching
+     * to LegendPresenter.
+     *
+     * @param  {event} event Click event
+     */
+    _toggleLayer: function(event) {
+      var layerSlug = $(event.currentTarget).data('sublayer');
+      this.presenter.toggleLayer(layerSlug);
     }
 
   });
