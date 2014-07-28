a543b5edd8d4715faee90e1b2a2cf4993edaea7b
Pedro Garcia <pedro@simbiotica.es>
2014-07-28 12:29:29 +0200

    url basemap working

--- app/assets/javascripts/map/views/maptypes/landsatMaptype.js
+++ app/assets/javascripts/map/views/maptypes/landsatMaptype.js
@@ -14,7 +14,7 @@ define([], function () {
       tileSize: new google.maps.Size(256, 256),
       getTileUrl: function(ll, z) {
         var x = Math.abs(ll.x % (1 << z)); // jshint ignore:line
-        return '//gfw-apis.appspot.com/gee/landsat_composites/' + z + '/' + x + '/' + ll.y + '.png?year='+year;
+        return '//gfw-apis.appspot.com/gee/landsat_composites/{0}/{1}/{2}.png?year={3}'.format(z, x, ll.y, year);
       },
     };
 
