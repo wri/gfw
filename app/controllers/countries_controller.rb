e7c4ed782890a19ea585345d67b4b0e28ea12d47
Adrián Pérez <adrian@simbiotica.es>
2014-09-08 12:54:54 +0200

    filter mongabay stories by country

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -20,7 +20,7 @@ class CountriesController < ApplicationController
     blog_story = Api::Blog.find_by_country(@country)
     @blog_story = blog_story.present? ? blog_story : nil
 
-    response = Typhoeus.get("https://wri-01.cartodb.com/api/v2/sql?q=SELECT%20*%20FROM%20mongabaydb%20WHERE%20position('#{I18n.transliterate(@country['name']).downcase.gsub(" ", "_")}'%20in%20keywords)%20%3C%3E%200", headers: { "Accept" => "application/json" })
+    response = Typhoeus.get("https://wri-01.cartodb.com/api/v2/sql?q=SELECT%20*%20FROM%20mongabaydb%20WHERE%20position('#{@country['name']}'%20in%20keywords)%20%3C%3E%200", headers: { "Accept" => "application/json" })
 
     @mongabay_story = if response.success?
                         Rails.cache.fetch 'mongabay_story', expires_in: 1.day do
