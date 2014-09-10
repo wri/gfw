cc0a359a5743c4188989e522e03725ae46bc8c64
Adrián Pérez <adrian@simbiotica.es>
2014-09-10 18:18:34 +0200

    hotfix

--- app/assets/javascripts/map/presenters/AnalysisResultsPresenter.js
+++ app/assets/javascripts/map/presenters/AnalysisResultsPresenter.js
@@ -252,7 +252,7 @@ define([
        *   - gainAlerts
        */
       if (layer.slug === 'loss') {
-        p.lossDateRange = '{0}-{1}'.format(dateRange[0].year(), dateRange[1].year());
+        p.lossDateRange = '{0}-{1}'.format(dateRange[0].year(), (dateRange[1].year() == 2013)?'2012':dateRange[1].year());
         p.lossAlerts = 0;
         p.gainAlerts = 0;
         p.threshold  = results.params.thresh || 10;
