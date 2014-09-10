3c6db6aa4f9d7c837f8e6c470872f32747fe871e
Pedro Garcia <pedro@simbiotica.es>
2014-09-10 19:56:50 +0200

    hotfix

--- app/assets/javascripts/map/views/MapView.js
+++ app/assets/javascripts/map/views/MapView.js
@@ -87,6 +87,7 @@ define([
       }, this));
 
       google.maps.event.addListener(this.map, 'click', _.bind(function(wdpa) {
+        console.log(wdpa);
         if (!(!!wdpa.wdpaid)) {
           return;
         }
