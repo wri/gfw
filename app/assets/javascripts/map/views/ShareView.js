cf79cf47fb6f1487ab43df1dc1c71445ba19e95c
Adrián Pérez <adrian@simbiotica.es>
2014-09-26 13:27:12 +0200

    weird fix to redraw share dialog

--- app/assets/javascripts/map/views/ShareView.js
+++ app/assets/javascripts/map/views/ShareView.js
@@ -50,8 +50,9 @@ define([
           text = 'Click and paste HTML to embed in website';
         this._setEmbed();
       }
-
-      $('.share_dialog').find('.help span').empty().append(text);
+      $dialog_ target = $('.share_dialog');
+      $dialog_ target.find('.help span').empty().append(text);
+      $dialog_ target.hide().show();
     },
 
     _show: function(e) {
