bbe6db53fb59928c3fb256b9144c0f3675786495
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2014-10-13 13:42:43 +0200

    SQL changed to retrieve round areas

--- app/assets/javascripts/map/views/layers/LandRightsLayer.js
+++ app/assets/javascripts/map/views/layers/LandRightsLayer.js
@@ -12,7 +12,7 @@ define([
   var LandRightsLayer = CartoDBLayerClass.extend({
 
     options: {
-      sql: 'SELECT *, \'{tableName}\' as layer FROM {tableName}',
+      sql: 'SELECT the_geom_webmercator,name, country, national_legal_term, legal_recognition, ROUND(area_ha) AS area_ha, category,  \'{tableName}\' as layer FROM {tableName}',
       infowindow: true,
       interactivity: 'name, country, national_legal_term, legal_recognition, area_ha, category'
     }
