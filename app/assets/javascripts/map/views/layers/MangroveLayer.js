268424a448d057819cd17b039c44db1f56846a49
Adrián Pérez <adrian@simbiotica.es>
2014-10-15 15:11:39 +0200

    Mangroves: add new layer file

---
+++ app/assets/javascripts/map/views/layers/MangroveLayer.js
@@ -0,0 +1,22 @@
+/**
+ * The Forma Coverage layer module for use on canvas.
+ *
+ * @return MangroveLayer class (extends CanvasLayerClass)
+ */
+define([
+  'abstract/layer/CartoDBLayerClass',
+], function(CartoDBLayerClass) {
+
+  'use strict';
+
+  var MangroveLayer = CartoDBLayerClass.extend({
+
+    options: {
+      sql: 'SELECT *, \'{tableName}\' AS layer, \'{tableName}\' AS name FROM {tableName}'
+    }
+
+  });
+
+  return MangroveLayer;
+
+});
