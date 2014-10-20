f0b6ea9af7074a6dbae3a9b214ad5c8688bdfc04
Adrián Pérez <adrian@simbiotica.es>
2014-10-20 11:42:41 +0200

    Grump: correct tiles origin

--- app/assets/javascripts/map/views/layers/GrumpLayer.js
+++ app/assets/javascripts/map/views/layers/GrumpLayer.js
@@ -12,7 +12,7 @@ define([
   var GrumpLayer = ImageLayerClass.extend({
 
     options: {
-      urlTemplate: '//s3.amazonaws.com/grump-tiles/grump-base{/z}{/x}{/y}.png',
+      urlTemplate: 'https://s3.amazonaws.com/grump-tiles/grump-base{/z}{/x}{/y}.png',
       dataMaxZoom: 8
     }
 
