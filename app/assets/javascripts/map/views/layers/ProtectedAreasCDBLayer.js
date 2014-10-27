ecc36c2c804447b2aba895a7662bd488b5520018
Adrián Pérez <adrian@simbiotica.es>
2014-10-27 11:59:36 +0100

    Map: throw analysis for protected areas

--- app/assets/javascripts/map/views/layers/ProtectedAreasCDBLayer.js
+++ app/assets/javascripts/map/views/layers/ProtectedAreasCDBLayer.js
@@ -14,7 +14,7 @@ define([
     options: {
       sql: 'SELECT the_geom_webmercator, the_geom, desig_eng, iso3 as country, name, wdpaid as id, {analysis} AS analysis, \'{tableName}\' as layer FROM {tableName}',
       infowindow: true,
-      interactivity: 'desig_eng, country, name, id',
+      interactivity: 'desig_eng, country, name, id, analysis',
       analysis: true
     },
 
