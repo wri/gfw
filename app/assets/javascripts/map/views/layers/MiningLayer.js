316b1bc46a00d3f26604bcacaa52f03d3b5bb70c
David Gonzalez <david@simbiotica.es>
2014-10-28 18:05:42 +0100

    colombia mining hotfix

--- app/assets/javascripts/map/views/layers/MiningLayer.js
+++ app/assets/javascripts/map/views/layers/MiningLayer.js
@@ -12,7 +12,7 @@ define([
   var MiningLayer = CartoDBLayerClass.extend({
 
     options: {
-      sql: 'SELECT cartodb_id, \'mining\' as tablename, the_geom_webmercator, type, status,company, country, area_ha, name, \'{tableName}\' AS layer, {analysis} AS analysis FROM {tableName}',
+      sql: 'SELECT cartodb_id, \'mining\' as tablename, the_geom_webmercator, type, status,company, country, area_ha, name, \'{tableName}\' AS layer, {analysis} AS analysis FROM {tableName} WHERE company <>\'(70085559) FABIO LEON CORDOBA RUA\' AND (company is not null OR country<>\'Colombia\')',
       infowindow: true,
       interactivity: 'cartodb_id, tablename, name, type, status,company, country, area_ha, analysis',
       analysis: true
