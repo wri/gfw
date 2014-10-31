8188529d768de8db6e27bb1206c71393e0f17ab7
David Gonzalez <david@simbiotica.es>
2014-10-31 10:05:11 +0100

    switch land rights table to synced

--- app/assets/javascripts/map/views/layers/LandRightsLayer.js
+++ app/assets/javascripts/map/views/layers/LandRightsLayer.js
@@ -12,9 +12,9 @@ define([
   var LandRightsLayer = CartoDBLayerClass.extend({
 
     options: {
-      sql: 'SELECT the_geom_webmercator,name, country, national_legal_term, legal_recognition, ROUND(area_ha) AS area_ha, category,  \'{tableName}\' as layer FROM {tableName}',
+      sql: 'SELECT the_geom_webmercator,name, country, legal_term, recognized, ROUND(area_ha::text::float) AS area_ha, category,  \'{tableName}\' as layer FROM {tableName}',
       infowindow: true,
-      interactivity: 'name, country, national_legal_term, legal_recognition, area_ha, category'
+      interactivity: 'name, country, legal_term, recognized, area_ha, category'
     }
 
   });
