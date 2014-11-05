df989854a021f396dfd88b5742d6915c2e1aac53
Miguel Barrenechea Sánchez <Miguel@iMac-de-Miguel.local>
2014-11-06 00:11:06 +0100

    Menu list, requirejs and fixes in header

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -1,1 +1,48 @@
-//alert(0)
+/**
+ * Application entry point.
+ */
+require([
+  'jquery',
+  'underscore',
+  'Class',
+  'backbone',
+  'mps',
+  'views/DialogView',
+  '_string'
+], function($, _, Class, Backbone, utils, mps, DialogView) {
+
+  'use strict';
+
+  var MapPage = Class.extend({
+
+    $el: $('body'),
+
+    init: function() {
+      this._initViews();
+      this._initApp();
+    },
+
+    /**
+     * Initialize the map by starting the history.
+     */
+    _initApp: function() {
+      if (!Backbone.History.started) {
+        Backbone.history.start({pushState: true});
+      }
+    },
+
+    /**
+     * Initialize Application Views.
+     * CAUTION: Don't change the order of initanciations if
+     * you are not completely sure.
+     */
+    _initViews: function() {
+
+    },
+
+
+  });
+
+  new MapPage();
+
+});
