d52dd2e90dc4edacfafedb0516dc6a2d064ddaa3
Adrián Pérez <adrian@simbiotica.es>
2014-11-20 16:53:50 +0100

    test deploy xii

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Gfw::Application.configure do
   # config.assets.compile = true
 
   # Generate digests for assets URLs.
-  config.assets.digest = true
+  config.assets.digest = false
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
