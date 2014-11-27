673996249038f0a73a4403ae90050978e4dffc21
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2014-11-27 09:45:27 +0100

    Howto: carrousel of videos, with pauseVideo before changing the slide

--- app/assets/javascripts/static/CarrouselView.js
+++ app/assets/javascripts/static/CarrouselView.js
@@ -21,13 +21,32 @@ define([
       if (!this.$el.length) {
         return
       }
+      this.current = 0;
+      this.tab = null;
+      this.$btnTab = this.$el.find('.btn-tab'); 
+      this.$slide = this.$el.find('.slide');
     },
 
     onTab: function(e) {
       e && e.preventDefault();
-      var tab = $(e.currentTarget).data('tab');
-      $(tab).addClass('visible').siblings().removeClass('visible');
+      // Pause video before changing slides
+      this.pauseVideo();
+      
+      // Vars
+      this.tab = $(e.currentTarget).data('tab');
+      this.current = $(e.currentTarget).parent().index();
+
+      //Toogle
+      this.$btnTab.removeClass('visible');
+      $(e.currentTarget).addClass('visible');
+
+      $(this.tab).addClass('visible').siblings().removeClass('visible');
     },
+
+    pauseVideo: function(){
+      var $video = this.$slide.eq(this.current).find('iframe').attr('id');
+      document.getElementById($video).contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');      
+    }
 
   });
 
