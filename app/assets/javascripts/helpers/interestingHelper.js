83a48dde420b0d77216c850e2dd045bdc19f6e53
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2014-12-18 17:06:49 +0100

    Global changes

---
+++ app/assets/javascripts/helpers/interestingHelper.js
@@ -0,0 +1,74 @@
+define([], function() {
+
+  var interestingHelper = {
+    stories_on_the_map: {
+      title: 'Stories on the map',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.',
+      link: 'http://0.0.0.0:5000/map/3/15.00/27.00/ALL/grayscale/loss,forestgain/580'
+    },
+    mongabay_stories: {
+      title: 'MONGABAY STORIES',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.',
+      link: 'http://0.0.0.0:5000/map/3/15.00/27.00/ALL/grayscale/loss,forestgain/586'
+    },
+    ejn_stories: {
+      title: 'EJN STORIES',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.',
+      link: '#'
+    },
+    explore_the_map: {
+      title: 'GFW Interactive Map',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.',
+      link: '/map'
+    },
+    develop_your_own_app: {
+      title: 'Develop your own app',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.',
+      link: '/getinvolved/develop-your-own-app'
+    },
+    discussion_forum: {
+      title: 'Global Forest Watch Discussion Forum',
+      description: 'Read conversations about forest change data, learn about interesting applications of GFW, and view others data-related questions.',
+      link: 'https://groups.google.com/forum/#!forum/globalforestwatch'
+    },
+    submit_a_story: {
+      title: 'Submit a story',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.',
+      link: '/getinvolved/submit-a-story'
+    },
+    download_the_data: {
+      title: 'Download the data',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.',
+      link: 'http://data.gfw.opendata.arcgis.com/'
+    },
+    get_involved: {
+      title: 'Get Involved',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.',
+      link: '/getinvolved'
+    },
+    keep_updated: {
+      title: 'Keep Updated',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.',
+      link: '/keepupdated'
+    },
+    fires_howto: {
+      title: 'Fires how to',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.',
+      link: '#'
+    },
+    commodities_howto: {
+      title: 'Commodities how to',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.',
+      link: '#'
+    },
+    coming_soon: {
+      title: 'Coming soon',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.',
+      link: '/keepupdated/coming-soon'
+    },
+
+  }
+  return interestingHelper;
+
+});
+
