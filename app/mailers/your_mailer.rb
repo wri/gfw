092bc51d27f999107984997aecb7ed11a9c50cb6
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2015-01-13 16:51:52 +0100

    Template for mail

--- app/mailers/your_mailer.rb
+++ app/mailers/your_mailer.rb
@@ -6,7 +6,6 @@ class YourMailer < ActionMailer::Base
 
     mail :subject => "GFW Feedback",
          :to      => ENV["FEEDBACK_MAIL"],
-         :from    => email,
          :template_path => 'your_mailer',
          :template_name => 'feedback'
   end
