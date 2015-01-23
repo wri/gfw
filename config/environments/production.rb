b1a4676da11220c7d69b1401eac9c08692398a18
Adrián Pérez <adrian@simbiotica.es>
2015-01-23 16:25:27 +0100

    production deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Gfw::Application.configure do
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '7'
+  config.assets.version = '8'
 
   config.static_cache_control = 'public, max-age=31536000'
 
