c0e8037f1acec00211836bf32f4ee0a54add7e6c
Adrián Pérez <adrian@simbiotica.es>
2015-02-02 14:04:28 +0100

    Alliance for Zero Extinction Sites layer class

---
+++ app/assets/javascripts/map/views/layers/AzepolyLayer.js
@@ -0,0 +1,22 @@
+/**
+ * The Azepoly layer module for use on canvas.
+ *
+ * @return AzepolyLayer class (extends CanvasLayerClass)
+ */
+define([
+  'abstract/layer/CartoDBLayerClass',
+], function(CartoDBLayerClass) {
+
+  'use strict';
+
+  var AzepolyLayer = CartoDBLayerClass.extend({
+
+    options: {
+      sql: 'SELECT *, \'{tableName}\' AS layer, \'{tableName}\' AS name FROM {tableName}'
+    }
+
+  });
+
+  return AzepolyLayer;
+
+});
