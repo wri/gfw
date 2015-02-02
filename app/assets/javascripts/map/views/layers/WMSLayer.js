579c53670ee6a00b5efca0e1bf4d68f839803c48
Adrián Pérez <adrian@simbiotica.es>
2015-02-02 18:29:40 +0100

    Honduras forest cartocss

--- app/assets/javascripts/map/views/layers/WMSLayer.js
+++ app/assets/javascripts/map/views/layers/WMSLayer.js
@@ -5,14 +5,16 @@
  */
 define([
   'abstract/layer/CartoDBLayerClass',
-], function(CartoDBLayerClass) {
+  'text!map/cartocss/honduras_forest.cartocss'
+], function(CartoDBLayerClass, honduras_forestCartoCSS) {
 
   'use strict';
 
   var WMSLayerLayer = CartoDBLayerClass.extend({
 
     options: {
-      sql: 'SELECT *, \'{tableName}\' AS layer, \'{tableName}\' AS name FROM {tableName}'
+      sql: 'SELECT *, \'{tableName}\' AS layer, \'{tableName}\' AS name FROM {tableName}',
+      cartocss: honduras_forestCartoCSS
     }
 
   });
