8e3fe82ce286f65f9ed4edcb9f005ff0cfd88f39
Adrián Pérez <adrian@simbiotica.es>
2015-02-05 19:13:27 +0100

    TigersLayer correcteed SQL

--- app/assets/javascripts/map/views/layers/TigersLayer.js
+++ app/assets/javascripts/map/views/layers/TigersLayer.js
@@ -12,9 +12,12 @@ define([
   var TigersLayer = CartoDBLayerClass.extend({
 
     options: {
-      sql: 'SELECT the_geom_webmercator, cartodb_id, the_geom, tcl_name as name, area_ha, tx2_tcl as type, tcl AS layer FROM tcl '
+      sql: 'SELECT the_geom_webmercator, cartodb_id, the_geom, tcl_name as name, area_ha, tx2_tcl as type, \'tcl\' as layer ' +
+           'FROM tcl ' +
+           'UNION ' +
+           'SELECT the_geom_webmercator, cartodb_id, the_geom, name,  area_ha, 2 as type, \'tal_corridor\' as layer ' +
+           'FROM tal_corridor ',
     }
-
   });
 
   return TigersLayer;
