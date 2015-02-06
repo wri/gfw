eff5b09b0fcc0a2f5cf136bbc00f747dad14a4ce
Adrián Pérez <adrian@simbiotica.es>
2015-02-06 11:45:00 +0100

    cache version ++

--- app/assets/javascripts/map/router.js
+++ app/assets/javascripts/map/router.js
@@ -18,7 +18,7 @@ define([
   var Router = Backbone.Router.extend({
 
     // temporary, we will do it with env variables
-    _cacheVersion: 18,
+    _cacheVersion: 19,
 
     routes: {
       'map(/:zoom)(/:lat)(/:lng)(/:iso)(/:maptype)(/:baselayers)(/:sublayers)(/)': 'map',
--- app/assets/javascripts/map/views/layers/LandRightsLayer.js
+++ app/assets/javascripts/map/views/layers/LandRightsLayer.js
@@ -12,7 +12,7 @@ define([
   var LandRightsLayer = CartoDBLayerClass.extend({
 
     options: {
-      sql: 'SELECT the_geom_webmercator,name, country, legal_term, legal_reco as legal_recognition, ROUND(area_ha::text::float) AS area_ha,  \'{tableName}\' as layer FROM {tableName}',
+      sql: 'SELECT the_geom_webmercator, name, country, legal_term, legal_reco as legal_recognition, ROUND(area_ha::text::float) AS area_ha,  \'{tableName}\' as layer FROM {tableName}',
       infowindow: true,
       interactivity: 'name, country, legal_term, legal_recognition, area_ha'
     }
