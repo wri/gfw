19ee38bfd94fefa1e6d5f445f82349f57767c204
David Gonzalez <david@simbiotica.es>
2015-02-06 09:56:03 +0100

    added company to woodfiber & oilpalm infowindows

--- app/assets/javascripts/map/views/layers/WoodFiberPlantationsLayer.js
+++ app/assets/javascripts/map/views/layers/WoodFiberPlantationsLayer.js
@@ -12,9 +12,9 @@ define([
   var WoodFiberPlantationsLayer = CartoDBLayerClass.extend({
 
     options: {
-      sql: 'SELECT \'fiber\' as tablename, cartodb_id, the_geom_webmercator,name, type, area_ha, \'{tableName}\' AS layer, {analysis} AS analysis FROM {tableName}',
+      sql: 'SELECT \'fiber\' as tablename, cartodb_id, the_geom_webmercator,name, type,group_comp as company, area_ha, \'{tableName}\' AS layer, {analysis} AS analysis FROM {tableName}',
       infowindow: true,
-      interactivity: 'cartodb_id, tablename,name, type, area_ha, analysis',
+      interactivity: 'cartodb_id, tablename,name, type, company, area_ha, analysis',
       analysis: true
     }
 
