34ac3e6fca5cd1b15ee27be7b18adb539fdb461e
David Gonzalez <david@simbiotica.es>
2015-02-06 09:44:54 +0100

    new synced oilpalm layer & add name to infowindow

--- app/assets/javascripts/map/views/layers/OilPalmLayer.js
+++ app/assets/javascripts/map/views/layers/OilPalmLayer.js
@@ -12,9 +12,9 @@ define([
   var OilPalmLayer = CartoDBLayerClass.extend({
 
     options: {
-      sql: 'SELECT \'oilpalm\' as tablename, cartodb_id, the_geom_webmercator, type, country, area_ha, \'{tableName}\' AS layer, {analysis} AS analysis FROM {tableName}',
+      sql: 'SELECT \'oilpalm\' as tablename, cartodb_id, the_geom_webmercator, name, type, country, area_ha, \'{tableName}\' AS layer, {analysis} AS analysis FROM {tableName}',
       infowindow: true,
-      interactivity: 'cartodb_id, tablename, country, type, area_ha, analysis',
+      interactivity: 'cartodb_id, tablename, name, country, type, area_ha, analysis',
       analysis: true
     }
 
