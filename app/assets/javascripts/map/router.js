3ba13052c1056cae996858331fabd5556a289245
Adrián Pérez <adrian@simbiotica.es>
2015-02-13 12:26:51 +0100

    Testing git hook, added cache version ++ for commiting whatever

--- app/assets/javascripts/map/router.js
+++ app/assets/javascripts/map/router.js
@@ -18,7 +18,7 @@ define([
   var Router = Backbone.Router.extend({
 
     // temporary, we will do it with env variables
-    _cacheVersion: 21,
+    _cacheVersion: 20,
 
     routes: {
       'map(/:zoom)(/:lat)(/:lng)(/:iso)(/:maptype)(/:baselayers)(/:sublayers)(/)': 'map',
