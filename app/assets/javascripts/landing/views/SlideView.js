94fc72a33f0f0b2ad9cb535eed234ff02cc98dbb
Adrián Pérez <adrian@simbiotica.es>
2015-02-17 17:08:04 +0100

    HOTFIX: added ga events to home start button options

--- app/assets/javascripts/landing/views/SlideView.js
+++ app/assets/javascripts/landing/views/SlideView.js
@@ -165,14 +165,18 @@ define([
         $target = $(e.target).parent();
       }
 
-      var dialog = JSON.stringify(
-      {
-        "display": true,
-        "type" : $target.data('dialog')
-      });
+      if ($target.data('dialog') === true) {
+        var dialog = JSON.stringify(
+        {
+          "display": true,
+          "type" : $target.data('dialog')
+        });
 
-      sessionStorage.setItem('DIALOG', dialog);
-      location.assign($target.data('href'));
+        sessionStorage.setItem('DIALOG', dialog);
+      }
+      console.log($target.data('ga'))
+      ga('send', 'event', 'Home', 'Click', $target.data('ga'))
+      location.assign($target.attr('href'));
     }
 
   });
