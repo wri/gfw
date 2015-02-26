706d04438f27240d2635277528bebeafb152e97b
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2015-02-26 17:11:41 +0100

    Stories: Stories with pagination, videos fixed in mobile

--- app/assets/javascripts/stories/views/CarrouselStoriesView.js
+++ app/assets/javascripts/stories/views/CarrouselStoriesView.js
@@ -4,10 +4,61 @@
 define([
   'jquery',
   'backbone',
-  'mps'
-], function($,Backbone,mps) {
+  'mps',
+  'handlebars',
+  'text!landing/templates/paginationSlider.handlebars',
+], function($, Backbone, mps, Handlebars, tpl) {
 
   'use strict';
+  //HELPER for handlebars
+  Handlebars.registerHelper('for', function(from, to, incr, block) {
+    var accum = '';
+    for(var i = from; i < to; i += incr)
+      accum += block.fn(i);
+    return accum;
+  });
+
+  // PAGINATION
+  var PaginationView = Backbone.View.extend({
+
+    el: '#paginationView',
+
+    template: Handlebars.compile(tpl),
+
+    events: {
+      'click li' : '_navigateTo'
+    },
+
+    initialize: function(model){
+      this.len = model.len;
+      this.render();
+      this._setListeners()
+    },
+
+    _setListeners: function(){
+      mps.subscribe('slider:index', _.bind(function(index){
+        this._changeCurrent(index);
+      },this));
+    },
+
+    render: function(){
+      this.$el.html(this.template({len: this.len}));
+      this.$pages = this.$el.find('li');
+      this._changeCurrent(0);
+    },
+
+    _changeCurrent: function(index){
+      this.$pages.eq(index).addClass('current').siblings().removeClass('current');
+    },
+
+    _navigateTo: function(e){
+      e && e.preventDefault();
+      var index = $(e.currentTarget).index();
+      mps.publish('slider:change',[index]);
+    }
+  });
+
+
 
   var CarrouselStoriesView = Backbone.View.extend({
     el: $('#carrousel-stories'),
@@ -31,14 +82,26 @@ define([
       this.$slide = this.$el.find('.slide');
       this.len = this.$slide.length;
 
+      //Init Pagination
+      this.pagination = new PaginationView({ len: this.len });
+
 
       // inits
       this.setCurrent();
+      this.setListeners();
+    },
+
+    setListeners: function(){
+      mps.subscribe('slider:change', _.bind(function(index){
+        this.current = index;
+        this.setCurrent();
+      },this));
     },
 
     setCurrent: function(){
       this.$slide.removeClass('current');
       this.$slide.eq(this.current).addClass('current');
+      mps.publish('slider:index',[this.current]);
     },
 
     onChange: function(e) {
