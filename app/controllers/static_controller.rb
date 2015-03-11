7821f141150b7617b321599e694f731cda1ad570
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2015-03-11 17:46:15 +0100

    Little fixes. Recover env.example and skip check only for old action

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,5 @@
 class StaticController < ApplicationController
-  skip_before_action :check_browser
+  skip_before_action :check_browser, :only => :old
   #before_filter :load_stories
   skip_before_filter :check_terms, :except => [:data]
   respond_to :html
