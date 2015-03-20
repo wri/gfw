4d8d6e99cf609db9ff3fab76e11606430823f98e
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2015-03-20 11:45:01 +0100

    Js env vars. Countries call is not working

--- app/assets/javascripts/map/services/CountryService.js
+++ app/assets/javascripts/map/services/CountryService.js
@@ -13,7 +13,7 @@ define([
 
     requestId: 'CountryService',
 
-    _uriTemplate:'http://beta.gfw-apis.appspot.com/countries/{iso}',
+    _uriTemplate: window.gfw.config.GFW_API_HOST + '/countries/{iso}',
 
     /**
      * Constructs a new instance of CountryService.
--- app/assets/javascripts/map/services/SitesService.js
+++ app/assets/javascripts/map/services/SitesService.js
@@ -13,7 +13,7 @@ define([
 
     requestId: 'SitesService',
 
-    _uriTemplate:'//gfw-apis.appspot.com/wdpa/sites',
+    _uriTemplate: window.gfw.config.GFW_API_HOST + '/wdpa/sites',
 
     /**
      * Constructs a new instance of SitesService.
