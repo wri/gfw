3c06d9c65b113437bc3b34939bce3182e9c1fb7a
Adam Mulligan <adammulligan@fastmail.fm>
2015-03-27 10:03:04 +0000

    Countries now have static, universal stats files

--- app/controllers/concerns/countries_concern.rb
+++ app/controllers/concerns/countries_concern.rb
@@ -2,6 +2,6 @@ module CountriesConcern
   extend ActiveSupport::Concern
 
   def download_link iso
-    "https://wri-01.cartodb.com/api/v2/sql?q=SELECT iso as country_code,id1 as region_code, country,region, year, thresh as min_percent_canopy_density, loss as loss_ha, loss_perc as loss_percent, extent as extent_ha, extent_perc as extent_percent, gain as gain_ha, gain_perc as gain_percent from umd_subnat where iso='#{iso}'&format=csv&filename='#{iso}'"
+    "http://gfw2-data.s3.amazonaws.com/country/umd_country_stats/umd_country_stats.xlsx"
   end
 end
