1edc444b69e591241a23d6160b03dfa366987c1d
Adrián Pérez <adrian@simbiotica.es>
2015-03-30 12:50:14 +0200

    cache version ++

--- app/assets/javascripts/map/router.js
+++ app/assets/javascripts/map/router.js
@@ -18,7 +18,7 @@ define([
   var Router = Backbone.Router.extend({
 
     // temporary, we will do it with env variables
-    _cacheVersion: 33,
+    _cacheVersion: 34,
 
     routes: {
       'map(/:zoom)(/:lat)(/:lng)(/:iso)(/:maptype)(/:baselayers)(/:sublayers)(/)': 'map',
