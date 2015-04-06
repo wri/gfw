beb3949dafd52412303664ed0ae6f735cd9f5800
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2015-04-06 13:41:53 +0200

    Map: mobile country and layers

--- app/assets/javascripts/map/views/tabs/CountriesView.js
+++ app/assets/javascripts/map/views/tabs/CountriesView.js
@@ -40,7 +40,8 @@ define([
       //countries
       'click #countries-analyze-button' : 'analyzeIso',
       'change #countries-country-select' : 'changeIso',
-      'change #countries-region-select' : 'changeArea',
+      'click #countries-country-ul li' : 'changeIsoMobile',
+      'click #countries-country-reset' : 'changeIsoMobile',
       'click .layer': 'toggleLayer'
     },
 
@@ -88,7 +89,8 @@ define([
       this.$selects = this.$el.find('.chosen-select');
       this.$countrySelect = $('#countries-country-select');
       this.$countryUl = $('#countries-country-ul');
-      this.$regionSelect = $('#countries-region-select');
+      this.$countryReset = $('#countries-country-reset');
+      this.$countryName = $('#countries-name');
       this.inits();
     },
 
@@ -179,10 +181,6 @@ define([
     /**
      * COUNTRY
      */
-
-    /**
-     * Ajax for getting countries.
-     */
     getCountries: function(){
       if (!amplify.store('countries')) {
         var sql = ['SELECT c.iso, c.name FROM gfw2_countries c WHERE c.enabled = true'];
@@ -200,21 +198,6 @@ define([
       }else{
         this.printCountries()
       }
-    },
-
-    getSubCountries: function(){
-      this.$regionSelect.attr('disabled', true).trigger("liszt:updated");
-      var sql = ["SELECT gadm_1_all.cartodb_id, gadm_1_all.iso, gadm2_provinces_simple.id_1, gadm2_provinces_simple.name_1 as name_1 FROM gadm_1_all, gadm2_provinces_simple where gadm_1_all.iso = '"+this.iso+"' AND gadm2_provinces_simple.iso = '"+this.iso+"' AND gadm2_provinces_simple.id_1 = gadm_1_all.id_1 order by id_1 asc"];
-      $.ajax({
-        url: 'https://wri-01.cartodb.com/api/v2/sql?q='+sql,
-        dataType: 'json',
-        success: _.bind(function(data){
-          this.printSubareas(data.rows);
-        }, this ),
-        error: function(error){
-          console.log(error);
-        }
-      });
     },
 
     /**
@@ -247,40 +230,45 @@ define([
       }
     },
 
-    printSubareas: function(subareas){
-      var subareas = subareas;
-      var options = "<option></option>";
-      _.each(_.sortBy(subareas, function(area){ return area.name_1 }), _.bind(function(area, i){
-        options += '<option value="'+ area.id_1 +'">'+ area.name_1 + '</option>';
-      }, this ));
-      this.$regionSelect.empty().append(options).removeAttr('disabled');
-      this.$regionSelect.val(this.area).trigger("liszt:updated");
-    },
-
     // Select change iso
     changeIso: function(e){
       this.iso = $(e.currentTarget).val() || null;
-      // ($(e.currentTarget).val()) ? this.getSubCountries() : this.$regionSelect.val(null).attr('disabled', true).trigger("liszt:updated");
-      this.setIsoLayers();
-      this.setButtons(!!this.iso);
+      this.commonIsoChanges();
       this.presenter.changeIso({country: this.iso, region: null});
     },
-    // changeArea: function(e){
-    //   this.area = $(e.currentTarget).val();
-    //   this.presenter.changeIso({country: this.iso, region: this.area});
-    // },
+
+    changeIsoMobile: function(e){
+      this.iso = $(e.currentTarget).data('value') || null;
+      this.commonIsoChanges();
+      this.presenter.changeIso({country: this.iso, region: null});
+    },
 
     // For autoselect country and region when youn reload page
     setSelects: function(iso){
       this.iso = iso.country;
-      // this.area = iso.region;
+      this.commonIsoChanges();
+      this.$countrySelect.val(this.iso).trigger("liszt:updated");
+    },
+
+    commonIsoChanges: function(){
       this.setIsoLayers();
       this.setButtons(!!this.iso);
-      this.$countrySelect.val(this.iso).trigger("liszt:updated");
-      // this.$regionSelect.val(this.area).trigger("liszt:updated");
-      // if (this.iso) {
-      //   this.getSubCountries();
-      // }
+
+
+      if (this.mobile) {
+        var country = _.find(amplify.store('countries'), _.bind(function(country){
+          return country.iso === this.iso;
+        }, this ));
+        var name = (country) ? country.name + ' Data' : 'Country Data';
+        this.$countryName.text(name);
+        if(!!this.iso) {
+          this.$countryReset.show(0);
+          this.$countryUl.addClass('hidden');
+        }else{
+          this.$countryReset.hide(0);
+          this.$countryUl.removeClass('hidden');
+        }
+      };
     },
 
   });
