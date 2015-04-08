5fa02a03dff4b036d5382948b5e18c9f5f882ed6
Alicia Arenzana Gil de muro <alicia.arenzana@gmail.com>
2015-04-08 16:26:31 +0200

    New local layer added: Congo eucalyptus plantations

--- app/assets/javascripts/map/helpers/layersHelper.js
+++ app/assets/javascripts/map/helpers/layersHelper.js
@@ -39,6 +39,7 @@ define([
   'map/views/layers/OilPalmLayer',
   'map/views/layers/CogOilPalmLayer',
   'map/views/layers/WoodFiberPlantationsLayer',
+  'map/views/layers/CogWoodFiberPlantationsLayer',
   'map/views/layers/ProtectedAreasLayer',
   'map/views/layers/ProtectedAreasCDBLayer',
   'map/views/layers/BiodiversityHotspotsLayer',
@@ -107,6 +108,7 @@ define([
   OilPalmLayer,
   CogOilPalmLayer,
   WoodFiberPlantationsLayer,
+  CogWoodFiberPlantationsLayer,
   ProtectedAreasLayer,
   ProtectedAreasCDBLayer,
   BiodiversityHotspotsLayer,
@@ -253,6 +255,9 @@ define([
     },
     wood_fiber_plantations: {
       view: WoodFiberPlantationsLayer
+    },
+    cog_wood_fiber: {
+      view: CogWoodFiberPlantationsLayer
     },
     protected_areas: {
       view: ProtectedAreasLayer
