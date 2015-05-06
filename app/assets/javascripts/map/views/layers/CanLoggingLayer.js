e488e86ee90c1ec93a1adf092063d55974fbb71b
Alicia Arenzana Gil de muro <alicia.arenzana@gmail.com>
2015-05-06 18:58:43 +0200

    added analisis functionality

--- app/assets/javascripts/map/views/layers/CanLoggingLayer.js
+++ app/assets/javascripts/map/views/layers/CanLoggingLayer.js
@@ -15,7 +15,7 @@ define([
       sql: 'SELECT \'can_logging\' as tablename, cartodb_id, the_geom_webmercator, company, country, round(area_ha::float) as area_ha, name, source, type, \'{tableName}\' AS layer, {analysis} AS analysis FROM {tableName}' ,
       infowindow: true,
       interactivity: 'cartodb_id, tablename, name, company, area_ha, analysis',
-      analysis: false
+      analysis: true
     }
   });
 
--- app/assets/javascripts/map/views/layers/CodMiningLayer.js
+++ app/assets/javascripts/map/views/layers/CodMiningLayer.js
@@ -15,7 +15,7 @@ define([
       sql: 'SELECT \'cod_mining\' as tablename, cartodb_id, the_geom_webmercator, parties as company, resource as name, sup_sig_ha as area_ha, \'{tableName}\' AS layer, {analysis} AS analysis FROM {tableName}' ,
       infowindow: true,
       interactivity: 'cartodb_id, tablename, company, name, area_ha, analysis',
-      analysis: false
+      analysis: true
     }
     
 
