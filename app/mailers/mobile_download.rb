f121d99c04c28ec607d5818ffa3dbcc28e3cac80
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2015-05-14 16:37:56 +0200

    Download: mails text change

--- app/mailers/mobile_download.rb
+++ app/mailers/mobile_download.rb
@@ -5,7 +5,6 @@ class MobileDownload < ActionMailer::Base
   def download_email email_address, download_link, type
     @download_link = download_link
     @type = type
-    subject = (@type.present?) ? 'Link to download tree cover statistics' : 'Link to browse and download country data'
-    mail to: email_address, subject: subject
+    mail to: email_address
   end
 end
