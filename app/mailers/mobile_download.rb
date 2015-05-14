28c1fade161aad868fff8cbe136d57023a8f148f
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2015-05-14 16:54:27 +0200

    Download: mails text change

--- app/mailers/mobile_download.rb
+++ app/mailers/mobile_download.rb
@@ -4,7 +4,7 @@ class MobileDownload < ActionMailer::Base
   # Set subject in I18n: en.mobile_download.download_email.subject
   def download_email email_address, download_link, type
     @download_link = download_link
-    # @type = type
+    @type = type
     mail to: email_address
   end
 end
