ba4fcafc8394942f43f9ec20a995aa9540c12fb9
Alicia Arenzana Gil de muro <alicia.arenzana@gmail.com>
2015-05-19 10:04:50 +0200

    added analisis functionality

--- app/assets/javascripts/map/views/layers/ResourceRightsLayer.js
+++ app/assets/javascripts/map/views/layers/ResourceRightsLayer.js
@@ -12,9 +12,10 @@ define([
   var ResourceRightsLayer = CartoDBLayerClass.extend({
 
     options: {
-      sql: 'SELECT the_geom_webmercator, name, country, round(area_ha::float) as area_ha, legal_term, legal_reco as legal_recognition, \'{tableName}\' AS layer FROM {tableName}',
+      sql: 'SELECT the_geom_webmercator, cartodb_id, name, country, round(area_ha::float) as area_ha, legal_term, legal_reco as legal_recognition,\'{tableName}\' AS tablename, \'{tableName}\' AS layer, {analysis} AS analysis FROM {tableName}',
       infowindow: true,
-      interactivity: 'country, name, area_ha, legal_term, legal_recognition'
+      interactivity: 'cartodb_id, tablename, layer,country, name, area_ha, legal_term, legal_recognition, analysis'
+      analysis: true
     }
 
   });
