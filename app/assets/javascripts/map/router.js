e02eddd11cc4addb5139ec97b940e2a705b025c6
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2015-06-05 09:45:05 +0200

    Map: private mode alert typo change

--- app/assets/javascripts/map/router.js
+++ app/assets/javascripts/map/router.js
@@ -88,14 +88,14 @@ define([
       // throw QuotaExceededError. We're going to detect this and just silently drop any calls to setItem
       // to avoid the entire page breaking, without having to do a check at each usage of Storage.
       if (typeof localStorage === 'object') {
-          try {
-            localStorage.setItem('localStorage', 1);
-            localStorage.removeItem('localStorage');
-          } catch (e) {
-            Storage.prototype._setItem = Storage.prototype.setItem;
-            Storage.prototype.setItem = function() {};
-            alert('Your web browser does not support storing settings locally. In Safari, the most common cause of this is using "Private Browsing Mode". Some settings may not save or some features may not work properly for you.');
-          }
+        try {
+          localStorage.setItem('localStorage', 1);
+          localStorage.removeItem('localStorage');
+        } catch (e) {
+          Storage.prototype._setItem = Storage.prototype.setItem;
+          Storage.prototype.setItem = function() {};
+          alert('You may not be able to experience full functionality of the map while in private mode because your web browser does not support storing settings locally. In Safari, the most common cause of this is using "Private Browsing Mode".');
+        }
       }
     },
 
