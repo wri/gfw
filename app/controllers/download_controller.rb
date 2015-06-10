d1fdf00018dcd736a5888a293eeff8e4245d9bfc
Adrián Pérez <adrian@simbiotica.es>
2015-06-10 10:33:15 +0200

    merge develop into master REDUX

---
+++ app/controllers/download_controller.rb
@@ -0,0 +1,17 @@
+class DownloadController < ApplicationController
+
+  skip_before_filter :verify_authenticity_token, only: [:create_download]
+
+  def create_download
+    if (params[:email].present?)
+      MobileDownload.download_email(
+        params[:email],
+        params[:link],
+        params[:type],
+      ).deliver
+
+      return render json: true
+    end
+    return render json: false, status: :unprocessable_entity
+  end
+end
