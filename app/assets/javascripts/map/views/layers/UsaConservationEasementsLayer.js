69b7972cf40ea5018a8c1f206d2158b540119ad6
Alicia Arenzana Gil de muro <alicia.arenzana@gmail.com>
2015-06-19 10:37:46 +0200

    Conflict merge

--- app/assets/javascripts/map/views/layers/UsaConservationEasementsLayer.js
+++ app/assets/javascripts/map/views/layers/UsaConservationEasementsLayer.js
@@ -12,10 +12,10 @@ define([
   var UsaConservationEasementsLayer = CartoDBLayerClass.extend({
 
     options: {
-      sql: 'SELECT the_geom_webmercator, sitename as name, round(gis_acres::float) as gis_acres, round(rep_acres::float) as rep_acres, state, purpose, pubaccess, duration, dataagg, datapvdr, esmthldr, eholdtyp, {analysis} AS analysis, \'{tableName}\' as layer FROM {tableName}',
+      sql: 'SELECT cartodb_id, \'{tableName}\' as tablename, the_geom_webmercator, sitename as name, round(gis_acres::float) as gis_acres, round(rep_acres::float) as rep_acres, state, purpose, pubaccess, duration, dataagg, datapvdr, esmthldr, eholdtyp, \'{tableName}\' as layer, {analysis} AS analysis FROM {tableName}',
       infowindow: true,
-      interactivity: ' name, gis_acres, rep_acres, state, purpose, pubaccess, duration, dataagg, datapvdr, esmthldr, eholdtyp, analysis',
-      analysis: false
+      interactivity: ' cartodb_id, tablename, name, gis_acres, rep_acres, state, purpose, pubaccess, duration, dataagg, datapvdr, esmthldr, eholdtyp, analysis',
+      analysis: true
     },
 
   });
