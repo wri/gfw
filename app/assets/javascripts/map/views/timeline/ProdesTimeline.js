e03c0c3ab485abfa8dd6b7028174d2c9c1370b2b
Adrián Pérez <adrian@simbiotica.es>
2015-07-24 15:11:43 +0200

    Timeline for prodes layer

--- app/assets/javascripts/map/views/timeline/ProdesTimeline.js
+++ app/assets/javascripts/map/views/timeline/ProdesTimeline.js
@@ -1,30 +1,34 @@
 /**
- * The Imazon timeline.
+ * The Prodes loss timeline.
  *
- * @return ImazonTimeline class (extends TimelineMonthClass)
+ * @return ProdesTimeline class (extends TimelineYearClass)
  */
 define([
   'moment',
   'abstract/timeline/TimelineYearClass',
   'map/presenters/TimelineClassPresenter'
-], function(moment, TimelineMonthClass, Presenter) {
+], function(moment, TimelineYearClass, Presenter) {
 
   'use strict';
 
-  var ProdesTimeline = TimelineMonthClass.extend({
+  var ProdesTimeline = TimelineYearClass.extend({
 
+    /**
+     * Get the layer spec.
+     *
+     * @param  {object} layer The layer object
+     */
     initialize: function(layer, currentDate) {
+      this.presenter = new Presenter(this);
+
       this.options = {
         dateRange: [moment(layer.mindate), moment(layer.maxdate)],
-        player: false
+        play: false
       };
 
-      this.presenter = new Presenter(this);
       ProdesTimeline.__super__.initialize.apply(this, [layer, currentDate]);
     }
-
   });
 
   return ProdesTimeline;
-
 });
