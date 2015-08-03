d4514f3e3bd50fc7466fed1b7b31e459aff08721
Adrián Pérez <adrian@simbiotica.es>
2015-08-03 18:45:18 +0200

    Puma server conf

--- config/puma.rb
+++ config/puma.rb
@@ -7,9 +7,3 @@ preload_app!
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'production'
-
-on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
-end
