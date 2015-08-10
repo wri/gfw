71c0820cff3e305c7477b4740efae968c09cc21b
Adrián Pérez <adrian@simbiotica.es>
2015-08-10 18:53:44 +0200

    perpref infowindow

--- app/assets/javascripts/map/views/layers/PerPermForestLayer.js
+++ app/assets/javascripts/map/views/layers/PerPermForestLayer.js
@@ -14,7 +14,7 @@ define([
     options: {
       sql: 'SELECT cartodb_id, the_geom_webmercator, region, area_aprob as area_ha, doc_legal, zone, \'{tableName}\' AS tablename, {analysis} AS analysis, \'{tableName}\' AS layer FROM {tableName}',
       infowindow: true,
-      interactivity: 'cartodb_id, tablename, region, doc_legal,zone, area_ha, analysis',
+      interactivity: 'cartodb_id, tablename, region, doc_legal, zone, area_ha, analysis',
       analysis: true
     }
 
