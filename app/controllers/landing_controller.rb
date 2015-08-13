48c276fcf1e8144c5cafceacd72b19540c92dfa4
Adrián Pérez <adrian@simbiotica.es>
2015-08-13 16:14:52 +0200

    get only the required rss items

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -6,25 +6,27 @@ class LandingController < ApplicationController
     @title = 'Monitoring forests in near real time'
     @desc = 'Empowering people everywhere to better manage and conserve forest landscapes through an interactive online forest monitoring system.'
     @keywords = 'forests, forest data, forest monitoring, forest landscapes, maps, gis, visualize, geospatial, forest analysis, forest news, forest alerts, conservation, forest updates, forest watch, analysis, deforestation, deforesting, tree cover loss, explore forests, mapping, trees, forest loss'
-    @feedview    = {}
-    @storiesview = {}
+    @feedview    = []
+    @storiesview = []
     fview     = RSS::Parser.parse('https://gfw-huggin.herokuapp.com/users/1/web_requests/14/feedviewrss.xml')
-    @feedview = fview.items.each do |i|
-      {
+    fview.items.each do |i|
+      @feedview.push({
         'title' => i.title,
         'link' => i.link,
         'date' => i.pubDate,
         'description' => i.description
-      }
+      })
+      break if @feedview.length > 4
     end
     fstories  = RSS::Parser.parse('https://gfw-huggin.herokuapp.com/users/1/web_requests/15/keepupdatedgfwrss.xml')
-    @storiesview = fstories.items.each do |i|
-      {
+    fstories.items.each do |i|
+      @storiesview.push({
         'title' => i.title,
         'link' => i.link,
         'date' => i.pubDate,
         'description' => i.description
-      }
+      })
+      break if @storiesview.length > 2
     end
   end
 
