94653b5d820e8f88cb03abcaf02415fb9fe938c4
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2015-08-20 11:43:04 +0200

    Seo: keywords updated

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -4,7 +4,7 @@ class LandingController < ApplicationController
   def index
     @title = 'Monitoring forests in near real time'
     @desc = 'Empowering people everywhere to better manage and conserve forest landscapes through an interactive online forest monitoring system.'
-    @keywords = 'forests, forest data, forest monitoring, forest landscapes, maps, gis, visualize, geospatial, forest analysis, forest news, forest alerts, conservation, forest updates, forest watch, analysis, deforestation, deforesting, tree cover loss, explore forests, mapping, trees, forest loss'
+    @keywords = 'GFW, forests, forest data, forest monitoring, forest landscapes, maps, gis, visualize, geospatial, forest analysis, forest news, forest alerts, conservation, forest updates, forest watch, analysis, deforestation, deforesting, tree cover loss, explore forests, mapping, trees, forest loss'
   end
 
 end
