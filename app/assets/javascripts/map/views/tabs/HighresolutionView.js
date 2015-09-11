a9ef168fdd28675f358e13a78c0442d436f40905
Adrián Pérez <adrian@simbiotica.es>
2015-09-11 10:14:12 +0200

    urthecast params console improvement

--- app/assets/javascripts/map/views/tabs/HighresolutionView.js
+++ app/assets/javascripts/map/views/tabs/HighresolutionView.js
@@ -20,7 +20,6 @@ define([
     template: Handlebars.compile(tpl),
 
     events: {
-      'click .maptype': '_setMaptype',
       'click button' : '_setParams'
     },
 
@@ -30,44 +29,17 @@ define([
       this.render();
     },
 
+    selectMaptype: function(maptype) {
+      return;
+    },
+
     render: function(){
       this.$el.html(this.template());
-      this.$maptypeslist = this.$el;
-      this.$maptypes = this.$el.find('.maptype');
-      enquire.register("screen and (min-width:"+window.gfw.config.GFW_MOBILE+"px)", {
-        match: _.bind(function(){
-          this.mobile = false;
-        },this)
-      });
-      enquire.register("screen and (max-width:"+window.gfw.config.GFW_MOBILE+"px)", {
-        match: _.bind(function(){
-          this.mobile = true;
-        },this)
-      });
-
     },
 
     _setMaptype: function(e) {
-      e && e.preventDefault();
-      if (!$(e.target).hasClass('source') && !$(e.target).parent().hasClass('source')) {
-        var $currentTarget = $(e.currentTarget);
-        var maptype = $currentTarget.data('maptype');
-        if (maptype) {
-          this.presenter.setMaptype(maptype);
-          ga('send', 'event', 'Map', 'Toggle', maptype);
-        }
-      }
-    },
-
-    /**
-     * Add selected mapview to .maptype-selected
-     * and close the widget.
-     *
-     * @param  {string} maptype
-     */
-    selectMaptype: function(maptype) {
-      this.$maptypes.removeClass('selected');
-      this.$maptypeslist.find('.' + maptype).addClass('selected');
+      this.presenter.setMaptype('positron');
+      this.presenter.setMaptype('urthe');
     },
 
     _setParams: function(e) {
@@ -79,8 +51,8 @@ define([
            'mindate': ($objTarget.find('.mindate').val().length > 0) ? $objTarget.find('.mindate').val() : '2000-09-01',
            'maxdate': ($objTarget.find('.maxdate').val().length > 0) ? $objTarget.find('.maxdate').val() : '2015-09-01'
          });
-      console.log(params)
       sessionStorage.setItem($objTarget.data('maptype'), params);
+      this._setMaptype();
     }
   });
 
