9117ae2b0542ca274efea3dd6497093efb51698c
Adrián Pérez <adrian@simbiotica.es>
2015-09-21 15:56:43 +0200

    parsing correctly the feed

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,33 +2,31 @@ class LandingController < ApplicationController
   layout 'landing'
 
   def index
-    require 'rss'
+    require 'open-uri'
     @title = 'Monitoring forests in near real time'
     @desc = 'Empowering people everywhere to better manage and conserve forest landscapes through an interactive online forest monitoring system.'
     @keywords = 'forests, forest data, forest monitoring, forest landscapes, maps, gis, visualize, geospatial, forest analysis, forest news, forest alerts, conservation, forest updates, forest watch, analysis, deforestation, deforesting, tree cover loss, explore forests, mapping, trees, forest loss'
     @feedview    = []
     @storiesview = []
-    fview     = RSS::Parser.parse('https://gfw-huggin.herokuapp.com/users/1/web_requests/14/feedviewrss.xml')
-    fview.items.each do |i|
+    fview     = Nokogiri::HTML(open('https://gfw-huggin.herokuapp.com/users/1/web_requests/14/feedviewrss.xml'))
+    fview.css('item').each do |i|
       @feedview.push({
-        'title' => i.title,
-        'link' => i.link,
-        'date' => i.pubDate,
-        'description' => i.description,
-        'id' => i.gfwid
+        'title' => i.css('title').text,
+        'link' => i.css('link').text,
+        'date' => i.css('pubDate').text,
+        'description' => i.css('description').text
       })
       break if @feedview.length > 5
     end
-    fstories  = RSS::Parser.parse('https://gfw-huggin.herokuapp.com/users/1/web_requests/15/keepupdatedgfwrss.xml')
-    fstories.items.each do |i|
-      puts i
+    fstories  = Nokogiri::HTML(open('https://gfw-huggin.herokuapp.com/users/1/web_requests/15/keepupdatedgfwrss.xml'))
+    fstories.css('item').each do |i|
       @storiesview.push({
-        'title' => i.title,
-        'link' => i.link,
-        'date' => i.pubDate,
-        'description' => i.description,
+        'title' => i.css('title').text,
+        'link' => i.css('link').text,
+        'date' => i.css('pubDate').text,
+        'description' => i.css('description').text,
         'avatar' => '',
-        'id' => i.gfwid
+        'id' => i.css('gfwid')
       })
       break if @storiesview.length > 2
     end
