8d7a0cf36d74db9468c788801abe2914aa96540c
Adrián Pérez <adrian@simbiotica.es>
2015-09-25 12:55:32 +0200

    vizzuality logo fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Gfw::Application.configure do
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '15'
+  config.assets.version = '16'
 
   config.static_cache_control = 'public, max-age=31536000'
 
