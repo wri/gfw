eafb93f74ac3f387ea09258cec91ec15bdb00427
paulahaertel <paulahaertel@yahoo.de>
2015-10-09 17:37:45 +0200

    adds minor fixes, adds event listener to toggle button

--- app/assets/javascripts/static/HowToToggleView.js
+++ app/assets/javascripts/static/HowToToggleView.js
@@ -33,37 +33,47 @@ define([
 
 
       this.render();
+      this.setListeners();
+    },
+
+    setListeners: function() {
+      mps.subscribe('Howto/toggle', _.bind(function(mobile){
+        console.log(mobile);
+        this.toggleStates(mobile);
+      }, this ))
     },
 
     //hide view-global-data and view-country-data
     //show view-global-data-m and view-country-data-m
     //change tags of desktop to span, tags of mobile to strong
     toggle: function(e) {
-      if ($(e.currentTarget).hasClass('active') != true){
-
-        //make the other one inactive
-        this.$el.find('span').removeClass('active');
-
-        //make clicked element active
-        $(e.currentTarget).addClass('active');
-
+      if (!$(e.currentTarget).hasClass('active')){
         //wenn auf mobile geklickt wurde
         if ($(e.currentTarget).data('classname') == 'mobile'){
-          this.desktop.addClass('hide');
-          this.mobile.removeClass('hide');
+          mps.publish('Howto/toggle', [true])
         }else{
-          this.mobile.addClass('hide');
-          this.desktop.removeClass('hide');
+          mps.publish('Howto/toggle', [false])
         }
       }
-
-
     },
 
+    toggleStates: function(mobile) {
+      var $desktop = this.$el.find('span[data-classname="desktop"]');
+      var $mobile = this.$el.find('span[data-classname="mobile"]');
+      //wenn auf mobile geklickt wurde
+      if (mobile){
+        $mobile.addClass('active').siblings().removeClass('active');
+        this.desktop.addClass('hide');
+        this.mobile.removeClass('hide');
+      }else{
+        $desktop.addClass('active').siblings().removeClass('active');
+        this.mobile.addClass('hide');
+        this.desktop.removeClass('hide');
+      }
+    },
 
     render: function(){
-      this.$el.html(this.template());
-
+      this.$el.html(this.template({ desktop: !isMobile.any, mobile: isMobile.any }));
     }
 
   });
