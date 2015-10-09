3be1a45688d5b41b331a2a00db4263129fca98c1
Alicia <alicia@MacBook-Pro-de-Alicia.local>
2015-10-09 10:55:35 +0200

    added infowindow

--- app/assets/javascripts/map/views/layers/LandsatAlertsLayer.js
+++ app/assets/javascripts/map/views/layers/LandsatAlertsLayer.js
@@ -12,8 +12,10 @@ define([
 
   var LandsatAlertsLayer = CartoDBLayerClass.extend({
     options: {
-      sql:  'SELECT cartodb_id, the_geom_webmercator, grid_code, \'{tableName}\' as layer, \'{tableName}\' AS name FROM {tableName} ',
-      cartocss: LandsatAlertsCartoCSS
+      sql:  "select * from (SELECT cartodb_id, the_geom_webmercator, to_char((date '2014-12-31' + grid_code::int),'mm/dd/yyyy') as dates, \'{tableName}\' as layer, \'{tableName}\' AS name FROM {tableName}) p",
+      cartocss: LandsatAlertsCartoCSS,
+      infowindow: true,
+      interactivity: 'cartodb_id, dates'
     }
   });
 
