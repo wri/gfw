97cc370de4d6efa923bcbf008e1e1285c8106b7b
Adam Mulligan <adammulligan@gmail.com>
2015-11-30 09:47:47 +0000

    Fixes tests

--- jstest/spec/canvasCartoCSSHelper_spec.js
+++ jstest/spec/canvasCartoCSSHelper_spec.js
@@ -21,28 +21,7 @@ define([
 
         it('generates cartocss with default layer rules', function() {
           var expectedCss = "#layer { marker-fill-opacity: 1;marker-line-width: 0;marker-placement: point;marker-width: 1;marker-height: 1;marker-allow-overlap: true";
-          console.log(css);
-          console.log(expectedCss);
           expect(css.indexOf(expectedCss)).toBeGreaterThan(-1);
-        });
-
-        it('generates cartocss with a rule for each day', function() {
-          var expectedCss = "[date=\"2015-01-01\"] { marker-fill: rgba(1, 0, 0, 1); } [date=\"2015-01-02\"] { marker-fill: rgba(2, 0, 0, 1); } }";
-          expect(css.indexOf(expectedCss)).toBeGreaterThan(0);
-        });
-      });
-
-      describe('given a start and end date spanning more than 255 days', function() {
-        var css;
-
-        beforeEach(function() {
-          css = helper.generateDaily('date',
-            moment('2015-01-01'), moment('2015-12-01'));
-        });
-
-        it('uses the G channel for the overflow', function() {
-          var cssRegex = new RegExp("\\[date=\"2015-09-13\"\\] { marker-fill: rgba\\(0, 1, 0, 1\\); }");
-          expect(css).toMatch(cssRegex);
         });
       });
     });
