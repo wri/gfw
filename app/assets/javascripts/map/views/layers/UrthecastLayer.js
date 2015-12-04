a780f3f5b0bd1e3cece2fc7889c5818e813aa813
Adrián Pérez <adrian@simbiotica.es>
2015-12-04 18:42:55 +0100

    all aps icon for navbar

--- app/assets/javascripts/map/views/layers/UrthecastLayer.js
+++ app/assets/javascripts/map/views/layers/UrthecastLayer.js
@@ -11,7 +11,8 @@ define([
 
   var UrthecastLayer = ImageMaptypeLayerClass.extend({
     options: {
-      urlTemplate:'http://uc.gfw-apis.appspot.com/urthecast/map-tiles{/sat}{/z}{/x}{/y}?cloud_coverage_lte={cloud}&acquired_gte={mindate}T00:00:00Z&acquired_lte={maxdate}T00:00:z00Z'
+      urlTemplate:'http://uc.gfw-apis.appspot.com/urthecast/map-tiles{/sat}{/z}{/x}{/y}?cloud_coverage_lte={cloud}&acquired_gte={mindate}T00:00:00Z&acquired_lte={maxdate}T00:00:z00Z',
+      dataMaxZoom: 13
     }
 
   });
