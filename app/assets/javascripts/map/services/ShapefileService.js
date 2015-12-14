da26cd2869b76a535f2a3147e3417f170a85c37d
Adam Mulligan <adammulligan@gmail.com>
2015-12-14 16:49:30 +0100

    Simple service to convert shapefiles to geojson

---
+++ app/assets/javascripts/map/services/ShapefileService.js
@@ -0,0 +1,37 @@
+define([
+  'Class',
+  'jquery'
+], function(Class, $) {
+
+	var CONVERTER_URL = "http://ogre.adc4gis.com/convert";
+
+  var ShapefileService = Class.extend({
+
+    init: function(options) {
+      options = options || {};
+      this.shapefile = options.shapefile;
+    },
+
+    toGeoJSON: function() {
+      var deferred = $.Deferred();
+
+			var xhr = new XMLHttpRequest();
+			xhr.open("POST", CONVERTER_URL, true);
+			xhr.onreadystatechange = function() {
+				if (xhr.readyState == 4 && xhr.status == 200) {
+					deferred.resolve(JSON.parse(xhr.responseText));
+				}
+			};
+
+      var formData = new FormData();
+      formData.append('upload', this.shapefile);
+			xhr.send(formData);
+
+      return deferred.promise();
+    }
+
+  });
+
+  return ShapefileService;
+
+});
