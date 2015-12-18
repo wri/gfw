fbda07feefb27baf2bbfb3b082fbb506f56bcb95
Alicia Arenzana Gil de muro <alicia.arenzana@gmail.com>
2015-12-18 17:35:14 +0100

    added date

--- app/assets/javascripts/map/views/layers/MysPALayer.js
+++ app/assets/javascripts/map/views/layers/MysPALayer.js
@@ -13,9 +13,9 @@ define([
   var MysPALayer = CartoDBLayerClass.extend({
 
     options: {
-      sql: 'SELECT \'{tableName}\' as tablename, cartodb_id, the_geom_webmercator, category, status, gaz_eff_da as date, round(area_ha::float) as area_ha, name, \'{tableName}\' AS layer, {analysis} AS analysis FROM {tableName}' ,
+      sql: 'SELECT \'{tableName}\' as tablename, cartodb_id, the_geom_webmercator, category, status, gaz_eff_da as date_create, round(area_ha::float) as area_ha, name, \'{tableName}\' AS layer, {analysis} AS analysis FROM {tableName}' ,
       infowindow: true,
-      interactivity: 'cartodb_id, tablename, name, status, category, area_ha, date, analysis',
+      interactivity: 'cartodb_id, tablename, name, status, category, area_ha, date_create, analysis',
       analysis: true,
       cartocss: MysPACartocss
     }
