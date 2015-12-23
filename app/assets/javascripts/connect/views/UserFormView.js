9ce5660167bc49567407a01a40409d7f795b55f4
Adrián Pérez <adrian@simbiotica.es>
2015-12-23 12:15:22 +0100

    render on the same view MY GFW tabs

--- app/assets/javascripts/connect/views/UserFormView.js
+++ app/assets/javascripts/connect/views/UserFormView.js
@@ -7,27 +7,31 @@ define([
   'backbone',
   'handlebars',
   'mps',
-  'text!connect/templates/userForm.handlebars'
-], function(Backbone, Handlebars, mps, tpl) {
+  'text!connect/templates/userForm.handlebars',
+  'text!connect/templates/subscriptionList.handlebars'
+], function(Backbone, Handlebars, mps, tpl_form, tpl_list) {
 
   'use strict';
 
   var UserFormView = Backbone.View.extend({
     className: 'user-form',
-    el: '.user-form',
+    el: '#my-gfw',
     events: {
       'click #sendform' : '_submit',
-      'click #skipform' : '_destroy'
+      'click #skipform' : '_destroy',
+      'click .tabs h3'  : '_toggleTab'
     },
 
-    template: Handlebars.compile(tpl),
+    template_form: Handlebars.compile(tpl_form),
+    template_list: Handlebars.compile(tpl_list),
 
     initialize: function(parent) {
-      // this.render();
+      this.render();
     },
 
     render: function() {
-      this.$el.html(this.template({'action': window.gfw.config.GFW_API_HOST+'/user/setuser','redirect':window.location.href}));
+      this.$el.find('.user-form').html(this.template_form({'action': window.gfw.config.GFW_API_HOST+'/user/setuser','redirect':window.location.href}));
+      this.$el.find('.subscription-list').html(this.template_list());
     },
 
     _submit: function() {
@@ -36,6 +40,11 @@ define([
 
     _destroy: function() {
 
+    },
+
+    _toggleTab: function(e) {
+      if ($(e.target).hasClass('current')) return;
+      console.log(e)
     }
   });
 
