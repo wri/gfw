c1cc561e165e2f5952c11e6046461a5a3583553d
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2016-01-05 16:02:21 +0100

    Fix

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -105,9 +105,8 @@ class StaticController < ApplicationController
     signup   = params["signup"]
     email    = params["email"]
     feedback = params["feedback"]
-    hostname = params["hostname"]
 
-    YourMailer.feedback(feedback,signup,email,hostname).deliver
+    YourMailer.feedback(feedback,signup,email).deliver
     @loggedin = !!cookies[:_eauth]
   end
 
@@ -115,7 +114,9 @@ class StaticController < ApplicationController
     signup   = params["signup"]
     email    = params["email"]
     feedback = params["feedback"]
-    YourMailer.feedback(feedback,signup,email).deliver
+    hostname = params["hostname"]
+
+    YourMailer.feedback(feedback,signup,email,hostname).deliver
     respond_to do |format|
       format.js do
 
