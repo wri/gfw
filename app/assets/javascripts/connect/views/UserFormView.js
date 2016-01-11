426c92e910ab7b9198e844e1680ee8f6be4c72df
Adam Mulligan <adammulligan@gmail.com>
2016-01-11 16:57:20 +0000

    Redirect back to previous page when user first adds their email

--- app/assets/javascripts/connect/views/UserFormView.js
+++ app/assets/javascripts/connect/views/UserFormView.js
@@ -40,7 +40,24 @@ define([
         }, {});
 
       this.user.set(formValues);
-      this.user.save();
+      this.user.save().then(this._redirect);
+    },
+
+    _redirect: function() {
+                        var match,
+        pl     = /\+/g,
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function (s) { return decodeURIComponent(s.replace(pl, ' ')); },
+        query  = window.location.search.substring(1),
+        urlParams = {};
+
+      while((match = search.exec(query)) !== null) {
+        urlParams[decode(match[1])] = decode(match[2]);
+      }
+
+      if (urlParams.redirect !== undefined) {
+        window.location.href = urlParams.redirect;
+      }
     }
   });
 
