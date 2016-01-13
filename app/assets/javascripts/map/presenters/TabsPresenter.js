d7d30eac7621c31e53b06d118e61f6750884564f
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2016-01-13 10:24:18 +0100

    Tabs are now saved in url.

--- app/assets/javascripts/map/presenters/TabsPresenter.js
+++ app/assets/javascripts/map/presenters/TabsPresenter.js
@@ -1,27 +1,47 @@
 /**
- * The MapControlsPresenter class for the MapControlsPresenter view.
+ * The TabsPresenter class for the TabsPresenter view.
  *º
- * @return MapControlsPresenter class.
+ * @return TabsPresenter class.
  */
 define([
   'underscore',
+  'backbone',
   'mps',
   'map/presenters/PresenterClass'
-], function(_, mps, PresenterClass) {
+], function(_, Backbone, mps, PresenterClass) {
 
   'use strict';
 
-  var MapControlsPresenter = PresenterClass.extend({
+  var StatusModel = Backbone.Model.extend({
+    defaults: {
+      tab: 'countries-tab'
+    }
+  });
+
+  var TabsPresenter = PresenterClass.extend({
 
     init: function(view) {
       this.view = view;
+      this.status = new StatusModel();
       this._super();
+      enquire.register("screen and (min-width:"+window.gfw.config.GFW_MOBILE+"px)", {
+        match: _.bind(function(){
+          mps.publish('Place/register', [this]);
+        },this)
+      });
     },
 
     // /**
     //  * Application subscriptions.
     //  */
     _subscriptions: [{
+      'Place/go': function(place) {
+        var tab = place.params.tab || this.status.get('tab');
+        var button = '#'+tab+'-button';
+        this.status.set('tab', tab);
+        this.view.openTab(button, null);
+      }
+    },{
       'Tab/open': function(id, backbutton) {
         this.view.openTab(id, backbutton);
       }
@@ -32,14 +52,29 @@ define([
     }],
 
     onTabOpen: function(id){
+      this.status.set('tab', id);
       mps.publish('Tab/opened', [id]);
+      mps.publish('Place/update', [{go: false}]);
     },
 
     onTabMobileClose : function () {
       mps.publish('Tab/closed');
-    }
+    },
+
+    /**
+     * Used by PlaceService to get the current tab value.
+     *
+     * @return {Object} tab
+     */
+    getPlaceParams: function() {
+      var p = {};
+      p.tab = this.status.get('tab');
+      return p;
+    },
+
+
 
   });
 
-  return MapControlsPresenter;
+  return TabsPresenter;
 });
