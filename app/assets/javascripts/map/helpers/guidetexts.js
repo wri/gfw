e3396cb521ed7a13ced9c02e82024e77de4d40a3
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2016-01-14 21:52:20 +0100

    Typo change

--- app/assets/javascripts/map/helpers/guidetexts.js
+++ app/assets/javascripts/map/helpers/guidetexts.js
@@ -1,20 +1,19 @@
 define([], function() {
 
   var guideTexts = {
-    intro:            '<p>Welcome to the Global Forest Watch Interactive Map, where you can explore forest-related data sets, perform analyses, and subscribe to alerts for areas you are interested in. This walk-through guide will introduce you to some of the map’s main features.</p>' +
+    intro:            '<p>Welcome to the Global Forest Watch Interactive Map, where you can <strong>explore forest-related data sets, perform analyses</strong>, and <strong>subscribe to alerts</strong> for areas you are interested in. This walk-through guide will introduce you to some of the map’s main features.</p>' +
                       '<p>Already familiar with GFW? <a href="/map" class="skip-tour">Skip the walk through</a>.</p>',
-    layersmenu:       '<p>By default, the map displays tree cover gain and loss data. Use the tabs at the top of the map to turn on other global data layers.</p>'+
-                      '<p>To learn more about a data set, click on the green information icon in the drop down menu.</p>',
-    legend:           '<p>The data legend on the left side of the map shows which layers are activated. Use this legend to get more information about data, adjust the canopy density for tree cover layers, or turn data layers off.</p>',
-    mapcontrols:      '<p>Use this navigation panel to zoom in and out of the map, share a link of your map view, hide panels, search for a location, or refresh the page.</p>',
-    countrytab:       '<p>Use the Country Data feature in the panel on the right side of the map to view country-specific datasets. Select a country and then turn on local datasets you’re interested in.</p>',
-    analysistab:      '<p>Click the Analysis icon to perform an analysis for a drawn shape, a country or subnational region, or for a polygon in another data layer.</p>',
+    layersmenu_1:     '<p>By default, the map displays tree cover gain and loss data. <strong>Use the tabs at the top of the map to turn on other global data layers</strong>.</p>',
+    layersmenu_2:     '<p>To learn more about a data set, click on the <strong>green information icon</strong> in the drop down menu.</p>',
+    legend:           '<p>The data legend on the left side of the map shows which layers are activated. Use this legend to <strong>learn more about a data set, adjust the canopy density</strong> for tree cover layers, or <strong>turn data layers off.</strong></p>',
+    mapcontrols:      '<p>Use this navigation panel to <strong>zoom in and out of the map, share a link</strong> of your map view, <strong>hide panels, search for a location</strong>, or <strong>refresh the page</strong>.</p>',
+    countrytab:       '<p>Use the <strong>Country Data feature</strong> in the panel on the right side of the map to <strong>view country-specific datasets</strong>. Select a country and then turn on local datasets you’re interested in.</p>',
+    analysistab:      '<p>Click the <strong>Analysis icon</strong> to <strong>perform an analysis</strong> for a drawn shape, a country or subnational region, or for a polygon in another data layer.</p>',
     subscriptiontab : '<p>The Subscription icon allows you to sign up to receive an email when there is a new forest change alert or user story for an area of interest.</p>',
-    basemaptab:       '<p>Click on the Basemap icon to change the default map layer. You can choose from terrain, satellite imagery, roads, and many more.</p>',
-    timeline:         '<p>Use the timeline on the bottom of the map to animate data over time by pressing the play button. You can also select a time interval to view or analyze by dragging the ends of the timeline to a specific time period.</p>',
-    howto:            '<p>For more information about how to use any of the features on the Interactive Map, visit our <a href="/howto">How-To</a> page.</p>'
-
-
+    hrestab:          '<p>The <strong>UrtheCast feature</strong> gives you access to <strong>high resolution satellite imagery</strong>. Zoom in to an area on the map and select a date range to view the latest imagery available. You can also use the advanced settings to filter by maximum cloud coverage or apply a renderer.</p>',
+    basemaptab:       '<p>Click on the <strong>Basemap icon</strong> to <strong>change the default map layer</strong>. You can choose from terrain, satellite imagery, roads, and many more.</p>',
+    timeline:         '<p>Use the <strong>timeline</strong> on the bottom of the map to <strong>animate forest change data sets over time</strong> by pressing the play button. You can also <strong>select a time interval to view or analyze</strong> by dragging the ends of the timeline to a specific time period.</p>',
+    howto:            '<p>For more information about how to use any of the features on the Interactive Map, <strong>visit our <a href="/howto">How-To</a> page</strong>.</p>'
   }
   return guideTexts;
 
