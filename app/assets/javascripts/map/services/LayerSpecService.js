e8c9b7b01bd49221e8a0ba82aaa07df2d000d053
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2016-02-11 18:33:24 +0100

    Allow the possibility of turn on all the forest change layers with the tree cover extent

--- app/assets/javascripts/map/services/LayerSpecService.js
+++ app/assets/javascripts/map/services/LayerSpecService.js
@@ -19,7 +19,18 @@ define([
       forbidCombined: {
         forest_clearing: {
           except: [
-            ['loss', 'forestgain', 'forest2000']
+            ['loss', 'forestgain', 'forest2000'],
+            ['forma', 'forest2000'],
+            ['loss', 'forest2000'],
+            ['forestgain', 'forest2000'],
+            ['forma', 'forest2000'],
+            ['imazon', 'forest2000'],
+            ['fires', 'forest2000'],
+            ['modis', 'forest2000'],
+            ['terrailoss', 'forest2000'],
+            ['prodes', 'forest2000'],
+            ['guyra', 'forest2000'],
+            ['forest2000', 'forest2000'],
           ]
         },
         geographic_coverage: {}
