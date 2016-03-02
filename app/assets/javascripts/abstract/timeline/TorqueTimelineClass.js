f937bf5cabd4d57b023cd2b0d9777f0d57c4665d
Adam Mulligan <adammulligan@gmail.com>
2016-03-02 14:30:39 +0000

    Show end of date range when changing date

--- app/assets/javascripts/abstract/timeline/TorqueTimelineClass.js
+++ app/assets/javascripts/abstract/timeline/TorqueTimelineClass.js
@@ -108,6 +108,8 @@ define([
       var onChange = function(dateRange) {
         this.slider.reScale(dateRange);
         this.currentDate = dateRange;
+        this.status.set('currentDate', moment.utc(dateRange[1]));
+        this.renderDate();
       };
 
       this.datePicker = new TorqueTimelineDatePicker({
