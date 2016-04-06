551c6f6c1c92add0e84a9f04d5f6f444df96f276
Adam Mulligan <adammulligan@gmail.com>
2016-04-06 12:33:10 +0100

    Rename feedback controller and clean up static controller

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,4 +1,4 @@
-class YourMailer < ActionMailer::Base
+class FeedbackMailer < ActionMailer::Base
   def feedback(feedback,signup,email,hostname)
     @feedback = feedback.present? ? feedback : nil
     @email = email.present? ? email : nil
@@ -20,7 +20,7 @@ class YourMailer < ActionMailer::Base
         when 'fires.globalforestwatch.org' then ['SMinnemeyer@wri.org', 'gfw@wri.org']
         when 'commodities.globalforestwatch.org' then ['slake@wri.org', 'gfw@wri.org']
         when 'climate.globalforestwatch.org' then ['cciciarelli@wri.org', 'gfw@wri.org']
-        else ['abarrett@wri.org', 'gfw@wri.org']
+        else ['adam.mulligan@vizzuality.com']
       end
     end
 end
