6e585e18a2bf6929df75d5abeb67c84504b1bc27
Adam Mulligan <adammulligan@gmail.com>
2016-04-06 10:50:01 +0100

    Fixes issue where certain combinations of media would break the story show page

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -24,6 +24,16 @@ module StoriesHelper
     @page == @total_pages
   end
 
+  def has_media? story
+    return story.media.length > 0
+  end
+
+  def media_image media
+    if media['url'].present?
+      return "#{ENV['AWS_HOST']}/#{media['url']}"
+    end
+  end
+
   def story_image_or_map(media, coords = nil)
     return "#{ENV['AWS_HOST']}/#{media[1]['preview_url']}" if media[1].present?
     unless coords.nil?
