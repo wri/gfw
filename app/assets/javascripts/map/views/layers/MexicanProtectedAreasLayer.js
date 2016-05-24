55d0a95b47bc0818f5de1fb438fc04a703e210c4
Adrián <aloha@adrianperez.info>
2016-05-24 19:05:59 +0200

    fix typo

--- app/assets/javascripts/map/views/layers/MexicanProtectedAreasLayer.js
+++ app/assets/javascripts/map/views/layers/MexicanProtectedAreasLayer.js
@@ -12,7 +12,7 @@ define([
   var MexicanProtectedAreasLayer = CartoDBLayerClass.extend({
 
     options: {
-      sql: 'SELECT cartodb_id, the_geom_webmercator, the_geom, nombre as name, cartodb_id, cat_magenjo as category, superficie as surface, prim_dec as date_create, tipo as type, vigencia as validity, \'{tableName}\' AS tablename, {analysis} AS analysis, \'{tableName}\' as layer FROM {tableName}',
+      sql: 'SELECT cartodb_id, the_geom_webmercator, the_geom, nombre as name, cartodb_id, cat_manejo as category, superficie as surface, prim_dec as date_create, tipo as type, vigencia as validity, \'{tableName}\' AS tablename, {analysis} AS analysis, \'{tableName}\' as layer FROM {tableName}',
       infowindow: true,
       interactivity: 'cartodb_id, tablename, name, category, surface, date_create, type, validity, analysis',
       analysis: true,
