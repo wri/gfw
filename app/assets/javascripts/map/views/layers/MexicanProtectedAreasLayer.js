180ba551e058b8f8c148f2b9878705b2371c7b55
Adrián <aloha@adrianperez.info>
2016-05-25 12:07:49 +0200

    add cartocss to query and change type for TIPO

--- app/assets/javascripts/map/views/layers/MexicanProtectedAreasLayer.js
+++ app/assets/javascripts/map/views/layers/MexicanProtectedAreasLayer.js
@@ -5,17 +5,19 @@
  */
 define([
   'abstract/layer/CartoDBLayerClass',
-], function(CartoDBLayerClass) {
+  'text!map/cartocss/mexicanPA.cartocss'
+], function(CartoDBLayerClass,mexicanPA) {
 
   'use strict';
 
   var MexicanProtectedAreasLayer = CartoDBLayerClass.extend({
 
     options: {
-      sql: 'SELECT cartodb_id, the_geom_webmercator, the_geom, nombre as name, cartodb_id, cat_manejo as category, superficie as surface, prim_dec as date_create, tipo as type, vigencia as validity, \'{tableName}\' AS tablename, {analysis} AS analysis, \'{tableName}\' as layer FROM {tableName}',
+      sql: 'SELECT the_geom_webmercator, the_geom, nombre as name, cartodb_id, cat_manejo as category, superficie as original_size_h, prim_dec as date_create, tipo, vigencia as validity, \'{tableName}\' AS tablename, {analysis} AS analysis, \'{tableName}\' as layer FROM {tableName}',
       infowindow: true,
-      interactivity: 'cartodb_id, tablename, name, category, surface, date_create, type, validity, analysis',
+      interactivity: 'cartodb_id, tablename, name, category, original_size_h, date_create, tipo, validity, analysis',
       analysis: true,
+      cartocss: mexicanPA
     },
 
   });
