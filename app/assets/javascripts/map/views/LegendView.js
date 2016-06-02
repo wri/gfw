455f65742ec6cc10a82c2052deba534c3e8c3619
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2016-06-02 19:27:17 +0200

    Legend does not render country info until it has a layer selected

--- app/assets/javascripts/map/views/LegendView.js
+++ app/assets/javascripts/map/views/LegendView.js
@@ -222,10 +222,12 @@ define([
       categoriesGlobal = this.statusCategories(_.groupBy(layersGlobal, function(layer){ return layer.category_slug }));
       categoriesIso = this.statusCategories(_.groupBy(layersIso, function(layer){ return layer.category_slug }));
 
+      console.log(categoriesIso);
+
       // Render
       this.render(this.template({
-        categories: (jQuery.isEmptyObject(categoriesGlobal)) ? false : categoriesGlobal,
-        categoriesIso: categoriesIso,
+        categories: (_.isEmpty(categoriesGlobal)) ? false : categoriesGlobal,
+        categoriesIso: (_.isEmpty(categoriesIso)) ? false : categoriesIso,
         layersLength: layers.length,
         country: (!!iso) ? _.findWhere(this.countries.toJSON(), { iso: iso.country }) : null,
       }));
