fcffb87c5a2073b22330b995dbc4f8c2849f13da
Adam Mulligan <adammulligan@gmail.com>
2016-06-08 16:45:05 +0100

    Proxy howto URL to new Github Pages versoin

---
+++ app/assets/javascripts/stories/models/StoryModel.js
@@ -0,0 +1,35 @@
+define([
+ 'backbone', 'moment', 'underscore'
+], function(Backbone, moment, _) {
+
+  'use strict';
+
+  var Story = Backbone.Model.extend({
+    urlRoot: window.gfw.config.GFW_API_HOST + '/story/',
+
+    parse: function(response) {
+      var attributes;
+      if (response.data) {
+        attributes = response.data.attributes;
+      } else {
+        attributes = response.attributes;
+      }
+
+      attributes.id = response.id;
+
+      return attributes;
+    },
+
+    formattedDate: function() {
+      var date = moment(this.get('date'));
+      return date.format('MMMM DD, YYYY');
+    },
+
+    hasLocation: function() {
+      return _.isNumber(this.get('lat')) && _.isNumber(this.get('lng'));
+    }
+  });
+
+  return Story;
+
+});
