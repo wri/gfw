5e74e6c811f1d5d67708a1e6401b31930e00b663
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2016-06-13 12:46:55 +0200

    Structure and styles

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -17,16 +17,22 @@ class StoriesController < ApplicationController
       respond_with @stories
       return
     else
-      stories_per_page = 5
-
-      @page        = (params[:page] || 1).to_i
-      @stories_per_page = stories_per_page
-      @total_stories = Api::Story.visible.count
-      @total_stories_paginated     = Api::Story.find_by_page(@page, stories_per_page)
-      
+      @total_stories_paginated = Api::Story.find_by_page(1, 4)
       @title = 'Crowdsourced Stories'
       return
     end
+  end
+
+  def crowdsourcedstories
+    stories_per_page = 5
+
+    @page        = (params[:page] || 1).to_i
+    @stories_per_page = stories_per_page
+    @total_stories = Api::Story.visible.count
+    @total_stories_paginated     = Api::Story.find_by_page(@page, stories_per_page)
+    
+    @title = 'Crowdsourced Stories'
+    return
   end
 
   def show
