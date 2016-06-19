3c1c99d4dd38ec46cd022f979fdebf8cdba36f12
Adam Mulligan <adammulligan@gmail.com>
2016-06-19 08:22:39 +0100

    Fix tests

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -7,10 +7,6 @@ describe ApplicationController do
     end
   end
 
-  before do
-    ENV['TERMS_COOKIE'] = "terms_cookie"
-  end
-
   describe 'GET :index' do
     let(:user_agent) {
       "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
@@ -22,66 +18,6 @@ describe ApplicationController do
     before :each do
       @request.user_agent = user_agent
       ActionDispatch::Request.any_instance.stub(:remote_ip).and_return(ip)
-    end
-
-    context "with a Google Snippet bot" do
-      let(:user_agent) {
-        "Mozilla/5.0 (Windows NT 6.1; rv:6.0) Gecko/20110814 Firefox/6.0 Google (+https://developers.google.com/+/web/snippet/)"
-      }
-
-      it "does not redirect to /notsupportedbrowser" do
-        is_expected.to_not redirect_to("/notsupportedbrowser")
-      end
-
-      it "does not redirect to accept_terms_path" do
-        is_expected.to_not redirect_to(accept_terms_path)
-      end
-    end
-
-    context "with a Facebook Snippet bot" do
-      let(:user_agent) {
-        "facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)"
-      }
-
-      it "does not redirect to /notsupportedbrowser" do
-        is_expected.to_not redirect_to("/notsupportedbrowser")
-      end
-
-      it "does not redirect to accept_terms_path" do
-        is_expected.to_not redirect_to(accept_terms_path)
-      end
-    end
-
-    context "with a whitelisted IP" do
-      let(:ip) { "80.74.134.135" }
-
-      it "does not redirect to accept_terms_path" do
-        is_expected.to_not redirect_to(accept_terms_path)
-      end
-    end
-
-    context "with a non whitelisted IP" do
-      let(:ip) { "1.3.3.7" }
-
-      before :each do
-        controller.request.stub(:host).and_return("www.globalforestwatch.org")
-      end
-
-      it "redirects to accept_terms_path" do
-        is_expected.to redirect_to(accept_terms_path)
-      end
-    end
-
-    context "with a bot" do
-      let(:user_agent) { "bot" }
-
-      it "does not redirect to /notsupportedbrowser" do
-        is_expected.to_not redirect_to("/notsupportedbrowser")
-      end
-
-      it "does not redirect to accept_terms_path" do
-        is_expected.to_not redirect_to(accept_terms_path)
-      end
     end
 
     context "with a supported browser" do
@@ -96,10 +32,6 @@ describe ApplicationController do
 
       it "does not redirect to /notsupportedbrowser" do
         is_expected.to_not redirect_to("/notsupportedbrowser")
-      end
-
-      it "redirects to accept_terms_path" do
-        is_expected.to redirect_to(accept_terms_path)
       end
     end
 
