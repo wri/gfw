b395508f0c2195160de8426d680e70d227799ce1
Adam Mulligan <adammulligan@gmail.com>
2016-06-19 21:39:27 +0100

    Truncate story details in lists

--- app/assets/javascripts/stories.js
+++ app/assets/javascripts/stories.js
@@ -1,14 +1,17 @@
 require([
   'backbone',
+  'stories/handlebarsHelpers',
   'views/HeaderView', 'views/FooterView',
   'stories/routers/StoriesRouter'
 ], function(
   Backbone,
+  handlebarsHelpers,
   HeaderView, FooterView,
   StoriesRouter) {
 
   'use strict';
 
+  handlebarsHelpers.register();
   new StoriesRouter();
   Backbone.history.start({pushState: true});
 
