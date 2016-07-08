3961f6697dd74865ab5785be30afaa7a75adcdf3
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2016-07-08 10:44:15 +0200

    Basemap: new osm image

--- app/assets/javascripts/map/views/maptypes/openStreetMaptype.js
+++ app/assets/javascripts/map/views/maptypes/openStreetMaptype.js
@@ -14,7 +14,7 @@ define([], function () {
       tileSize: new google.maps.Size(256, 256),
       getTileUrl: function(ll, z) {
         var x = Math.abs(ll.x % (1 << z)); // jshint ignore:line
-        return 'https://{s}.tile.openstreetmap.org/{0}/{1}/{2}.png'.format(z, x, ll.y);
+        return 'https://a.tile.openstreetmap.org/{0}/{1}/{2}.png'.format(z, x, ll.y);
       },
     };
 
