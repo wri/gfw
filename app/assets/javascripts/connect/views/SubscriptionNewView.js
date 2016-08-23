29aebb581ab3f22f46c33a9b81211d1ea0977432
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2016-08-23 12:31:46 +0200

    Subscriptions: AOI change

--- app/assets/javascripts/connect/views/SubscriptionNewView.js
+++ app/assets/javascripts/connect/views/SubscriptionNewView.js
@@ -11,20 +11,52 @@ define([
 
   var SubscriptionNewView = Backbone.View.extend({
     
+    status: new (Backbone.Model.extend({
+      aoi: null
+    })),
+
     template: Handlebars.compile(tpl),
 
+    events: {
+      'change #aoi': 'onChangeAOI'
+    },
+
     initialize: function() {
+      this.listeners();
       this.render();
     },
 
+    listeners: function() {
+      this.status.on('change:aoi', this.changeAOI.bind(this));
+    },
+
     render: function() {
-      this.$el.html(this.template({}));      
+      this.$el.html(this.template({}));
+      this.cache();
     },
 
-    show: function() {
-      console.log('show');
+    cache: function() {
+      this.$form = this.$el.find('#new-subscription');
     },
 
+    /**
+     * CHANGE EVENTS
+     * - changeAOI
+     */
+    changeAOI: function() {
+      var aoi = this.status.get('aoi');
+      console.log(aoi);
+    },
+
+
+    /**
+     * UI EVENTS
+     * - onChangeAOI
+     */
+    onChangeAOI: function(e) {
+      e && e.preventDefault();
+      this.status.set('aoi', $(e.currentTarget).val());
+    }
   });
 
   return SubscriptionNewView;
