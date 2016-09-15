edb1f2b9c8eb4a8483ec865cbe2f50443cb1f61a
Simao Belchior <simaobelchior@gmail.com>
2016-09-15 11:32:04 +0100

    Removes unnecessary nesting. Removes unnecessary comma

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,6 @@
 Aws.config.update({
   region: 'us-west-2',
-  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
+  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
 })
 
 S3_DATA_BUCKET_NAME = Aws::S3::Resource.new.bucket(ENV['S3_DATA_BUCKET_NAME'])
