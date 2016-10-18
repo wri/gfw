8f1da2c22116d926c39f65219f24f6668ea25075
Alicia <alicia.arenzana@gmail.com>
2016-10-18 13:59:04 +0200

    added new changes to ptw layer

--- app/assets/javascripts/map/views/layers/Places2WatchLayer.js
+++ app/assets/javascripts/map/views/layers/Places2WatchLayer.js
@@ -13,9 +13,9 @@ define([
   var Places2WatchLayer = CartoDBLayerClass.extend({
 
     options: {
-      sql: 'with paths as (select (ST_Dump( st_makevalid(st_memunion(the_geom_webmercator)))).geom as the_geom_webmercator, (ST_Dump( st_makevalid(st_memunion(the_geom_webmercator)))).path as path from borneo_peru_roc_final_alerts_copy where six_score>25 order by path asc) select  \'{tableName}\' as tablename,  {analysis} AS analysis, st_convexhull(paths.the_geom_webmercator) as the_geom_webmercator,paths.path, sum (t.count_6months)as total_alerts, round(avg(six_score)::numeric,2) as avg_score, sum (t.count_6months)*avg(six_score) as final_score, min(t.cartodb_id) as cartodb_id from paths, borneo_peru_roc_final_alerts_copy t where st_contains(paths.the_geom_webmercator, t.the_geom_webmercator) group by paths.the_geom_webmercator,paths.path',
+      sql: 'with r as (SELECT cartodb_id, the_geom_webmercator, st_asgeojson(the_geom_webmercator), grid_id FROM ptw_grid_only_scoreover0 order by cartodb_id asc ) select r.*, emissions_sum,glad_count, iso, score,\'{tableName}\' as tablename, \'{tableName}\' AS layer,{analysis} AS analysis from r inner join {tableName} s on r.grid_id=s.grid_id',
       infowindow: true,
-      interactivity: 'cartodb_id, tablename, total_alerts, avg_score, analysis',
+      interactivity: 'cartodb_id, tablename, emissions_sum, score, glad_count, analysis',
       analysis: true,
       cartocss: Places2WatchCartocss
     }
