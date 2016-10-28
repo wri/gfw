59ad3480ac38f42765bc3c86554e3180c124e3d5
Gerardo <gerardo.sicart@gmail.com>
2016-10-28 08:16:54 +0200

    Wip: Subscriptions - Coverage service integration

--- app/assets/javascripts/map/services/CoverageService.js
+++ app/assets/javascripts/map/services/CoverageService.js
@@ -14,7 +14,7 @@ define([
   var APIURLS = {
     'country'      : '/coverage/intersect/admin/{country}',
     'region'       : '/coverage/intersect/admin/{country}/{region}',
-    'wdpaid'       : '/coverage/intersect/wdpa/{wdpaid}>',
+    'wdpaid'       : '/coverage/intersect/wdpa/{wdpaid}',
     'use'          : '/coverage/intersect/use/{use}/{useid}',
     'use-geostore' : '/coverage/intersect?geostore={geostore}',
   };
@@ -24,40 +24,44 @@ define([
     get: function(params) {
       return new Promise(function(resolve, reject) {
         this.coverage = this.buildEndpointFromParams(params);
-        var url = this.getUrl();
 
-        ds.define(GET_REQUEST_ID, {
-          cache: {type: 'persist', duration: 1, unit: 'days'},
-          url: APIURL + url,
-          type: 'GET',
-          dataType: 'json',
-          contentType: 'application/json; charset=utf-8',
-          decoder: function ( data, status, xhr, success, error ) {
-            if ( status === "success" ) {
-              success( data, xhr );
-            } else if ( status === "fail" || status === "error" ) {
-              error( JSON.parse(xhr.responseText) );
-            } else if ( status === "abort") {
+        if (this.coverage.type) {
+          var url = this.getUrl();
 
-            } else {
-              error( JSON.parse(xhr.responseText) );
+          ds.define(GET_REQUEST_ID, {
+            cache: {type: 'persist', duration: 1, unit: 'days'},
+            url: APIURL + url,
+            type: 'GET',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            decoder: function ( data, status, xhr, success, error ) {
+              if ( status === "success" ) {
+                success( data, xhr );
+              } else if ( status === "fail" || status === "error" ) {
+                error( JSON.parse(xhr.responseText) );
+              } else if ( status === "abort") {
+
+              } else {
+                error( JSON.parse(xhr.responseText) );
+              }
             }
-          }
-        });
+          });
 
-        var requestConfig = {
-          resourceId: GET_REQUEST_ID,
-          success: function(data, status) {
-            resolve(data,status);
-          },
-          error: function(errors) {
-            reject(errors);
-          }
-        };
+          var requestConfig = {
+            resourceId: GET_REQUEST_ID,
+            success: function(data, status) {
+              resolve(data.data.attributes.layers, status);
+            },
+            error: function(errors) {
+              reject(errors);
+            }
+          };
 
-        this.abortRequest();
-        this.currentRequest = ds.request(requestConfig);
-
+          this.abortRequest();
+          this.currentRequest = ds.request(requestConfig);
+        } else {
+          resolve();
+        }
       }.bind(this));
     },
 
@@ -66,24 +70,29 @@ define([
     },
 
     buildEndpointFromParams: function(params) {
-      console.log(params);
-      // // To allow layerOptions
-      // // I really think that this should be an object instead of an array, or an array of objects
-      // var layerOptions = {};
-      // _.each(status.layerOptions, function(val) {
-      //   layerOptions[val] = true;
-      // });
-      //
-      // return _.extend({}, status, layerOptions, {
-      //   country: status.iso.country,
-      //   region: status.iso.region,
-      //   thresh: status.threshold,
-      //   period: status.begin + ',' + status.end,
-      //
-      //   // If a userGeostore exists we need to set geostore and type manually
-      //   geostore: (status.useGeostore) ? status.useGeostore : status.geostore,
-      //   type: (status.useGeostore) ? 'use-geostore' : status.type
-      // }, layerOptions);
+      var status = _.extend({}, params);
+
+      if (status.country && !status.region) {
+        status.type = 'country';
+      }
+
+      if (status.country && status.region) {
+        status.type = 'region';
+      }
+
+      if (status.wdpaid) {
+        status.type = 'wdpaid';
+      }
+
+      if (status.use) {
+        status.type = 'use';
+      }
+
+      if (status.geostore) {
+        status.type = 'use-geostore';
+      }
+
+      return status;
     },
 
     /**
