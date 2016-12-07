caeaf33bb776c86b7acce9f565279376c3e74cef
Alicia <alicia.arenzana@gmail.com>
2016-12-07 10:54:17 +0100

    added ammends to liberia layers

--- app/assets/javascripts/map/views/LegendView.js
+++ app/assets/javascripts/map/views/LegendView.js
@@ -60,13 +60,17 @@ define([
   'text!map/templates/legend/mex_forest_conserv.handlebars',
   'text!map/templates/legend/mex_forest_prod.handlebars',
   'text!map/templates/legend/mex_forest_rest.handlebars',
-  'text!map/templates/legend/lbr_mining.handlebars'
+  'text!map/templates/legend/lbr_mining.handlebars',
+  'text!map/templates/legend/lbr_forest.handlebars',
+  'text!map/templates/legend/lbr_community.handlebars'
 ], function(_, Handlebars, Presenter, tpl, lossTpl, imazonTpl, firesTpl,
     forest2000Tpl, pantropicalTpl, idnPrimaryTpl, intact2013Tpl, grumpTpl, storiesTpl, terra_iTpl, concesionesTpl,
     concesionesTypeTpl, hondurasForestTPL,colombiaForestChangeTPL, tigersTPL, dam_hotspotsTPL, us_land_coverTPL,
     global_land_coverTPL, formaTPL,bra_biomesTPL, gfwPlantationByTypeTpl, gfwPlantationBySpeciesTpl, oil_palmTpl,
     gtm_forest_changeTpl,gtm_forest_coverTpl,gtm_forest_densityTpl,khm_eco_land_concTpl,usa_forest_ownershipTpl,guyra_deforestationTpl,logging_roadsTpl,
-    rus_hrvTpl, raisg_land_rightsTpl, mysPATpl, idn_peatTpl, mys_peatTpl,raisg_miningTpl, per_miningTpl, gladTpl, urtheTpl,mex_forest_catTpl,mex_forest_subcatTpl, paTpl, places2watchTPL, mex_landrightsTpl, mexPATpl, perPATpl,mex_land_coverTpl,mex_forest_conservTPL,mex_forest_prodTPL,mex_forest_restTPL, lbr_miningTPL) {
+    rus_hrvTpl, raisg_land_rightsTpl, mysPATpl, idn_peatTpl, mys_peatTpl,raisg_miningTpl, per_miningTpl, gladTpl, urtheTpl,mex_forest_catTpl,mex_forest_subcatTpl,
+    paTpl, places2watchTPL, mex_landrightsTpl, mexPATpl, perPATpl,mex_land_coverTpl,mex_forest_conservTPL,mex_forest_prodTPL,mex_forest_restTPL, lbr_miningTPL,
+    lbr_forestTpl,lbr_communityTpl) {
 
   'use strict';
 
@@ -162,6 +166,7 @@ define([
       lbr_development_exploration_license:Handlebars.compile(lbr_miningTPL),
       lbr_mineral_development_agreement:Handlebars.compile(lbr_miningTPL),
       lbr_mineral_exploration_license:Handlebars.compile(lbr_miningTPL),
+      lbr_resource_rights:Handlebars.compile(lbr_communityTpl)
     },
 
     events: {
