ad23242845f9c52b1eaddab7e4e92b0589eb4817
Alicia <alicia.arenzana@gmail.com>
2016-12-13 20:36:56 +0100

    fixed column labeling change in liberia datasets oil palm & logging

--- app/assets/javascripts/map/views/layers/LbrLoggingLayer.js
+++ app/assets/javascripts/map/views/layers/LbrLoggingLayer.js
@@ -13,7 +13,7 @@ define([
   var LbrLoggingLayer = CartoDBLayerClass.extend({
 
     options: {
-      sql: 'SELECT \'lbr_logging\' as tablename, cartodb_id, the_geom_webmercator, round(area_ha::float) as area_ha, name, county, status, last_updat, \'{tableName}\' AS layer, {analysis} AS analysis FROM {tableName}' ,
+      sql: 'SELECT \'{tableName}\' as tablename, cartodb_id, the_geom_webmercator, round(area_ha::float) as area_ha, name, county, status, last_updat, \'{tableName}\' AS layer, {analysis} AS analysis FROM {tableName}' ,
       infowindow: true,
       interactivity: 'cartodb_id, tablename, name, county, status, last_updat, area_ha, analysis',
       cartocss: lbrForestCartocss,
