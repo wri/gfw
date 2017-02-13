9a447489e8fa44e9fd1e4056690bac9d9a6d128c
pjosh <jorgegar@gmail.com>
2017-02-13 17:11:17 +0100

    Testimonials switcher in the user examples block

--- app/assets/javascripts/landing/views/UseExamplesView.js
+++ app/assets/javascripts/landing/views/UseExamplesView.js
@@ -14,20 +14,52 @@ define([
 
     el: '.c-home-use-examples',
 
-    initialize: function() {
-      this._initSliders();
+    events: {
+      'click .c-home-use-examples__users .js_slide' : '_onClickUser'
     },
 
-    _initSliders: function(){
+    options: {
+      selectedClass: '-selected',
+    },
+
+    loadedFilters: [0],
+
+    initialize: function() {
+      this._initVisibleSliders();
+    },
+
+    _initVisibleSliders: function () {
       new SliderView({
         el: this.$el.find('.c-home-use-examples__users.js_slider')
       });
-      _.each($('.c-home-use-examples__testimonials.js_slider'), function(element) {
-        new SliderView({
-          el: $(element)
-        });
+      new SliderView({
+        el: this.$el.find('.c-home-use-examples__testimonials.js_slider.' + this.options.selectedClass)
       });
     },
+
+    _initSlider: function (index) {
+      new SliderView({
+        el: this.$el.find('.c-home-use-examples__testimonials.js_slider[data-index="' + index + '"]')
+      });
+    },
+
+    _onClickUser: function (e) {
+      var index = $(e.currentTarget).data('index');
+
+      this._switchTestimonials(index);
+      if(this.loadedFilters.indexOf(index) == -1) {
+        this._initSlider(index);
+        this.loadedFilters.push(index);
+      }
+    },
+
+    _switchTestimonials: function (index) {
+      $('.c-home-use-examples__avatar.' + this.options.selectedClass).removeClass(this.options.selectedClass);
+      $('.c-home-use-examples__testimonials.' + this.options.selectedClass).removeClass(this.options.selectedClass);
+
+      $('.c-home-use-examples__users [data-index="' + index + '"] .c-home-use-examples__avatar').addClass(this.options.selectedClass);
+      $('.c-home-use-examples__testimonials[data-index="' + index + '"]').addClass(this.options.selectedClass);
+    }
 
   });
   return UseExamplesView;
