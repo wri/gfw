381c7370f1346086b09b505123da2917d478da19
Simao Belchior <simaobelchior@gmail.com>
2017-03-09 19:45:15 +0000

    don't debug assets in production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,7 @@ Gfw::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
 
   config.action_dispatch.default_headers = {
     'X-Frame-Options' => 'ALLOWALL'
