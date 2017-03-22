1725f6cfd94f64f08e539df90599469792dfdc0c
pjosh <jorgegar@gmail.com>
2017-03-22 13:12:04 +0100

    Applications slider buttons

--- app/assets/javascripts/landing/views/AppsView.js
+++ app/assets/javascripts/landing/views/AppsView.js
@@ -32,9 +32,6 @@ define([
     },
 
     _cache: function () {
-      this.$prevButton = this.$el.find('.js_slide_prev');
-      this.$nextButton = this.$el.find('.js_slide_next');
-
       enquire.register("screen and (min-width: 768px)", {
         match: function() {
           this.options.sliderButtonDefaultMargin = 18;
@@ -47,8 +44,7 @@ define([
         el: this.$el.find('.js_slider'),
         sliderOptions: {
           infinite: 1
-        },
-        afterSlideCallback: this._afterSlide.bind(this)
+        }
       });
     },
 
@@ -59,56 +55,6 @@ define([
     _onClickNextSlide: function() {
       this.slider.$slider.next();
     },
-
-    _afterSlide: function () {
-      var currentSlide = this.slider.$el.find('.js_slide.active');
-      var prevSlide = currentSlide.prev();
-      var nextSlide = currentSlide.next();
-
-      this._updateSliderButtons(prevSlide.data('section'), nextSlide.data('section'), this.slider.lastSlideDirection);
-    },
-
-    _updateSliderButtons: function (prevKey, nextKey, direction) {
-      if(direction === 'next') {
-        this.$prevButton.append('<li>' + this._getSliderButtonIconHTML(prevKey) + '</li>');
-        this.$nextButton.append('<li>' + this._getSliderButtonIconHTML(nextKey) + '</li>');
-
-        this.$prevButton.children().first().animate({
-          marginLeft: this.options.sliderButtonAnimateMargin
-        }, this.options.sliderButtonAnimateTime, function() {
-          this.remove();
-        });
-
-        this.$nextButton.children().first().animate({
-          marginLeft: this.options.sliderButtonAnimateMargin
-        }, this.options.sliderButtonAnimateTime, function() {
-          this.remove();
-        });
-      } else {
-        this.$prevButton
-          .prepend('<li>' + this._getSliderButtonIconHTML(prevKey) + '</li>')
-          .children().first().css("margin-left", this.options.sliderButtonAnimateMargin);
-        this.$nextButton
-          .prepend('<li>' + this._getSliderButtonIconHTML(nextKey) + '</li>')
-          .children().first().css("margin-left", this.options.sliderButtonAnimateMargin);
-
-        this.$prevButton.children().first().animate({
-          marginLeft: this.options.sliderButtonDefaultMargin
-        }, this.options.sliderButtonAnimateTime, function() {
-          this.$prevButton.children().last().remove();
-        }.bind(this));
-
-        this.$nextButton.children().first().animate({
-          marginLeft: this.options.sliderButtonDefaultMargin
-        }, this.options.sliderButtonAnimateTime, function() {
-          this.$nextButton.children().last().remove();
-        }.bind(this));
-      }
-    },
-
-    _getSliderButtonIconHTML: function (key) {
-      return '<svg class="icon"><use xlink:href="#icon-app-' + key + '"></use></svg>';
-    }
 
   });
   return AppsView;
