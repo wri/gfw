d5c3705f832d755d83c47179b14a8c778739b87e
pjosh <jorgegar@gmail.com>
2017-03-23 11:43:15 +0100

    News services from cartoDB

--- app/assets/javascripts/landing/views/NewsView.js
+++ app/assets/javascripts/landing/views/NewsView.js
@@ -5,26 +5,71 @@ define([
   'jquery',
   'backbone',
   'underscore',
-  'landing/views/SliderView'
-], function($, Backbone, _, SliderView) {
+  'handlebars',
+  'services/NewsService',
+  'landing/views/SliderView',
+  'text!landing/templates/c-home-news-item.handlebars',
+  'text!landing/templates/c-home-news-slider-dots.handlebars'
+], function($, Backbone, _, Handlebars, newsService, SliderView, newsItemTpl, newsSliderDotsTpl) {
 
   'use strict';
+
+  Handlebars.registerHelper('for', function(from, to, incr, block) {
+    var accum = '';
+    for(var i = from; i < to; i += incr)
+      accum += block.fn(i);
+    return accum;
+  });
+  Handlebars.registerHelper('ifCond', function(v1, v2, options) {
+    if(v1 === v2) {
+      return options.fn(this);
+    }
+    return options.inverse(this);
+  });
 
   var NewsView = Backbone.View.extend({
 
     el: '.c-home-news',
 
-    initialize: function() {
-      this._initSlider();
+    newsItemTpl: Handlebars.compile(newsItemTpl),
+    newsSliderDotsTpl: Handlebars.compile(newsSliderDotsTpl),
+
+    initialize: function () {
+      this._cache();
+      this._loadNews();
     },
 
-    _initSlider: function(){
+    _cache: function () {
+      this.$slider = this.$el.find('.js_slider');
+      this.$slides = this.$el.find('.js_slides');
+      this.$sliderDots = this.$el.find('.js_slider_dots');
+    },
+
+    _initSlider: function () {
       new SliderView({
-        el: this.$el.find('.js_slider')
+        el: this.$slider
       });
     },
 
+    _loadNews: function () {
+      newsService.getNews().then(function(results) {
+        _.each(results, function(item) {
+          this.$slides.append(this.newsItemTpl({
+            name: item.name,
+            description: item.description,
+            link: item.link
+          }));
+        }.bind(this));
+        this.$sliderDots.html(this.newsSliderDotsTpl({
+          count: results.length
+        }));
+
+        this._initSlider();
+      }.bind(this));
+    }
+
   });
+
   return NewsView;
 
 });
