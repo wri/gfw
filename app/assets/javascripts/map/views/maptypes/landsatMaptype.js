2c4e1eff7cad8146ace5920c55ab7f7e6cf0d2e9
Gerardo <gerardo.pacheco@vizzuality.com>
2017-04-03 18:43:52 +0200

    Feat: Landsat 2015

--- app/assets/javascripts/map/views/maptypes/landsatMaptype.js
+++ app/assets/javascripts/map/views/maptypes/landsatMaptype.js
@@ -14,7 +14,11 @@ define([], function () {
       tileSize: new google.maps.Size(256, 256),
       getTileUrl: function(ll, z) {
         var x = Math.abs(ll.x % (1 << z)); // jshint ignore:line
-        return window.gfw.config.GFW_API_HOST + '/gee/landsat_composites/{0}/{1}/{2}.png?year={3}'.format(z, x, ll.y, year);
+        var url = window.gfw.config.GFW_API_HOST + '/gee/landsat_composites/{0}/{1}/{2}.png?year={3}'.format(z, x, ll.y, year);
+        if (year[0] === 2015) {
+          url = 'https://storage.googleapis.com/landsat-cache/2015/{0}/{1}/{2}.png'.format(z, x, ll.y);
+        }
+        return url;
       },
     };
 
