6eb1e168607ba10376b05305e4cfe7f45bc33a01
Benjamin A. Laken <benlaken@gmail.com>
2017-05-03 11:43:28 +0200

    updated carto table name to use new Lesur layer

--- app/assets/javascripts/map/views/layers/IdnLeuserLayer.js
+++ app/assets/javascripts/map/views/layers/IdnLeuserLayer.js
@@ -12,7 +12,7 @@ define([
   var IdnLeuserLayer = CartoDBLayerClass.extend({
 
     options: {
-      sql: 'SELECT \'idn_leuser\' as tablename, cartodb_id, the_geom_webmercator, name,  round(area_ha::float) as area_ha, \'{tableName}\' AS layer, {analysis} AS analysis FROM {tableName}' ,
+      sql: 'SELECT \'leuser_ecosystem_province_boundary\' as tablename, cartodb_id, the_geom_webmercator, name,  round(area_ha::float) as area_ha, \'{tableName}\' AS layer, {analysis} AS analysis FROM {tableName}' ,
       infowindow: true,
       interactivity: 'cartodb_id, tablename, name, area_ha, analysis',
       analysis: true
