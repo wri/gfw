caf7e96f631eed6685205ce1031ef8c17c6255ef
Benjamin A. Laken <benlaken@gmail.com>
2017-05-12 16:59:24 +0200

    added test fixture decorator and yaml to create multiple tests per dictionary added to yaml

--- tests/test_gfw.py
+++ tests/test_gfw.py
@@ -1,5 +1,6 @@
 import pytest
 import os
+import yaml
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -9,6 +10,11 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
+#
+# Note, that an environemnt variable TEST_TARGET controlls whether the tests
+# run on localhost, staging, or production (set to 'local', 'staging',
+# or 'production'). If not set, it will run on staging.
+#
 
 def extract_element_info(s, expression):
     """Extract information from the analysis object extracted from the site"""
@@ -30,36 +36,37 @@ def return_analysis_content(url):
 
 
 def url_target(d):
-    """Use the TEST_TARGET environment variable (staging, production or local
-    flag to set target) or default to testing on staging and construct an
-    analysis url"""
+    """Use the TEST_TARGET environment variable (staging, production or local flag to set target)
+       or default to testing on staging and construct an analysis url"""
+    log = logging.getLogger('gfw_analysis')
     environment = os.getenv("TEST_TARGET", None)
+    #environment='production'
     if not environment:
-        environment='staging'
+       environment='staging'
     if environment.lower() == 'production':
-        u = d[environment.lower()]
-        u = ''.join([u, d['url_analysis']])
-        u = u.replace('{start_date}', d['start_date'])
-        u = u.replace('{end_date}', d['end_date'])
-        u = u.replace('{threshold}', d['threshold'])
-        u = u.replace('{data_id}', d['production_data_id'])
-        return u
+       u = d[environment.lower()]
+       u = ''.join([u, d['url_analysis']])
+       u = u.replace('{start_date}', str(d['start_date']))
+       u = u.replace('{end_date}', str(d['end_date']))
+       if 'threshold' in d: u = u.replace('{threshold}', str(d['threshold']))
+       u = u.replace('{data_id}', str(d['production_data_id']))
+       return u
     elif environment.lower() == 'local':
-        u = d[environment.lower()]
-        u = ''.join([u, d['url_analysis']])
-        u = u.replace('{start_date}', d['start_date'])
-        u = u.replace('{end_date}', d['end_date'])
-        u = u.replace('{threshold}', d['threshold'])
-        u = u.replace('{data_id}', d['production_data_id'])
-        return u
+       u = d[environment.lower()]
+       u = ''.join([u, d['url_analysis']])
+       u = u.replace('{start_date}', str(d['start_date']))
+       u = u.replace('{end_date}', str(d['end_date']))
+       if 'threshold' in d: u = u.replace('{threshold}', str(d['threshold']))
+       u = u.replace('{data_id}', str(d['production_data_id']))
+       return u
     elif environment.lower() == 'staging':
-        u = d['staging']
-        u = ''.join([u, d['url_analysis']])
-        u = u.replace('{start_date}', d['start_date'])
-        u = u.replace('{end_date}', d['end_date'])
-        u = u.replace('{threshold}', d['threshold'])
-        u = u.replace('{data_id}', d['staging_local_data_id'])
-        return u
+       u = d['staging']
+       u = ''.join([u, d['url_analysis']])
+       u = u.replace('{start_date}', str(d['start_date']))
+       u = u.replace('{end_date}', str(d['end_date']))
+       if 'threshold' in d: u = u.replace('{threshold}', str(d['threshold']))
+       u = u.replace('{data_id}', str(d['staging_local_data_id']))
+       return u
 
 
 def check_value(expected, actual, tolerance):
@@ -68,59 +75,71 @@ def check_value(expected, actual, tolerance):
     assert expected == pytest.approx(actual, rel=tolerance), error
 
 
-def test_analysis_conservation_area(tolerance=0.1):
-    d={'staging':  "http://staging.globalforestwatch.org/map/",
-       'production': "http://globalforestwatch.org/map/",
-       'local': "localhost:5000/map/",
-       'url_analysis': "11/42.94/-4.56/ALL/grayscale/loss,forestgain/{data_id}?tab=analysis-tab&wdpaid=555549019&begin={start_date}&end={end_date}&threshold={threshold}&dont_analyze=true&tour=default",
-       'staging_local_data_id': '612',
-       'production_data_id': '612',
-       'start_date':'2001-01-01',
-       'end_date':'2016-01-01',
-       'threshold': '30',
-       'total':  78212,
-       'loss':  3078,
-       'gain':  556,
-      }
+def analyze_areas_data():
+    """Read the dictionary data held in the YAML file into a list of
+    dictionary objects, so that each can be evaluated seperatley."""
+    with open("./tests/analyze_areas.yaml", 'r') as stream:
+        try:
+            d = yaml.load(stream)
+        except yaml.YAMLError as exc:
+            raise IOError("Unable to read YAML file")
+    dictionary_list = []
+    for key in d.keys():
+        dictionary_list.append(d[key])
+    return dictionary_list
+
+
+@pytest.fixture(params=analyze_areas_data())
+def analyse_area(request, tolerance=0.1):
+    """Test GFW front-end. Tolerance = amount by which the values can be wrong (from 0 to 1)
+    """
+    d = request.param
+    #print(d)
     url = url_target(d)
     #print(f"Extracting values from {url.split('/map')[0]} target.")
     stats = return_analysis_content(url)
-    #print(stats)
-    total = extract_element_info(stats, 'TOTAL SELECTED AREA')
-    loss = extract_element_info(stats, "LOSS")
-    gain = extract_element_info(stats, "GAIN")
-    check_value(expected=d['total'], actual=total, tolerance=tolerance)
-    check_value(expected=d['loss'], actual=loss, tolerance=tolerance)
-    check_value(expected=d['gain'], actual=gain, tolerance=tolerance)
+    if 'total' in d:
+        total = extract_element_info(stats, 'TOTAL SELECTED AREA')
+        check_value(expected=d['total'], actual=total, tolerance=tolerance)
+    if 'loss' in d:
+        loss = extract_element_info(stats, "LOSS")
+        check_value(expected=d['loss'], actual=loss, tolerance=tolerance)
+    if 'gain' in d:
+        gain = extract_element_info(stats, "GAIN")
+        check_value(expected=d['gain'], actual=gain, tolerance=tolerance)
     return
 
 
-def non_increasing(L):
-    """Test that a given list does not increase at all as it progresses."""
-    return all(x>=y for x, y in zip(L, L[1:]))
+def test_analyze_areas(analyse_area):
+    return
 
-
-def test_analysis_loss_over_time():
-    """By its nature, loss should be larger the more time you consider. This test
-    checks that behaviour is correct for a dataset."""
-    d={'staging':  "http://staging.globalforestwatch.org/map/",
-       'production': "http://globalforestwatch.org/map/",
-       'local': "localhost:5000/map/",
-       'url_analysis': "11/42.94/-4.56/ALL/grayscale/loss,forestgain/{data_id}?tab=analysis-tab&wdpaid=555549019&begin={start_date}&end={end_date}&threshold={threshold}&dont_analyze=true&tour=default",
-       'staging_local_data_id': '612',
-       'production_data_id': '612',
-       'start_date':'2001-01-01',
-       'end_date':'2016-01-01',
-       'threshold': '30',
-       }
-    losses={}
-    first_year = int(d['start_date'].split('-')[0]) - 2000
-    last_year = int(d['end_date'].split('-')[0]) - 2000
-    start_dates = [f'{year + 2000}-01-01' for year in range(first_year, last_year + 1, 4)]
-    for start in start_dates:
-        d['start_date'] = start
-        url = url_target(d)
-        stats = return_analysis_content(url)
-        losses[start] = extract_element_info(stats, "LOSS")
-    loss_in_progressivley_shorter_time = list(losses.values())
-    assert non_increasing(loss_in_progressivley_shorter_time), "The amount of loss changed in an impossible way; It cannot increase."
+#
+# def non_increasing(L):
+#     """Test that a given list does not increase at all as it progresses."""
+#     return all(x>=y for x, y in zip(L, L[1:]))
+#
+#
+# def test_analysis_loss_over_time():
+#     """By its nature, loss should be larger the more time you consider. This test
+#     checks that behaviour is correct for a dataset."""
+#     d={'staging':  "http://staging.globalforestwatch.org/map/",
+#        'production': "http://globalforestwatch.org/map/",
+#        'local': "localhost:5000/map/",
+#        'url_analysis': "11/42.94/-4.56/ALL/grayscale/loss,forestgain/{data_id}?tab=analysis-tab&wdpaid=555549019&begin={start_date}&end={end_date}&threshold={threshold}&dont_analyze=true&tour=default",
+#        'staging_local_data_id': '612',
+#        'production_data_id': '612',
+#        'start_date':'2001-01-01',
+#        'end_date':'2016-01-01',
+#        'threshold': '30',
+#        }
+#     losses={}
+#     first_year = int(d['start_date'].split('-')[0]) - 2000
+#     last_year = int(d['end_date'].split('-')[0]) - 2000
+#     start_dates = [f'{year + 2000}-01-01' for year in range(first_year, last_year + 1, 4)]
+#     for start in start_dates:
+#         d['start_date'] = start
+#         url = url_target(d)
+#         stats = return_analysis_content(url)
+#         losses[start] = extract_element_info(stats, "LOSS")
+#     loss_in_progressivley_shorter_time = list(losses.values())
+#     assert non_increasing(loss_in_progressivley_shorter_time), "The amount of loss changed in an impossible way; It cannot increase."
