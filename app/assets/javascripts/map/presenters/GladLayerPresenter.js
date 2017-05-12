6b91008bd10bca0635b2988b1f23bd9643720509
Hector Vizzuality <hectorvizzuality@MacBook-Air-de-Hector.local>
2017-05-12 13:02:32 +0200

    add alertmessage on glad layer

--- app/assets/javascripts/map/presenters/GladLayerPresenter.js
+++ app/assets/javascripts/map/presenters/GladLayerPresenter.js
@@ -10,6 +10,8 @@ define([
   var GladLayerPresenter = PresenterClass.extend({
 
     init: function(view) {
+      this.publishNotification('notification-glad-data-alert');
+      console.log('heeello');
       this.view = view;
       this._super();
 
@@ -55,6 +57,10 @@ define([
 
     animationStopped: function() {
       mps.publish('Torque/stopped', []);
+    },
+
+    publishNotification: function(id){
+      mps.publish('Notification/open', [id]);
     },
 
     updateTimelineDate: function(change) {
