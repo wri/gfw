5d5447258cb7ca7e6b2c7ec36347e413a582cee4
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-07-05 14:46:45 +0100

    More controller specs

--- spec/controllers/countries_controller_spec.rb
+++ spec/controllers/countries_controller_spec.rb
@@ -14,4 +14,21 @@ describe CountriesController, type: :controller do
     it_behaves_like 'renders index'
     it_behaves_like 'assigns title', 'Country Profiles'
   end
+
+  describe 'GET overview' do
+    subject { get :overview }
+    it 'renders overview' do
+      subject
+      expect(response).to render_template('overview')
+    end
+    it_behaves_like 'assigns title', 'Country Rankings'
+  end
+
+  describe 'GET show' do
+    subject { get :show, params: {id: 'POL'} }
+    it 'renders show' do
+      subject
+      expect(response).to render_template('show')
+    end
+  end
 end
