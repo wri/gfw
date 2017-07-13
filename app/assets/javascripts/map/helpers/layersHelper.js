4a23692e6e11ec331897fd6a5c9d4db6b7137cbc
Benjamin A. Laken <benlaken@gmail.com>
2017-07-13 15:10:58 -0400

    paraguay layer

--- app/assets/javascripts/map/helpers/layersHelper.js
+++ app/assets/javascripts/map/helpers/layersHelper.js
@@ -140,6 +140,7 @@ define([
   'map/views/layers/MexMiningLayer',
   'map/views/layers/BraMiningLayer',
   'map/views/layers/BraRTRSLayer',
+  'map/views/layers/PryRTRSLayer',
   'map/views/layers/ViirsLayer',
   'map/views/layers/PerMinamCoverLayer',
   'map/views/layers/CanIntactForestLayer',
@@ -323,6 +324,7 @@ define([
   MexMiningLayer,
   BraMiningLayer,
   BraRTRSLayer,
+  PryRTRSLayer,
   ViirsLayer,
   PerMinamCoverLayer,
   CanIntactForestLayer,
@@ -798,6 +800,9 @@ define([
     },
     bra_rtrs: {
       view: BraRTRSLayer
+    },
+    pry_rtrs:{
+      view: PryRTRSLayer
     },
     per_minam_tree_cover: {
       view:  PerMinamCoverLayer
