f474b83ce8c55c6f6c95b28f665357ffe727892e
Hector <hi@hectorarce.es>
2017-07-18 11:29:25 +0200

    fix legend cover land

--- app/assets/javascripts/map/views/LegendView.js
+++ app/assets/javascripts/map/views/LegendView.js
@@ -309,7 +309,7 @@ define([
       return _.groupBy(filteredLayers, function(layer) {
         layer.allowSubscription = layer && subscriptionsAllowed.indexOf(layer.slug) > -1;
 
-        // Hack to keep the forest_clearing slug in layers which have to be analyzed but not grouped by the said slug in the legend
+        // Hack to keep the forest_clearing slug in layers which have to be analyzed but not grouped by the said slug in the legend 
         if (layer.category_slug === 'forest_clearing' && !layer.is_forest_clearing) return 'forest_cover';
         return layer.category_slug;
       })
