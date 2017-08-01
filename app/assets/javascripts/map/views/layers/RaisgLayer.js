6b4856c765adef61aef1059bc374ea5caee04cf7
Benjamin A. Laken <benlaken@gmail.com>
2017-08-01 17:58:51 +0200

    verified the styles are the problem - reverted to default

--- app/assets/javascripts/map/views/layers/RaisgLayer.js
+++ app/assets/javascripts/map/views/layers/RaisgLayer.js
@@ -1,6 +1,7 @@
 /**
  * The RaisgLayer layer module.
  *
+ *     wmsUrl += '&STYLES=polygonSymbolizer';
  * @return RaisgLayer class (extends WMSLayerClass)
  */
 define([
@@ -10,12 +11,12 @@ define([
 
   'use strict';
 
-  var wmsUrl = 'https://geo.socioambiental.org/arcgis/services/raisg/raisg_tis/MapServer/WMSServer?';
+  var wmsUrl = 'https://geo.socioambiental.org/webadaptor2/services/raisg/raisg_tis/MapServer/WMSServer?';
     wmsUrl += '&SERVICE=WMS';
     wmsUrl += '&REQUEST=GetMap';
     wmsUrl += '&VERSION=1.1.1';
     wmsUrl += '&LAYERS=0';
-    wmsUrl += '&STYLES=polygonSymbolizer';
+    wmsUrl += '&STYLES=default';
     wmsUrl += '&FORMAT=image%2Fpng';
     wmsUrl += '&TRANSPARENT=true';
     wmsUrl += '&HEIGHT=256';
@@ -24,7 +25,7 @@ define([
     wmsUrl += '&SRS=EPSG%3A3857';
 
 
-  var wmsInfowindowUrl = 'https://geo.socioambiental.org/arcgis/rest/services/raisg/raisg_tis/MapServer/3/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=tis.pais,tis.categoria,tis.nombre,tis.status,tis.etnias,tis.no_habitantes,tis.fuente_data_habitantes,tis.norma,tis.fecha_norma,tis.fecha_ultima_atualizacion_norma,tis.area_oficial_ha,tis.institucionraisg,tis.fuente,tis.fecha_atualizacion_dato,tis.leyenda,tis.codigo_tis&geometryType=esriGeometryPoint&geometry={longitude},{latitude}';
+  var wmsInfowindowUrl = 'https://geo.socioambiental.org/webadaptor2/services/raisg/raisg_tis/MapServer/WMSServer/3/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=tis.pais,tis.categoria,tis.nombre,tis.status,tis.etnias,tis.no_habitantes,tis.fuente_data_habitantes,tis.norma,tis.fecha_norma,tis.fecha_ultima_atualizacion_norma,tis.area_oficial_ha,tis.institucionraisg,tis.fuente,tis.fecha_atualizacion_dato,tis.leyenda,tis.codigo_tis&geometryType=esriGeometryPoint&geometry={longitude},{latitude}';
 
 
   var RaisgLayer = WMSLayerClass.extend({
--- app/assets/javascripts/map/views/layers/RaisgMiningLayer.js
+++ app/assets/javascripts/map/views/layers/RaisgMiningLayer.js
@@ -1,5 +1,6 @@
 /*
  * @return RaisgLayer class (extends WMSLayerClass)
+ *     wmsUrl += '&STYLES=polygonSymbolizer';
  */
 define([
   'uri',
@@ -8,21 +9,21 @@ define([
 
   'use strict';
 
-  var wmsUrl = 'https://geo.socioambiental.org/arcgis/services/raisg/raisg_mineria/MapServer/WMSServer?';
+  var wmsUrl = 'https://geo.socioambiental.org/webadaptor2/services/raisg/raisg_mineria/MapServer/WMSServer?';
     wmsUrl += '&SERVICE=WMS';
     wmsUrl += '&REQUEST=GetMap';
     wmsUrl += '&VERSION=1.3.0';
     wmsUrl += '&LAYERS=0';
-    wmsUrl += '&STYLES=polygonSymbolizer';
+    wmsUrl += '&STYLES=default';
     wmsUrl += '&FORMAT=image%2Fpng';
     wmsUrl += '&TRANSPARENT=true';
     wmsUrl += '&HEIGHT=256';
     wmsUrl += '&WIDTH=256';
-    wmsUrl += '&SLD=http://gfw-nav.herokuapp.com/assets/map/cartocss/raisg_mining.xml'; 
+    wmsUrl += '&SLD=http://gfw-nav.herokuapp.com/assets/map/cartocss/raisg_mining.xml';
     wmsUrl += '&CRS=EPSG%3A3857';
 
 
-  var wmsInfowindowUrl = 'https://geo.socioambiental.org/arcgis/rest/services/raisg/raisg_mineria/MapServer/1/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=pais,nombre,cia,situacion,tipo_minerio,fuente,area_oficial_ha,leyenda,grupo&geometryType=esriGeometryPoint&geometry={longitude},{latitude}';
+  var wmsInfowindowUrl = 'https://geo.socioambiental.org/webadaptor2/services/raisg/raisg_mineria/MapServer/WMSServer/1/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=pais,nombre,cia,situacion,tipo_minerio,fuente,area_oficial_ha,leyenda,grupo&geometryType=esriGeometryPoint&geometry={longitude},{latitude}';
 
 
   var RaisgMiningLayer = WMSLayerClass.extend({
