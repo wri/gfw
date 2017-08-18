ff32e29a11397dbc018bfbf436e1c0e7997f4556
Hector <hi@hectorarce.es>
2017-08-18 09:46:01 +0200

    functions to open Subscribe Modal

--- app/javascript/pages/general/components/ButtonRegular.jsx
+++ app/javascript/pages/general/components/ButtonRegular.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const ButtonRegular= (props) => {
-  const {text, color} = props
+  const {text, color, clickFunction} = props
   return (
-    <div className={`c-regular-button -${color}`}>
+    <div className={`c-regular-button -${color}`} onClick={clickFunction}>
       {text}
     </div>
   );
