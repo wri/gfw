d3612e85229b86510723245a4f92dc8821fffe83
Simao Belchior <simaobelchior@gmail.com>
2017-09-04 17:36:30 +0100

    maybe this makes bundler smarter

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+#Bundler.require(:default, Rails.env)
+Bundler.require(*Rails.groups)
 
 
 module Gfw
