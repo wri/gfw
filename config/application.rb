426934f588f65bb1b862c95c224ccd1281bd17b5
Simao Belchior <simaobelchior@gmail.com>
2017-09-04 17:05:22 +0100

    Removes railties that are not needed and application_record

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,11 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
-require "active_record/railtie"
+# require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
-require "active_model"
+# require "active_model"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
