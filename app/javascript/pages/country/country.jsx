b6e782d592e742b25eec17eec547cbcccb8e3743
pjosh <jorgegar@gmail.com>
2017-09-11 13:40:22 +0200

    Map component WIP

--- app/javascript/pages/country/country.jsx
+++ app/javascript/pages/country/country.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
 import initialState from './initial-state';
 import reducers from './reducers';
 import store from '../../common/store';
 
+import Map from './components/map/map';
+
 const Country = () => {
   return (
     <Provider store={store(initialState, reducers)}>
-      <div>
+      <div className="row">
+        <div className="small-9 columns"></div>
+        <div className="small-4 columns">
+          <Map />
+        </div>
       </div>
     </Provider>
   );
--- app/javascript/pages/country/reducers.js
+++ app/javascript/pages/country/reducers.js
@@ -1,6 +1,12 @@
 import { combineReducers } from 'redux'
+import { handleActions } from '../../utils/redux';
+
+import initialState from './initial-state';
+import allActions from './actions';
+import { reducers as mapReducers } from './components/map/map';
 
 const reducers = combineReducers({
+  map: handleActions('map', allActions, mapReducers, initialState),
 });
 
 export default reducers
