fc90b7c0fb63cf69fac5fb69da2aaec78fa0d629
pjosh <jorgegar@gmail.com>
2017-09-17 17:04:04 +0200

    Some header styles

--- app/javascript/pages/country/components/header/header-component.js
+++ app/javascript/pages/country/components/header/header-component.js
@@ -18,17 +18,21 @@ class Header extends PureComponent {
     return (
       <div className="c-header">
         <div className="row">
-          <div className="small-9 columns">
-            <select value={iso} onChange={this.countriesSelectOnChange}>
-              {countriesList.map((country, i) => {
-                return <option key={i} value={country.iso}>{country.name}</option>;
-              })}
-            </select>
-            <select value={iso} onChange={this.regionsSelectOnChange}>
-              {countryRegions.map((region, i) => {
-                return <option key={i} value={region.id}>{region.name}</option>;
-              })}
-            </select>
+          <div className="small-12 columns">
+            <div className="c-header__select">
+              <select value={iso} onChange={this.countriesSelectOnChange}>
+                {countriesList.map((country, i) => {
+                  return <option key={i} value={country.iso}>{country.name}</option>;
+                })}
+              </select>
+            </div>
+            <div className="c-header__select">
+              <select value={iso} onChange={this.regionsSelectOnChange}>
+                {countryRegions.map((region, i) => {
+                  return <option key={i} value={region.id}>{region.name}</option>;
+                })}
+              </select>
+            </div>
           </div>
         </div>
       </div>
