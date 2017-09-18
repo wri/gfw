32af4b4004541297e85dce0969e5f63cc355bde8
pjosh <jorgegar@gmail.com>
2017-09-18 12:19:40 +0200

    Tree cover widget WIP

--- app/javascript/pages/country/components/widget-tree-cover/widget-tree-cover-actions.js
+++ app/javascript/pages/country/components/widget-tree-cover/widget-tree-cover-actions.js
@@ -1,9 +1,7 @@
 import { createAction } from 'redux-actions';
 
-const setIsLoading = createAction('setIsLoading');
 const setValues = createAction('setValues');
 
 export default {
-  setIsLoading,
   setValues
 };
