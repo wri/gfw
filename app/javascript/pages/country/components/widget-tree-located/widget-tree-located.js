81fe9053451e5d1fe296dac2b1134c7ee0bb1b63
pjosh <jorgegar@gmail.com>
2017-09-20 09:46:03 +0200

    Tree located widget WIP

--- app/javascript/pages/country/components/widget-tree-located/widget-tree-located.js
+++ app/javascript/pages/country/components/widget-tree-located/widget-tree-located.js
@@ -21,43 +21,53 @@ const WidgetTreeLocatedContainer = (props) => {
     props.setTreeLocatedValues([
       {
         name: 'Minas Gerais',
-        value: 1200000
+        value: 1200000,
+        percent: 10
       },
       {
         name: 'Bahia',
-        value: 1100000
+        value: 1100000,
+        percent: 8
       },
       {
         name: 'Amazonas',
-        value: 900000
+        value: 900000,
+        percent: 8
       },
       {
         name: 'Maranhao',
-        value: 550000
+        value: 550000,
+        percent: 5
       },
       {
         name: 'Distrito Federal',
-        value: 464000
+        value: 464000,
+        percent: 5
       },
       {
         name: 'Ceará',
-        value: 460000
+        value: 460000,
+        percent: 5
       },
       {
         name: 'Espírito Santo',
-        value: 440000
+        value: 440000,
+        percent: 5
       },
       {
         name: 'Goiás',
-        value: 420000
+        value: 420000,
+        percent: 5
       },
       {
         name: 'Maranhao',
-        value: 300000
+        value: 300000,
+        percent: 5
       },
       {
         name: 'Mato Grosso',
-        value: 203000
+        value: 203000,
+        percent: 5
       }
     ]);
   };
