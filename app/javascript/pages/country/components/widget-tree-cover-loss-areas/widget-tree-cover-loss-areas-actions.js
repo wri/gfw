2843525a4550d6bbfd053d26bbb68e96e927b3cc
Hector <hi@hectorarce.es>
2017-09-25 17:14:09 +0200

    include tree cover loss areas widget

---
+++ app/javascript/pages/country/components/widget-tree-cover-loss-areas/widget-tree-cover-loss-areas-actions.js
@@ -0,0 +1,7 @@
+import { createAction } from 'redux-actions';
+
+const setTreeCoverLossAreasValues = createAction('setTreeCoverLossAreasValues');
+
+export default {
+  setTreeCoverLossAreasValues
+};
