10a3b146b6ffe7e5f7bd8aed5b97f4d37bb041ab
Hector <hi@hectorarce.es>
2017-09-26 16:07:03 +0200

    fix errors

--- app/javascript/pages/country/actions.js
+++ app/javascript/pages/country/actions.js
@@ -3,11 +3,13 @@ import { actions as mapActions } from './components/map/map';
 import { actions as widgetTreeCoverActions } from './components/widget-tree-cover/widget-tree-cover';
 import { actions as widgetTreeLocatedActions } from './components/widget-tree-located/widget-tree-located';
 import { actions as widgetTreeLossActions } from './components/widget-tree-loss/widget-tree-loss';
+import { actions as widgetTreeCoverLossAreasActions } from './components/widget-tree-cover-loss-areas/widget-tree-cover-loss-areas';
 
 export default {
   ...rootActions,
   ...mapActions,
   ...widgetTreeCoverActions,
   ...widgetTreeLocatedActions,
-  ...widgetTreeLossActions
+  ...widgetTreeLossActions,
+  ...widgetTreeCoverLossAreasActions
 };
