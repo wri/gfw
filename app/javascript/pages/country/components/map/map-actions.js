59ffc3cca395798595614f501ce6604e3ce7b345
pjosh <jorgegar@gmail.com>
2017-09-27 08:34:54 +0200

    Map layers WIP

--- app/javascript/pages/country/components/map/map-actions.js
+++ app/javascript/pages/country/components/map/map-actions.js
@@ -1,7 +1,11 @@
 import { createAction } from 'redux-actions';
 
 const setMapZoom = createAction('setMapZoom');
+const setLayer = createAction('setLayer');
+const addLayer = createAction('addLayer');
 
 export default {
-  setMapZoom
+  setMapZoom,
+  setLayer,
+  addLayer
 };
---
+++ app/javascript/utils/layers.js
@@ -0,0 +1,7 @@
+import Forest2000 from '../layers/forest2000';
+
+const layersMap = {
+  'forest2000': Forest2000
+};
+
+export default layersMap;
