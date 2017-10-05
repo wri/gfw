7152c7eaef543f371698df541bf0bfff51ebb438
Hector <hi@hectorarce.es>
2017-10-05 09:21:25 +0200

    improve charts

--- app/javascript/pages/country/components/tooltip-chart/tooltip-chart-component.js
+++ app/javascript/pages/country/components/tooltip-chart/tooltip-chart-component.js
@@ -5,9 +5,10 @@ import numeral from 'numeral';
 const TooltipChart = (props) => {
   const { active } = props;
   if(active) {
-    const { payload, label } = props;
+    const { payload, label, showCountry } = props;
     return (
       <div className="c-tooltip-chart">
+        {showCountry && <span className="name">{payload[0].name}</span>}
         <p className="label">{`${numeral(Math.round(payload[0].value / 1000)).format('0,0')} Ha`}</p>
       </div>
     );
