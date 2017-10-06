6bb05e374ef9b92aa68f55490f5444ea0feb94cb
Hector <hi@hectorarce.es>
2017-10-06 08:32:09 +0200

    include settings tooltip on located widget

---
+++ app/javascript/pages/country/components/widget-tree-located/widget-tree-located-settings-component.js
@@ -0,0 +1,64 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import Select from 'react-select-me';
+
+class WidgetTreeLocatedSettings extends PureComponent {
+
+  // regionChange = (value) => {
+  //   this.props.onRegionChange(value.value);
+  // };
+  //
+  // unitChange = (value) => {
+  //   this.props.onUnitChange(value.value);
+  // };
+  //
+  // canopyChange = (value) => {
+  //   this.props.onCanopyChange(value.value);
+  // };
+
+  iconRenderer = () => {
+    return(<svg className="icon icon-angle-arrow-down"><use xlinkHref="#icon-angle-arrow-down">{}</use></svg>);
+  }
+
+  render() {
+    const {
+      units,
+      canopies,
+      settings,
+      dataSource,
+    } = this.props;
+    return (
+      <div className="c-widget-settings">
+        <div className="c-widget-settings__select">
+          <div className="c-widget-settings__title">DATA SOURCE</div>
+          <Select
+            iconRenderer={this.iconRenderer}
+            value={settings.dataSources}
+            options={dataSource}/>
+        </div>
+        <div className="c-widget-settings__select">
+          <div className="c-widget-settings__title">UNIT</div>
+          <Select
+            iconRenderer={this.iconRenderer}
+            value={settings.unit}
+            options={units}/>
+        </div>
+        <div className="c-widget-settings__button-select">
+          <div className="c-widget-settings__title">CANOPY DENSITY</div>
+          <Select
+            value={settings.canopy}
+            options={canopies}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+ WidgetTreeLocatedSettings.propTypes = {
+  dataSource: PropTypes.array.isRequired,
+  units: PropTypes.array.isRequired,
+  canopies: PropTypes.array.isRequired,
+  settings: PropTypes.object.isRequired,
+};
+
+export default  WidgetTreeLocatedSettings;
