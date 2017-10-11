2a5c380d401664552ac91bc17727c20af5fa4632
Hector <hi@hectorarce.es>
2017-10-11 11:36:08 +0200

    change forest cover widget filters and chart hover actions

--- app/javascript/pages/country/components/tooltip-chart/tooltip-chart-component.js
+++ app/javascript/pages/country/components/tooltip-chart/tooltip-chart-component.js
@@ -5,8 +5,14 @@ import numeral from 'numeral';
 const TooltipChart = (props) => {
   const { active } = props;
   if (active) {
-    const { payload, label, showCountry, percentage } = props;
-    const valueShow = !percentage ? numeral(Math.round(payload[0].value / 1000)).format('0,0') + 'Ha' :numeral(Math.round(payload[0].value)).format('0,0') + '%';
+    const { payload, label, showCountry, percentage, percentageAndArea } = props;
+    let valueShow;
+    console.log(props);
+    if (percentageAndArea) {
+      valueShow = numeral(Math.round(payload[0].payload.percentage)).format('0,0') + '%'
+    } else {
+      valueShow = !percentage ? numeral(Math.round(payload[0].value / 1000)).format('0,0') + 'Ha' : numeral(Math.round(payload[0].value)).format('0,0') + '%';
+    }
     return (
       <div className="c-tooltip-chart">
         {showCountry && <span className="name">{payload[0].name}</span>}
