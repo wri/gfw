ac9cdbbd44d3fa1f0bf0d2c176b46156f0586f04
Hector <hi@hectorarce.es>
2017-10-13 13:04:59 +0200

    fix percentage functions

--- app/javascript/pages/country/components/widget-areas-most-cover-gain/widget-areas-most-cover-gain-actions.js
+++ app/javascript/pages/country/components/widget-areas-most-cover-gain/widget-areas-most-cover-gain-actions.js
@@ -3,13 +3,13 @@ import { createAction } from 'redux-actions';
 const setPieCharDataAreas = createAction('setPieCharDataAreas');
 const setArrayCoverAreasGain = createAction('setArrayCoverAreasGain');
 const setPieCharDataAreasTotal = createAction('setPieCharDataAreasTotal');
-const setTreeLocatedSettingsUnit = createAction('setTreeLocatedSettingsUnit');
+const setTreeAreasTreeGainSettingsUnit = createAction('setTreeAreasTreeGainSettingsUnit');
 const setAreaMostCoverIsUpdating = createAction('setAreaMostCoverIsUpdating');
 
 export default {
   setPieCharDataAreas,
   setArrayCoverAreasGain,
   setPieCharDataAreasTotal,
-  setTreeLocatedSettingsUnit,
+  setTreeAreasTreeGainSettingsUnit,
   setAreaMostCoverIsUpdating
 };
--- app/javascript/pages/country/components/widget-areas-most-cover-gain/widget-areas-most-cover-gain-reducers.js
+++ app/javascript/pages/country/components/widget-areas-most-cover-gain/widget-areas-most-cover-gain-reducers.js
@@ -69,7 +69,7 @@ const setArrayCoverAreasGain = (state, { payload }) => ({
   endArray: payload.endArray
 });
 
-const setTreeLocatedSettingsUnit = (state, { payload }) => ({
+const setTreeAreasTreeGainSettingsUnit = (state, { payload }) => ({
   ...state,
   settings: {
     ...state.settings,
@@ -86,6 +86,6 @@ export default {
   setPieCharDataAreas,
   setArrayCoverAreasGain,
   setPieCharDataAreasTotal,
-  setTreeLocatedSettingsUnit,
+  setTreeAreasTreeGainSettingsUnit,
   setAreaMostCoverIsUpdating
 };
