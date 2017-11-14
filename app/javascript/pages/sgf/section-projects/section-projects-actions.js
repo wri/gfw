1af90bed7ba5b1bd5031abe9345c5ba75219242f
j8seangel <jseangel@gmail.com>
2017-11-14 13:48:01 +0100

    Mark section as active

--- app/javascript/pages/sgf/section-projects/section-projects-actions.js
+++ app/javascript/pages/sgf/section-projects/section-projects-actions.js
@@ -7,21 +7,24 @@ export const fetchProjectsFail = createAction('fetchProjectsFail');
 
 export const fetchProjects = createThunkAction(
   'fetchProjects',
-  () => dispatch => {
-    dispatch(fetchProjectsInit());
-    fetch(
-      'http://wri-01.carto.com/api/v2/sql?q=SELECT%20a.*%2C%20b.latitude_average%2C%20b.longitude_average%20FROM%20gfw_use_cases_for_about_page%20a%2C%20country_list_iso_3166_codes_latitude_longitude%20b%20WHERE%20a.country_iso_code%20%3D%20b.alpha_3_code'
-    )
-      .then(response => {
-        if (response.ok) return response.json();
-        throw Error(response.statusText);
-      })
-      .then(data => {
-        dispatch(fetchProjectsReady(data.rows));
-      })
-      .catch(error => {
-        console.warn(error);
-        dispatch(fetchProjectsFail());
-      });
+  () => (dispatch, getState) => {
+    const { data } = getState().projects;
+    if (!data || !data.length) {
+      dispatch(fetchProjectsInit());
+      fetch(
+        'http://wri-01.carto.com/api/v2/sql?q=SELECT%20a.*%2C%20b.latitude_average%2C%20b.longitude_average%20FROM%20gfw_use_cases_for_about_page%20a%2C%20country_list_iso_3166_codes_latitude_longitude%20b%20WHERE%20a.country_iso_code%20%3D%20b.alpha_3_code'
+      )
+        .then(response => {
+          if (response.ok) return response.json();
+          throw Error(response.statusText);
+        })
+        .then(response => {
+          dispatch(fetchProjectsReady(response.rows));
+        })
+        .catch(error => {
+          console.warn(error);
+          dispatch(fetchProjectsFail());
+        });
+    }
   }
 );
