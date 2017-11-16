966ba21b7424fb50e08d954e1206120f5d511a97
j8seangel <jseangel@gmail.com>
2017-11-16 16:44:37 +0100

    Globe usable refactor

--- app/javascript/components/globe/globe-actions.js
+++
@@ -1,7 +0,0 @@
-import { createAction } from 'redux-actions';
-
-const globeAction = createAction('globeAction');
-
-export default {
-  globeAction
-};
--- app/javascript/components/globe/globe-selectors.js
+++
@@ -1,7 +0,0 @@
-import { createSelector } from 'reselect';
-
-export const globeSelector = createSelector(state => state, state => state);
-
-export default {
-  globeSelector
-};
