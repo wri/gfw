4da10ba3a5468bfdedbfac7506cf3a8364f1a5fc
j8seangel <jseangel@gmail.com>
2017-11-16 16:45:54 +0100

    Projects filter by category, globe and modal

--- app/javascript/pages/sgf/section-projects/section-projects-reducers.js
+++ app/javascript/pages/sgf/section-projects/section-projects-reducers.js
@@ -1,8 +1,11 @@
+import { allProjectsCategory } from './section-projects-selectors';
+
 export const initialState = {
   loading: false,
   loaded: false,
   error: false,
-  data: []
+  data: [],
+  categorySelected: allProjectsCategory
 };
 
 const setLoading = (state, loading) => ({ ...state, loading });
@@ -13,5 +16,9 @@ export default {
   fetchProjectsInit: state => setLoading(state, true),
   fetchProjectsReady: (state, { payload }) =>
     setLoaded(setLoading({ ...state, data: payload }, false), true),
-  fetchProjectsFail: state => setError(state, true)
+  fetchProjectsFail: state => setError(state, true),
+  setCategorySelected: (state, { payload }) => ({
+    ...state,
+    categorySelected: payload
+  })
 };
