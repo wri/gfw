2d609dfdd98849e952230378f0856df694bf1679
pjosh <jorgegar@gmail.com>
2017-11-20 17:11:22 +0100

    Remove local dependency

--- app/javascript/map/layers/intact-forest.js
+++ app/javascript/map/layers/intact-forest.js
@@ -1,13 +1,12 @@
 import CartoDB from './abstract/cartoDB';
-import IntactForestCartoCSS from 'raw-loader!../cartocss/intact-forest.cartocss';
+import IntactForestCartoCSS from '../cartocss/intact-forest.cartocss';
 
 const OPTIONS = {
-  sql: `SELECT *, '{tableName}' as layer, '{tableName}' as name FROM {tableName}`,
+  sql: 'SELECT *, \'{tableName}\' as layer, \'{tableName}\' as name FROM {tableName}',
   cartocss: IntactForestCartoCSS
 };
 
 class IntactForest extends CartoDB {
-
   constructor(map, options) {
     super(map, OPTIONS);
     this.options = Object.assign({}, this.options, options);
