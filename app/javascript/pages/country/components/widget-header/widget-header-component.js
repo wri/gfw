55f8a54bcb67102cd9622f10d06c45779e19a1d0
pjosh <jorgegar@gmail.com>
2017-11-22 11:05:28 +0100

    eslint warnings

--- app/javascript/pages/country/components/widget-header/widget-header-component.js
+++ app/javascript/pages/country/components/widget-header/widget-header-component.js
@@ -1,49 +1,50 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import {
-  Tooltip,
-} from 'react-tippy';
+import { Tooltip } from 'react-tippy';
 
 class WidgetHeader extends PureComponent {
-
   render() {
-    const {
-      children,
-      title,
-      noMap,
-      viewOnMapCallback
-    } = this.props;
+    const { children, title, noMap, viewOnMapCallback } = this.props;
 
     return (
       <div className="c-widget-header">
         <div className="c-widget-header__title">{title}</div>
         <ul className="c-widget-header__options">
-          { noMap === null || !noMap ?
-            <li className="c-widget-header__option-button" onClick={viewOnMapCallback}>VIEW ON MAP</li> : null }
+          {noMap === null || !noMap ? (
+            <button
+              className="c-widget-header__option-button"
+              onClick={viewOnMapCallback}
+            >
+              VIEW ON MAP
+            </button>
+          ) : null}
           <li className="c-widget-header__option-circle c-widget-header__option-circle--green">
             <svg className="icon icon-info">
-              <use xlinkHref="#icon-info"></use>
+              <use xlinkHref="#icon-info" />
             </svg>
           </li>
           <li>
-            {children !== undefined && children.props.type === 'settings' ?
+            {children !== undefined && children.props.type === 'settings' ? (
               <Tooltip
                 theme="light"
                 position="bottom-right"
                 offset={-95}
                 trigger="click"
-                interactive={true}
-                arrow={true}
-                html={children}>
-                <div className={`c-widget-header__option-circle c-widget-header__option-circle--green`}>
-                  <svg className="icon icon-settings"><use xlinkHref="#icon-settings"></use></svg>
+                interactive
+                arrow
+                html={children}
+              >
+                <div className="c-widget-header__option-circle c-widget-header__option-circle--green">
+                  <svg className="icon icon-settings">
+                    <use xlinkHref="#icon-settings" />
+                  </svg>
                 </div>
               </Tooltip>
-              : null}
+            ) : null}
           </li>
           <li className="c-widget-header__option-circle c-widget-header__option-circle--white">
             <svg className="icon icon-share -dark">
-              <use xlinkHref="#icon-share"></use>
+              <use xlinkHref="#icon-share" />
             </svg>
           </li>
         </ul>
@@ -55,7 +56,8 @@ class WidgetHeader extends PureComponent {
 WidgetHeader.propTypes = {
   title: PropTypes.string.isRequired,
   noMap: PropTypes.bool,
-  viewOnMapCallback: PropTypes.func
+  viewOnMapCallback: PropTypes.func,
+  children: PropTypes.object
 };
 
 export default WidgetHeader;
