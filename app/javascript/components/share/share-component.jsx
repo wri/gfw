cd71c90b163bd6087a1a2b29b9d5b13e619c5cc0
pjosh <jorgegar@gmail.com>
2017-11-25 00:19:39 +0100

    share component with social buttons

--- app/javascript/components/share/share-component.jsx
+++ app/javascript/components/share/share-component.jsx
@@ -3,6 +3,11 @@ import PropTypes from 'prop-types';
 import Modal from 'components/modal';
 
 import ButtonRegular from 'components/button-regular';
+import Icon from 'components/icon/icon';
+
+import googleplusIcon from 'assets/icons/googleplus.svg';
+import twitterIcon from 'assets/icons/twitter.svg';
+import facebookIcon from 'assets/icons/facebook.svg';
 
 import './share-styles.scss';
 
@@ -67,6 +72,29 @@ class Share extends PureComponent {
             />
           </div>
         ) : null}
+        <div className="c-share__social-container">
+          <a
+            href={`https://plus.google.com/share?url=${url}`}
+            target="_blank"
+            className="c-share__social-button -googleplus"
+          >
+            <Icon icon={googleplusIcon} className="googleplus-icon" />
+          </a>
+          <a
+            href={`https://twitter.com/share?url=${url}`}
+            target="_blank"
+            className="c-share__social-button -twitter"
+          >
+            <Icon icon={twitterIcon} className="twitter-icon" />
+          </a>
+          <a
+            href={`https://www.facebook.com/sharer.php?u=${url}`}
+            target="_blank"
+            className="c-share__social-button -facebook"
+          >
+            <Icon icon={facebookIcon} className="facebook-icon" />
+          </a>
+        </div>
       </div>
     );
   }
