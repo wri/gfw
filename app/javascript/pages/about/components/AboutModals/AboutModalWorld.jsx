a026ee26358a8976c65412c637e1e616f5443ccd
Hector <hi@hectorarce.es>
2017-12-01 10:14:19 +0100

    include tooltip and close background on modal

--- app/javascript/pages/about/components/AboutModals/AboutModalWorld.jsx
+++ app/javascript/pages/about/components/AboutModals/AboutModalWorld.jsx
@@ -6,6 +6,7 @@ class AboutModalWorld extends Component {
     if (this.props.isVisible) {
       return (
         <div className="c-about-modal-world">
+          <div className="c-about-modal-world__back" onClick={this.props.hideModal}>{}</div>
           <div className="c-about-modal-world__text">
             <svg className="icon-close" onClick={this.props.hideModal}><use xlinkHref="#icon-close"></use></svg>
             <div>
@@ -14,7 +15,7 @@ class AboutModalWorld extends Component {
             </div>
             <div className="contain-buttons">
               {this.props.userData.link ? <a href={this.props.userData.link} target="_blank" className="c-regular-button -green ">LEARN MORE</a> : null}
-              {this.props.userData.sgf ? <div className="text -color-2 button-round">$</div> : null}
+              {this.props.userData.sgf ? <div className="contain-tooltip"><div className="tooltip-modal"><div className="triangle"><span>small grants fund</span></div></div><a href="/small-grants-fund" className="text -color-2 button-round">$</a></div> : null}
             </div>
           </div>
         </div>
