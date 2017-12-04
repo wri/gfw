74eec8fb9271ea14b9c1288c743a265c404168fe
Edward Brett <e.j.a.brett@gmail.com>
2017-12-04 22:58:05 +0100

    WIP: fetch and show gadm extent in header

--- app/javascript/services/tree-extent.js
+++ app/javascript/services/tree-extent.js
@@ -1,22 +1,27 @@
 import axios from 'axios';
-import datasets from './datasets.json';
 
 const APIURL = process.env.GFW_API_HOST_NEW_API;
 
 const APIURLS = {
   getExtent:
-    '/query?sql=SELECT area as value FROM {dataset} WHERE {location} AND thresh = {threshold}'
+    "/query/8e272e2a-8874-4476-a339-66cabb464bd6?sql=SELECT SUM(area_extent) as value FROM data WHERE {location} AND thresh = {threshold} AND polyname = '{indicator}'"
 };
 
-export const getExtent = (country, region, subRegion, indicator, threshold) => {
+export const getExtent = (
+  country,
+  region,
+  subRegion,
+  indicator,
+  threshold = 30
+) => {
   const url = `${APIURL}${APIURLS.getExtent}`
-    .replace('{dataset}', datasets[indicator].extent)
     .replace(
       '{location}',
       `iso = '${country}'${region ? ` AND adm1 = ${region}` : ''}${
         region ? ` AND adm2 = ${subRegion}` : ''
       }`
     )
-    .replace('{threshold}', threshold);
+    .replace('{threshold}', threshold)
+    .replace('{indicator}', 'gadm28');
   return axios.get(url);
 };
