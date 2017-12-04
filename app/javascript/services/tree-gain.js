449b4fb1fb2080a48450906115d5d952c969fd5c
pjosh <jorgegar@gmail.com>
2017-12-04 16:11:24 +0100

    proper gain call

--- app/javascript/services/tree-gain.js
+++ app/javascript/services/tree-gain.js
@@ -1,30 +1,29 @@
 import axios from 'axios';
+import datasets from './datasets.json';
 
 const CONFIG = {
   treeGain: 'fd3b3a12-121e-4189-96a3-d8c26b70952d'
 };
 
-const APIURL = process.env.GFW_API_AUTH;
+const APIURL = process.env.GFW_API_HOST_NEW_API;
 
 const APIURLS = {
   treeCoverGain:
-    "/query?sql=select sum(area) as value from {dataset} WHERE iso='{admin0}' AND year >= {minYear} AND year <= {maxYear} AND thresh >= {threshValue} {groupBy}",
+    '/query?sql=SELECT SUM(area) as value FROM {dataset} WHERE {location} AND thresh = 0',
   treeCoverGainAllCountries:
     '/query?sql=select sum(area) as value from {dataset} WHERE year >= {minYear} AND year <= {maxYear} AND thresh >= {threshValue}',
   treeCoverGainRegion:
     "/query?sql=select sum(area) as value from {dataset} WHERE iso='{admin0}' AND year >= {minYear} AND year <= {maxYear} AND thresh >= {threshValue} GROUP by adm1 ORDER BY value DESC LIMIT 10"
 };
 
-export const getTreeCoverGain = (admin0, admin1, years, thresh) => {
+export const getTreeGain = (country, region, subRegion, indicator) => {
   const url = `${APIURL}${APIURLS.treeCoverGain}`
-    .replace('{dataset}', CONFIG.treeGain)
-    .replace('{admin0}', admin0)
-    .replace('{minYear}', years.minYear)
-    .replace('{maxYear}', years.maxYear)
-    .replace('{threshValue}', thresh)
+    .replace('{dataset}', datasets[indicator].gain)
     .replace(
-      '{groupBy}',
-      admin1 ? `AND adm1 = ${admin1} GROUP BY iso, adm1` : ''
+      '{location}',
+      `iso = '${country}'${region ? ` AND adm1 = ${region}` : ''}${
+        region ? ` AND adm2 = ${subRegion}` : ''
+      }`
     );
   return axios.get(url);
 };
