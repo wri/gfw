4714edaf8e1081634de436f2c1c9d338d487cb33
Edward Brett <e.j.a.brett@gmail.com>
2017-12-05 15:54:01 +0100

    WIP

--- app/javascript/services/forest-data.js
+++ app/javascript/services/forest-data.js
@@ -12,12 +12,12 @@ const SQL_QUERIES = {
     "SELECT sum(area) as area, sum(emissions) as emissions FROM data WHERE {location} AND thresh = {threshold} AND polyname = '{indicator}'"
 };
 
-const getLocationQuery = (country, region, subRegion) =>
+const getLocationQuery = ({ country, region, subRegion }) =>
   `iso = '${country}'${region ? ` AND adm1 = ${region}` : ''}${
     subRegion ? ` AND adm2 = ${subRegion}` : ''
   }`;
 
-export const getExtent = (country, region, subRegion, indicator, threshold) => {
+export const getExtent = ({ country, region, subRegion, indicator, threshold }) => {
   const url = `${REQUEST_URL}${SQL_QUERIES.treeExtent}`
     .replace('{location}', getLocationQuery(country, region, subRegion))
     .replace('{threshold}', threshold)
@@ -25,14 +25,14 @@ export const getExtent = (country, region, subRegion, indicator, threshold) => {
   return axios.get(url);
 };
 
-export const getGain = (country, region, subRegion, indicator) => {
+export const getGain = ({ country, region, subRegion, indicator }) => {
   const url = `${REQUEST_URL}${SQL_QUERIES.treeGain}`
     .replace('{location}', getLocationQuery(country, region, subRegion))
     .replace('{indicator}', indicator);
   return axios.get(url);
 };
 
-export const getLoss = (country, region, subRegion, indicator, threshold) => {
+export const getLoss = ({ country, region, subRegion, indicator, threshold }) => {
   const url = `${REQUEST_URL}${SQL_QUERIES.getLoss}`
     .replace('{location}', getLocationQuery(country, region, subRegion))
     .replace('{threshold}', threshold)
