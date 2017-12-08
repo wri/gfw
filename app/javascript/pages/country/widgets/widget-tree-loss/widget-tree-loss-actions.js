49744d2c9d2239bfb6bbf0eeddd6aac3acc0b39d
Edward Brett <e.j.a.brett@gmail.com>
2017-12-08 12:31:50 +0100

    migrate loss to new format, apply selectors for years

--- app/javascript/pages/country/widgets/widget-tree-loss/widget-tree-loss-actions.js
+++ app/javascript/pages/country/widgets/widget-tree-loss/widget-tree-loss-actions.js
@@ -1,16 +1,38 @@
 import { createAction } from 'redux-actions';
+import { createThunkAction } from 'utils/redux';
+import axios from 'axios';
+
+import { getExtent, getLoss } from 'services/forest-data';
+
+const setTreeLossIsLoading = createAction('setTreeLossIsLoading');
 
 const setTreeLossValues = createAction('setTreeLossValues');
 const setTreeLossSettingsIndicator = createAction(
   'setTreeLossSettingsIndicator'
 );
 const setTreeLossSettingsCanopy = createAction('setTreeLossSettingsCanopy');
-const setTreeLossIsLoading = createAction('setTreeLossIsLoading');
 const setTreeLossSettingsStartYear = createAction(
   'setTreeLossSettingsStartYear'
 );
 const setTreeLossSettingsEndYear = createAction('setTreeLossSettingsEndYear');
 const setLayers = createAction('setLayers');
+
+const getTreeLoss = createThunkAction(
+  'getTreeLoss',
+  params => (dispatch, state) => {
+    if (!state().widgetTreeLoss.isLoading) {
+      dispatch(setTreeLossIsLoading(true));
+      axios.all([getExtent(params), getLoss(params)]).then(
+        axios.spread((loss, extent) => {
+          setTreeLossValues({
+            loss: loss.data.data,
+            extent: extent.data.data[0].value
+          });
+        })
+      );
+    }
+  }
+);
 
 export default {
   setTreeLossValues,
@@ -19,5 +41,6 @@ export default {
   setTreeLossIsLoading,
   setTreeLossSettingsStartYear,
   setTreeLossSettingsEndYear,
-  setLayers
+  setLayers,
+  getTreeLoss
 };
--- app/javascript/pages/country/widgets/widget-tree-loss/widget-tree-loss-reducers.js
+++ app/javascript/pages/country/widgets/widget-tree-loss/widget-tree-loss-reducers.js
@@ -1,108 +1,13 @@
 export const initialState = {
-  isLoading: true,
-  loss: [],
-  lossSentence: '',
-  treeExtent: 0,
-  yearsLoss: [
-    {
-      label: '2001',
-      value: 2001
-    },
-    {
-      label: '2002',
-      value: 2002
-    },
-    {
-      label: '2003',
-      value: 2003
-    },
-    {
-      label: '2004',
-      value: 2004
-    },
-    {
-      label: '2005',
-      value: 2005
-    },
-    {
-      label: '2006',
-      value: 2006
-    },
-    {
-      label: '2007',
-      value: 2007
-    },
-    {
-      label: '2008',
-      value: 2008
-    },
-    {
-      label: '2009',
-      value: 2009
-    },
-    {
-      label: '2010',
-      value: 2010
-    },
-    {
-      label: '2011',
-      value: 2011
-    },
-    {
-      label: '2012',
-      value: 2012
-    },
-    {
-      label: '2013',
-      value: 2013
-    },
-    {
-      label: '2014',
-      value: 2014
-    },
-    {
-      label: '2015',
-      value: 2015
-    },
-    {
-      label: '2016',
-      value: 2016
-    }
-  ],
-  indicators: [
-    {
-      label: 'All Regions',
-      value: 'gadm28_only'
-    },
-    {
-      label: 'Tree Plantations',
-      value: 'gfw_plantations'
-    },
-    {
-      label: 'Managed',
-      value: 'gfw_managed_forests'
-    },
-    {
-      label: 'Protected Areas',
-      value: 'wdpa'
-    },
-    {
-      label: 'Intact Forest Landscapes (2000)',
-      value: 'IFL_2000'
-    },
-    {
-      label: 'Intact Forest Landscapes (2013)',
-      value: 'IFL_2013'
-    },
-    {
-      label: 'Primary Forest',
-      value: 'primary_forests'
-    }
-  ],
+  isLoading: false,
+  data: {
+    loss: [],
+    extent: 0
+  },
   settings: {
     startYear: 2001,
     endYear: 2016,
-    indicator: 'gadm28_only',
+    indicator: 'gadm28',
     canopy: 30
   }
 };
@@ -110,9 +15,7 @@ export const initialState = {
 const setTreeLossValues = (state, { payload }) => ({
   ...state,
   isLoading: false,
-  loss: payload.loss,
-  lossSentence: payload.lossSentence,
-  treeExtent: payload.treeExtent
+  ...payload
 });
 
 const setTreeLossSettingsIndicator = (state, { payload }) => ({
