b198a67da88b50c1f9d05ab861d6f61780be8ce1
Edward Brett <e.j.a.brett@gmail.com>
2017-12-11 16:58:56 +0100

    make pie chart reusable

---
+++ app/javascript/pages/country/widget/components/widget-pie-chart/widget-pie-chart-tooltip-component.jsx
@@ -0,0 +1,22 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { format } from 'd3-format';
+
+class WidgetPieChartTooltip extends PureComponent {
+  render() {
+    const { data } = this.props;
+    const payload = data && data.length && data[0].payload;
+    return payload ? (
+      <div>
+        <p>{payload.name}</p>
+        <span>{format('.3s')(payload.percentage)}%</span>
+      </div>
+    ) : null;
+  }
+}
+
+WidgetPieChartTooltip.propTypes = {
+  data: PropTypes.array
+};
+
+export default WidgetPieChartTooltip;
