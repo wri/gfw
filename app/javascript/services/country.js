14533abac880fd210256c2d263b44421b5d92c1a
pjosh <jorgegar@gmail.com>
2017-12-14 15:12:33 +0100

    simplify REQUEST_URL & unify names

--- app/javascript/services/country.js
+++ app/javascript/services/country.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.CARTO_API_URL;
-const REQUEST_URL = `${API_URL}/sql?q=`;
+const REQUEST_URL = `${process.env.CARTO_API_URL}/sql?q=`;
 
 const SQL_QUERIES = {
   getCountries:
--- app/javascript/services/layer-spec.js
+++ app/javascript/services/layer-spec.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
-const API_URL = process.env.CARTO_API_URL;
+const REQUEST_URL = `${process.env.CARTO_API_URL}/sql?q=`;
 
 const SQL_QUERIES = {
   layerSpec:
-    "/sql?q=SELECT cartodb_id AS id, slug, title, title_color, analyzable, subtitle, sublayer, table_name, source, source_json, category_color, category_slug, is_forest_clearing, category_name, external, iso, zmin, zmax, mindate, maxdate, ST_XMAX(the_geom) AS xmax, ST_XMIN(the_geom) AS xmin, ST_YMAX(the_geom) AS ymax, ST_YMIN(the_geom) AS ymin, tileurl, does_wrapper, user_data, parent_layer, true AS visible FROM {dataset} WHERE display = 'true' ORDER BY displaylayer, title ASC"
+    "SELECT cartodb_id AS id, slug, title, title_color, analyzable, subtitle, sublayer, table_name, source, source_json, category_color, category_slug, is_forest_clearing, category_name, external, iso, zmin, zmax, mindate, maxdate, ST_XMAX(the_geom) AS xmax, ST_XMIN(the_geom) AS xmin, ST_YMAX(the_geom) AS ymax, ST_YMIN(the_geom) AS ymin, tileurl, does_wrapper, user_data, parent_layer, true AS visible FROM {dataset} WHERE display = 'true' ORDER BY displaylayer, title ASC"
 };
 
 export const fetchLayerSpec = () => {
-  const url = `${API_URL}${SQL_QUERIES.layerSpec}`.replace(
+  const url = `${REQUEST_URL}${SQL_QUERIES.layerSpec}`.replace(
     '{dataset}',
     window.gfw.layer_spec
   );
