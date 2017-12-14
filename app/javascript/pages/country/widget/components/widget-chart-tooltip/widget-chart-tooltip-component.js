9833398b5b90a0b21aeef29671a3caf72e659d17
Edward Brett <e.j.a.brett@gmail.com>
2017-12-14 19:44:03 +0100

    calc percentage on selector

--- app/javascript/pages/country/widget/components/widget-chart-tooltip/widget-chart-tooltip-component.js
+++ app/javascript/pages/country/widget/components/widget-chart-tooltip/widget-chart-tooltip-component.js
@@ -13,7 +13,9 @@ class WidgetChartTooltip extends PureComponent {
       <div className="c-widget-chart-tooltip">
         {settings.map(d => (
           <div key={d.key} className="data-line">
-            {format(d.unit === '%' ? '.1f' : '.3s')(values[d.key])}
+            {d.unit
+              ? format(d.unit === '%' ? '.1f' : '.3s')(values[d.key])
+              : values[d.key]}
             {d.unit}
           </div>
         ))}
