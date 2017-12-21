974e176194de9aa03b0aae2c81f9b4d4bf1dbd22
Ed Brett <edbrett@users.noreply.github.com>
2017-12-21 17:19:31 +0000

    Update/bug fixes and design changes (#3175)
    * update gray to tan for non forest, introduce constants
    
    * dynamic sentence colors
    
    * disable green links, update variables to be black for now
    
    * remove canopy density, fix threshold
    
    * fix: null values from api reset widget
    
    * show zero values correctly in tooltip
    
    * FAO fix calcs
    
    * no content placeholder for FAO
    
    * basic structure for widget config
    
    * add tabs for navigating widget config
    
    * only show widget based on config
    
    * gain widget update to config
    
    * tree loss and location reading from config
    
    * update fao extent to use config
    
    * fao forest widget updatE
    
    * add title and anchor link to settings
    
    * remove gaps between donut slices
    
    * fix header sentence when no extent
    
    * dynamic sentence for loss fix
    
    * only show fao on country level
    
    * fix: no threshold actions for gain

--- app/javascript/pages/country/header/header.js
+++ app/javascript/pages/country/header/header.js
@@ -126,13 +126,13 @@ class HeaderContainer extends PureComponent {
     const location = locationNames.current && locationNames.current.label;
     let firstSentence = '';
     let secondSentence = '';
-    if (extent) {
+    if (extent > 0) {
       firstSentence = `
         In 2010, <b>${location}</b> had <b>${extent}ha</b> of tree cover, extending over <b>${percentageCover}%</b> of its land area.
       `;
     } else {
       firstSentence = `
-        In 2010, <b>${location}</b> had <b>${extent}ha</b> of tree cover.
+        In 2010, <b>${location}</b> had no tree cover.
       `;
     }
     if (data.totalLoss.area) {
