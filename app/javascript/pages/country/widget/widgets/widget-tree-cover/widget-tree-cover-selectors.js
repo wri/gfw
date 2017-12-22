dc1a27f010a2ab9e95f766dd3b8b009a5a685bd6
Benjamin Laken <benlaken@gmail.com>
2017-12-22 16:03:00 +0100

    resolve conflicts

--- app/javascript/pages/country/widget/widgets/widget-tree-cover/widget-tree-cover-selectors.js
+++ app/javascript/pages/country/widget/widgets/widget-tree-cover/widget-tree-cover-selectors.js
@@ -2,22 +2,35 @@ import { createSelector } from 'reselect';
 import COLORS from 'pages/country/data/colors.json';
 
 // get list data
-const getTotalCover = state => state.totalArea;
-const getTotalArea = state => state.cover;
-const getPlantationsCover = state => state.plantations;
-const getIndicator = state => state.indicator;
+const getTotalCover = state => state.totalArea || null;
+const getTotalArea = state => state.cover || null;
+const getPlantationsCover = state => state.plantations || null;
+const getIndicator = state => state.indicator || null;
+
+function getNameLabel(indicator, plantations) {
+  if (indicator === 'plantations') {
+    return 'Tree plantations';
+  }
+  return plantations ? 'Natural forest' : 'Tree cover';
+}
 
 // get lists selected
 export const getTreeCoverData = createSelector(
   [getTotalCover, getTotalArea, getPlantationsCover, getIndicator],
   (total, cover, plantations, indicator) => {
     if (!total) return null;
-    const data = [
+    return [
       {
-        name: plantations ? 'Natural forest' : 'Tree cover',
+        name: getNameLabel(indicator, plantations),
         value: cover - plantations,
         color: COLORS.darkGreen,
         percentage: (cover - plantations) / total * 100
+      },
+      {
+        name: 'Tree plantations',
+        value: plantations,
+        color: COLORS.mediumGreen,
+        percentage: plantations / total * 100
       },
       {
         name: 'Non-Forest',
@@ -26,14 +39,5 @@ export const getTreeCoverData = createSelector(
         percentage: (total - cover) / total * 100
       }
     ];
-    if (indicator === 'gadm28') {
-      data.splice(1, 0, {
-        name: 'Tree plantations',
-        value: plantations,
-        color: COLORS.mediumGreen,
-        percentage: plantations / total * 100
-      });
-    }
-    return data;
   }
 );
