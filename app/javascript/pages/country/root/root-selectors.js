06ed81b1ac6b353032f80d6570c0444156080b8e
Edward Brett <e.j.a.brett@gmail.com>
2017-12-22 13:25:20 +0000

    add selector to filter widgets correctly

---
+++ app/javascript/pages/country/root/root-selectors.js
@@ -0,0 +1,25 @@
+import { createSelector } from 'reselect';
+
+import WIDGETS from 'pages/country/data/widgets-config.json';
+
+// get list data
+const getCategory = state => state.category || null;
+const getAdminLevel = state => state.adminLevel || null;
+
+// get lists selected
+export const getWidgets = createSelector(
+  [getCategory, getAdminLevel],
+  (category, adminLevel) => {
+    const widgetKeys = Object.keys(WIDGETS);
+    return widgetKeys
+      .map(key => ({
+        name: key,
+        ...WIDGETS[key]
+      }))
+      .filter(
+        widget =>
+          widget.config.categories.indexOf(category) > -1 &&
+          widget.config.admins.indexOf(adminLevel) > -1
+      );
+  }
+);
