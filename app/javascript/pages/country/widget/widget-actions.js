f2494de09f3a9e579c5267ec007cbacbbc8a9e14
Edward Brett <e.j.a.brett@gmail.com>
2018-01-05 22:44:50 +0100

    resolve conflicts

--- app/javascript/pages/country/widget/widget-actions.js
+++ app/javascript/pages/country/widget/widget-actions.js
@@ -7,13 +7,15 @@ import * as treeCoverActions from 'pages/country/widget/widgets/widget-tree-cove
 import * as treeLocatedActions from 'pages/country/widget/widgets/widget-tree-located/widget-tree-located-actions';
 import * as treeGainActions from 'pages/country/widget/widgets/widget-tree-gain/widget-tree-gain-actions';
 import * as FAOReforestationActions from 'pages/country/widget/widgets/widget-fao-reforestation/widget-fao-reforestation-actions';
+import * as FAOCoverActions from 'pages/country/widget/widgets/widget-fao-cover/widget-fao-cover-actions';
 
 const widgetActions = {
   ...treeLossActions.default,
   ...treeCoverActions.default,
   ...treeLocatedActions.default,
   ...treeGainActions.default,
-  ...FAOReforestationActions.default
+  ...FAOReforestationActions.default,
+  ...FAOCoverActions.default
 };
 
 export const setWidgetSettingsUrl = createThunkAction(
