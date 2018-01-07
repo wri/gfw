2432c028403ba39eff6806c34005118dce416113
Edward Brett <e.j.a.brett@gmail.com>
2018-01-07 18:05:43 +0100

    move category to url for better sharing

--- app/javascript/pages/country/root/root-selectors.js
+++ app/javascript/pages/country/root/root-selectors.js
@@ -1,10 +1,14 @@
 import { createSelector } from 'reselect';
+import compact from 'lodash/compact';
+import qs from 'query-string';
 
 import WIDGETS from 'pages/country/data/widgets-config.json';
 
 // get list data
+const getCategories = state => state.categories || null;
 const getCategory = state => state.category || null;
 const getAdminLevel = state => state.adminLevel || null;
+const getLocation = state => state.location || null;
 
 // get lists selected
 export const getWidgets = createSelector(
@@ -23,4 +27,23 @@ export const getWidgets = createSelector(
           widget.active
       );
   }
+);
+
+export const getLinks = createSelector(
+  [getCategories, getCategory, getLocation],
+  (categories, activeCategory, location) =>
+    categories.map(category => {
+      const locationUrl = compact(
+        Object.keys(location.payload).map(key => location.payload[key])
+      ).join('/');
+      const newQuery = {
+        ...location.query,
+        category: category.value
+      };
+      return {
+        label: category.label,
+        path: `/country/${locationUrl}?${qs.stringify(newQuery)}`,
+        active: activeCategory === category.value
+      };
+    })
 );
