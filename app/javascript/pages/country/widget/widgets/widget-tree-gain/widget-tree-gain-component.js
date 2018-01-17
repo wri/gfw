2df02be1202685356e36c64a64eaf105858416da
pjosh <jorgegar@gmail.com>
2018-01-17 08:24:34 +0100

    remove behaviour managed by the parent widget

--- app/javascript/pages/country/widget/widgets/widget-tree-gain/widget-tree-gain-component.js
+++ app/javascript/pages/country/widget/widgets/widget-tree-gain/widget-tree-gain-component.js
@@ -3,28 +3,17 @@ import PropTypes from 'prop-types';
 
 import WidgetDynamicSentence from 'pages/country/widget/components/widget-dynamic-sentence';
 import WidgetNumberedList from 'pages/country/widget/components/widget-numbered-list';
-import NoContent from 'components/no-content';
 import COLORS from 'pages/country/data/colors.json';
 
 import './widget-tree-gain-styles.scss';
 
 class WidgetTreeCoverGain extends PureComponent {
   render() {
-    const { locationNames, loading, data, settings, getSentence } = this.props;
+    const { data, settings, getSentence } = this.props;
 
     return (
       <div className="c-widget-tree-cover-gain">
-        {!loading &&
-          data &&
-          data.ranking.length === 0 && (
-            <NoContent
-              message={`No data for ${locationNames.current &&
-                locationNames.current.label}`}
-              icon
-            />
-          )}
-        {!loading &&
-          data &&
+        {data &&
           data.ranking.length > 0 && (
             <div className="gain-data">
               <WidgetDynamicSentence sentence={getSentence()} />
@@ -43,8 +32,6 @@ class WidgetTreeCoverGain extends PureComponent {
 }
 
 WidgetTreeCoverGain.propTypes = {
-  loading: PropTypes.bool.isRequired,
-  locationNames: PropTypes.object,
   data: PropTypes.object.isRequired,
   settings: PropTypes.object.isRequired,
   getSentence: PropTypes.func.isRequired
