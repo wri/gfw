2c53741fb50c17d6954d7f54f5eea26518c3a8d0
Ed Brett <e.j.a.brett@gmail.com>
2018-01-22 11:02:01 +0100

    add dynamic sentence

--- app/javascript/pages/country/widget/widgets/widget-tree-cover-plantations/widget-tree-cover-plantations-selectors.js
+++ app/javascript/pages/country/widget/widgets/widget-tree-cover-plantations/widget-tree-cover-plantations-selectors.js
@@ -2,13 +2,13 @@ import { createSelector } from 'reselect';
 import COLORS from 'pages/country/data/colors.json';
 import isEmpty from 'lodash/isEmpty';
 import sumBy from 'lodash/sumBy';
-import { getColorPalette } from 'utils/data';
+import { getColorPalette, sortByKey } from 'utils/data';
+import { format } from 'd3-format';
 
 // get list data
 const getData = state => state.data;
 const getSettings = state => state.settings;
 const getLocationNames = state => state.locationNames;
-const getActiveIndicator = state => state.activeIndicator;
 const getIndicatorWhitelist = state => state.whitelist;
 
 // get lists selected
@@ -22,32 +22,48 @@ export const getTreeCoverPlantationsData = createSelector(
       [COLORS.darkGreen, COLORS.nonForest],
       plantations.length
     );
-    return plantations.map((d, i) => ({
-      label: d[settings.type],
-      value: d.plantation_extent,
-      color: colorRange[i],
-      percentage: d.plantation_extent / totalPlantations * 100
-    }));
+    return sortByKey(
+      plantations.map((d, i) => ({
+        label: d[settings.type],
+        value: d.plantation_extent,
+        color: colorRange[i],
+        percentage: d.plantation_extent / totalPlantations * 100
+      })),
+      'value'
+    );
   }
 );
 
 export const getSentence = createSelector(
-  [getData, getSettings, getLocationNames, getActiveIndicator],
-  (data, settings, locationNames, indicator) => {
-    if (!data) return null;
-    const { totalArea, cover } = data;
-    const coverStatus = cover / totalArea > 0.5 ? 'tree covered' : 'non-forest';
+  [getTreeCoverPlantationsData, getSettings, getLocationNames],
+  (data, settings, locationNames) => {
+    if (isEmpty(data)) return null;
     const locationLabel = locationNames.current && locationNames.current.label;
-    const locationIntro = `${
-      indicator.value !== 'gadm28'
-        ? `<b>${indicator.label}</b> in <b>${locationLabel}</b> are `
-        : `<b>${locationLabel}</b> is `
-    }`;
-    const first = `${locationIntro} mainly ${coverStatus}, `;
-    const second = `considering tree cover extent in <b>${
-      settings.extentYear
-    }</b> where tree canopy is greater than <b>${settings.threshold}%</b>.`;
+    let topTypesSentence = '';
+    let top = null;
+    let remainSentence = '';
+    if (settings.type === 'bound2') {
+      top = data.slice(0, 2);
+      topTypesSentence = `<b>${top[0].label}</b> and <b>${
+        top[1].label
+      }</b> represents the largest plantations by <b>species</b>`;
+      remainSentence = `The remaining <b>${format('.2s')(
+        sumBy(data.slice(2), 'value')
+      )}ha</b> of tree cover is distributed between <b>${data.length -
+        top.length}</b> other plantation species.`;
+    } else {
+      top = data.slice(0, 1);
+      topTypesSentence = `the largest plantation type by area is <b>${
+        top[0].label
+      }</b>`;
+    }
 
-    return `${first} ${second}`;
+    return `In <b>${locationLabel}</b>, ${topTypesSentence} in <b>${
+      settings.extentYear
+    }</b>, spanning <b>${format('.2s')(
+      sumBy(top, 'value')
+    )}ha</b> where the canopy cover is greater than <b>${
+      settings.threshold
+    }%</b>. ${remainSentence}`;
   }
 );
