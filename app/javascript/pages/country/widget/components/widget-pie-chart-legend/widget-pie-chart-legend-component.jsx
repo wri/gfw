f8db9b5f9735b76320ebcb14b6316aad0becd149
Ed Brett <e.j.a.brett@gmail.com>
2018-01-23 12:41:11 +0100

    update widget pie legend to handle large numbers

--- app/javascript/pages/country/widget/components/widget-pie-chart-legend/widget-pie-chart-legend-component.jsx
+++ app/javascript/pages/country/widget/components/widget-pie-chart-legend/widget-pie-chart-legend-component.jsx
@@ -7,25 +7,34 @@ import './widget-pie-chart-legend-styles.scss';
 class WidgetPieChartLegend extends PureComponent {
   render() {
     const { data, config, className } = this.props;
+    let sizeClass = '';
+    if (data.length > 5) {
+      sizeClass = 'x-small';
+    } else if (data.length > 3) {
+      sizeClass = 'small';
+    }
 
     return (
-      <ul
-        className={`c-pie-chart-legend ${className} ${
-          data.length > 4 ? 'small' : ''
-        }`}
-      >
-        {data.map((item, index) => (
-          <li key={index.toString()}>
-            <div className="legend-title">
-              <span style={{ backgroundColor: item.color }}>{}</span>
-              <p>{item.label}</p>
-            </div>
-            <div className="legend-value" style={{ color: item.color }}>
-              {format(config.format)(item[config.key])}
-              {config.unit}
-            </div>
-          </li>
-        ))}
+      <ul className={`c-pie-chart-legend ${className} ${sizeClass}`}>
+        {data.map((item, index) => {
+          const value = `${format(config.format)(item[config.key])}${
+            config.unit
+          }`;
+          return (
+            <li className="legend-item" key={index.toString()}>
+              <div className="legend-title">
+                <span style={{ backgroundColor: item.color }}>{}</span>
+                <p>
+                  {item.label}
+                  {data.length > 5 && ` - ${value}`}
+                </p>
+              </div>
+              <div className="legend-value" style={{ color: item.color }}>
+                {value}
+              </div>
+            </li>
+          );
+        })}
       </ul>
     );
   }
