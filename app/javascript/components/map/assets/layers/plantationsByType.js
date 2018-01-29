6427c203ded1cb06c45c9c61647aefc8b72aba3c
Ed Brett <e.j.a.brett@gmail.com>
2018-01-29 17:04:15 +0000

    plantations by type layer

---
+++ app/javascript/components/map/assets/layers/plantationsByType.js
@@ -0,0 +1,21 @@
+import CartoDB from './abstract/cartoDB';
+import PlantationsByTypeCartoCSS from '../cartocss/plantations_by_type.cartocss';
+
+const OPTIONS = {
+  sql:
+    "SELECT the_geom_webmercator, cartodb_id, type_text, spec_org, spec_simp, percent, round(area_ha::numeric,1) as area_ha, '{tableName}' AS tablename, '{tableName}' AS layer, {analysis} AS analysis FROM {tableName}",
+  cartocss: PlantationsByTypeCartoCSS,
+  infowindow: true,
+  interactivity:
+    'cartodb_id, tablename, layer, analysis, type_text, spec_org, spec_simp, percent, area_ha',
+  analysis: true
+};
+
+class PlantationsByType extends CartoDB {
+  constructor(map, options) {
+    super(map, OPTIONS);
+    this.options = Object.assign({}, this.options, options);
+  }
+}
+
+export default PlantationsByType;
