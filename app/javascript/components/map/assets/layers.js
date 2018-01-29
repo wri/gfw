eb9338a8486801ebc17749e0d9d86d933725657e
Ed Brett <e.j.a.brett@gmail.com>
2018-01-29 12:21:05 +0000

    update forest 2010 layer

--- app/javascript/components/map/assets/layers.js
+++ app/javascript/components/map/assets/layers.js
@@ -1,9 +1,11 @@
 import Forest2000 from './layers/forest2000';
+import Forest2010 from './layers/forest2010';
 import IntactForest from './layers/intact-forest';
 import Loss from './layers/loss';
 
 const layersMap = {
   forest2000: Forest2000,
+  forest2010: Forest2010,
   ifl_2013_deg: IntactForest,
   loss: Loss
 };
