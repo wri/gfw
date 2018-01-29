47eb369ec0b9338fa2eccf6df74553c842e5b3b2
pjosh <jorgegar@gmail.com>
2018-01-29 17:30:49 +0000

    resolve conflicts

--- app/javascript/components/map/assets/layers.js
+++ app/javascript/components/map/assets/layers.js
@@ -3,7 +3,7 @@ import Forest2010 from './layers/forest2010';
 import ForestGain from './layers/forestGain';
 import IntactForest from './layers/intact-forest';
 import Loss from './layers/loss';
-import plantationsByType from './layers/plantationsByType';
+import PlantationsBySpecies from './layers/plantations-by-species';
 
 const layersMap = {
   forest2000: Forest2000,
@@ -11,7 +11,7 @@ const layersMap = {
   forestGain: ForestGain,
   ifl_2013_deg: IntactForest,
   loss: Loss,
-  plantations_by_type: plantationsByType
+  plantations_by_species: PlantationsBySpecies
 };
 
 export default layersMap;
