e0f3800fccf430382c92e1d53967ba3af62c6290
pjosh <jorgegar@gmail.com>
2018-01-30 11:01:32 +0000

    fix file naming

--- app/javascript/components/map/assets/layers.js
+++ app/javascript/components/map/assets/layers.js
@@ -1,9 +1,9 @@
 import Forest2000 from './layers/forest2000';
 import Forest2010 from './layers/forest2010';
-import ForestGain from './layers/forestGain';
+import ForestGain from './layers/forest-gain';
 import IntactForest from './layers/intact-forest';
 import Loss from './layers/loss';
-import plantationsByType from './layers/plantationsByType';
+import PlantationsByType from './layers/plantations-by-type';
 import PlantationsBySpecies from './layers/plantations-by-species';
 import Glad from './layers/glad';
 import Viirs from './layers/viirs';
@@ -16,7 +16,7 @@ const layersMap = {
   forestGain: ForestGain,
   ifl_2013_deg: IntactForest,
   loss: Loss,
-  plantations_by_type: plantationsByType,
+  plantations_by_type: PlantationsByType,
   plantations_by_species: PlantationsBySpecies,
   umd_as_it_happens: Glad,
   viirs_fires_alerts: Viirs,
