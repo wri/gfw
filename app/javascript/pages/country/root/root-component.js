6344fd3992ca32df50dae0b0f6b9cd519e689fdd
Ed Brett <e.j.a.brett@gmail.com>
2018-01-30 16:33:58 +0000

    add highlight area when changing location

--- app/javascript/pages/country/root/root-component.js
+++ app/javascript/pages/country/root/root-component.js
@@ -36,7 +36,8 @@ class Root extends PureComponent {
       locationNames,
       category,
       loading,
-      widgetAnchor
+      widgetAnchor,
+      locationGeoJson
     } = this.props;
 
     return (
@@ -108,6 +109,7 @@ class Root extends PureComponent {
                   center: { lat: -34.397, lng: 150.644 },
                   zoom: 8
                 }}
+                areaHighlight={locationGeoJson}
                 isParentLoading={isGeostoreLoading}
               />
             </Sticky>
@@ -135,7 +137,8 @@ Root.propTypes = {
   category: PropTypes.string,
   locationOptions: PropTypes.object,
   locationNames: PropTypes.object,
-  widgetAnchor: PropTypes.oneOfType([PropTypes.string, PropTypes.object])
+  widgetAnchor: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  locationGeoJson: PropTypes.object
 };
 
 export default Root;
