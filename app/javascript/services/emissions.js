d564e8f35667e15c12aed4cd63188a19104280fa
pjosh <jorgegar@gmail.com>
2018-01-31 10:20:45 +0000

    change emissions params by env

--- app/javascript/services/emissions.js
+++ app/javascript/services/emissions.js
@@ -5,6 +5,8 @@ const REQUEST_URL = `${
 }/emissions?gas=107&location={adm0}&source=25`;
 
 export const getEmissions = ({ country }) => {
-  const url = REQUEST_URL.replace('{adm0}', country);
+  const url = REQUEST_URL.replace('{adm0}', country)
+    .replace('{gas}', process.env.RAILS_ENV === 'production' ? 107 : 253)
+    .replace('{source}', process.env.RAILS_ENV === 'production' ? 25 : 60);
   return axios.get(url);
 };
