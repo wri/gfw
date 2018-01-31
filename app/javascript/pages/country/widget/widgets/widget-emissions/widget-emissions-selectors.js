bfc3ec5eb627b7d7a91a6c4462067589d87a4711
pjosh <jorgegar@gmail.com>
2018-01-31 16:17:08 +0000

    resolve conflicts

--- app/javascript/pages/country/widget/widgets/widget-emissions/widget-emissions-selectors.js
+++ app/javascript/pages/country/widget/widgets/widget-emissions/widget-emissions-selectors.js
@@ -1,46 +1,136 @@
 import { createSelector } from 'reselect';
 import isEmpty from 'lodash/isEmpty';
-import sumBy from 'lodash/sumBy';
+import { format } from 'd3-format';
+import { getColorPalette } from 'utils/data';
+
+const EMISSIONS_KEYS = [
+  'Total including LUCF',
+  'Land-Use Change and Forestry',
+  'Agriculture'
+];
 
 // get list data
 const getData = state => state.data || null;
-const getLocation = state => state.location || null;
+const getLocationNames = state => state.locationNames || null;
 const getColors = state => state.colors || null;
 
-const getSortedData = createSelector(
-  [getData, getLocation, getColors],
-  (data, location, colors) => {
-    if (!data || isEmpty(data)) return null;
+const getSortedData = createSelector([getData], data => {
+  if (!data || isEmpty(data)) return null;
 
-    console.log(data);
-    console.log(location);
-    console.log(colors);
-    return null;
+  const sortedData = {
+    data: [],
+    total: {}
+  };
+  Object.keys(data)
+    .filter(key => EMISSIONS_KEYS.includes(data[key].sector))
+    .forEach(key => {
+      if (data[key].sector === 'Total including LUCF') {
+        sortedData.total = data[key];
+      } else {
+        sortedData.data.push(data[key]);
+      }
+    });
+  return sortedData;
+});
+
+export const getChartData = createSelector([getSortedData], sortedData => {
+  if (!sortedData || !sortedData.data.length) return null;
+
+  const { data, total } = sortedData;
+  const chartData = [];
+  data[0].emissions.forEach((item, i) => {
+    chartData.push({
+      year: item.year,
+      e1_value: item.value,
+      e1_percentage: item.value / total.emissions[i].value * 100,
+      e2_value: data[1].emissions[i].value,
+      e2_percentage: data[1].emissions[i].value / total.emissions[i].value * 100
+    });
+  });
+  return chartData;
+});
+
+export const chartConfig = createSelector(
+  [getChartData, getColors],
+  (data, colors) => {
+    if (!data) return null;
+
+    const colorRange = getColorPalette(colors.ramp, 2);
+    return {
+      xKey: 'year',
+      yKeys: {
+        areas: {
+          e1_value: {
+            fill: colorRange[0],
+            stroke: colorRange[0],
+            opacity: 1,
+            strokeWidth: 0,
+            background: false,
+            activeDot: false
+          },
+          e2_value: {
+            fill: colorRange[1],
+            stroke: colorRange[1],
+            opacity: 1,
+            strokeWidth: 0,
+            background: false,
+            activeDot: false
+          }
+        }
+      },
+      tooltip: [
+        {
+          key: 'e1_percentage',
+          unit: '%',
+          label: 'Agriculture',
+          color: colorRange[0]
+        },
+        {
+          key: 'e2_percentage',
+          unit: '%',
+          label: 'Land-Use Change and Forestry',
+          color: colorRange[1]
+        }
+      ]
+    };
   }
 );
 
-export const getChartData = createSelector([getSortedData], data => {
-  if (!data || !data.length) return null;
-  const topRegions = data.length > 10 ? data.slice(0, 10) : data;
-  const totalExtent = sumBy(data, 'extent');
-  const otherRegions = data.length > 10 ? data.slice(10) : [];
-  const othersExtent = otherRegions.length && sumBy(otherRegions, 'extent');
-  const otherRegionsData = otherRegions.length
-    ? {
-      label: 'Other regions',
-      percentage: othersExtent ? othersExtent / totalExtent * 100 : 0,
-      color: otherRegions[0].color
-    }
-    : {};
-
-  return [...topRegions, otherRegionsData];
-});
-
 export const getSentence = createSelector(
-  [getSortedData, getLocation],
-  (data, location) => {
-    if (!data) return '';
-    const sentence = '';
+  [getSortedData, getLocationNames],
+  (sortedData, locationNames) => {
+    if (!sortedData || !sortedData.data.length) return '';
+
+    const { data, total } = sortedData;
+    const currentLocation =
+      locationNames && locationNames.current && locationNames.current.label;
+    const emissionsCount = data.reduce((accumulator, item) => {
+      const accumulatorCount =
+        typeof accumulator !== 'object'
+          ? accumulator
+          : accumulator.emissions
+            .map(a => a.value)
+            .reduce((iSum, value) => iSum + value);
+      const itemCount = item.emissions
+        .map(a => a.value)
+        .reduce((iSum, value) => iSum + value);
+      return accumulatorCount + itemCount;
+    });
+    const totalEmissionsCount = total.emissions.reduce(
+      (accumulator, item) =>
+        (typeof accumulator !== 'object' ? accumulator : accumulator.value) +
+        item.value
+    );
+    const emissionFraction = emissionsCount / totalEmissionsCount * 100;
+    const sentence = `In <b>${currentLocation}</b>, land-use change and forestry combined with agriculture contributed <b>${format(
+      '.3s'
+    )(emissionsCount)}tCO₂e</b> of emissions emissions from <b>${
+      data[0].emissions[0].year
+    }–${data[0].emissions[data[0].emissions.length - 1].year}</b>, <b>${format(
+      '.0f'
+    )(
+      emissionFraction
+    )}%</b> of <b>${currentLocation}'s</b> total over this period.`;
 
     return sentence;
   }
