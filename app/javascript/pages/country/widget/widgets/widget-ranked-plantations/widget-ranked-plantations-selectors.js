bb15fcd2a713d881bbdeb110cbdd0579f8d40fc3
Adam J. Pain <aj.pain89@gmail.com>
2018-01-31 10:09:40 +0000

    Updates dynamic sentence for consistency between type and species

--- app/javascript/pages/country/widget/widgets/widget-ranked-plantations/widget-ranked-plantations-selectors.js
+++ app/javascript/pages/country/widget/widgets/widget-ranked-plantations/widget-ranked-plantations-selectors.js
@@ -116,6 +116,14 @@ export const getSentence = createSelector(
       )}%</b>, most of which is in <b>${plantationLabel}${
         isPlural ? '' : 's'
       }</b>.`;
+    } else if (type === 'bound2') {
+      sentence = `<b>${
+        topRegion.region
+      }</b> has the largest relative tree cover due to plantations in <b>${currentLocation}</b>${
+        location.payload.region ? ' ' : ''
+      } at <b>${format('.1f')(data[0].total)}%</b>, most of which is in <b>${
+        topPlantation.label
+      } plantations</b>.`;
     } else {
       sentence = `Within <b>${currentLocation}</b>, <b>${(topRegion.region &&
         topRegion.region) ||
