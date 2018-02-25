16693d5097138b2dbf2e58de75ad42d391565377
Ed Brett <e.j.a.brett@gmail.com>
2018-02-25 21:12:38 +0100

    awards

--- app/javascript/pages/about/section-impacts/section-impacts-component.jsx
+++ app/javascript/pages/about/section-impacts/section-impacts-component.jsx
@@ -9,7 +9,7 @@ import './section-impacts-styles.scss';
 class SectionImpacts extends PureComponent {
   // eslint-disable-line react/prefer-stateless-function
   render() {
-    const { data } = this.props;
+    const { data, awards } = this.props;
     return (
       <section className="l-section-impacts">
         <div className="row">
@@ -25,13 +25,29 @@ class SectionImpacts extends PureComponent {
               </div>
             ))}
         </Slider>
+        <div className="row awards">
+          <div className="column small-12">
+            <h3>Awards</h3>
+          </div>
+          {awards.map(l => (
+            <a
+              key={l.title}
+              className="columns small-6 medium-4 large-3"
+              href={l.link}
+              target="_blank"
+            >
+              <img alt={l.title} src={l.img} />
+            </a>
+          ))}
+        </div>
       </section>
     );
   }
 }
 
 SectionImpacts.propTypes = {
-  data: PropTypes.array.isRequired
+  data: PropTypes.array.isRequired,
+  awards: PropTypes.array.isRequired
 };
 
 export default SectionImpacts;
