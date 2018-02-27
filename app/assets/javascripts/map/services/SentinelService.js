06116bdde872b429166e6b5d05af3ed360e3ebb8
pjosh <jorgegar@gmail.com>
2018-02-27 08:54:55 +0100

    fix sentinel requests

--- app/assets/javascripts/map/services/SentinelService.js
+++ app/assets/javascripts/map/services/SentinelService.js
@@ -1,18 +1,18 @@
-define([
-  'Class',
-  'uri',
-  'bluebird',
-  'map/services/DataService'
-], function(Class, UriTemplate, Promise, ds) {
-
+/* eslint-disable */
+define(['Class', 'uri', 'bluebird', 'map/services/DataService'], function(
+  Class,
+  UriTemplate,
+  Promise,
+  ds
+) {
   'use strict';
 
   var GET_REQUEST_SENTINEL_TILES_ID = 'SentinelService:getTiles';
 
-  var APIURL = 'https://staging-api.globalforestwatch.org/v1';
+  var APIURL = 'https://production-api.globalforestwatch.org/v1';
 
   var APIURLS = {
-    'getTiles': '/sentinel-tiles?lat={lat}&lon={lon}&start={start}&end={end}'
+    getTiles: '/sentinel-tiles?lat={lat}&lon={lon}&start={start}&end={end}'
   };
 
   var SentinelService = Class.extend({
@@ -21,46 +21,48 @@ define([
     },
 
     getTiles: function(lat, lon, start, end) {
-      return new Promise(function(resolve, reject) {
-        var url = new UriTemplate(APIURLS.getTiles).fillFromObject({
-          lat: lat,
-          lon: lon,
-          start: start,
-          end: end
-        });
+      return new Promise(
+        function(resolve, reject) {
+          var url = new UriTemplate(APIURLS.getTiles).fillFromObject({
+            lat: lat,
+            lon: lon,
+            start: start,
+            end: end
+          });
 
-        var requestId = GET_REQUEST_SENTINEL_TILES_ID + '_' + lat + '_' + lon;
-        this.defineRequest(
-          requestId,
-          APIURL + url,
-          { type: 'persist', duration: 1, unit: 'days' }
-        );
+          var requestId = GET_REQUEST_SENTINEL_TILES_ID + '_' + lat + '_' + lon;
+          this.defineRequest(requestId, APIURL + url, {
+            type: 'persist',
+            duration: 1,
+            unit: 'days'
+          });
 
-        var requestConfig = {
-          resourceId: requestId,
-          success: function(res, status) {
-            resolve(res.data, status);
-          },
-          error: function(errors) {
-            reject(errors);
-          }
-        };
+          var requestConfig = {
+            resourceId: requestId,
+            success: function(res, status) {
+              resolve(res.data, status);
+            },
+            error: function(errors) {
+              reject(errors);
+            }
+          };
 
-        this.abortRequest(requestId);
-        this.currentRequest[requestId] = ds.request(requestConfig);
-      }.bind(this));
+          this.abortRequest(requestId);
+          this.currentRequest[requestId] = ds.request(requestConfig);
+        }.bind(this)
+      );
     },
 
-    defineRequest: function (id, url, cache) {
+    defineRequest: function(id, url, cache) {
       ds.define(id, {
         cache: cache,
         url: url,
         type: 'GET',
         dataType: 'json',
         contentType: 'application/json; charset=utf-8',
-        decoder: function ( data, status, xhr, success, error ) {
-          if ( status === "success" ) {
-            success( data, xhr );
+        decoder: function(data, status, xhr, success, error) {
+          if (status === 'success') {
+            success(data, xhr);
           }
         }
       });
@@ -75,9 +77,7 @@ define([
         this.currentRequest[request] = null;
       }
     }
-
   });
 
   return new SentinelService();
-
 });
