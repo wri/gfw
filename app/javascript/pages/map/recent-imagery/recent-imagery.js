1b473af7fc1e4e6ac23147a03751be08f6e7a2d3
pjosh <jorgegar@gmail.com>
2018-02-28 20:34:01 +0100

    action improvements & polygon overlay

--- app/javascript/pages/map/recent-imagery/recent-imagery.js
+++ app/javascript/pages/map/recent-imagery/recent-imagery.js
@@ -8,35 +8,123 @@ import reducers, { initialState } from './recent-imagery-reducers';
 import RecentImageryComponent from './recent-imagery-component';
 
 const mapStateToProps = ({ recentImagery }) => {
-  const { enabled, needEvents } = recentImagery;
+  const { enabled, data } = recentImagery;
   return {
     enabled,
-    needEvents
+    data
   };
 };
 
 class RecentImageryContainer extends PureComponent {
   componentDidMount() {
     this.middleView = window.App.Views.ReactMapMiddleView;
+    this.boundsPolygon = null;
   }
 
   componentWillReceiveProps(nextProps) {
-    const { enabled, needEvents, setLayer, setEvents } = nextProps;
+    const { enabled, data, getTiles } = nextProps;
     const { map } = this.middleView;
-    if (enabled && !isEqual(enabled, this.props.enabled)) {
-      setLayer({
-        middleView: this.middleView,
+    if (enabled && enabled !== this.props.enabled) {
+      getTiles({
         latitude: map.getCenter().lng(),
         longitude: map.getCenter().lat(),
         start: '2016-01-01',
         end: '2016-09-01'
       });
     }
-    if (needEvents && !isEqual(needEvents, this.props.setEvents)) {
-      setEvents({
-        middleView: this.middleView
-      });
+    if (!enabled && enabled !== this.props.enabled) {
+      this.removeEvents();
+      this.removeBoundsPolygon();
     }
+    if (!isEqual(data, this.props.data)) {
+      if (this.props.data.url === '') {
+        this.showLayer(data.url);
+        this.setEvents();
+      } else {
+        this.updateLayer(data.url);
+      }
+      this.addBoundsPolygon(data.bounds);
+    }
+  }
+
+  setEvents() {
+    const { getTiles } = this.props;
+    const { map } = this.middleView;
+    map.addListener('dragend', () => {
+      const needNewTile = !google.maps.geometry.poly.containsLocation(
+        // eslint-disable-line
+        map.getCenter(),
+        this.boundsPolygon
+      );
+      if (needNewTile) {
+        getTiles({
+          latitude: map.getCenter().lng(),
+          longitude: map.getCenter().lat(),
+          start: '2016-01-01',
+          end: '2016-09-01'
+        });
+      }
+    });
+  }
+
+  removeEvents() {
+    const { map } = this.middleView;
+    google.maps.event.clearListeners(map, 'dragend'); // eslint-disable-line
+  }
+
+  showLayer(url) {
+    this.middleView.toggleLayer('sentinel_tiles', {
+      urlTemplate: url
+    });
+  }
+
+  updateLayer(url) {
+    this.middleView.updateLayer('sentinel_tiles', {
+      urlTemplate: url
+    });
+  }
+
+  addBoundsPolygon(bounds) {
+    const { map } = this.middleView;
+
+    if (this.boundsPolygon !== null) {
+      this.removeBoundsPolygon();
+    }
+    const coords = [];
+    bounds.forEach(item => {
+      coords.push({
+        lat: item[1],
+        lng: item[0]
+      });
+    });
+    this.boundsPolygon = new google.maps.Polygon({ // eslint-disable-line
+      paths: coords,
+      fillColor: 'transparent',
+      strokeWeight: 0
+    });
+    google.maps.event.addListener(this.boundsPolygon, 'mouseover', function () { // eslint-disable-line
+      this.setOptions({
+        fillColor: '#000000',
+        fillOpacity: 0.1,
+        strokeColor: '#000000',
+        strokeOpacity: 0.5,
+        strokeWeight: 1
+      });
+    });
+    google.maps.event.addListener(this.boundsPolygon, 'mouseout', function () { // eslint-disable-line
+      this.setOptions({
+        fillColor: 'transparent',
+        strokeWeight: 0
+      });
+    });
+    google.maps.event.addListener(this.boundsPolygon, 'click', (e) => { // eslint-disable-line
+      console.log(e); // eslint-disable-line
+    });
+    this.boundsPolygon.setMap(map);
+  }
+
+  removeBoundsPolygon() {
+    this.boundsPolygon.setMap(null);
   }
 
   render() {
@@ -48,9 +136,8 @@ class RecentImageryContainer extends PureComponent {
 
 RecentImageryContainer.propTypes = {
   enabled: PropTypes.bool,
-  needEvents: PropTypes.bool,
-  setLayer: PropTypes.func,
-  setEvents: PropTypes.func
+  data: PropTypes.object,
+  getTiles: PropTypes.func
 };
 
 export { actions, reducers, initialState };
