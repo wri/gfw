0bfdc1953b1f8874ddde1b7795d2c5c589cba406
Ed Brett <e.j.a.brett@gmail.com>
2018-03-01 18:16:40 +0100

    fix about images for awards

--- app/javascript/pages/about/section-impacts/section-impacts.js
+++ app/javascript/pages/about/section-impacts/section-impacts.js
@@ -1,28 +1,33 @@
 import { connect } from 'react-redux';
 
+import awards0 from 'pages/about/section-impacts/images/awards.png';
+import awards1 from 'pages/about/section-impacts/images/awards1.png';
+import awards2 from 'pages/about/section-impacts/images/awards2.png';
+import awards3 from 'pages/about/section-impacts/images/awards3.png';
+
 import Component from './section-impacts-component';
 import actions from './section-impacts-actions';
 import reducers, { initialState } from './section-impacts-reducers';
 
 const awards = [
   {
-    img: '/assets/backgrounds/awards.png',
+    img: awards0,
     link: 'http://events.esri.com/conference/sagList/',
     title: 'SAG list'
   },
   {
-    img: '/assets/backgrounds/awards2.png',
+    img: awards1,
     link:
       'http://www.unglobalpulse.org/big-data-climate-challenge-winners-announced',
     title: 'Big data climate challenge'
   },
   {
-    img: '/assets/backgrounds/awards3.png',
+    img: awards2,
     link: 'http://www.socialtech.org.uk/projects/global-forest-watch/',
     title: 'Social tech'
   },
   {
-    img: '/assets/backgrounds/awards1.png',
+    img: awards3,
     link:
       'http://www.computerworld.com/article/2977562/data-analytics/world-resources-institute.html',
     title: 'WRI'
