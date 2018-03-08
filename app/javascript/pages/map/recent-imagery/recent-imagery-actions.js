d9e61e357fe95c2bcd763fe04bc6bad3834143f5
pjosh <jorgegar@gmail.com>
2018-03-08 11:44:04 +0100

    fetch more tiles & thumbnails

--- app/javascript/pages/map/recent-imagery/recent-imagery-actions.js
+++ app/javascript/pages/map/recent-imagery/recent-imagery-actions.js
@@ -1,7 +1,8 @@
 import { createAction } from 'redux-actions';
 import { createThunkAction } from 'utils/redux';
+import findIndex from 'lodash/findIndex';
 
-import { getRecentTiles, getTiles } from 'services/recent-imagery';
+import { getRecentTiles, getTiles, getThumbs } from 'services/recent-imagery';
 
 const setRecentImageryData = createAction('setRecentImageryData');
 const toogleRecentImagery = createAction('toogleRecentImagery');
@@ -16,7 +17,7 @@ const getData = createThunkAction('getData', params => dispatch => {
   getRecentTiles(params)
     .then(response => {
       if (response.data.data.length) {
-        dispatch(setRecentImageryData(response.data.data));
+        dispatch(setRecentImageryData({ data: response.data.data }));
       }
     })
     .catch(error => {
@@ -24,28 +25,39 @@ const getData = createThunkAction('getData', params => dispatch => {
     });
 });
 
-const getMoreTiles = createThunkAction('getMoreTiles', params => dispatch => {
+const getMoreTiles = createThunkAction('getMoreTiles', params => (dispatch, state) => {
   getTiles(params)
-    .then(response => {
-      if (response.data.data.length) {
-        const data = response.data.data[0].attributes;
-        const sources = [];
-        response.data.data.forEach((item, i) => {
-          if (i !== 0) {
-            sources.push(item.attributes.source);
+    .then(getTilesResponse => {
+      getThumbs(params)
+        .then(getThumbsResponse => {
+          if (
+            getTilesResponse.data.data.attributes.length &&
+            getThumbsResponse.data.data.attributes.length
+          ) {
+            const data = state().recentImagery.data;
+            getTilesResponse.data.data.attributes.forEach(item => {
+              const index = findIndex(
+                data,
+                d => d.attributes.source === item.source_id
+              );
+              data[index].attributes.tile_url = item.tile_url;
+            });
+            getThumbsResponse.data.data.attributes.forEach(item => {
+              const index = findIndex(
+                data,
+                d => d.attributes.source === item.source
+              );
+              data[index].attributes.thumbnail_url = item.thumbnail_url;
+            });
+            dispatch(setRecentImageryData({
+              data,
+              haveAllData: true
+            }));
           }
+        })
+        .catch(error => {
+          console.error(error);
         });
-        dispatch(
-          setRecentImageryData({
-            url: data.tile_url,
-            bounds: data.bbox.geometry.coordinates,
-            cloudScore: data.cloud_score,
-            dateTime: data.date_time,
-            instrument: data.instrument,
-            sources
-          })
-        );
-      }
     })
     .catch(error => {
       console.error(error);
