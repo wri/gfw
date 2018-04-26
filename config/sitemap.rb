c423e59212fe566d888a21997ae88cb8a4b0a22f
Ed Brett <e.j.a.brett@gmail.com>
2018-04-26 11:01:26 +0200

    rails site map generator

---
+++ config/sitemap.rb
@@ -0,0 +1,25 @@
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "https://www.globalforestwatch.org"
+SitemapGenerator::Sitemap.compress = false
+
+SitemapGenerator::Sitemap.create do
+  add '/', :changefreq => 'daily', :priority => 1.0
+
+  add '/map', :changefreq => 'weekly', :priority => 0.8
+  add '/countries', :changefreq => 'weekly', :priority => 0.8
+  add '/country/', :changefreq => 'weekly', :priority => 0.8
+  add '/about', :changefreq => 'weekly', :priority => 0.8
+  add '/small-grants-fund', :changefreq => 'weekly', :priority => 0.8
+  add '/search', :changefreq => 'weekly', :priority => 0.8
+  add '/stories', :changefreq => 'weekly', :priority => 0.8
+
+  add '/contribute-data', :changefreq => 'weekly', :priority => 0.4
+  add '/sitemap', :changefreq => 'weekly', :priority => 0.4
+  add '/terms', :changefreq => 'weekly', :priority => 0.4
+
+  # external apps
+  add '/howto', :changefreq => 'weekly', :priority => 0.4
+  add 'https://developers.globalforestwatch.org', :changefreq => 'weekly', :priority => 0.4
+  add 'http://data.globalforestwatch.org', :changefreq => 'weekly', :priority => 0.4
+
+end
