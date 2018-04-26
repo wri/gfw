999561f1f46eee5f24127b0479b8aac3e402b69e
Ed Brett <e.j.a.brett@gmail.com>
2018-04-26 12:46:27 +0200

    update tree cover located

--- app/javascript/components/widget/widgetz/extent/tree-cover-located/initial-state.js
+++ app/javascript/components/widget/widgetz/extent/tree-cover-located/initial-state.js
@@ -2,10 +2,26 @@ export const initialState = {
   title: 'Where is tree cover located',
   config: {
     size: 'small',
-    indicators: ['gadm28', 'ifl_2013', 'mining', 'wdpa', 'plantations', 'landmark', 'primary_forest', 'ifl_2013',
-      'ifl_2013__wdpa', 'ifl_2013__mining', 'ifl_2013__landmark',
-      'primary_forest', 'primary_forest__mining', 'primary_forest__wdpa', 'primary_forest__landmark',
-      'plantations__mining', 'plantations__wdpa', 'plantations__landmark'],
+    indicators: [
+      'gadm28',
+      'ifl_2013',
+      'mining',
+      'wdpa',
+      'plantations',
+      'landmark',
+      'primary_forest',
+      'ifl_2013',
+      'ifl_2013__wdpa',
+      'ifl_2013__mining',
+      'ifl_2013__landmark',
+      'primary_forest',
+      'primary_forest__mining',
+      'primary_forest__wdpa',
+      'primary_forest__landmark',
+      'plantations__mining',
+      'plantations__wdpa',
+      'plantations__landmark'
+    ],
     units: ['ha', '%'],
     categories: ['summary', 'land-cover'],
     admins: ['country', 'region'],
@@ -19,10 +35,25 @@ export const initialState = {
       landCover: 2
     },
     sentences: {
-      initial: 'In {location}, the top {count} regions represent {percentage} of all tree cover. {region} has the largest tree cover at {value} compared to an average of {average}.',
-      hasIndicator: 'For {indicator} in {location}, the top {count} regions represent {percentage} of all tree cover. {region} has the largest tree cover at {value} compared to an average of {average}.',
-      hasPercentage: '{region} had the largest relative tree cover of {percentage}, compared to a regional average of {average}.'
-    }
+      initial:
+        'In {location}, the top {count} regions represent {percentage} of all tree cover. {region} has the largest tree cover at {value} compared to an average of {average}.',
+      hasIndicator:
+        'For {indicator} in {location}, the top {count} regions represent {percentage} of all tree cover. {region} has the largest tree cover at {value} compared to an average of {average}.',
+      hasPercentage:
+        '{region} had the largest relative tree cover of {percentage}, compared to a regional average of {average}.'
+    },
+    data: [
+      {
+        data: 'extent',
+        threshold: 'current',
+        indicator: 'gadm28'
+      },
+      {
+        data: 'extent',
+        threshold: 'current',
+        indicator: 'current'
+      }
+    ]
   },
   settings: {
     indicator: 'gadm28',
