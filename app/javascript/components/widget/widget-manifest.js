aa0505380f3e61d171c470ff4faef4cbd7f482bc
Ed Brett <e.j.a.brett@gmail.com>
2018-05-01 19:26:19 +0200

    rename folder

--- app/javascript/components/widget/widget-manifest.js
+++ app/javascript/components/widget/widget-manifest.js
@@ -1,38 +1,38 @@
 // Land cover
-import * as treeCover from './widgetz/land-cover/tree-cover';
-import * as treeCoverRanked from './widgetz/land-cover/tree-cover-ranked';
-import * as treeCoverLocated from './widgetz/land-cover/tree-cover-located';
-import * as intactTreeCover from './widgetz/land-cover/intact-tree-cover';
-import * as primaryForest from './widgetz/land-cover/primary-forest';
-import * as faoCover from './widgetz/land-cover/fao-cover';
+import * as treeCover from './widgets/land-cover/tree-cover';
+import * as treeCoverRanked from './widgets/land-cover/tree-cover-ranked';
+import * as treeCoverLocated from './widgets/land-cover/tree-cover-located';
+import * as intactTreeCover from './widgets/land-cover/intact-tree-cover';
+import * as primaryForest from './widgets/land-cover/primary-forest';
+import * as faoCover from './widgets/land-cover/fao-cover';
 
 // Forest change
-import * as gladAlerts from './widgetz/forest-change/glad-alerts';
-import * as gladRanked from './widgetz/forest-change/glad-ranked';
-import * as fires from './widgetz/forest-change/fires';
-import * as treeLossLocated from './widgetz/forest-change/tree-loss-located';
-import * as treeCoverGain from './widgetz/forest-change/tree-cover-gain';
-import * as treeGainLocated from './widgetz/forest-change/tree-gain-located';
-import * as treeLossRanked from './widgetz/forest-change/tree-loss-ranked';
-import * as treeLossPlantations from './widgetz/forest-change/tree-loss-plantations';
-import * as treeLoss from './widgetz/forest-change/tree-loss';
-import * as faoDeforest from './widgetz/forest-change/fao-deforest';
-import * as faoReforest from './widgetz/forest-change/fao-reforest';
+import * as gladAlerts from './widgets/forest-change/glad-alerts';
+import * as gladRanked from './widgets/forest-change/glad-ranked';
+import * as fires from './widgets/forest-change/fires';
+import * as treeLossLocated from './widgets/forest-change/tree-loss-located';
+import * as treeCoverGain from './widgets/forest-change/tree-cover-gain';
+import * as treeGainLocated from './widgets/forest-change/tree-gain-located';
+import * as treeLossRanked from './widgets/forest-change/tree-loss-ranked';
+import * as treeLossPlantations from './widgets/forest-change/tree-loss-plantations';
+import * as treeLoss from './widgets/forest-change/tree-loss';
+import * as faoDeforest from './widgets/forest-change/fao-deforest';
+import * as faoReforest from './widgets/forest-change/fao-reforest';
 
 // Land use
-import * as treeCoverPlantations from './widgetz/land-use/tree-cover-plantations';
-import * as rankedPlantations from './widgetz/land-use/ranked-plantations';
+import * as treeCoverPlantations from './widgets/land-use/tree-cover-plantations';
+import * as rankedPlantations from './widgets/land-use/ranked-plantations';
 
 // Conservation
-import * as gladBiodiversity from './widgetz/conservation/glad-biodiversity';
+import * as gladBiodiversity from './widgets/conservation/glad-biodiversity';
 
 // People
-import * as economicImpact from './widgetz/people/economic-impact';
-import * as forestryEmployment from './widgetz/people/forestry-employment';
+import * as economicImpact from './widgets/people/economic-impact';
+import * as forestryEmployment from './widgets/people/forestry-employment';
 
 // Climate
-import * as emissions from './widgetz/climate/emissions';
-import * as emissionsDeforestation from './widgetz/climate/emissions-deforestation';
+import * as emissions from './widgets/climate/emissions';
+import * as emissionsDeforestation from './widgets/climate/emissions-deforestation';
 
 export {
   // land cover
