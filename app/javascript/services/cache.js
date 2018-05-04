e6e76babb720e05909e19fd762c287de63128d50
pjosh <jorgegar@gmail.com>
2018-05-04 12:55:01 +0200

    cache service methods

--- app/javascript/services/cache.js
+++ app/javascript/services/cache.js
@@ -12,3 +12,21 @@ export const getKeys = () => {
   const url = REQUEST_URL + QUERIES.keys;
   return axios.get(url);
 };
+
+export const getKey = id => {
+  const url = REQUEST_URL + QUERIES.get.replace('{id}', id);
+  return axios.get(url);
+};
+
+export const addKey = (id, data) => {
+  const url = REQUEST_URL + QUERIES.add;
+  return axios({
+    method: 'POST',
+    headers: { 'content-type': 'application/json' },
+    data: {
+      id,
+      data
+    },
+    url
+  });
+};
