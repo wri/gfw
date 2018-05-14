3bf11c73c797036a5b91fc01c18cd85970b7ea3d
Adam J. Pain <aj.pain89@gmail.com>
2018-05-14 12:21:56 +0200

    Fixes global gain widget

--- app/javascript/components/widgets/widgets/forest-change/tree-cover-gain/selectors.js
+++ app/javascript/components/widgets/widgets/forest-change/tree-cover-gain/selectors.js
@@ -105,12 +105,15 @@ export const getSentence = createSelector(
       initial,
       withIndicator,
       regionInitial,
-      regionWithIndicator
+      regionWithIndicator,
+      globalInitial,
+      globalWithIndicator
     } = sentences;
     const locationData =
       currentLocation && data.find(l => l.id === currentLocation.value);
     const gain = locationData ? locationData.gain : sumBy(data, 'gain');
-    const globalPercent = gain ? 100 * gain / sumBy(data, 'gain') : 0;
+    const globalPercent = gain ? 100 * gain / sumBy(data, 'extent') : 0;
+    const gainPercent = gain ? 100 * gain / sumBy(data, 'gain') : 0;
     const areaPercent = (locationData && locationData.percentage) || 0;
 
     const params = {
@@ -120,6 +123,8 @@ export const getSentence = createSelector(
       percent: areaPercent >= 0.1 ? `${format('.1f')(areaPercent)}%` : '<0.1%',
       globalPercent:
         globalPercent >= 0.1 ? `${format('.1f')(globalPercent)}%` : '<0.1%',
+      gainPercent:
+        gainPercent >= 0.1 ? `${format('.1f')(gainPercent)}%` : '<0.1%',
       extentYear: settings.extentYear,
       parent: parent && parent.label
     };
@@ -127,6 +132,8 @@ export const getSentence = createSelector(
     let sentence = indicator ? withIndicator : initial;
     if (adminLevel === 'region' || adminLevel === 'subRegion') {
       sentence = indicator ? regionWithIndicator : regionInitial;
+    } else if (adminLevel === 'global' || adminLevel === 'subRegion') {
+      sentence = indicator ? globalWithIndicator : globalInitial;
     }
 
     return {
