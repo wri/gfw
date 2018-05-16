93498e4d683cc89d3574877008569da04fb25cf5
pjosh <jorgegar@gmail.com>
2018-05-16 09:57:34 +0200

    improve request interceptor

--- app/javascript/utils/request.js
+++ app/javascript/utils/request.js
@@ -25,9 +25,10 @@ const request = {
       const axiosInstance = axios.create();
       const haveException = checkException(exceptionId);
       if (!cacheError && !haveException) {
-        axiosInstance.interceptors.response.use(response =>
-          addKey(key, response.data, expire, exceptionId).then(() => response)
-        );
+        axiosInstance.interceptors.response.use(response => {
+          addKey(key, response.data, expire, exceptionId);
+          return response;
+        });
       }
       return axiosInstance.get(url);
     }
