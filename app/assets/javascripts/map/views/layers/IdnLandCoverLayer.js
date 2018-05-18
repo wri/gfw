60673a56283a572e2f894852492bfe7238b947e1
Benjamin A. Laken <benlaken@gmail.com>
2018-05-18 16:33:56 +0200

    table source suffered a change to column name - this returns the layer to functionality

--- app/assets/javascripts/map/views/layers/IdnLandCoverLayer.js
+++ app/assets/javascripts/map/views/layers/IdnLandCoverLayer.js
@@ -13,7 +13,7 @@ define([
     var IdnLandCoverLayer = CartoDBLayerClass.extend({
 
       options: {
-        sql: 'SELECT cartodb_id, the_geom_webmercator, keterangan, \'{tableName}\' AS tablename, {analysis} AS analysis, \'{tableName}\' AS layer FROM {tableName}',
+        sql: 'SELECT cartodb_id, the_geom_webmercator, desc_in, \'{tableName}\' AS tablename, {analysis} AS analysis, \'{tableName}\' AS layer FROM {tableName}',
         infowindow: false,
         cartocss: IdnLandCoverCartoCSS,
         interactivity: 'cartodb_id, tablename',
