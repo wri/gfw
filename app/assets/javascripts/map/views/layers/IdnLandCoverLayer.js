eb3a1f4ca6aca1cddf7324528561e2382bd2339c
Benjamin A. Laken <benlaken@gmail.com>
2018-05-18 16:45:46 +0200

    added transmigration area

--- app/assets/javascripts/map/views/layers/IdnLandCoverLayer.js
+++ app/assets/javascripts/map/views/layers/IdnLandCoverLayer.js
@@ -3,25 +3,23 @@
  *
  * @return
  */
-define([
+define(
+  [
     'abstract/layer/CartoDBLayerClass',
     'text!map/cartocss/idn_land_cover.cartocss'
-  ], function(CartoDBLayerClass, IdnLandCoverCartoCSS) {
-
-    'use strict';
-
-    var IdnLandCoverLayer = CartoDBLayerClass.extend({
-
+  ],
+  (CartoDBLayerClass, IdnLandCoverCartoCSS) => {
+    const IdnLandCoverLayer = CartoDBLayerClass.extend({
       options: {
-        sql: 'SELECT cartodb_id, the_geom_webmercator, desc_in, \'{tableName}\' AS tablename, {analysis} AS analysis, \'{tableName}\' AS layer FROM {tableName}',
+        sql:
+          "SELECT cartodb_id, the_geom_webmercator, desc_in, '{tableName}' AS tablename, {analysis} AS analysis, '{tableName}' AS layer FROM {tableName}",
         infowindow: false,
         cartocss: IdnLandCoverCartoCSS,
         interactivity: 'cartodb_id, tablename',
         analysis: false
       }
-
     });
 
     return IdnLandCoverLayer;
-
-  });
+  }
+);
