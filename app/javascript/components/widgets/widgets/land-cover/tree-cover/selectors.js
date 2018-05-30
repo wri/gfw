2bc28bd5b6d5bc08da18ca12260cf66bdf030fda
Ed Brett <e.j.a.brett@gmail.com>
2018-05-30 16:25:18 +0200

    hide plantations on global tree cover

--- app/javascript/components/widgets/widgets/land-cover/tree-cover/selectors.js
+++ app/javascript/components/widgets/widgets/land-cover/tree-cover/selectors.js
@@ -14,13 +14,14 @@ const getSentences = state => state.config && state.config.sentences;
 
 // get lists selected
 export const parseData = createSelector(
-  [getData, getSettings, getWhitelist, getColors],
-  (data, settings, whitelist, colors) => {
+  [getData, getSettings, getWhitelist, getColors, getCurrentLocation],
+  (data, settings, whitelist, colors, currentLabel) => {
     if (isEmpty(data)) return null;
     const { totalArea, cover, plantations } = data;
     const { indicator } = settings;
     const hasPlantations =
-      isEmpty(whitelist) || whitelist.indexOf('plantations') > -1;
+      (currentLabel !== 'global' && isEmpty(whitelist)) ||
+      whitelist.indexOf('plantations') > -1;
     const colorRange = getColorPalette(colors.ramp, hasPlantations ? 2 : 1);
     const parsedData = [
       {
