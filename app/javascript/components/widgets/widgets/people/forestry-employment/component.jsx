1994a56c3e42ef3ee30c9d7d3904721a0cff26d8
Jorge García <jorgegar@gmail.com>
2018-05-31 17:37:51 +0200

    vertical alignment

--- app/javascript/components/widgets/widgets/people/forestry-employment/component.jsx
+++ app/javascript/components/widgets/widgets/people/forestry-employment/component.jsx
@@ -4,14 +4,20 @@ import PropTypes from 'prop-types';
 import NoContent from 'components/ui/no-content';
 import WidgetPieChartLegend from 'components/widgets/components/widget-pie-chart-legend';
 
+import './styles';
+
 class WidgetForestryEmployment extends PureComponent {
   render() {
     const { data } = this.props;
 
-    return data[0].noContent ? (
-      <NoContent message="No gender data available" />
-    ) : (
-      <WidgetPieChartLegend {...this.props} />
+    return (
+      <div className="c-widget-forestry-employment">
+        {data[0].noContent ? (
+          <NoContent message="No gender data available" />
+        ) : (
+          <WidgetPieChartLegend {...this.props} />
+        )}
+      </div>
     );
   }
 }
