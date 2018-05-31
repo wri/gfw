28eb6a0aa72cfd2245ce5067466597a35960c673
Jorge García <jorgegar@gmail.com>
2018-05-31 13:41:51 +0200

    simplify isMouseOnBounds calculation

--- app/javascript/utils/dom.js
+++ app/javascript/utils/dom.js
@@ -1,10 +1,8 @@
 export const isMouseOnBounds = (event, bounds) => {
   const { x, y, width, height } = bounds;
   return (
-    event.x >= x &&
-    event.x <= x + width &&
-    event.y >= y &&
-    event.y <= y + height
+    (event.x - x) * (event.x - x + width) <= 0 &&
+    (event.y - y) * (event.y - y + height) <= 0
   );
 };
 
