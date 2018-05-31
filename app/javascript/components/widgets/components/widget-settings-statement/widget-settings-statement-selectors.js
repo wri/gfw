9a8aa22d6e0e82e2d3de087e80327214c230c4d1
Ed Brett <e.j.a.brett@gmail.com>
2018-05-31 14:59:10 +0200

    add statement for loss widgets to indicate no gain consideration

--- app/javascript/components/widgets/components/widget-settings-statement/widget-settings-statement-selectors.js
+++ app/javascript/components/widgets/components/widget-settings-statement/widget-settings-statement-selectors.js
@@ -3,15 +3,22 @@ import compact from 'lodash/compact';
 
 // get list data
 const getSettings = state => state.settings || null;
+const getType = state => state.type || null;
 
 // get lists selected
-export const getStatement = createSelector([getSettings], settings => {
-  if (!settings) return '';
-  const { extentYear, threshold } = settings;
-  const statements = compact([
-    extentYear ? `${extentYear} tree cover extent` : null,
-    threshold ? `>${threshold}% tree canopy` : null
-  ]);
+export const getStatement = createSelector(
+  [getSettings, getType],
+  (settings, type) => {
+    if (!settings) return '';
+    const { extentYear, threshold } = settings;
+    const statements = compact([
+      extentYear ? `${extentYear} tree cover extent` : null,
+      threshold ? `>${threshold}% tree canopy` : null,
+      type === 'loss'
+        ? 'these estimates do not take tree cover gain into account'
+        : null
+    ]);
 
-  return statements.join(' | ');
-});
+    return statements.join(' | ');
+  }
+);
