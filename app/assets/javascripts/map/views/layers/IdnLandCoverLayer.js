45bd4d90b331fabce6e0879c2cc0041239d2b629
Benjamin A. Laken <benlaken@gmail.com>
2018-06-06 17:17:35 +0200

    added tooltip to idn land cover layer

--- app/assets/javascripts/map/views/layers/IdnLandCoverLayer.js
+++ app/assets/javascripts/map/views/layers/IdnLandCoverLayer.js
@@ -12,10 +12,10 @@ define(
     const IdnLandCoverLayer = CartoDBLayerClass.extend({
       options: {
         sql:
-          "SELECT cartodb_id, the_geom_webmercator, desc_in, '{tableName}' AS tablename, {analysis} AS analysis, '{tableName}' AS layer FROM {tableName}",
-        infowindow: false,
+          "SELECT cartodb_id, the_geom_webmercator, desc_in, desc_en as name, '{tableName}' AS tablename, {analysis} AS analysis, '{tableName}' AS layer FROM {tableName}",
+        infowindow: true,
         cartocss: IdnLandCoverCartoCSS,
-        interactivity: 'cartodb_id, tablename',
+        interactivity: 'cartodb_id, tablename, name',
         analysis: false
       }
     });
