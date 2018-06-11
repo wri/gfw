53eff33a63cdaf26ee6d494003dd8db8067d6789
Ed Brett <e.j.a.brett@gmail.com>
2018-06-11 15:20:22 +0100

    fix border bottom on filter options in tooltip

--- app/javascript/components/widgets/components/widget-settings/widget-settings-component.jsx
+++ app/javascript/components/widgets/components/widget-settings/widget-settings-component.jsx
@@ -24,6 +24,7 @@ class WidgetSettings extends PureComponent {
       years,
       startYears,
       endYears,
+      datasets,
       extentYears,
       types,
       weeks
@@ -35,6 +36,7 @@ class WidgetSettings extends PureComponent {
       startYears ||
       endYears ||
       extentYears ||
+      datasets ||
       types ||
       weeks;
 
@@ -90,138 +92,144 @@ class WidgetSettings extends PureComponent {
             )}
           </div>
         )}
-        {types && (
-          <Dropdown
-            theme="theme-select-light"
-            label="DISPLAY TREES BY"
-            value={settings.type}
-            options={types}
-            disabled={loading}
-            onChange={option => {
-              const layers = [...settings.layers];
-              if (layers.length) {
-                const type = settings.type === 'bound2' ? 'species' : 'type';
-                const newType = option.value === 'bound2' ? 'species' : 'type';
-                const activeIndex = settings.layers.indexOf(
-                  `plantations_by_${type}`
-                );
-                layers[activeIndex] = `plantations_by_${newType}`;
-              }
-              onSettingsChange({
-                value: {
-                  type: option.value,
-                  layers
-                },
-                widget
-              });
-            }}
-            infoAction={() => setModalMeta('widget_tree_cover_extent')}
-          />
-        )}
-        {weeks && (
-          <Dropdown
-            theme="theme-select-light"
-            label="SHOW DATA FOR THE LAST"
-            value={settings.weeks}
-            options={weeks}
-            disabled={loading}
-            onChange={option =>
-              onSettingsChange({ value: { weeks: option.value }, widget })
-            }
-          />
-        )}
-        {extentYears && (
-          <Dropdown
-            theme="theme-select-light"
-            label="EXTENT YEAR"
-            value={settings.extentYear}
-            options={extentYears}
-            disabled={loading}
-            onChange={option => {
-              const layers = [...settings.layers];
-              if (layers.length) {
-                const activeIndex = settings.layers.indexOf(
-                  `forest${settings.extentYear}`
-                );
-                layers[activeIndex] = `forest${option.value}`;
-              }
-              onSettingsChange({
-                value: {
-                  extentYear: option.value,
-                  layers
-                },
-                widget
-              });
-            }}
-          />
-        )}
-        {units && (
-          <Dropdown
-            theme="theme-select-light"
-            label="UNIT"
-            value={settings.unit}
-            options={units}
-            onChange={option =>
-              onSettingsChange({ value: { unit: option.value }, widget })
-            }
-          />
-        )}
-        {periods && (
-          <Dropdown
-            theme="theme-select-light"
-            label="YEAR"
-            value={settings.period}
-            options={periods}
-            onChange={option =>
-              onSettingsChange({ value: { period: option.value }, widget })
-            }
-          />
-        )}
-        {years && (
-          <Dropdown
-            theme="theme-select-light"
-            label="YEAR"
-            value={settings.year}
-            options={years}
-            onChange={option =>
-              onSettingsChange({ value: { year: option.value }, widget })
-            }
-          />
-        )}
-        {startYears &&
-          endYears && (
-            <div className="years-select">
-              <span className="label">YEARS</span>
-              <div className="select-container">
-                <Dropdown
-                  className="years-dropdown"
-                  theme="theme-dropdown-button"
-                  value={settings.startYear}
-                  options={startYears}
-                  onChange={option =>
-                    onSettingsChange({
-                      value: { startYear: option.value },
-                      widget
-                    })
+        {hasExtraOptions && (
+          <div className="filters">
+            {types && (
+              <Dropdown
+                theme="theme-select-light"
+                label="DISPLAY TREES BY"
+                value={settings.type}
+                options={types}
+                disabled={loading}
+                onChange={option => {
+                  const layers = [...settings.layers];
+                  if (layers.length) {
+                    const type =
+                      settings.type === 'bound2' ? 'species' : 'type';
+                    const newType =
+                      option.value === 'bound2' ? 'species' : 'type';
+                    const activeIndex = settings.layers.indexOf(
+                      `plantations_by_${type}`
+                    );
+                    layers[activeIndex] = `plantations_by_${newType}`;
                   }
-                  disabled={loading}
-                />
-                <span className="text-date">to</span>
-                <Dropdown
-                  theme="theme-dropdown-button"
-                  value={settings.endYear}
-                  options={endYears}
-                  onChange={option =>
-                    onSettingsChange({
-                      value: { endYear: option.value },
-                      widget
-                    })
+                  onSettingsChange({
+                    value: {
+                      type: option.value,
+                      layers
+                    },
+                    widget
+                  });
+                }}
+                infoAction={() => setModalMeta('widget_tree_cover_extent')}
+              />
+            )}
+            {weeks && (
+              <Dropdown
+                theme="theme-select-light"
+                label="SHOW DATA FOR THE LAST"
+                value={settings.weeks}
+                options={weeks}
+                disabled={loading}
+                onChange={option =>
+                  onSettingsChange({ value: { weeks: option.value }, widget })
+                }
+              />
+            )}
+            {extentYears && (
+              <Dropdown
+                theme="theme-select-light"
+                label="EXTENT YEAR"
+                value={settings.extentYear}
+                options={extentYears}
+                disabled={loading}
+                onChange={option => {
+                  const layers = [...settings.layers];
+                  if (layers.length) {
+                    const activeIndex = settings.layers.indexOf(
+                      `forest${settings.extentYear}`
+                    );
+                    layers[activeIndex] = `forest${option.value}`;
                   }
-                  disabled={loading}
-                />
-              </div>
-            </div>
-          )}
+                  onSettingsChange({
+                    value: {
+                      extentYear: option.value,
+                      layers
+                    },
+                    widget
+                  });
+                }}
+              />
+            )}
+            {units && (
+              <Dropdown
+                theme="theme-select-light"
+                label="UNIT"
+                value={settings.unit}
+                options={units}
+                onChange={option =>
+                  onSettingsChange({ value: { unit: option.value }, widget })
+                }
+              />
+            )}
+            {periods && (
+              <Dropdown
+                theme="theme-select-light"
+                label="YEAR"
+                value={settings.period}
+                options={periods}
+                onChange={option =>
+                  onSettingsChange({ value: { period: option.value }, widget })
+                }
+              />
+            )}
+            {years && (
+              <Dropdown
+                theme="theme-select-light"
+                label="YEAR"
+                value={settings.year}
+                options={years}
+                onChange={option =>
+                  onSettingsChange({ value: { year: option.value }, widget })
+                }
+              />
+            )}
+            {startYears &&
+              endYears && (
+                <div className="years-select">
+                  <span className="label">YEARS</span>
+                  <div className="select-container">
+                    <Dropdown
+                      className="years-dropdown"
+                      theme="theme-dropdown-button"
+                      value={settings.startYear}
+                      options={startYears}
+                      onChange={option =>
+                        onSettingsChange({
+                          value: { startYear: option.value },
+                          widget
+                        })
+                      }
+                      disabled={loading}
+                    />
+                    <span className="text-date">to</span>
+                    <Dropdown
+                      theme="theme-dropdown-button"
+                      value={settings.endYear}
+                      options={endYears}
+                      onChange={option =>
+                        onSettingsChange({
+                          value: { endYear: option.value },
+                          widget
+                        })
+                      }
+                      disabled={loading}
+                    />
+                  </div>
+                </div>
+              )}
+          </div>
+        )}
         {thresholds && (
           <Dropdown
             className={hasExtraOptions ? 'threshold-border' : ''}
