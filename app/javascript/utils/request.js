53a50e3bf188d753a6f57ebcf910da56c6ccba47
Jorge García <jorgegar@gmail.com>
2018-06-18 16:12:29 +0200

    set cache keys from rails

--- app/javascript/utils/request.js
+++ app/javascript/utils/request.js
@@ -5,26 +5,27 @@ import { getKey, addKey } from 'services/cache';
 import CACHE_EXCEPTIONS from 'data/cache-exceptions.json';
 
 const EXPIRE_DEFAULT = 86400;
+let cacheKeys = [];
 
 const request = {
   get(url, expire = EXPIRE_DEFAULT, exceptionId = null) {
+    if (cacheKeys.length) {
+      cacheKeys = window.RequestCache.keys;
+    }
     const key = btoa(url);
-    return axios.get(`/cache/${key}`).then(cache => {
-      const { data } = cache;
-      if (!data) {
-        const axiosInstance = axios.create();
-        const haveException = checkException(exceptionId);
-        if (!haveException) {
-          axiosInstance.interceptors.response.use(response =>
-            addKey(key, response.data, expire, exceptionId)
-              .then(() => response)
-              .catch(() => response)
-          );
-        }
-        return axiosInstance.get(url);
+    if (cacheKeys.indexOf(key) === -1) {
+      const axiosInstance = axios.create();
+      const haveException = checkException(exceptionId);
+      if (!haveException) {
+        axiosInstance.interceptors.response.use(response =>
+          addKey(key, response.data, expire, exceptionId)
+            .then(() => response)
+            .catch(() => response)
+        );
       }
-      return getKey(key);
-    });
+      return axiosInstance.get(url);
+    }
+    return getKey(key);
   }
 };
 
