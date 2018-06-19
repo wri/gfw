a89a919e8449658a0a3db4555228c048636ac6f6
Jorge García <jorgegar@gmail.com>
2018-06-19 11:39:24 +0200

    add map component

--- app/javascript/pages/map/page/page-component.js
+++ app/javascript/pages/map/page/page-component.js
@@ -1,10 +1,16 @@
 import React, { PureComponent } from 'react';
 
+import Map from 'components/map';
+
 import './page-styles.scss';
 
 class Page extends PureComponent {
   render() {
-    return <div className="l-map" />;
+    return (
+      <div className="l-map">
+        <Map />
+      </div>
+    );
   }
 }
 
