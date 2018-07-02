f75a87782361220d9b65b4ddbc5520769ff5a3bc
Ed Brett <e.j.a.brett@gmail.com>
2018-07-02 10:07:45 +0200

    remove auto lint changes to legacy map

--- app/assets/javascripts/map/services/GeostoreService.js
+++ app/assets/javascripts/map/services/GeostoreService.js
@@ -1,88 +1,100 @@
-define(['Class', 'uri', 'bluebird', 'map/services/DataService'], (
-  Class,
-  UriTemplate,
-  Promise,
-  ds
-) => {
-  let GET_REQUEST_ID = 'GeostoreService:get',
-    SAVE_REQUEST_ID = 'GeostoreService:save';
+define([
+  'Class',
+  'uri',
+  'bluebird',
+  'map/services/DataService'
+], function(Class, UriTemplate, Promise, ds) {
 
-  const URL = 'https://production-api.globalforestwatch.org' + '/geostore/{id}';
+  'use strict';
 
-  const GeostoreService = Class.extend({
-    get(id) {
-      return new Promise(((resolve, reject) => {
-        const url = new UriTemplate(URL).fillFromObject({ id });
+  var GET_REQUEST_ID = 'GeostoreService:get',
+      SAVE_REQUEST_ID = 'GeostoreService:save';
+
+  var URL = 'https://production-api.globalforestwatch.org' + '/geostore/{id}';
+
+  var GeostoreService = Class.extend({
+
+    get: function(id) {
+      return new Promise(function(resolve, reject) {
+
+        var url = new UriTemplate(URL).fillFromObject({id: id});
 
         ds.define(GET_REQUEST_ID, {
           cache: false,
-          url,
+          url: url,
           type: 'GET'
         });
 
-        const requestConfig = {
+        var requestConfig = {
           resourceId: GET_REQUEST_ID,
           success: resolve
         };
 
         ds.request(requestConfig);
-      }));
+
+      });
     },
 
-    save(geojson) {
-      return new Promise(((resolve, reject) => {
-        const url = new UriTemplate(URL).fillFromObject({});
+    save: function(geojson) {
+      return new Promise(function(resolve, reject) {
+
+        var url = new UriTemplate(URL).fillFromObject({});
 
         ds.define(SAVE_REQUEST_ID, {
           cache: false,
-          url,
+          url: url,
           type: 'POST',
           dataType: 'json',
           contentType: 'application/json; charset=utf-8'
         });
 
-        const requestConfig = {
+        var requestConfig = {
           resourceId: SAVE_REQUEST_ID,
           data: JSON.stringify({
-            geojson
+            geojson: geojson
           }),
-          success(response) {
+          success: function(response) {
             resolve(response.data.id);
           },
           error: reject
         };
 
         ds.request(requestConfig);
-      }));
+
+      });
     },
 
-    use(provider) {
-      return new Promise(((resolve, reject) => {
-        const url = new UriTemplate(URL).fillFromObject({});
+    use: function(provider) {
+      return new Promise(function(resolve, reject) {
+
+        var url = new UriTemplate(URL).fillFromObject({});
 
         ds.define(SAVE_REQUEST_ID, {
           cache: false,
-          url,
+          url: url,
           type: 'POST',
           dataType: 'json',
           contentType: 'application/json; charset=utf-8'
         });
 
-        const requestConfig = {
+        var requestConfig = {
           resourceId: SAVE_REQUEST_ID,
           data: JSON.stringify({
-            provider
+            provider: provider
           }),
-          success(response) {
+          success: function(response) {
             resolve(response.data.id);
           },
           error: reject
         };
 
         ds.request(requestConfig);
-      }));
+
+      });
     }
+
   });
 
   return new GeostoreService();
+
 });
