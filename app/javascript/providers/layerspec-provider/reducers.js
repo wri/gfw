e3b8edd4f65d7a45093a87e82b253458d9ff55b7
Ed Brett <e.j.a.brett@gmail.com>
2018-07-03 16:19:15 +0200

    layerspec provider

---
+++ app/javascript/providers/layerspec-provider/reducers.js
@@ -0,0 +1,21 @@
+export const initialState = {
+  loading: false,
+  error: false,
+  data: []
+};
+
+const setLayerSpecLoading = (state, { payload }) => ({
+  ...state,
+  ...payload
+});
+
+const setLayers = (state, { payload }) => ({
+  ...state,
+  data: payload,
+  loading: false
+});
+
+export default {
+  setLayers,
+  setLayerSpecLoading
+};
