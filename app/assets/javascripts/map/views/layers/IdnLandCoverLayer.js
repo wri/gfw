7e84464ad23d409fbbbac7fa44b0ea373782b603
Benjamin Laken <benlaken@gmail.com>
2018-07-18 11:40:29 +0200

    Update IdnLandCoverLayer.js
    Deactivating tooltip

--- app/assets/javascripts/map/views/layers/IdnLandCoverLayer.js
+++ app/assets/javascripts/map/views/layers/IdnLandCoverLayer.js
@@ -13,7 +13,7 @@ define(
       options: {
         sql:
           "SELECT cartodb_id, the_geom_webmercator, desc_in, desc_en as name, '{tableName}' AS tablename, {analysis} AS analysis, '{tableName}' AS layer FROM {tableName}",
-        infowindow: true,
+        infowindow: false,
         cartocss: IdnLandCoverCartoCSS,
         interactivity: 'cartodb_id, tablename, name',
         analysis: false
