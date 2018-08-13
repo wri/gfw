996d687552e3c9b44441451e5265803f311b88f3
Ed Brett <e.j.a.brett@gmail.com>
2018-08-13 19:14:32 +0200

    filter datasets in menu by country

--- app/javascript/pages/map/menu/menu-selectors.js
+++ app/javascript/pages/map/menu/menu-selectors.js
@@ -1,116 +1,10 @@
 import { createSelector, createStructuredSelector } from 'reselect';
 import flatten from 'lodash/flatten';
 
-import treesIcon from 'assets/icons/trees.svg';
-import landTreeIcon from 'assets/icons/land-tree.svg';
-import truckIcon from 'assets/icons/truck.svg';
-import climateBubblesIcon from 'assets/icons/climate-bubbles.svg';
-import featherIcon from 'assets/icons/feather.svg';
-import searchIcon from 'assets/icons/search.svg';
-
 import { getLayers } from 'components/map/map-selectors';
 
 import initialState from './menu-initial-state';
-
-import Datasets from './components/sections/datasets';
-import Explore from './components/sections/explore';
-
-const menuSections = [
-  {
-    slug: 'forestChange',
-    name: 'FOREST CHANGE',
-    icon: treesIcon,
-    Component: Datasets,
-    subCategories: [
-      {
-        slug: 'deforestationAlerts',
-        title: 'Deforestation Alerts',
-        subTitle: 'near real time'
-      },
-      {
-        slug: 'fireAlerts',
-        title: 'Fire Alerts',
-        subTitle: 'near real time'
-      },
-      {
-        slug: 'treeCoverChange',
-        title: 'Tree Cover Change'
-      }
-    ]
-  },
-  {
-    slug: 'landCover',
-    name: 'LAND COVER',
-    icon: landTreeIcon,
-    Component: Datasets
-  },
-  {
-    slug: 'landUse',
-    name: 'LAND USE',
-    icon: truckIcon,
-    Component: Datasets,
-    subCategories: [
-      {
-        slug: 'concessions',
-        title: 'Concessions'
-      },
-      {
-        slug: 'infrastructure',
-        title: 'Infrastructure'
-      },
-      {
-        slug: 'people',
-        title: 'People'
-      }
-    ]
-  },
-  {
-    slug: 'climate',
-    name: 'CLIMATE',
-    icon: climateBubblesIcon,
-    Component: Datasets,
-    subCategories: [
-      {
-        slug: 'carbonDensity',
-        title: 'Carbon Density'
-      },
-      {
-        slug: 'carbonEmissions',
-        title: 'Carbon Emissions'
-      },
-      {
-        slug: 'carbonGains',
-        title: 'Carbon Gains'
-      }
-    ]
-  },
-  {
-    slug: 'biodiversity',
-    name: 'BIODIVERSITY',
-    icon: featherIcon,
-    Component: Datasets,
-    subCategories: [
-      {
-        slug: 'conservation',
-        title: 'Conservation'
-      }
-    ]
-  },
-  {
-    slug: 'explore',
-    name: 'EXPLORE',
-    icon: truckIcon,
-    Component: Explore,
-    large: true,
-    section: 'topics'
-  },
-  {
-    slug: 'search',
-    name: 'SEARCH',
-    icon: searchIcon,
-    Component: Datasets
-  }
-];
+import menuSections from './menu-sections';
 
 const getMenuUrlState = state => (state.query && state.query.menu) || null;
 const getDatasets = state => state.datasets || null;
@@ -150,47 +44,65 @@ const getActiveCountries = createSelector(
   }
 );
 
-export const getSections = createSelector(getDatasets, datasets =>
-  menuSections.map(s => {
-    const sectionDatasets =
-      datasets &&
-      datasets
-        .filter(
-          d => flatten(d.vocabulary.map(v => v.tags)).indexOf(s.slug) > -1
-        )
-        .map(d => {
-          const { layer, metadata, vocabulary } = d;
-          const appMeta = metadata.find(m => m.application === 'gfw') || {};
-          const { info } = appMeta || {};
-          const defaultLayer =
-            layer &&
-            layer.length &&
-            (layer.find(
-              l => l.applicationConfig && l.applicationConfig.default
-            ) ||
-              layer[0]);
+export const getSections = createSelector(
+  [getDatasets, getActiveCountries],
+  (datasets, countries) =>
+    menuSections.map(s => {
+      const { slug, subCategories } = s;
+      const sectionDatasets =
+        datasets &&
+        datasets
+          .filter(
+            d => flatten(d.vocabulary.map(v => v.tags)).indexOf(slug) > -1
+          )
+          .map(d => {
+            const { layer, metadata, vocabulary } = d;
+            const appMeta = metadata.find(m => m.application === 'gfw') || {};
+            const { info } = appMeta || {};
+            const defaultLayer =
+              layer &&
+              layer.length &&
+              (layer.find(
+                l => l.applicationConfig && l.applicationConfig.default
+              ) ||
+                layer[0]);
+            const { iso, id } = defaultLayer || {};
 
-          return {
-            ...d,
-            ...info,
-            layer: defaultLayer && defaultLayer.id,
-            tags: flatten(vocabulary.map(v => v.tags)),
-            ...(defaultLayer && defaultLayer.applicationConfig)
-          };
-        });
-    let subCategoriesWithDatasets = s.subCategories;
-    if (subCategoriesWithDatasets) {
-      subCategoriesWithDatasets = s.subCategories.map(subCat => ({
-        ...subCat,
-        datasets: sectionDatasets.filter(d => d.tags.indexOf(subCat.slug) > -1)
-      }));
-    }
-    return {
-      ...s,
-      datasets: sectionDatasets,
-      subCategories: subCategoriesWithDatasets
-    };
-  })
+            return {
+              ...d,
+              ...info,
+              iso,
+              layer: id,
+              tags: flatten(vocabulary.map(v => v.tags)),
+              ...(defaultLayer && defaultLayer.applicationConfig)
+            };
+          });
+      let subCategoriesWithDatasets = [];
+      if (subCategories) {
+        subCategoriesWithDatasets = subCategories.map(subCat => ({
+          ...subCat,
+          datasets: sectionDatasets.filter(
+            d => d.tags.indexOf(subCat.slug) > -1 && d.global
+          )
+        }));
+      }
+      let countriesWithDatasets = [];
+      if (countries) {
+        countriesWithDatasets = countries.map(c => ({
+          title: c.label,
+          slug: c.value,
+          datasets: sectionDatasets.filter(
+            d => !d.global && d.iso.indexOf(c.value) > -1
+          )
+        }));
+      }
+
+      return {
+        ...s,
+        datasets: sectionDatasets,
+        subCategories: countriesWithDatasets.concat(subCategoriesWithDatasets)
+      };
+    })
 );
 
 export const getSectionsWithData = createSelector(
