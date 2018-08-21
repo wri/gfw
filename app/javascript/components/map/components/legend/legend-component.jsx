92f4f1f83d1757b4642a5953e80cf46297977ea5
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2018-08-21 12:43:32 +0200

    fix: legend component active layer

--- app/javascript/components/map/components/legend/legend-component.jsx
+++ app/javascript/components/map/components/legend/legend-component.jsx
@@ -27,7 +27,6 @@ class MapLegend extends Component {
   render() {
     const {
       layerGroups,
-      layers,
       onChangeOrder,
       onChangeTimeline,
       onChangeThreshold,
@@ -58,13 +57,14 @@ class MapLegend extends Component {
                   color,
                   metadata,
                   id,
+                  layers,
                   statementConfig
                 } =
                   lg || {};
 
-                const activeLayer = (lg && lg.layers.find(l => l.active)) || [];
-                const { legendConfig, params, timelineConfig } =
-                  activeLayer || {};
+                const activeLayer =
+                  (layers && layers.find(l => l.active)) || {};
+                const { legendConfig, params, timelineConfig } = activeLayer;
 
                 return (
                   <LegendListItem
