25e76832124405984e5ac2c44429d352dfc64117
Adam J. Pain <aj.pain89@gmail.com>
2018-08-23 12:12:04 +0200

    Update legend and timeline

--- app/assets/javascripts/map/views/layers/LossByDriverLayer.js
+++ app/assets/javascripts/map/views/layers/LossByDriverLayer.js
@@ -21,7 +21,7 @@ define(
         dataMaxZoom: 4,
         urlTemplate:
           'https://storage.googleapis.com/wri-public/lossyear_classification_map/2017/gfw/tiles/hansen_world/v2/tc{threshold}{/z}{/x}{/y}.png',
-        currentDate: ['2001-01-01', '2018-01-01']
+        currentDate: ['2001-01-01', '2015-01-01']
       },
 
       init: function(layer, options, map) {
