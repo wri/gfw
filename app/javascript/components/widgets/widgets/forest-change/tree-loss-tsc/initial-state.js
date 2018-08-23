d196081bd57c13d2f16aae71c28465b5f4842551
Adam J. Pain <aj.pain89@gmail.com>
2018-08-23 09:29:48 +0200

    Restricts years, adds meta key

--- app/javascript/components/widgets/widgets/forest-change/tree-loss-tsc/initial-state.js
+++ app/javascript/components/widgets/widgets/forest-change/tree-loss-tsc/initial-state.js
@@ -7,6 +7,46 @@ export default {
     admins: ['global', 'country'],
     selectors: ['thresholds', 'extentYears', 'startYears', 'endYears'],
     layers: ['loss_by_driver'],
+    startYears: [
+      2001,
+      2002,
+      2003,
+      2004,
+      2005,
+      2006,
+      2007,
+      2008,
+      2009,
+      2010,
+      2011,
+      2012,
+      2013,
+      2014,
+      2015
+    ],
+    endYears: [
+      2001,
+      2002,
+      2003,
+      2004,
+      2005,
+      2006,
+      2007,
+      2008,
+      2009,
+      2010,
+      2011,
+      2012,
+      2013,
+      2014,
+      2015
+    ],
+    metaKey: 'tsc_drivers',
+    sortOrder: {
+      summary: 0,
+      forestChange: 0,
+      global: 0
+    },
     sentences: {
       initial:
         "Between {startYear} and {endYear}, {location} lost {loss} of tree cover loss. This loss is equal to {percent} of the area's tree cover in {extentYear}, and equivalent to {emissions} of CO\u2082 emissions."
@@ -15,7 +55,7 @@ export default {
   settings: {
     threshold: 30,
     startYear: 2001,
-    endYear: 2017,
+    endYear: 2015,
     layers: ['loss_by_driver']
   },
   enabled: true
