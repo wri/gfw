e5f9cc13b63120154b406ff00cff7864436d5091
Adam J. Pain <aj.pain89@gmail.com>
2018-08-24 10:10:30 +0200

    Upates sentence

--- app/javascript/components/widgets/widgets/forest-change/tree-loss-tsc/initial-state.js
+++ app/javascript/components/widgets/widgets/forest-change/tree-loss-tsc/initial-state.js
@@ -20,9 +20,9 @@ export default {
       globalInitial:
         '{location} from {startYear} to {endYear}, {percent} of tree cover loss occurred in areas where {driver} ',
       perm:
-        '(permenant deforestation) is the dominant driver of land cover/land use change.',
+        '(permenant change) is the dominant driver of land cover/land use change.',
       temp:
-        '(temporary deforestation) is the dominant driver of land cover/land use change.'
+        '(temporary change) is the dominant driver of land cover/land use change.'
     }
   },
   settings: {
