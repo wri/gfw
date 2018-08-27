4c4d5cf4e7f54bc7231ffe88a454041912fc3cb5
Ed Brett <e.j.a.brett@gmail.com>
2018-08-27 11:11:52 +0200

    fix issues with map branch and chart height

--- app/javascript/components/widgets/widgets/forest-change/tree-loss-tsc/initial-state.js
+++ app/javascript/components/widgets/widgets/forest-change/tree-loss-tsc/initial-state.js
@@ -7,7 +7,7 @@ export default {
     admins: ['global', 'country'],
     selectors: ['thresholds', 'startYears', 'endYears'],
     layers: ['loss_by_driver'],
-    yearRange: ['2001', '2015'],
+    yearRange: [2001, 2015],
     metaKey: 'tsc_drivers',
     sortOrder: {
       summary: 0,
@@ -20,14 +20,12 @@ export default {
       globalInitial:
         '{location} from {startYear} to {endYear}, {percent} of tree cover loss occurred in areas where {driver} ',
       perm:
-        '(permenant change) is the dominant driver of land cover/land use change.',
+        '(permanant change) is the dominant driver of land cover/land use change.',
       temp:
         '(temporary change) is the dominant driver of land cover/land use change.'
     }
   },
   settings: {
-    threshold: 30,
-    startYear: 2001,
     endYear: 2015,
     layers: ['loss_by_driver']
   },
