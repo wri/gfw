8ea482e95836e0a4fe1fea548aa77d12dbc370c1
Ed Brett <e.j.a.brett@gmail.com>
2018-08-31 18:39:00 +0200

    increase padding on layer statement

--- app/javascript/components/map-v2/components/legend/components/layer-statement/config.js
+++ app/javascript/components/map-v2/components/legend/components/layer-statement/config.js
@@ -1,7 +1,7 @@
 export default {
   lossLayer: {
     statementPlain: 'Tree cover loss',
-    statementHighlight: 'is not always deforestation',
+    statementHighlight: 'is not always deforestation.',
     tooltipDesc:
       'Loss of tree cover may occur for many reasons, including deforestation, fire, and logging within the course of sustainable forestry operations. In sustainably managed forests, the “loss” will eventually show up as “gain”, as young trees get large enough to achieve canopy closure.'
   },
