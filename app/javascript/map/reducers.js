bf31cd6cb7cf5f5e5a6672a514e9d6fe34f207fe
Ed Brett <e.j.a.brett@gmail.com>
2018-09-11 22:23:59 +0200

    tidy map reducers

--- app/javascript/map/reducers.js
+++ app/javascript/map/reducers.js
@@ -7,20 +7,10 @@ import router from './router';
 
 // Pages
 import { reduxModule as map } from './map';
-
-const pagesReducers = {
-  root: handleModule(map)
-};
-
-// Components
 import { reduxModule as RecentImagery } from './map/recent-imagery';
-// Component Reducers
-const componentsReducers = {
-  recentImagery: handleModule(RecentImagery)
-};
 
 export default combineReducers({
-  ...pagesReducers,
-  ...componentsReducers,
+  recentImagery: handleModule(RecentImagery),
+  root: handleModule(map),
   location: router.reducer
 });
