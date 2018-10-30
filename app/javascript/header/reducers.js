3dbac644c7ef36849d01694f40d7f62f81d8911a
Ed Brett <e.j.a.brett@gmail.com>
2018-10-30 12:53:50 +0100

    remove header pack

--- app/javascript/header/reducers.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/first */
-import { combineReducers } from 'redux';
-import { handleModule } from 'redux-tools';
-
-import * as MyGFWProviderComponent from 'providers/mygfw-provider';
-
-export default combineReducers({
-  myGfw: handleModule(MyGFWProviderComponent)
-});
