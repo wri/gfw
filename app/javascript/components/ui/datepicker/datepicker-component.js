382d3edfd283a89e4a1dd5d7b0af77131388c13e
Daniel Fernández Rico <deferrico@gmail.com>
2018-11-20 17:18:28 +0100

    Styled the dropdown with the weekdays row, fixed some :bug:

--- app/javascript/components/ui/datepicker/datepicker-component.js
+++ app/javascript/components/ui/datepicker/datepicker-component.js
@@ -4,6 +4,7 @@ import { SingleDatePicker } from 'react-dates';
 import cx from 'classnames';
 
 import moment from 'moment';
+import Dropdown from 'components/ui/dropdown';
 
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
@@ -20,6 +21,20 @@ class Datepicker extends PureComponent {
 
   render() {
     const { className, date, handleOnDateChange, settings, theme } = this.props;
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ];
     return (
       <div className={cx('c-datepicker', theme, className)}>
         <SingleDatePicker
@@ -30,10 +45,12 @@ class Datepicker extends PureComponent {
           focused={this.state.focused}
           onFocusChange={({ focused }) => this.setState({ focused })}
           // renderCaption={(e) => console.log(e)}
+          navPrev={<div />}
+          navNext={<div />}
           renderMonthElement={({ month, onMonthSelect, onYearSelect }) => (
             <div style={{ display: 'flex', justifyContent: 'center' }}>
               <div>
-                <select
+                {/* <select
                   value={month.month()}
                   onChange={e => {
                     onMonthSelect(month, e.target.value);
@@ -44,10 +61,21 @@ class Datepicker extends PureComponent {
                     .map((label, value) => (
                       <option value={value}>{label}</option>
                     ))}
-                </select>
+                </select> */}
+                <Dropdown
+                  className="c-date-dropdown"
+                  theme="theme-dropdown-button theme-dropdown-button-small -short"
+                  placeholder={months[month.month()]}
+                  noItemsFound="No months found"
+                  noSelectedValue={month.month().toString()}
+                  options={moment.months().map((m, i) => ({ value: i, label: m }))}
+                  onChange={e => {
+                    onMonthSelect(month, e.value);
+                  }}
+                />
               </div>
               <div>
-                <select
+                {/* <select
                   value={month.year()}
                   onChange={e => {
                     onYearSelect(month, e.target.value);
@@ -56,7 +84,23 @@ class Datepicker extends PureComponent {
                   <option value={moment().year() - 1}>Last year</option>
                   <option value={moment().year()}>{moment().year()}</option>
                   <option value={moment().year() + 1}>Next year</option>
-                </select>
+                </select> */}
+                <Dropdown
+                  className="c-date-dropdown"
+                  theme="theme-dropdown-button theme-dropdown-button-small -short"
+                  placeholder={month.year().toString()}
+                  noItemsFound="No years found"
+                  noSelectedValue={month.year().toString()}
+                  options={
+                    // [...Array(3).keys()]
+                    // .map(n => ({value:moment().year()-n, label: moment().year()-n}))
+                    // .reverse()
+                    []
+                  } // temporal
+                  onChange={e => {
+                    onYearSelect(month, e.value);
+                  }}
+                />
               </div>
             </div>
           )}
