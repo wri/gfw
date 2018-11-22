43518b2d2e28a0520d725a9d51ebd36fe955d389
Ed Brett <e.j.a.brett@gmail.com>
2018-11-22 15:11:08 +0000

    add metadata to recent imagery

--- app/javascript/components/map-v2/components/recent-imagery/components/recent-imagery-settings/recent-imagery-settings.js
+++ app/javascript/components/map-v2/components/recent-imagery/components/recent-imagery-settings/recent-imagery-settings.js
@@ -2,10 +2,11 @@ import { connect } from 'react-redux';
 
 import * as mapActions from 'components/map-v2/actions';
 import { getRecentImageryProps } from 'components/map-v2/components/recent-imagery/recent-imagery-selectors';
-import * as recentImageryActions from 'components/map-v2/components/recent-imagery/recent-imagery-actions';
+import { setRecentImagerySettings } from 'components/map-v2/components/recent-imagery/recent-imagery-actions';
+import { setModalMeta } from 'components/modals/meta/meta-actions';
 
 import Component from './recent-imagery-settings-component';
 
-const actions = { ...mapActions, ...recentImageryActions };
+const actions = { ...mapActions, setRecentImagerySettings, setModalMeta };
 
 export default connect(getRecentImageryProps, actions)(Component);
