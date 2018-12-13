302118e04d5075bccf199ae65a76c10b6f5543c5
Ed Brett <e.j.a.brett@gmail.com>
2018-12-13 16:01:21 +0100

    remove redux persist

--- app/javascript/app/index.js
+++ app/javascript/app/index.js
@@ -1,18 +1,15 @@
 import 'babel-polyfill';
 import React from 'react';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 
 import configureStore from './store';
 import Root from './layouts/root';
 
-const { store, persistor } = configureStore();
+const store = configureStore();
 
 const App = () => (
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <Root />
-    </PersistGate>
+    <Root />
   </Provider>
 );
 
--- app/javascript/app/store.js
+++ app/javascript/app/store.js
@@ -1,19 +1,11 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { handleActionTrack } from 'utils/analytics';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import { reducer as formReducer } from 'redux-form';
 
 import reducerRegistry from './registry';
 import router from './router';
 import reducers from './reducers';
-
-const persistConfig = {
-  key: 'root',
-  whitelist: ['modalWelcome'],
-  storage
-};
 
 // register fixed reducers
 reducerRegistry.register('location', router.reducer);
@@ -24,22 +16,19 @@ Object.entries(reducers).map(([name, reducer]) =>
   reducerRegistry.register(name, reducer)
 );
 
-//
 const initialReducers = combineReducers(reducerRegistry.getReducers());
-const persistedReducer = persistReducer(persistConfig, initialReducers);
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const middlewares = [handleActionTrack, thunk, router.middleware];
 
 export default () => {
   const store = createStore(
-    persistedReducer,
+    initialReducers,
     composeEnhancers(router.enhancer, applyMiddleware(...middlewares))
   );
-  const persistor = persistStore(store);
   reducerRegistry.setChangeListener(asyncReducers =>
     store.replaceReducer(combineReducers(asyncReducers))
   );
 
-  return { store, persistor };
+  return store;
 };
