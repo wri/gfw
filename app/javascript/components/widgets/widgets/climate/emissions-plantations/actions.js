3e0d91aaec63d5162a9d3b7d03e4b7be56f8bc63
Daniel Fernández Rico <deferrico@gmail.com>
2019-01-14 16:46:58 +0100

    minYear adjusted in actions, missing getRangeYears

--- app/javascript/components/widgets/widgets/climate/emissions-plantations/actions.js
+++ app/javascript/components/widgets/widgets/climate/emissions-plantations/actions.js
@@ -1,7 +1,6 @@
 import { getAdmin } from 'services/forest-data';
 import axios from 'axios';
 import maxBy from 'lodash/maxBy';
-import minBy from 'lodash/minBy';
 import range from 'lodash/range';
 
 export default ({ params }) =>
@@ -15,15 +14,11 @@ export default ({ params }) =>
         const adminData = admin.data && admin.data.data;
         const plantData = plantations.data && plantations.data.data;
 
-        const minYear = Math.max(
-          minBy(adminData, 'year').year,
-          minBy(adminData, 'year').year
-        );
         const maxYear = Math.max(
           maxBy(adminData, 'year').year,
           maxBy(adminData, 'year').year
         );
 
-        return { adminData, plantData, years: range(minYear, maxYear) };
+        return { adminData, plantData, years: range(2013, maxYear) };
       })
     );
