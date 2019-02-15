fdee9820ebf8934d1695f30c3c9bb61ba65ec945
Daniel Fernández Rico <deferrico@gmail.com>
2019-02-15 12:34:53 +0100

    Fixed issue with empty list with NaN values

--- app/javascript/components/widgets/widgets/biodiversity/intactness/selectors.js
+++ app/javascript/components/widgets/widgets/biodiversity/intactness/selectors.js
@@ -11,7 +11,7 @@ const getData = state => state.data && state.data;
 const getLocationName = state => state.locationName || null;
 const getLocation = state => state.location || null;
 const getAllLocation = state => state.allLocation || null;
-const getLocationDict = state => state.locationDict || null;
+const getChildLocationDict = state => state.childLocationDict || null;
 const getSettings = state => state.settings || null;
 
 const normalizeInt = d => ({
@@ -28,7 +28,7 @@ export const parsePayload = payload =>
   payload && { percentile: payload.activeLabel };
 
 const parseData = createSelector(
-  [getData, getSettings, getLocationDict, getLocation],
+  [getData, getSettings, getChildLocationDict, getLocation],
   (data, settings, childLocations, location) => {
     if (isEmpty(data) || isEmpty(childLocations)) {
       return null;
