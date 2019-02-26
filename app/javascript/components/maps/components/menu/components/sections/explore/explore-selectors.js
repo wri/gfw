7aaecea5b41dc55003f836b76bf1c35b4b02ef60
Ed Brett <e.j.a.brett@gmail.com>
2019-02-26 18:07:54 +0100

    reverse bbox in ptw

--- app/javascript/components/maps/components/menu/components/sections/explore/explore-selectors.js
+++ app/javascript/components/maps/components/menu/components/sections/explore/explore-selectors.js
@@ -1,6 +1,6 @@
 import { createSelector, createStructuredSelector } from 'reselect';
 import bbox from 'turf-bbox';
-import linestring from 'turf-linestring';
+import lineString from 'turf-linestring';
 
 import {
   getActiveDatasetsFromState,
@@ -17,56 +17,61 @@ const selectPTWData = state => {
 
   return (
     data &&
-    data.map(d => ({
-      id: d.cartodb_id,
-      image: d.image,
-      imageCredit: d.image_source,
-      title: d.name,
-      summary: d.description,
-      buttons: [
-        {
-          text: 'READ MORE',
-          extLink: d.link,
-          theme: 'theme-button-light theme-button-small'
-        },
-        {
-          text: 'VIEW ON MAP',
-          theme: 'theme-button-small'
-        }
-      ],
-      payload: {
-        mergeQuery: true,
-        map: {
-          datasets: [
-            // admin boundaries
-            {
-              dataset: 'fdc8dc1b-2728-4a79-b23f-b09485052b8d',
-              layers: [
-                '6f6798e6-39ec-4163-979e-182a74ca65ee',
-                'c5d1e010-383a-4713-9aaa-44f728c0571c'
-              ],
-              opacity: 1,
-              visibility: true
-            },
-            // GLADs
-            {
-              dataset: 'e663eb09-04de-4f39-b871-35c6c2ed10b5',
-              layers: [
-                '581ecc62-9a70-4ef4-8384-0d59363e511d',
-                'dd5df87f-39c2-4aeb-a462-3ef969b20b66'
-              ],
-              opacity: 1,
-              visibility: true,
-              bbox: bbox(linestring(d.bbox))
-            }
-          ],
-          basemap: {
-            value: 'default'
+    data.map(d => {
+      const bboxCoords = d.bbox.slice(0, 4).map(o => o.reverse());
+      const bboxFromCoords = bbox(lineString(bboxCoords));
+
+      return {
+        id: d.cartodb_id,
+        image: d.image,
+        imageCredit: d.image_source,
+        title: d.name,
+        summary: d.description,
+        buttons: [
+          {
+            text: 'READ MORE',
+            extLink: d.link,
+            theme: 'theme-button-light theme-button-small'
           },
-          label: 'default'
+          {
+            text: 'VIEW ON MAP',
+            theme: 'theme-button-small'
+          }
+        ],
+        payload: {
+          mergeQuery: true,
+          map: {
+            datasets: [
+              // admin boundaries
+              {
+                dataset: 'fdc8dc1b-2728-4a79-b23f-b09485052b8d',
+                layers: [
+                  '6f6798e6-39ec-4163-979e-182a74ca65ee',
+                  'c5d1e010-383a-4713-9aaa-44f728c0571c'
+                ],
+                opacity: 1,
+                visibility: true
+              },
+              // GLADs
+              {
+                dataset: 'e663eb09-04de-4f39-b871-35c6c2ed10b5',
+                layers: [
+                  '581ecc62-9a70-4ef4-8384-0d59363e511d',
+                  'dd5df87f-39c2-4aeb-a462-3ef969b20b66'
+                ],
+                opacity: 1,
+                visibility: true,
+                bbox: bboxFromCoords
+              }
+            ],
+            basemap: {
+              value: 'default'
+            },
+            label: 'default'
+          }
         }
-      }
-    }))
+      };
+    })
   );
 };
 
