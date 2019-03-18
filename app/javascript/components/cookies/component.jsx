86e41f163b174119f1d2c59db50713189aba3b1b
Daniel Fernández Rico <deferrico@gmail.com>
2019-03-18 12:29:44 +0100

    Fixed cookies text styles and form

--- app/javascript/components/cookies/component.jsx
+++ app/javascript/components/cookies/component.jsx
@@ -11,13 +11,15 @@ class Cookies extends PureComponent {
     return (
       <div className="c-cookies">
         <div className="row">
-          <div className="column small-12 medium-6 cookies-text">
-            This website uses cookies to provide you with an improved user
-            experience. By continuing to browse this site, you consent to the
-            use of cookies and similar technologies. Please visit our privacy
-            policy(linked to privacy policy) for further details.
+          <div className="column small-12 medium-8">
+            <p className="cookies-text">
+              This website uses cookies to provide you with an improved user
+              experience. By continuing to browse this site, you consent to the
+              use of cookies and similar technologies. Please visit our
+              <a href=""> privacy policy </a> for further details.
+            </p>
           </div>
-          <div className="column small-12 medium-6 cookies-button">
+          <div className="column small-12 medium-4 cookies-button">
             <Button
               theme="theme-button-grey theme-button-small"
               onClick={agreeCookies}
