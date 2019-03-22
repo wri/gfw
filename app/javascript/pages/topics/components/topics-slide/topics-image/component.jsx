2ba288b8640ff9e9346325c29e78bc029052c5f5
Ed Brett <e.j.a.brett@gmail.com>
2019-03-22 15:38:50 +0100

    add animations

--- app/javascript/pages/topics/components/topics-slide/topics-image/component.jsx
+++ app/javascript/pages/topics/components/topics-slide/topics-image/component.jsx
@@ -1,14 +1,25 @@
 import React, { PureComponent, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import lottie from 'lottie-web';
 
 import Button from 'components/ui/button';
 import Icon from 'components/ui/icon';
 import infoIcon from 'assets/icons/info.svg';
 import { Tooltip } from 'react-tippy';
+import data from './data.json';
 
 import './styles.scss';
 
 class TopicsImage extends PureComponent {
+  componentDidMount() {
+    lottie.loadAnimation({
+      wrapper: this.wrapper,
+      animType: 'svg',
+      loop: true,
+      animationData: data
+    });
+  }
+
   render() {
     const { img1x, img2x, description, prompts } = this.props;
 
@@ -18,6 +29,12 @@ class TopicsImage extends PureComponent {
           srcSet={`${img1x} 2x, ${img2x} 1x`}
           src={`${img1x} 1x`}
           alt={description}
+        />
+        <div
+          ref={ref => {
+            this.wrapper = ref;
+          }}
+          id="svg-container"
         />
         {prompts &&
           prompts.map(p => (
