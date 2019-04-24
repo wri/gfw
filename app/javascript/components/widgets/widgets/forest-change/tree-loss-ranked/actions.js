0efd43f69cfcbc15f3ded19d5ad08102950a5819
Adam J. Pain <aj.pain89@gmail.com>
2019-04-24 11:57:26 +0200

    Update loss ranked for iso

--- app/javascript/components/widgets/widgets/forest-change/tree-loss-ranked/actions.js
+++ app/javascript/components/widgets/widgets/forest-change/tree-loss-ranked/actions.js
@@ -1,23 +1,35 @@
 import axios from 'axios';
 import { getLossGrouped, getExtentGrouped } from 'services/forest-data';
 
-export default ({ params }) =>
-  axios.all([getLossGrouped(params), getExtentGrouped(params)]).then(
-    axios.spread((lossResponse, extentResponse) => {
-      const { data } = lossResponse.data;
-      let mappedData = [];
-      if (data && data.length) {
-        mappedData = data.map(item => {
-          const loss = item.loss || 0;
-          return {
-            ...item,
-            loss
-          };
-        });
-      }
-      return {
-        loss: mappedData,
-        extent: extentResponse.data.data
-      };
-    })
-  );
+export default ({ params }) => {
+  const { adm0, adm1, adm2, ...rest } = params || {};
+  const parentLocation = {
+    adm0: adm0 && !adm1 ? null : adm0,
+    adm1: adm1 && !adm2 ? null : adm1,
+    adm2: null
+  };
+  return axios
+    .all([
+      getLossGrouped({ ...rest, ...parentLocation, test: 'test' }),
+      getExtentGrouped({ ...rest, ...parentLocation })
+    ])
+    .then(
+      axios.spread((lossResponse, extentResponse) => {
+        const { data } = lossResponse.data;
+        let mappedData = [];
+        if (data && data.length) {
+          mappedData = data.map(item => {
+            const loss = item.area_loss || 0;
+            return {
+              ...item,
+              loss
+            };
+          });
+        }
+        return {
+          loss: mappedData,
+          extent: extentResponse.data.data
+        };
+      })
+    );
+};
