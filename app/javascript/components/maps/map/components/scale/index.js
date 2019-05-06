8dd45074924c01b9788b9fcfcc1f4390fd795ab8
Ed Brett <e.j.a.brett@gmail.com>
2019-05-06 18:14:30 +0200

    remove old map

--- app/javascript/components/maps/map/components/scale/index.js
+++
@@ -1,6 +0,0 @@
-import { connect } from 'react-redux';
-
-import { getMapScaleProps } from './selectors';
-import Component from './component';
-
-export default connect(getMapScaleProps)(Component);
