787a8c7d89b84fdec7226984415bc3ac8d4b9a69
Ed Brett <e.j.a.brett@gmail.com>
2019-05-08 13:25:52 +0200

    check reponse for data

--- app/javascript/components/widgets/widgets/land-use/trase-commodities/actions.js
+++ app/javascript/components/widgets/widgets/land-use/trase-commodities/actions.js
@@ -15,53 +15,56 @@ const TRASE_COUNTRY_NAMES = {
 
 export default ({ params }) =>
   fetchTraseContexts().then(response => {
-    const { startYear, endYear, commodity } = params;
+    if (response.data && response.data.data) {
+      const { startYear, endYear, commodity } = params;
 
-    const contextsForLocation = response.data.data.filter(
-      d => d.countryName === TRASE_COUNTRY_NAMES[params.adm0]
-    );
+      const contextsForLocation = response.data.data.filter(
+        d => d.countryName === TRASE_COUNTRY_NAMES[params.adm0]
+      );
 
-    const allCommodities = uniqBy(contextsForLocation, 'commodityName').map(
-      c => c.commodityName
-    );
-    const commoditiesForLocation = sortBy(
-      allCommodities.map(c => ({
-        label: c,
-        value: c
-      })),
-      'label'
-    );
+      const allCommodities = uniqBy(contextsForLocation, 'commodityName').map(
+        c => c.commodityName
+      );
+      const commoditiesForLocation = sortBy(
+        allCommodities.map(c => ({
+          label: c,
+          value: c
+        })),
+        'label'
+      );
 
-    const activeCommodity =
-      commodity && allCommodities.find(c => c === commodity);
-    const selectedCommodity = activeCommodity || allCommodities[0];
+      const activeCommodity =
+        commodity && allCommodities.find(c => c === commodity);
+      const selectedCommodity = activeCommodity || allCommodities[0];
 
-    const selectedContext = contextsForLocation.find(
-      c => c.commodityName === selectedCommodity
-    );
+      const selectedContext = contextsForLocation.find(
+        c => c.commodityName === selectedCommodity
+      );
 
-    const minYear = selectedContext.years[0];
-    const maxYear = selectedContext.years[selectedContext.years.length - 1];
-    const yearsRange = [minYear, maxYear];
+      const minYear = selectedContext.years[0];
+      const maxYear = selectedContext.years[selectedContext.years.length - 1];
+      const yearsRange = [minYear, maxYear];
 
-    return fetchTraseLocationData(
-      selectedContext.id,
-      selectedContext.worldMap.countryColumnId,
-      startYear,
-      endYear
-    ).then(data => ({
-      data: {
-        context: selectedContext,
-        topNodes: data.data.data
-      },
-      options: {
-        yearsRange,
-        commodities: commoditiesForLocation
-      },
-      settings: {
-        startYear: !startYear || startYear < minYear ? minYear : startYear,
-        endYear: !endYear || endYear > maxYear ? maxYear : endYear,
-        commodity: selectedCommodity
-      }
-    }));
+      return fetchTraseLocationData(
+        selectedContext.id,
+        selectedContext.worldMap.countryColumnId,
+        startYear,
+        endYear
+      ).then(data => ({
+        data: {
+          context: selectedContext,
+          topNodes: data.data.data
+        },
+        options: {
+          yearsRange,
+          commodities: commoditiesForLocation
+        },
+        settings: {
+          startYear: !startYear || startYear < minYear ? minYear : startYear,
+          endYear: !endYear || endYear > maxYear ? maxYear : endYear,
+          commodity: selectedCommodity
+        }
+      }));
+    }
+    return {};
   });
