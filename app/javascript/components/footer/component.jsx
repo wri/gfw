e52bcfdf81cf00ff6a0093dd697734147571098a
Ed Brett <e.j.a.brett@gmail.com>
2019-05-26 23:55:58 +0200

    fix grid on footer for carousel

--- app/javascript/components/footer/component.jsx
+++ app/javascript/components/footer/component.jsx
@@ -39,181 +39,166 @@ class Footer extends PureComponent {
               </div>
             </div>
             <div className="footer-main">
+              <div className="row footer-links">
+                <div className="column small-12 medium-6">
+                  <ul className="footer-links-texts">
+                    <li>
+                      <a href="http://www.globalforestwatch.org/">GFW</a>
+                    </li>
+                    <li>
+                      <a href="http://blog.globalforestwatch.org/">Blog</a>
+                    </li>
+                    <li>
+                      <a href="http://www.globalforestwatch.org/howto/">
+                        How to portal
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <div className="column small-12 medium-6">
+                  <ul className="footer-links-social">
+                    <li>
+                      <a
+                        href="https://twitter.com/globalforests"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <Icon icon={twitter} />
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://www.facebook.com/globalforests/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <Icon icon={facebook} />
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://www.instagram.com/globalforests/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <Icon icon={instagram} />
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://www.youtube.com/channel/UCAsamYre1KLulf4FD-xJfLA"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <Icon icon={youtube} />
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://groups.google.com/forum/#!forum/globalforestwatch"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <Icon icon={googleplus} />
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <div className="column small-12">
+                  <div className="footer-contact-us">
+                    <button
+                      onClick={() => this.props.setModalContactUsOpen(true)}
+                    >
+                      CONTACT US
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="row footer-partners">
+                <div className="column small-12 medium-3">
+                  <div className="footer-wri">
+                    <p>A partnership convened by</p>
+                    <a
+                      href="http://www.wri.org/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Icon className="wri-logo" icon={wri} />
+                    </a>
+                  </div>
+                </div>
+                <div className="column small-12 medium-9">
+                  <div className="footer-partners-slide">
+                    <div className="footer-logos">
+                      <p>Partners</p>
+                      <Carousel
+                        className="partners-slide"
+                        settings={{
+                          slidesToShow: isDesktop ? 4 : 2,
+                          slidesToScroll: isDesktop ? 4 : 2,
+                          infinite: true,
+                          nextArrow: (
+                            <Button theme="theme-button-clear square">
+                              <Icon icon={arrowIcon} />
+                            </Button>
+                          ),
+                          prevArrow: (
+                            <Button theme="theme-button-clear square">
+                              <Icon icon={arrowIcon} />
+                            </Button>
+                          )
+                        }}
+                      >
+                        {partners.map((p, i) => (
+                          <li className="carousel-slide" key={`${p.name}${i}`}>
+                            <a
+                              target="_blank"
+                              hrel="noopener noreferrer"
+                              href={p.url}
+                            >
+                              <img
+                                onMouseEnter={e => {
+                                  e.currentTarget.src = e.currentTarget.src.replace(
+                                    '.png',
+                                    'hover.png'
+                                  );
+                                }}
+                                onMouseLeave={e => {
+                                  e.currentTarget.src = e.currentTarget.src.replace(
+                                    'hover',
+                                    ''
+                                  );
+                                }}
+                                src={`/assets/logos/${p.name}.png`}
+                                alt={p.name}
+                              />
+                            </a>
+                          </li>
+                        ))}
+                      </Carousel>
+                    </div>
+                  </div>
+                </div>
+              </div>
               <div className="row">
                 <div className="column small-12">
-                  <div className="footer-links">
-                    <div className="row">
-                      <div className="column small-12 medium-6">
-                        <ul className="footer-links-texts">
-                          <li>
-                            <a href="http://www.globalforestwatch.org/">GFW</a>
-                          </li>
-                          <li>
-                            <a href="http://blog.globalforestwatch.org/">
-                              Blog
-                            </a>
-                          </li>
-                          <li>
-                            <a href="http://www.globalforestwatch.org/howto/">
-                              How to portal
-                            </a>
-                          </li>
-                        </ul>
-                      </div>
-                      <div className="column small-12 medium-6">
-                        <ul className="footer-links-social">
-                          <li>
-                            <a
-                              href="https://twitter.com/globalforests"
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            >
-                              <Icon icon={twitter} />
-                            </a>
-                          </li>
-                          <li>
-                            <a
-                              href="https://www.facebook.com/globalforests/"
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            >
-                              <Icon icon={facebook} />
-                            </a>
-                          </li>
-                          <li>
-                            <a
-                              href="https://www.instagram.com/globalforests/"
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            >
-                              <Icon icon={instagram} />
-                            </a>
-                          </li>
-                          <li>
-                            <a
-                              href="https://www.youtube.com/channel/UCAsamYre1KLulf4FD-xJfLA"
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            >
-                              <Icon icon={youtube} />
-                            </a>
-                          </li>
-                          <li>
-                            <a
-                              href="https://groups.google.com/forum/#!forum/globalforestwatch"
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            >
-                              <Icon icon={googleplus} />
-                            </a>
-                          </li>
-                        </ul>
-                      </div>
-                      <div className="column small-12">
-                        <div className="footer-contact-us">
-                          <button
-                            onClick={() =>
-                              this.props.setModalContactUsOpen(true)
-                            }
-                          >
-                            CONTACT US
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="footer-partners">
-                    <div className="row">
-                      <div className="column small-12 medium-3">
-                        <div className="footer-wri">
-                          <p>A partnership convened by</p>
-                          <a
-                            href="http://www.wri.org/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                          >
-                            <Icon className="wri-logo" icon={wri} />
-                          </a>
-                        </div>
-                      </div>
-                      <div className="column small-12 medium-9">
-                        <div className="footer-partners-slide">
-                          <div className="footer-logos">
-                            <p>Partners</p>
-                            <Carousel
-                              className="partners-slide"
-                              settings={{
-                                slidesToShow: isDesktop ? 4 : 2,
-                                slidesToScroll: isDesktop ? 4 : 2,
-                                infinite: true,
-                                nextArrow: (
-                                  <Button theme="theme-button-clear square">
-                                    <Icon icon={arrowIcon} />
-                                  </Button>
-                                ),
-                                prevArrow: (
-                                  <Button theme="theme-button-clear square">
-                                    <Icon icon={arrowIcon} />
-                                  </Button>
-                                )
-                              }}
-                            >
-                              {partners.map((p, i) => (
-                                <li
-                                  className="carousel-slide"
-                                  key={`${p.name}${i}`}
-                                >
-                                  <a
-                                    target="_blank"
-                                    hrel="noopener noreferrer"
-                                    href={p.url}
-                                  >
-                                    <img
-                                      onMouseEnter={e => {
-                                        e.currentTarget.src = e.currentTarget.src.replace(
-                                          '.png',
-                                          'hover.png'
-                                        );
-                                      }}
-                                      onMouseLeave={e => {
-                                        e.currentTarget.src = e.currentTarget.src.replace(
-                                          'hover',
-                                          ''
-                                        );
-                                      }}
-                                      src={`/assets/logos/${p.name}.png`}
-                                      alt={p.name}
-                                    />
-                                  </a>
-                                </li>
-                              ))}
-                            </Carousel>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="column small-12">
-                      <div className="footer-terms">
-                        <a className="terms" href="/terms">
-                          Terms of Service
-                        </a>
-                        {' · '}
-                        <a className="terms" href="/privacy-policy">
-                          Privacy Policy
-                        </a>
-                        {' · '}
-                        <a
-                          className="terms"
-                          href="http://stats.pingdom.com/ghabapk9rihc"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          Global Forest Watch System Status
-                        </a>
-                      </div>
-                    </div>
+                  <div className="footer-terms">
+                    <a className="terms" href="/terms">
+                      Terms of Service
+                    </a>
+                    {' · '}
+                    <a className="terms" href="/privacy-policy">
+                      Privacy Policy
+                    </a>
+                    {' · '}
+                    <a
+                      className="terms"
+                      href="http://stats.pingdom.com/ghabapk9rihc"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Global Forest Watch System Status
+                    </a>
                   </div>
                 </div>
               </div>
