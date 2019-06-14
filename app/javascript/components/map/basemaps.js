a362de9668454e26e403f2443dba6740932146cb
Ed Brett <e.j.a.brett@gmail.com>
2019-06-14 17:13:25 +0200

    add roads and hillshade basemaps for all

--- app/javascript/components/map/basemaps.js
+++ app/javascript/components/map/basemaps.js
@@ -10,7 +10,7 @@ export default {
     backgroundColor: '#A2DFFF',
     image: defaultImage,
     mapStyle:
-      'mapbox://styles/resourcewatch/cjwhvxi9005u51cpa6dxjac7o?fresh=true'
+      'mapbox://styles/resourcewatch/cjww7iv8i07yx1cmjtgazn3r0?fresh=true'
   },
   dark: {
     label: 'dark matter',
@@ -18,7 +18,7 @@ export default {
     color: '#31312F',
     image: darkImage,
     mapStyle:
-      'mapbox://styles/resourcewatch/cjwt4gn0m04uu1cpanx2wq1bu?fresh=true'
+      'mapbox://styles/resourcewatch/cjww836hy1kep1co5xp717jek?fresh=true'
   },
   satellite: {
     label: 'Satellite',
@@ -26,7 +26,7 @@ export default {
     color: '#131620',
     image: satelliteImage,
     mapStyle:
-      'mapbox://styles/resourcewatch/cjvfcpw8n0dfd1fqfz59o63uq?fresh=true',
+      'mapbox://styles/resourcewatch/cjww89e5j08o91cmjsbrd47qt?fresh=true',
     url: 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'
   },
   landsat: {
@@ -35,7 +35,7 @@ export default {
     color: '#0C0045',
     image: landsatImage,
     mapStyle:
-      'mapbox://styles/resourcewatch/cjvfcryns0hww1fqnxc44yeyr?fresh=true',
+      'mapbox://styles/resourcewatch/cjww8drml27wc1cn3mk2872h9?fresh=true',
     url:
       'https://production-api.globalforestwatch.org/v2/landsat-tiles/{year}/{z}/{x}/{y}',
     availableYears: [2017, 2016, 2015, 2014, 2013],
@@ -47,7 +47,7 @@ export default {
     color: '#131620',
     image: satelliteImage,
     mapStyle:
-      'mapbox://styles/resourcewatch/cjvfcpw8n0dfd1fqfz59o63uq?fresh=true',
+      'mapbox://styles/resourcewatch/cjww89e5j08o91cmjsbrd47qt?fresh=true',
     url: `https://tiles.planet.com/basemaps/v1/planet-tiles/global_{frequency}_{period}_mosaic/gmap/{z}/{x}/{y}.png?api_key=${
       process.env.PLANET_API_KEY
     }`
