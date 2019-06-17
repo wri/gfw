9a63c738e70c2c4f365063b0dd398f331a2c4ffb
Ed Brett <e.j.a.brett@gmail.com>
2019-06-17 18:45:59 +0200

    update styles for not found with new icon

--- app/javascript/pages/not-found/component.jsx
+++ app/javascript/pages/not-found/component.jsx
@@ -1,35 +1,18 @@
 import React, { PureComponent } from 'react';
 
-import treeImage from './assets/tree.png';
+import Icon from 'components/ui/icon';
+import treeImage from 'assets/icons/error.svg';
 
 import './styles.scss';
-
-const supportedBrowsers = [
-  {
-    label: 'Chrome',
-    version: 50,
-    source: 'https://www.google.com/intl/en/chrome/browser'
-  },
-  {
-    label: 'Firefox',
-    version: 48,
-    source: 'https://www.mozilla.org/en-US/firefox/all'
-  },
-  {
-    label: 'Safari',
-    version: 10,
-    source: 'https://www.apple.com/safari'
-  }
-];
 
 class BrowserSupportPage extends PureComponent {
   render() {
     return (
-      <div className="l-browser-support-page">
+      <div className="l-not-found-page">
         <div className="row">
           <div className="column small-12 medium-8 medium-offset-2">
-            <div className="browser-message">
-              <img src={treeImage} alt="thank-you-tree" />
+            <div className="not-found-message">
+              <Icon icon={treeImage} className="error-tree" />
               <h1>Page Not Found</h1>
               <p>
                 You may have mistyped the address or the page may have moved.
