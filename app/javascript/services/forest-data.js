d73b6af97e4cbdaca699841da4e1ab0b3352938a
Ed Brett <e.j.a.brett@gmail.com>
2019-06-17 12:03:03 +0200

    add checks for category in dynamic where case

--- app/javascript/services/forest-data.js
+++ app/javascript/services/forest-data.js
@@ -90,19 +90,38 @@ const getRequestUrl = (adm0, adm1, adm2, grouped) => {
 };
 
 const getWHEREQuery = params => {
+  const allPolynames = forestTypes.concat(landCategories);
   const paramKeys = params && Object.keys(params);
   const paramKeysFiltered = paramKeys.filter(
     p => (params[p] || p === 'threshold') && ALLOWED_PARAMS.includes(p)
   );
+
   if (params) {
     let paramString = 'WHERE ';
     paramKeysFiltered.forEach((p, i) => {
       const isLast = paramKeysFiltered.length - 1 === i;
       const isPolyname = ['forestType', 'landCategory'].includes(p);
       const value = isPolyname ? 1 : params[p];
-      const polynameString = `${isPolyname ? params[p] : p} = ${
-        typeof value === 'number' ? value : `'${value}'`
-      }${isLast ? '' : ' AND '}`;
+      const polynameMeta = allPolynames.find(
+        pname => pname.value === params[p]
+      );
+
+      const polynameString = `
+        ${isPolyname ? `${params[p]} is not "0"` : ''}
+        ${
+  isPolyname &&
+          polynameMeta &&
+          polynameMeta.default &&
+          polynameMeta.categories
+    ? ` AND ${params[p]} = '${polynameMeta.default}'`
+    : ''
+}
+        ${
+  !isPolyname
+    ? `${p} = ${typeof value === 'number' ? value : `'${value}'`}`
+    : ''
+}
+        ${isLast ? '' : ' AND '}`;
 
       paramString = paramString.concat(polynameString);
     });
@@ -172,7 +191,17 @@ export const getAreaIntersection = ({
   }`
     .replace(/{location}/g, getLocationSelect({ adm0, adm1, adm2 }))
     .replace(/{intersection}/g, forestType || landCategory)
-    .replace('{WHERE}', getWHEREQuery({ iso: adm0, adm1, adm2, ...params }));
+    .replace(
+      '{WHERE}',
+      getWHEREQuery({
+        iso: adm0,
+        adm1,
+        adm2,
+        forestType,
+        landCategory,
+        ...params
+      })
+    );
 
   return request.get(url);
 };
@@ -191,7 +220,17 @@ export const getAreaIntersectionGrouped = ({
   }`
     .replace(/{location}/g, getLocationSelectGrouped({ adm0, adm1, adm2 }))
     .replace(/{intersection}/g, forestType || landCategory)
-    .replace('{WHERE}', getWHEREQuery({ iso: adm0, adm1, adm2, ...params }));
+    .replace(
+      '{WHERE}',
+      getWHEREQuery({
+        iso: adm0,
+        adm1,
+        adm2,
+        forestType,
+        landCategory,
+        ...params
+      })
+    );
 
   return request.get(url);
 };
