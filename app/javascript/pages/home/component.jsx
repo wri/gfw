f6456dfbe7d6623ae452767704f96164807bffc0
Ed Brett <e.j.a.brett@gmail.com>
2019-06-26 17:59:27 +0200

    fix links for cards for homepage news

--- app/javascript/pages/home/component.jsx
+++ app/javascript/pages/home/component.jsx
@@ -28,6 +28,7 @@ class Page extends PureComponent {
 
   render() {
     const { summary, uses, apps, news, newsLoading, isDesktop } = this.props;
+
     return (
       <div className="l-home-page">
         <Cover
@@ -228,15 +229,20 @@ class Page extends PureComponent {
                     }}
                   >
                     {news.map(item => (
-                      <Card
+                      <a
                         key={item.name}
-                        className="news-card"
-                        data={{
-                          title: item.name,
-                          summary: item.description,
-                          extLink: item.link
-                        }}
-                      />
+                        href={item.link}
+                        target="_blank"
+                        rel="noopener nofollower"
+                      >
+                        <Card
+                          className="news-card"
+                          data={{
+                            title: item.name,
+                            summary: item.description
+                          }}
+                        />
+                      </a>
                     ))}
                   </Carousel>
                 ) : (
