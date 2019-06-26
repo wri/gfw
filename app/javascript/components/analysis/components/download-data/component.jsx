cb39981ef17bbb6a6c6129b3da6d6cd7fed4e624
Daniel Fernández Rico <deferrico@gmail.com>
2019-06-26 12:29:46 +0200

    GLAD alerts now allows downloading data as a zip with correct file format extensions

--- app/javascript/components/analysis/components/download-data/component.jsx
+++ app/javascript/components/analysis/components/download-data/component.jsx
@@ -1,5 +1,8 @@
 import React, { PureComponent, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import JSZip from 'jszip';
+import JSZipUtils from 'jszip-utils';
+import { saveAs } from 'file-saver';
 
 import Icon from 'components/ui/icon';
 
@@ -8,13 +11,40 @@ import closeIcon from 'assets/icons/close.svg';
 import './styles.scss';
 
 class DownloadData extends PureComponent {
+  generateZipFromURL = (url, format) => {
+    const urlToPromise = _url =>
+      new Promise((resolve, reject) => {
+        JSZipUtils.getBinaryContent(_url, (err, data) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(data);
+          }
+        });
+      });
+
+    const zip = new JSZip();
+    let filename;
+    try {
+      const keywords = url.split('?')[0].split('/');
+      filename = keywords[keywords.indexOf('download') - 1];
+    } catch (error) {
+      filename = 'data';
+    }
+    filename = filename.concat(`.${format}`);
+    zip.file(filename, urlToPromise(url), { binary: true });
+    zip.generateAsync({ type: 'blob' }).then(content => {
+      saveAs(content, 'data.zip');
+    });
+  };
+
   renderDownloadLinks = downloads => (
     <Fragment key={downloads.label}>
       <span>{downloads.label}</span>
       <ul className="download-list">
         {downloads.urls.map(l => (
           <li key={l.url}>
-            <a href={l.url} target="_blank">
+            <a href="#" onClick={() => this.generateZipFromURL(l.url, l.label)}>
               {l.label}
             </a>
           </li>
