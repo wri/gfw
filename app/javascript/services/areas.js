d93705d73b449ae9e44375a2aca566531751a7a2
Ed Brett <e.j.a.brett@gmail.com>
2019-06-27 16:28:07 +0200

    update to new area endpoint v2

--- app/javascript/services/areas.js
+++ app/javascript/services/areas.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const REQUEST_URL = `${process.env.GFW_API}/area`;
+const REQUEST_URL = `${process.env.GFW_API}/v2/area`;
 
 export const getAreasProvider = token =>
   axios.get(REQUEST_URL, {
