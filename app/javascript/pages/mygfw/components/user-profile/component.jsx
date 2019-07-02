02b6c60d1709066bf63833fd082aa1d11c822ef2
Ed Brett <e.j.a.brett@gmail.com>
2019-07-02 13:10:05 +0200

    update user profile to use real data

--- app/javascript/pages/mygfw/components/user-profile/component.jsx
+++ app/javascript/pages/mygfw/components/user-profile/component.jsx
@@ -1,9 +1,12 @@
 import React, { PureComponent } from 'react';
 import axios from 'axios';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 import Button from 'components/ui/button';
-// import Icon from 'components/ui/icon';
+import Icon from 'components/ui/icon';
+
+import pencilIcon from 'assets/icons/pencil.svg';
+import logoutIcon from 'assets/icons/logout.svg';
 
 import './styles.scss';
 
@@ -21,27 +24,39 @@ const logout = () =>
 
 class UserProfile extends PureComponent {
   render() {
-    // const { loggedIn, myGfwLoading } = this.props;
+    const { userData } = this.props;
+    const { fullName, email } = userData || {};
+
     return (
       <div className="c-user-profile">
-        <p>Jane Doe</p>
-        <p>
-          <i>jane@doe.org</i>
-        </p>
-        <Button theme="theme-button-clear theme-button-small">
+        {fullName && <p className="name">{fullName}</p>}
+        {email && (
+          <p className="email">
+            <i>{email}</i>
+          </p>
+        )}
+        <Button
+          className="user-btn"
+          theme="theme-button-clear theme-button-small"
+        >
           Update profile
-          {/* <Icon /> */}
+          <Icon className="user-btn-icon" icon={pencilIcon} />
         </Button>
-        <Button theme="theme-button-clear theme-button-small" onClick={logout}>
+        <Button
+          className="user-btn"
+          theme="theme-button-clear theme-button-small"
+          onClick={logout}
+        >
           Logout
-          {/* <Icon /> */}
+          <Icon className="user-btn-icon" icon={logoutIcon} />
         </Button>
       </div>
     );
   }
 }
 
-// UserProfile.propTypes = {
-// };
+UserProfile.propTypes = {
+  userData: PropTypes.object
+};
 
 export default UserProfile;
--- app/javascript/pages/mygfw/components/user-profile/index.js
+++ app/javascript/pages/mygfw/components/user-profile/index.js
@@ -1,3 +1,6 @@
+import { connect } from 'react-redux';
+
+import { getUserProfleProps } from './selectors';
 import Component from './component';
 
-export default Component;
+export default connect(getUserProfleProps)(Component);
