08ef5bff4ef2f6ad7d722af14e38978a1f5fe50d
Ed Brett <e.j.a.brett@gmail.com>
2019-07-11 16:57:50 +0200

    add no areas message to my gfw

--- app/javascript/pages/mygfw/component.jsx
+++ app/javascript/pages/mygfw/component.jsx
@@ -3,13 +3,15 @@ import PropTypes from 'prop-types';
 
 import MyGFWLogin from 'components/mygfw-login';
 import Loader from 'components/ui/loader';
+import Button from 'components/ui/button';
 
 import UserProfile from './components/user-profile';
+import NoAreasImage from './assets/no-areas@2x.png';
 import './styles.scss';
 
 class MyGFWPage extends PureComponent {
   render() {
-    const { loggedIn, myGfwLoading } = this.props;
+    const { loggedIn, myGfwLoading, areas } = this.props;
     return (
       <div className="l-mygfw-page">
         <div className="header-banner">
@@ -33,7 +35,34 @@ class MyGFWPage extends PureComponent {
             )}
             {loggedIn &&
               !myGfwLoading && (
-              <div className="column small-12">Areas of interest</div>
+              <div className="column small-12">
+                {areas && !!areas.length ? (
+                  <div className="areas-table">Look at them</div>
+                ) : (
+                  <div className="row no-areas">
+                    <div className="column small-12 medium-5">
+                      <img
+                        className="areas-image"
+                        srcSet={`${NoAreasImage} 2x, ${NoAreasImage} 1x`}
+                        src={`${NoAreasImage} 1x`}
+                        alt="no areas"
+                      />
+                    </div>
+                    <div className="column small-12 medium-6">
+                      <h4>You haven’t created any Areas of Interest yet</h4>
+                      <p>
+                          Creating an Area of Interest lets you customize and
+                          perform an in-depth analysis of the area, as well as
+                          receiving email notifications when new deforestation
+                          alerts are available.
+                      </p>
+                      <Button className="learn-btn" link="/map">
+                          Learn how
+                      </Button>
+                    </div>
+                  </div>
+                )}
+              </div>
             )}
           </div>
         </div>
@@ -44,7 +73,8 @@ class MyGFWPage extends PureComponent {
 
 MyGFWPage.propTypes = {
   loggedIn: PropTypes.bool,
-  myGfwLoading: PropTypes.bool
+  myGfwLoading: PropTypes.bool,
+  areas: PropTypes.array
 };
 
 export default MyGFWPage;
