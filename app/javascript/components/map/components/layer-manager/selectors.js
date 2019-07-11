26f87b5385f481b17007b0ef864e85f53f04e21d
Ed Brett <e.j.a.brett@gmail.com>
2019-07-11 16:12:38 +0200

    custom styles for aoi grojsons

--- app/javascript/components/map/components/layer-manager/selectors.js
+++ app/javascript/components/map/components/layer-manager/selectors.js
@@ -1,4 +1,4 @@
-import { createStructuredSelector } from 'reselect';
+import { createStructuredSelector, createSelector } from 'reselect';
 
 import {
   getActiveLayersWithDates,
@@ -7,9 +7,17 @@ import {
   selectGeostore
 } from 'components/map/selectors';
 
+export const selectLocation = state => state.location && state.location.payload;
+
+export const getIsAoI = createSelector(
+  [selectLocation],
+  location => location.type === 'aoi'
+);
+
 export const getLayerManagerProps = createStructuredSelector({
   layers: getActiveLayersWithDates,
   geostore: selectGeostore,
   basemap: getBasemap,
-  drawing: getDrawing
+  drawing: getDrawing,
+  isAoI: getIsAoI
 });
