3ed10fe1740dea2e1e7b7c4579b7080bee5cb633
Daniel Fernández Rico <deferrico@gmail.com>
2019-07-19 13:23:50 +0200

    Fixed typo in img imports

--- app/javascript/components/analysis/components/show-analysis/component.jsx
+++ app/javascript/components/analysis/components/show-analysis/component.jsx
@@ -12,7 +12,7 @@ import Button from 'components/ui/button';
 import Widgets from 'components/widgets';
 import DownloadData from 'components/analysis/components/download-data';
 
-import screensImg1x from 'assets/images/aois/double2x.png';
+import screensImg1x from 'assets/images/aois/double.png';
 import screensImg2x from 'assets/images/aois/double @2x.png';
 import arrowDownIcon from 'assets/icons/arrow-down.svg';
 import shareIcon from 'assets/icons/share.svg';
