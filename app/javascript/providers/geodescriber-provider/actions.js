79620e1367a8758cbb8fa6338bc417f4b6732372
Ed Brett <e.j.a.brett@gmail.com>
2019-08-14 11:59:45 +0200

    remove commented code

--- app/javascript/providers/geodescriber-provider/actions.js
+++ app/javascript/providers/geodescriber-provider/actions.js
@@ -5,28 +5,9 @@ import groupBy from 'lodash/groupBy';
 import sumBy from 'lodash/sumBy';
 import max from 'lodash/max';
 import reverse from 'lodash/reverse';
-// import tropicalIsos from 'data/tropical-isos.json';
-// import { format } from 'd3-format';
 
 import { getExtent, getLoss } from 'services/forest-data';
 import { getGeodescriberService } from 'services/geostore';
-
-// const adminSentences = {
-//   default:
-//     'In 2010, {location} had {extent} of tree cover, extending over {percentage} of its land area.',
-//   withLoss:
-//     'In 2010, {location} had {extent} of tree cover, extending over {percentage} of its land area. In {year}, it lost {loss} of tree cover',
-//   globalInitial:
-//     'In 2010, {location} had {extent} of tree cover, extending over {percentage} of its land area. In {year}, it lost {loss} of tree cover.',
-//   withPlantationLoss:
-//     'In 2010, {location} had {naturalForest} of natural forest, extending over {percentage} of its land area. In {year}, it lost {naturalLoss} of natural forest',
-//   countrySpecific: {
-//     IDN:
-//       'In 2010, {location} had {naturalForest} of natural forest, extending over {percentageNatForest} of its land area. In {year}, it lost {loss} of tree cover, equivalent to {emissionsTreeCover} of CO₂ of emissions. {primaryLoss} of this loss occurred within primary forests and {naturalLoss} within natural forest.'
-//   },
-//   co2Emissions: ', equivalent to {emission} of CO\u2082 of emissions.',
-//   end: '.'
-// };
 
 export const setGeodescriberLoading = createAction('setGeodescriberLoading');
 export const setGeodescriber = createAction('setGeodescriber');
@@ -151,92 +132,3 @@ export const getAdminStats = params =>
         }
       )
     );
-
-// export const getAdminSentence = (data, params) => {
-//   const {
-//     withLoss,
-//     withPlantationLoss,
-//     globalInitial,
-//     countrySpecific,
-//     co2Emissions,
-//     end
-//   } = adminSentences;
-
-//   const { adm0 } = params;
-
-//   const extent =
-//     data.extent < 1 ? format('.3r')(data.extent) : format('.3s')(data.extent);
-//   const naturalForest =
-//     data.extent - data.plantationsExtent < 1
-//       ? format('.3r')(data.extent - data.plantationsExtent)
-//       : format('.3s')(data.extent - data.plantationsExtent);
-//   const percentageCover = format('.2r')(data.extent / data.totalArea * 100);
-//   const percentageNatForest = format('.2r')(
-//     (data.extent - data.plantationsExtent) / data.totalArea * 100
-//   );
-//   const lossWithoutPlantations = format('.3s')(
-//     data.totalLoss.area - (data.plantationsLoss.area || 0)
-//   );
-//   const emissionsWithoutPlantations = format('.3s')(
-//     data.totalLoss.emissions - (data.plantationsLoss.emissions || 0)
-//   );
-//   const emissions = format('.3s')(data.totalLoss.emissions);
-//   const primaryLoss = format('.3s')(data.primaryLoss.area || 0);
-//   const loss = format('.3s')(data.totalLoss.area || 0);
-//   const location = locationNames && locationNames.label;
-
-//   const sentenceParams = {
-//     extent: `${extent}ha`,
-//     naturalForest: `${naturalForest}ha`,
-//     location: location || 'the world',
-//     percentage: `${percentageCover}%`,
-//     percentageNatForest: `${percentageNatForest}%`,
-//     naturalLoss: `${lossWithoutPlantations}ha`,
-//     loss: `${loss}ha`,
-//     emission: `${emissionsWithoutPlantations}t`,
-//     emissionsTreeCover: `${emissions}t`,
-//     year: data.totalLoss.year,
-//     treeCoverLoss: `${loss}ha`,
-//     primaryLoss: `${primaryLoss}ha`
-//   };
-
-//   let sentence = adminSentences.default;
-//   if (data.extent > 0 && data.totalLoss.area) {
-//     sentence =
-//       data.plantationsLoss.area && location ? withPlantationLoss : withLoss;
-//   }
-//   sentence = tropicalIsos.includes(adm0)
-//     ? sentence + co2Emissions
-//     : sentence + end;
-//   if (!location) sentence = globalInitial;
-//   if (adm0 in countrySpecific) {
-//     sentence = countrySpecific[adm0];
-//   }
-
-//   return {
-//     sentence,
-//     params: sentenceParams
-//   };
-// };
-
-// export const getAdminsSelected = createSelector(
-//   [getAdm0Data, getAdm1Data, getAdm2Data, selectLocation],
-//   (adm0s, adm1s, adm2s, location) => {
-//     const adm0 = (adm0s && adm0s.find(i => i.value === location.adm0)) || null;
-//     const adm1 = (adm1s && adm1s.find(i => i.value === location.adm1)) || null;
-//     const adm2 = (adm2s && adm2s.find(i => i.value === location.adm2)) || null;
-//     let current = adm0;
-//     if (location.adm2) {
-//       current = adm2;
-//     } else if (location.adm1) {
-//       current = adm1;
-//     }
-
-//     return {
-//       ...current,
-//       adm0,
-//       adm1,
-//       adm2
-//     };
-//   }
-// );
