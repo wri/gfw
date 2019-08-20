4aa2eb1b2707675b715e20839e6702bb8cb5f8a8
Daniel Fernández Rico <deferrico@gmail.com>
2019-08-20 14:38:09 +0200

    Fixed source and target nodes

--- app/javascript/components/widgets/widgets/land-cover/us-land-cover/selectors.js
+++ app/javascript/components/widgets/widgets/land-cover/us-land-cover/selectors.js
@@ -9,33 +9,36 @@ const getLocationName = state => state.locationName || null;
 const getColors = state => state.colors || null;
 const getSentences = state => state.config.sentence || null;
 
-export const cleanData = createSelector([getData], data => {
-  if (isEmpty(data)) return null;
-  // return data.filter(d => d.start !== d.end);
-  return data;
-});
+export const cleanData = createSelector(
+  [getData, getSettings],
+  (data, settings) => {
+    if (isEmpty(data)) return null;
+    // const { filter } = settings;
+    let { source } = settings;
+    if (!source) source = 'ipcc';
+    // if (filter)
+    return data.filter(
+      d => d[`from_class_${source}`] !== d[`to_class_${source}`]
+    );
+    // return data;
+  }
+);
 
 export const parseData = createSelector(
   [cleanData, getColors, getSettings],
   (data, colors, settings) => {
     if (isEmpty(data)) return null;
-    const { source } = settings;
+    let { source } = settings;
     const categories = {
-      forest: 'Forest',
+      forest_land: 'Forest',
       bare: 'Bare',
-      settlements: 'Settlement',
+      settlement: 'Settlement',
       cropland: 'Agriculture',
       grassland: 'Grassland',
-      wetlands: 'Wetland'
+      wetland: 'Wetland'
     };
-    /*
-    obj has:
-      area: 97736.7208320633
-      from_class_ipcc: "forest_land"
-      from_class_nlcd: "deciduous_forest"
-      to_class_ipcc: "settlement"
-      to_class_nlcd: "developed_low_intensity"
-    */
+
+    if (!source) source = 'ipcc';
     const nodes = [
       ...Object.keys(groupBy(data, `from_class_${source}`)).map(node => ({
         name: categories[node] ? categories[node] : 'Other',
@@ -53,8 +56,8 @@ export const parseData = createSelector(
       }))
     ];
     const links = data.map(d => ({
-      source: findIndex(nodes, { key: `${d.start}-start` }),
-      target: findIndex(nodes, { key: `${d.end}-end` }),
+      source: findIndex(nodes, { key: `${d[`from_class_${source}`]}-start` }),
+      target: findIndex(nodes, { key: `${d[`to_class_${source}`]}-end` }),
       value: d.area
       // abs_pct: d.perc_area
     }));
