b23fbc80fa2caac8269db411c1ddd5d048554151
Ed Brett <e.j.a.brett@gmail.com>
2019-08-22 18:18:17 +0200

    update passing of AoI geostore to layer manager

--- app/javascript/components/map/components/layer-manager/component.jsx
+++ app/javascript/components/map/components/layer-manager/component.jsx
@@ -6,6 +6,92 @@ import { PluginMapboxGl } from 'layer-manager';
 class LayerManagerComponent extends PureComponent {
   render() {
     const { layers, geostore, setMapLoading, basemap, map, isAoI } = this.props;
+    const geostoreLayer =
+      geostore && geostore.id && !isAoI
+        ? {
+          id: geostore.id,
+          name: 'Geojson',
+          provider: 'geojson',
+          layerConfig: {
+            data: geostore.geojson,
+            body: {
+              vectorLayers: [
+                {
+                  type: 'fill',
+                  paint: {
+                    'fill-color': 'transparent'
+                  }
+                },
+                {
+                  type: 'line',
+                  paint: {
+                    'line-color': '#000',
+                    'line-width': 2
+                  }
+                }
+              ]
+            }
+          },
+          zIndex: 1060
+        }
+        : null;
+
+    const aoiLayer =
+      geostore && geostore.id && isAoI
+        ? {
+          id: geostore.id,
+          name: 'Geojson',
+          provider: 'geojson',
+          layerConfig: {
+            data: geostore.geojson,
+            body: {
+              vectorLayers: [
+                {
+                  type: 'fill',
+                  paint: {
+                    'fill-color': 'transparent'
+                  }
+                },
+                {
+                  type: 'line',
+                  paint: {
+                    'line-color': '#C0FF24',
+                    'line-width': 3,
+                    'line-offset': 0
+                  }
+                },
+                {
+                  type: 'line',
+                  paint: {
+                    'line-color': '#000',
+                    'line-width': 2
+                  }
+                }
+              ]
+            }
+          },
+          zIndex: 1060
+        }
+        : null;
+
+    const basemapLayer =
+      basemap && basemap.url
+        ? {
+          id: basemap.url,
+          name: 'Basemap',
+          provider: 'leaflet',
+          layerConfig: {
+            body: {
+              url: basemap.url
+            }
+          },
+          zIndex: 100
+        }
+        : null;
+
+    const allLayers = [geostoreLayer, aoiLayer, basemapLayer]
+      .concat(layers)
+      .filter(l => l);
 
     return (
       <LayerManager
@@ -13,58 +99,7 @@ class LayerManagerComponent extends PureComponent {
         plugin={PluginMapboxGl}
         onLayerLoading={loading => setMapLoading(loading)}
       >
-        {geostore &&
-          geostore.id && (
-          <Layer
-            id={geostore.id}
-            name="Geojson"
-            provider="geojson"
-            layerConfig={{
-              data: geostore.geojson,
-              body: {
-                vectorLayers: [
-                  {
-                    type: 'fill',
-                    paint: {
-                      'fill-color': 'transparent'
-                    }
-                  },
-                  {
-                    type: 'line',
-                    paint: {
-                      'line-color': '#C0FF24',
-                      'line-width': isAoI ? 3 : 1,
-                      'line-offset': isAoI ? 2 : 0
-                    }
-                  },
-                  {
-                    type: 'line',
-                    paint: {
-                      'line-color': '#000',
-                      'line-width': 2
-                    }
-                  }
-                ]
-              }
-            }}
-            zIndex={1060}
-          />
-        )}
-        {layers && layers.map(l => <Layer key={l.id} {...l} />)}
-        {basemap &&
-          basemap.url && (
-          <Layer
-            id="basemap"
-            name="Basemap"
-            provider="leaflet"
-            layerConfig={{
-              body: {
-                url: basemap.url
-              }
-            }}
-            zIndex={100}
-          />
-        )}
+        {allLayers && allLayers.map(l => <Layer key={l.id} {...l} />)}
       </LayerManager>
     );
   }
