e5492e70d7ba7ba5715393507f7e1889b6cb3bbb
Daniel Fernández Rico <deferrico@gmail.com>
2019-08-29 15:59:23 +0200

    Cleaned sankey link into a function

--- app/javascript/components/charts/sankey-chart/sankey-link/sankey-link-component.jsx
+++ app/javascript/components/charts/sankey-chart/sankey-link/sankey-link-component.jsx
@@ -1,44 +1,36 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { PropTypes } from 'prop-types';
 
 import './sankey-link-styles.scss';
 
-class SankeyLink extends PureComponent {
-  constructor() {
-    super();
-    this.minLinkWidth = 2;
-  }
-
-  render() {
-    const {
-      sourceX,
-      sourceY,
-      sourceControlX,
-      targetX,
-      targetY,
-      targetControlX,
-      linkWidth,
-      config,
-      payload
-    } = this.props;
-    const updatedLinkWidth =
-      linkWidth < this.minLinkWidth ? this.minLinkWidth : linkWidth;
-    const linkStart = config.linkPadding || 140;
-    return (
-      <path
-        className="c-sankey-link"
-        d={`
-          M${sourceX + linkStart},${sourceY}
-          C${sourceControlX},${sourceY} ${targetControlX},${targetY} ${targetX -
-          linkStart},${targetY}
-        `}
-        fill="none"
-        stroke={config.highlight && config.highlight(payload) ? '#444' : '#ddd'}
-        strokeWidth={updatedLinkWidth}
-        strokeOpacity="0.4"
-      />
-    );
-  }
+function SankeyLink({
+  sourceX,
+  sourceY,
+  sourceControlX,
+  targetX,
+  targetY,
+  targetControlX,
+  linkWidth,
+  config,
+  payload
+}) {
+  const minLinkWidth = 2;
+  const updatedLinkWidth = linkWidth < minLinkWidth ? minLinkWidth : linkWidth;
+  const linkStart = config.linkPadding || 140;
+  return (
+    <path
+      className="c-sankey-link"
+      d={`
+        M${sourceX + linkStart},${sourceY}
+        C${sourceControlX},${sourceY} ${targetControlX},${targetY} ${targetX -
+        linkStart},${targetY}
+      `}
+      fill="none"
+      stroke={config.highlight && config.highlight(payload) ? '#444' : '#ddd'}
+      strokeWidth={updatedLinkWidth}
+      strokeOpacity="0.4"
+    />
+  );
 }
 
 SankeyLink.propTypes = {
