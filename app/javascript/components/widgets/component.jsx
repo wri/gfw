9feb6aec1bb569f651054643e4c813b88b4607f6
Ed Brett <e.j.a.brett@gmail.com>
2019-09-04 17:57:39 +0200

    fix responsive widget buttons and tabs

--- app/javascript/components/widgets/component.jsx
+++ app/javascript/components/widgets/component.jsx
@@ -1,7 +1,9 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
+import { SCREEN_M } from 'utils/constants';
 import { track } from 'app/analytics';
+import MediaQuery from 'react-responsive';
 
 import Loader from 'components/ui/loader';
 import NoContent from 'components/ui/no-content';
@@ -58,77 +60,82 @@ class Widgets extends PureComponent {
     } = this.props;
 
     return (
-      <div
-        className={cx(
-          'c-widgets',
-          className,
-          { simple: this.props.simple },
-          {
-            'no-widgets':
-              !loading &&
-              !noWidgetsMessage &&
-              (!widgets || widgets.length === 0)
-          }
-        )}
-      >
-        {loading && <Loader className="widgets-loader large" />}
-        {!loading &&
-          widgets &&
-          widgets.map(w => (
-            <Widget
-              key={w.widget}
-              {...w}
-              active={activeWidget && activeWidget.widget === w.widget}
-              embed={embed}
-              data={widgetsData && widgetsData[w.widget]}
-              settings={{
-                ...w.settings,
-                ...(query && query[w.widget])
-              }}
-              {...locationObj}
-              {...locationData}
-              location={location}
-              setWidgetData={data => setWidgetsData({ [w.widget]: data })}
-              handleChangeSettings={change =>
-                setWidgetSettings({ widget: w.widget, change })
+      <MediaQuery minWidth={SCREEN_M}>
+        {isDesktop => (
+          <div
+            className={cx(
+              'c-widgets',
+              className,
+              { simple: this.props.simple },
+              {
+                'no-widgets':
+                  !loading &&
+                  !noWidgetsMessage &&
+                  (!widgets || widgets.length === 0)
               }
-              handleShowMap={() => {
-                setActiveWidget(w.widget);
-                track('viewWidgetOnMap', {
-                  label: `${w.widget} in ${locationObj.locationLabel || ''}`
-                });
-              }}
-              handleSyncMap={setMapSettings}
-              handleShowInfo={setModalMetaSettings}
-              handleShowShare={() =>
-                setShareModal({
-                  title: 'Share this view',
-                  shareUrl: window.location.href.includes('embed')
-                    ? window.location.href.replace('/embed', '')
-                    : window.location.href,
-                  embedUrl: window.location.href.includes('embed')
-                    ? window.location.href
-                    : window.location.href.replace('/map', '/embed/map'),
-                  embedSettings: {
-                    width: 670,
-                    height: 490
+            )}
+          >
+            {loading && <Loader className="widgets-loader large" />}
+            {!loading &&
+              widgets &&
+              widgets.map(w => (
+                <Widget
+                  key={w.widget}
+                  {...w}
+                  large={w.large && isDesktop}
+                  active={activeWidget && activeWidget.widget === w.widget}
+                  embed={embed}
+                  data={widgetsData && widgetsData[w.widget]}
+                  settings={{
+                    ...w.settings,
+                    ...(query && query[w.widget])
+                  }}
+                  {...locationObj}
+                  {...locationData}
+                  location={location}
+                  setWidgetData={data => setWidgetsData({ [w.widget]: data })}
+                  handleChangeSettings={change =>
+                    setWidgetSettings({ widget: w.widget, change })
                   }
-                })
-              }
-              preventCloseSettings={modalClosing}
-              nonGlobalDatasets={nonGlobalDatasets}
-            />
-          ))}
-        {!loading &&
-          noWidgetsMessage &&
-          (!widgets || widgets.length === 0) && (
-          <NoContent
-            className="no-widgets-message large"
-            message={noWidgetsMessage}
-            icon
-          />
+                  handleShowMap={() => {
+                    setActiveWidget(w.widget);
+                    track('viewWidgetOnMap', {
+                      label: `${w.widget} in ${locationObj.locationLabel || ''}`
+                    });
+                  }}
+                  handleSyncMap={setMapSettings}
+                  handleShowInfo={setModalMetaSettings}
+                  handleShowShare={() =>
+                    setShareModal({
+                      title: 'Share this view',
+                      shareUrl: window.location.href.includes('embed')
+                        ? window.location.href.replace('/embed', '')
+                        : window.location.href,
+                      embedUrl: window.location.href.includes('embed')
+                        ? window.location.href
+                        : window.location.href.replace('/map', '/embed/map'),
+                      embedSettings: {
+                        width: 670,
+                        height: 490
+                      }
+                    })
+                  }
+                  preventCloseSettings={modalClosing}
+                  nonGlobalDatasets={nonGlobalDatasets}
+                />
+              ))}
+            {!loading &&
+              noWidgetsMessage &&
+              (!widgets || widgets.length === 0) && (
+              <NoContent
+                className="no-widgets-message large"
+                message={noWidgetsMessage}
+                icon
+              />
+            )}
+          </div>
         )}
-      </div>
+      </MediaQuery>
     );
   }
 }
