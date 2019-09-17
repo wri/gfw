d81543b5616f4b23acb8dcbf81e1a9c53d26e440
Ed Brett <e.j.a.brett@gmail.com>
2019-09-17 19:24:11 +0200

    update biodiversity widgets

--- app/javascript/components/widgets/biodiversity/glad-biodiversity/index.js
+++ app/javascript/components/widgets/biodiversity/glad-biodiversity/index.js
@@ -1,21 +1,55 @@
-import * as ParentWidget from '../../forest-change/glad-ranked';
-import childConfig from './config';
-import childSettings from './settings';
+import gladRanked from 'components/widgets/forest-change/glad-ranked';
 
-const Component = ParentWidget.Component;
-const getData = ParentWidget.getData;
-const getProps = ParentWidget.getProps;
-const parentConfig = ParentWidget.config;
-const parentSettings = ParentWidget.settings;
-
-const config = {
-  ...parentConfig,
-  ...childConfig
+export default {
+  ...gladRanked,
+  widget: 'gladBiodiversity',
+  title: 'Deforestation Alerts in Biodiversity Areas in {location}',
+  categories: ['biodiversity'],
+  options: {
+    landCategories: ['kba', 'aze', 'tiger_cl', 'wdpa'],
+    weeks: true,
+    extentYears: true,
+    units: ['%', 'ha'],
+    thresholds: true
+  },
+  settings: {
+    landCategory: 'kba',
+    period: 'week',
+    weeks: 13
+  },
+  settingsConfig: [
+    {
+      key: 'landCategory',
+      label: 'Land Category',
+      type: 'select',
+      whitelist: ['kba', 'aze', 'tiger_cl', 'wdpa'],
+      placeholder: 'All categories',
+      border: true
+    },
+    {
+      key: 'weeks',
+      label: 'weeks',
+      type: 'select',
+      whitelist: [13, 26, 52],
+      noSort: true
+    },
+    {
+      key: 'extentYear',
+      label: 'extent year',
+      type: 'switch'
+    },
+    {
+      key: 'unit',
+      label: 'unit',
+      whitelist: ['%', 'ha'],
+      type: 'switch'
+    },
+    {
+      key: 'threshold',
+      label: 'canopy density',
+      type: 'mini-select',
+      metaKey: 'widget_canopy_density'
+    }
+  ],
+  metaKey: 'widget_deforestation_alert_location_biodiversity'
 };
-
-const settings = {
-  ...parentSettings,
-  ...childSettings
-};
-
-export { Component, getProps, getData, config, settings };
