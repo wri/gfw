cd38ea3b2dc6bbec42f421770bb0a85b27c9ec71
Ed Brett <e.j.a.brett@gmail.com>
2019-10-14 18:17:43 +0200

    use landsat for areas basemap

--- app/javascript/components/map-geostore/selectors.js
+++ app/javascript/components/map-geostore/selectors.js
@@ -1,34 +1,17 @@
 import { createSelector, createStructuredSelector } from 'reselect';
+import max from 'lodash/max';
 
 import basemaps from 'components/map/basemaps';
 
 export const getBasemaps = () => basemaps;
-export const getPlanetBasemaps = state =>
-  state.planet && state.planet.data && state.planet.data;
 
-export const getLatestPlanetBasemap = createSelector(
-  [getPlanetBasemaps],
-  planetBasemaps => {
-    if (!planetBasemaps || !planetBasemaps.length) return null;
-    const quarterlyBasemaps = planetBasemaps.filter(
-      b => b.interval === '3 mons'
-    );
-    return quarterlyBasemaps[quarterlyBasemaps.length - 1];
-  }
-);
-
-export const getBasemap = createSelector(
-  [getBasemaps, getLatestPlanetBasemap],
-  (allBasemaps, latestPlanetBasemap) => {
-    if (!latestPlanetBasemap) return null;
-
-    const { planet } = allBasemaps;
-    return {
-      ...planet,
-      url: planet.url.replace('{name}', latestPlanetBasemap.name)
-    };
-  }
-);
+export const getBasemap = createSelector([getBasemaps], allBasemaps => {
+  const { landsat } = allBasemaps;
+  return {
+    ...landsat,
+    url: landsat.url.replace('{year}', max(landsat.availableYears))
+  };
+});
 
 export const getRecentImageMapProps = createStructuredSelector({
   basemap: getBasemap
