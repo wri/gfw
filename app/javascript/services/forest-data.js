d6eecb5538228bc42a4c40bc4090861e1c2b5e88
Daniel Fernández Rico <deferrico@gmail.com>
2020-01-07 17:55:41 +0100

    (WIP) renamed some FAO variables

--- app/javascript/services/forest-data.js
+++ app/javascript/services/forest-data.js
@@ -4,13 +4,13 @@ const CARTO_REQUEST_URL = `${process.env.CARTO_API}/sql?q=`;
 
 const NEW_SQL_QUERIES = {
   faoExtent:
-    'SELECT country AS iso, name, plantfor * 1000 AS planted_forest, primfor * 1000 AS forest_primary, natregfor * 1000 AS forest_regenerated, forest * 1000 AS extent, totarea as area_ha FROM table_1_forest_area_and_characteristics WHERE {location} AND year = 2015',
+    'SELECT country AS iso, name, plantfor * 1000 AS planted_forest__ha, primfor * 1000 AS primary_forest__ha, natregfor * 1000 AS regenerated_forest__ha, forest * 1000 AS fao_treecover__ha, totarea as area_ha FROM table_1_forest_area_and_characteristics WHERE {location} AND year = 2015',
   faoReforest:
-    'SELECT country AS iso, name, year, reforest * 1000 AS rate, forest*1000 AS extent FROM table_1_forest_area_and_characteristics as fao WHERE fao.year = {period} AND reforest > 0 ORDER BY rate DESC',
+    'SELECT country AS iso, name, year, reforest * 1000 AS reforestation__rate, forest*1000 AS fao_treecover_reforest__ha FROM table_1_forest_area_and_characteristics as fao WHERE fao.year = {period} AND reforest > 0 ORDER BY rate DESC',
   faoDeforest:
-    'SELECT fao.country, fao.name, fao.deforest * 1000 AS deforest, fao.humdef, fao.year FROM table_1_forest_area_and_characteristics as fao {location}',
+    'SELECT fao.country as iso, fao.name, fao.deforest * 1000 AS fao_treecover_deforest__ha, fao.humdef, fao.year FROM table_1_forest_area_and_characteristics as fao {location}',
   faoDeforestRank:
-    'WITH mytable AS (SELECT fao.country as iso, fao.name, fao.deforest * 1000 AS deforest, fao.humdef FROM table_1_forest_area_and_characteristics as fao WHERE fao.year = {year} AND deforest is not null), rank AS (SELECT deforest, iso, name from mytable ORDER BY mytable.deforest DESC) SELECT row_number() over () as rank, iso, name, deforest from rank',
+    'WITH mytable AS (SELECT fao.country as iso, fao.name, fao.deforest * 1000 AS deforest, fao.humdef FROM table_1_forest_area_and_characteristics as fao WHERE fao.year = {year} AND deforest is not null), rank AS (SELECT deforest, iso, name from mytable ORDER BY mytable.deforest DESC) SELECT row_number() over () as rank, iso, name, deforest as fao_treecover_deforest__ha from rank',
   faoEcoLive:
     'SELECT fao.country, fao.forempl, fao.femempl, fao.usdrev, fao.usdexp, fao.gdpusd2012, fao.totpop1000, fao.year FROM table_7_economics_livelihood as fao WHERE fao.year = 2000 or fao.year = 2005 or fao.year = 2010 or fao.year = 9999',
   globalLandCover: 'SELECT * FROM global_land_cover_adm2 WHERE {location}',
@@ -36,7 +36,25 @@ export const getFAOExtent = ({ adm0, download }) => {
     };
   }
 
-  return request.get(url);
+  return request.get(url).then(response => ({
+    ...response,
+    data: {
+      rows: response.data.rows.map(o => {
+        // delete old key, replace it with new
+        // delete Object.assign(o, {[newKey]: o[oldKey] })[oldKey]
+        delete Object.assign(o, { planted_forest: o.planted_forest__ha })
+          .planted_forest__ha;
+        delete Object.assign(o, { forest_primary: o.primary_forest__ha })
+          .primary_forest__ha;
+        delete Object.assign(o, {
+          forest_regenerated: o.regenerated_forest__ha
+        }).regenerated_forest__ha;
+        delete Object.assign(o, { extent: o.fao_treecover__ha })
+          .fao_treecover__ha;
+        return o;
+      })
+    }
+  }));
 };
 
 export const getFAOReforest = ({ period, download }) => {
@@ -45,8 +63,25 @@ export const getFAOReforest = ({ period, download }) => {
     period
   );
 
-  if (download) return url.concat('&format=csv');
-  return request.get(url);
+  if (download) {
+    return {
+      name: 'fao_treecover_reforestation__ha',
+      url: url.concat('&format=csv')
+    };
+  }
+
+  return request.get(url).then(response => ({
+    ...response,
+    data: {
+      rows: response.data.rows.map(o => {
+        delete Object.assign(o, { rate: o.reforestation__rate })
+          .reforestation__rate;
+        delete Object.assign(o, { extent: o.fao_treecover_reforest__ha })
+          .fao_treecover_reforest__ha;
+        return o;
+      })
+    }
+  }));
 };
 
 export const getFAODeforest = ({ adm0, download }) => {
@@ -55,17 +90,54 @@ export const getFAODeforest = ({ adm0, download }) => {
     adm0 ? `WHERE fao.country = '${adm0}'` : ''
   );
 
-  if (download) return url.concat('&format=csv');
-  return request.get(url);
+  if (download) {
+    return {
+      name: 'fao_treecover_deforestation__ha',
+      url: url.concat('&format=csv')
+    };
+  }
+
+  return request.get(url).then(response => ({
+    ...response,
+    data: {
+      rows: response.data.rows.map(o => {
+        delete Object.assign(o, { country: o.iso }).iso;
+        delete Object.assign(o, { deforest: o.fao_treecover_deforest__ha })
+          .fao_treecover_deforest__ha;
+        return o;
+      })
+    }
+  }));
 };
+
 export const getFAODeforestRank = ({ period, download }) => {
   const url = `${CARTO_REQUEST_URL}${NEW_SQL_QUERIES.faoDeforestRank}`.replace(
     '{year}',
     period
   );
 
-  if (download) return url.concat('&format=csv');
-  return request.get(url);
+  if (download) {
+    return {
+      name: 'fao_treecover_deforestation_rank',
+      url: url.concat('&format=csv')
+    };
+  }
+  return (
+    request
+      .get(url)
+      // .then(r => console.table(r.data.rows[2]) || r)
+      .then(response => ({
+        ...response,
+        data: {
+          rows: response.data.rows.map(o => {
+            delete Object.assign(o, { deforest: o.fao_treecover_deforest__ha })
+              .fao_treecover_deforest__ha;
+            return o;
+          })
+        }
+      }))
+  );
+  // .then(r => console.table(r.data.rows[2]) || r);
 };
 
 export const getFAOEcoLive = params => {
