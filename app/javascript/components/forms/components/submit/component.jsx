1542e77fa351383ae5722f09e43d5486d49edf88
Ed Brett <e.j.a.brett@gmail.com>
2020-01-23 17:31:33 +0100

    add honey pot field to all submissions

--- app/javascript/components/forms/components/submit/component.jsx
+++ app/javascript/components/forms/components/submit/component.jsx
@@ -1,8 +1,11 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, Fragment } from 'react';
 import PropTypes from 'prop-types';
+
+import { empty } from 'components/forms/validations';
 
 import Button from 'components/ui/button';
 import Loader from 'components/ui/loader';
+import Input from 'components/forms/components/input';
 
 import './styles.scss';
 
@@ -27,16 +30,24 @@ class Submit extends PureComponent {
     } = this.props;
 
     return (
-      <div className="c-form-submit">
-        {!submitError &&
-          !valid &&
-          submitFailed && <span>Required fields are empty!</span>}
-        {submitError && <span>{submitError}</span>}
-        {success && <span className="success">{success}</span>}
-        <Button className="submit-btn" type="submit" disabled={submitting}>
-          {submitting ? <Loader className="submit-loader" /> : children}
-        </Button>
-      </div>
+      <Fragment>
+        <Input
+          name="pardot_extra_field"
+          label="comments"
+          validate={[empty]}
+          hidden
+        />
+        <div className="c-form-submit">
+          {!submitError &&
+            !valid &&
+            submitFailed && <span>Required fields are empty!</span>}
+          {submitError && <span>{submitError}</span>}
+          {success && <span className="success">{success}</span>}
+          <Button className="submit-btn" type="submit" disabled={submitting}>
+            {submitting ? <Loader className="submit-loader" /> : children}
+          </Button>
+        </div>
+      </Fragment>
     );
   }
 }
