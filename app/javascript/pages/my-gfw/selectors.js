cf9e5b3af2aaac0da1a1288fef436900908628ac
Ed Brett <e.j.a.brett@gmail.com>
2020-01-29 10:13:40 +0100

    add areas back to my gfw, fit disabled button hover effect

--- app/javascript/pages/my-gfw/selectors.js
+++ app/javascript/pages/my-gfw/selectors.js
@@ -1,7 +1,9 @@
 import { createStructuredSelector } from 'reselect';
 
 const selectAreasLoading = state => state.areas && state.areas.loading;
+const selectAreas = state => state.areas && state.areas.data;
 
 export const getMyGFWProps = createStructuredSelector({
-  loading: selectAreasLoading
+  loading: selectAreasLoading,
+  areas: selectAreas
 });
