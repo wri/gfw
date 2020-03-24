22e27941699bdedb83a796d5d0cf9dd09e2154ae
Daniel Fernandez Rico <deferrico@gmail.com>
2020-03-24 18:43:49 +0100

    Adapted legacy fullName field to firstName and lastName

--- app/javascript/components/forms/profile/index.js
+++ app/javascript/components/forms/profile/index.js
@@ -3,12 +3,28 @@ import { connect } from 'react-redux';
 import Component from './component';
 import * as actions from './actions';
 
+const splitFirstName = fullName => {
+  const arr = fullName.split(' ');
+  return arr.slice(0, arr.length / 2).join(' ');
+};
+
+const splitLastName = fullName => {
+  const arr = fullName.split(' ');
+  return arr.slice(arr.length / 2, arr.length).join(' ');
+};
+
 const mapStateToProps = ({ myGfw, countryData }) => ({
   countries: countryData && countryData.countries,
   ...(myGfw &&
     myGfw.data && {
     initialValues: {
       ...myGfw.data,
+      firstName:
+          myGfw.data.firstName ||
+          (myGfw.data.fullName && splitFirstName(myGfw.data.fullName)),
+      lastName:
+          myGfw.data.lastName ||
+          (myGfw.data.fullName && splitLastName(myGfw.data.fullName)),
       subsector:
           myGfw.data.subsector && myGfw.data.subsector.includes('Other')
             ? // From `Other: ${subsector_otherInput}` -> 'Other_(write_in):' (value)
