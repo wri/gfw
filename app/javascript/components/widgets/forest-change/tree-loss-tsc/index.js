acefc06b4c7d1e8f924506387ef219146231cf75
Daniel Fernández Rico <deferrico@gmail.com>
2020-04-02 12:51:03 +0200

    Putting back old visibility and types params

--- app/javascript/components/widgets/forest-change/tree-loss-tsc/index.js
+++ app/javascript/components/widgets/forest-change/tree-loss-tsc/index.js
@@ -20,7 +20,7 @@ export default {
     initial: 'Annual tree cover loss by dominant driver in {location}',
     global: 'Global annual tree cover loss by dominant driver'
   },
-  types: ['global', 'country'],
+  types: ['global', 'country', 'geostore'],
   admins: ['global', 'adm0'],
   settingsConfig: [
     {
