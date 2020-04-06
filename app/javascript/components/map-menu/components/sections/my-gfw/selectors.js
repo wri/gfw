18dcad7e71ecc9fbaa872026b3226204130f2f71
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2020-04-06 14:14:31 +0100

    Show newly created areas on the top of the list - map menu my-gfw section

--- app/javascript/components/map-menu/components/sections/my-gfw/selectors.js
+++ app/javascript/components/map-menu/components/sections/my-gfw/selectors.js
@@ -1,5 +1,6 @@
-import { createStructuredSelector } from 'reselect';
+import { createSelector, createStructuredSelector } from 'reselect';
 import isEmpty from 'lodash/isEmpty';
+import sortBy from 'lodash/sortBy';
 
 import {
   getUserAreas,
@@ -7,17 +8,34 @@ import {
   getAreaTags
 } from 'providers/areas-provider/selectors';
 
+const PENDING_STATUS = 'pending';
+
+const isNewArea = area => area.status === PENDING_STATUS;
+
 const selectLoading = state =>
   state.areas && state.myGfw && (state.areas.loading || state.myGfw.loading);
 const selectLoggedIn = state => state.myGfw && !isEmpty(state.myGfw.data);
 const selectLocation = state => state.location && state.location.payload;
 const selectUserData = state => state.myGfw && state.myGfw.data;
 
+export const getUserAreasPendingOnTop = createSelector(
+  [getUserAreas],
+  areas => {
+    if (!areas) return null;
+
+    const newAreas = areas.filter(area => isNewArea(area));
+    return [
+      ...sortBy(newAreas, 'createdAt').reverse(),
+      ...areas.filter(area => !isNewArea(area))
+    ];
+  }
+);
+
 export const mapStateToProps = createStructuredSelector({
   loading: selectLoading,
   loggedIn: selectLoggedIn,
   location: selectLocation,
-  areas: getUserAreas,
+  areas: getUserAreasPendingOnTop,
   tags: getAreaTags,
   activeArea: getActiveArea,
   userData: selectUserData
