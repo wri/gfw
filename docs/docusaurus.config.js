1d8aafe9e241ff97019390e85ca56c3812e4a2d2
Ed Brett <e.j.a.brett@gmail.com>
2020-04-17 09:41:15 +0200

    add docasaurus

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,95 @@
+module.exports = {
+  title: 'Documentation | Global Forest Watch',
+  tagline: 'Forest Monitoring',
+  url: 'https://docs.globalforestwatch.org/',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'vizzuality', // Usually your GitHub org/user name.
+  projectName: 'gfw', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'GFW',
+      logo: {
+        alt: 'GFW',
+        src: 'img/logo.svg'
+      },
+      links: [
+        {
+          to: 'docs/doc1',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left'
+        },
+        {
+          href: 'https://github.com/Vizzuality/gfw',
+          label: 'GitHub',
+          position: 'right'
+        }
+      ]
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/doc1',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'Social',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/Vizzuality/gfw',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
