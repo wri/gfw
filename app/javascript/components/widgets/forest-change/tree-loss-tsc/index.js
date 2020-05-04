0a5ed7fd35485c87c53542432e3be067649ab9e2
Adam J. Pain <aj.pain89@gmail.com>
2020-05-04 11:50:49 +0200

    Update tcs driver key filter

--- app/javascript/components/widgets/forest-change/tree-loss-tsc/index.js
+++ app/javascript/components/widgets/forest-change/tree-loss-tsc/index.js
@@ -97,7 +97,9 @@ export default {
         let data = {};
         if (loss && loss.data && extent && extent.data) {
           data = {
-            loss: loss.data.data.filter(d => d.tcs_driver__type !== 'Unknown'),
+            loss: loss.data.data.filter(
+              d => d.tsc_tree_cover_loss_drivers__type !== 'Unknown'
+            ),
             extent: (loss.data.data && extent.data.data[0].value) || 0
           };
         }
