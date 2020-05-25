8e04c899826d2af02ca262c2fcf24dbc76713ad6
Ed Brett <e.j.a.brett@gmail.com>
2020-05-25 14:07:48 +0200

    calculate diff in days for maxRange changes in date pickers

--- app/javascript/components/map/components/legend/components/timeline/index.js
+++ app/javascript/components/map/components/legend/components/timeline/index.js
@@ -2,6 +2,7 @@ import { createElement, PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { track } from 'app/analytics';
+import moment from 'moment';
 
 import TimelineComponent from './component';
 import { getMarks } from './selectors';
@@ -25,11 +26,21 @@ const mapStateToProps = (
 
 class TimelineContainer extends PureComponent {
   handleOnDateChange = (date, position) => {
-    const { startDate, endDate, trimEndDate, handleChange } = this.props;
+    const { startDate, endDate, trimEndDate, handleChange, interval, maxRange } = this.props;
     const newRange = [startDate, endDate, trimEndDate];
     newRange[position] = date.format('YYYY-MM-DD');
     if (position) {
       newRange[position - 1] = date.format('YYYY-MM-DD');
+    }
+
+    const diffInterval = Math.abs(moment(newRange[2]).diff(moment(newRange[0]), interval));
+
+    if (diffInterval > maxRange) {
+      if (position) {
+        newRange[0] = date.subtract(maxRange, interval);
+      } else {
+        newRange[2] = date.add(maxRange, interval);
+      }
     }
     handleChange(newRange, this.props.activeLayer);
 
@@ -53,7 +64,9 @@ TimelineContainer.propTypes = {
   endDate: PropTypes.string,
   trimEndDate: PropTypes.string,
   handleChange: PropTypes.func,
-  activeLayer: PropTypes.object
+  activeLayer: PropTypes.object,
+  interval: PropTypes.string,
+  maxRange: PropTypes.number
 };
 
 export default connect(mapStateToProps, null)(TimelineContainer);
