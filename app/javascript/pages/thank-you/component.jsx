5db276098a147adc7084ef5481792f558c86a5b3
Ed Brett <e.j.a.brett@gmail.com>
2020-07-10 16:01:09 +0200

    fix thank you page

--- app/javascript/pages/thank-you/component.jsx
+++ app/javascript/pages/thank-you/component.jsx
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 
-import { NavLink } from 'next/link';
+import Link from 'next/link';
 import treeImage from 'assets/icons/tree-success.png';
 
 import './styles.scss';
@@ -21,7 +21,7 @@ class ThankyouPage extends PureComponent {
               <p>
                 You may wish to read our
                 {' '}
-                <NavLink to="/privacy-policy">privacy policy</NavLink>
+                <Link href="/privacy-policy">privacy policy</Link>
                 , which
                 provides further information about how we use personal data.
               </p>
