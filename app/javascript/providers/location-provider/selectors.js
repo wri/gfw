144cb248f3760761e19f60883bce1406a6c10851
Ed Brett <e.j.a.brett@gmail.com>
2020-07-15 09:36:41 +0200

    remove location from query params, fix dashboard breadcrumbs button

--- app/javascript/providers/location-provider/selectors.js
+++ app/javascript/providers/location-provider/selectors.js
@@ -1,6 +1,5 @@
 import { createSelector, createStructuredSelector } from 'reselect';
 
-export const selectLocation = (state) => state.location?.query?.location;
 export const selectMapSettings = (state) => state.map?.settings;
 export const selectMainMapSettings = (state) => state.mainMap;
 export const selectMapMenuSettings = (state) => state.mapMenu?.settings;
@@ -8,14 +7,12 @@ export const selectAnalysisSettings = (state) => state.analysis?.settings;
 
 export const getUrlParams = createSelector(
   [
-    selectLocation,
     selectMapSettings,
     selectMainMapSettings,
     selectMapMenuSettings,
     selectAnalysisSettings,
   ],
-  (location, map, mainMap, mapMenu, analysis) => ({
-    location,
+  (map, mainMap, mapMenu, analysis) => ({
     map,
     mainMap,
     mapMenu,
