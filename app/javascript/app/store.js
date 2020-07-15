e48859180e8de7c8995455af99c8e3d101e0afcf
Ed Brett <e.j.a.brett@gmail.com>
2020-07-15 09:40:44 +0200

    add redux dev tools sanitation back

--- app/javascript/app/store.js
+++ app/javascript/app/store.js
@@ -1,9 +1,37 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import { createWrapper } from 'next-redux-wrapper';
 
 import reducerRegistry from './registry';
+
+const isServer = typeof window === 'undefined';
+
+const reduxDevTools =
+  !isServer &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    maxAge: 10,
+    stateSanitizer: (state) => ({
+      ...state,
+      datasets: {
+        ...state.datasets,
+        data: 'NOT_SERIALIZED',
+      },
+      ptw: {
+        ...state.ptw,
+        data: 'NOT_SERIALIZED',
+      },
+      countryData: {
+        ...state.countryData,
+        countries: 'NOT_SERIALIZED',
+        gadmCountries: 'NOT_SERIALIZED',
+        faoCountries: 'NOT_SERIALIZED',
+      },
+    }),
+  });
+
+const composeEnhancers =
+  (process.env.NODE_ENV === 'development' && reduxDevTools) || compose;
 
 /**
  * @param {object} initialState
@@ -22,7 +50,7 @@ export const initStore = (initialState) => {
   return createStore(
     initialReducers,
     initialState,
-    composeWithDevTools(applyMiddleware(thunkMiddleware))
+    composeEnhancers(applyMiddleware(thunkMiddleware))
   );
 };
 
