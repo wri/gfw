6c298e1a708abc5a81995dd5628db5668a5e4b50
Adam J. Pain <aj.pain89@gmail.com>
2020-07-28 15:24:13 +0200

    Reimplement carbon equations

--- utils/calculations.js
+++ utils/calculations.js
@@ -1,19 +1,23 @@
-export const tonsToTonnes = value => value * 0.907185;
+export const tonsToTonnes = (value) => value * 0.907185;
 
-export const biomassToCO2 = value => value * 0.47;
+export const biomassToCO2 = (value) => value * 0.47;
 
-export const carbonToCO2 = value => value * 12 / 44;
+export const carbonToCO2 = (value) => (value * 12) / 44;
 
-export const agBiomass2agCarbon = agb => 0.5 * agb;
+// export const agBiomass2agCarbon = agb => 0.5 * agb;
+export const agBiomass2agCarbon = (agb) => 0.5 * agb;
 
-export const agBiomass2bgBiomass = agb => 0.489 * agb ** 0.89;
+// export const agBiomass2bgBiomass = agb => 0.489 * agb ** 0.89;
+export const agBiomass2bgBiomass = (agb) => 0.26 * agb;
 
-export const agBiomass2TotalBiomass = agb => agb + 0.489 * agb ** 0.89;
+// export const agBiomass2TotalBiomass = agb => agb + 0.489 * agb ** 0.89;
+export const agBiomass2TotalBiomass = (agb) => 1.26 * agb;
 
-export const agBiomass2bgCarbon = agb => 0.5 * 0.489 * agb ** 0.89;
+// export const agBiomass2bgCarbon = agb => 0.5 * 0.489 * agb ** 0.89;
+export const agBiomass2bgCarbon = (agb) => 0.5 * 0.26 * agb;
 
-export const agBiomass2TotalCarbon = agb =>
-  0.5 * agb + 0.5 * (0.489 * agb ** 0.89);
+// export const agBiomass2TotalCarbon = agb => 0.5 * agb + 0.5 * (0.489 * agb ** 0.89);
+export const agBiomass2TotalCarbon = (agb) => 0.5 * 1.26 * agb;
 
 export function ordinalSuffixOf(i) {
   const j = i % 10;
