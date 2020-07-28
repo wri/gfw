cc681acdbd4cea38306299201386bd9af5c96de5
Ed Brett <e.j.a.brett@gmail.com>
2020-07-28 11:20:31 +0200

    fix store no init on production build

--- store/index.js
+++ store/index.js
@@ -1,48 +1,20 @@
-import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { reduxModule as myGfwReduxModule } from 'providers/mygfw-provider';
 
 import reducerRegistry from './registry';
 
-const isServer = typeof window === 'undefined';
-
 reducerRegistry.registerModule('myGfw', myGfwReduxModule);
 
 const initialReducers = combineReducers(reducerRegistry.getReducers());
 
-const reduxDevTools =
-  !isServer &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-    maxAge: 10,
-    stateSanitizer: (state) => ({
-      ...state,
-      datasets: {
-        ...state.datasets,
-        data: 'NOT_SERIALIZED',
-      },
-      ptw: {
-        ...state.ptw,
-        data: 'NOT_SERIALIZED',
-      },
-      countryData: {
-        ...state.countryData,
-        countries: 'NOT_SERIALIZED',
-        gadmCountries: 'NOT_SERIALIZED',
-        faoCountries: 'NOT_SERIALIZED',
-      },
-    }),
-  });
-
-const composeEnhancers =
-  (process.env.NODE_ENV === 'development' && reduxDevTools) || compose;
-
-export default (initialState) => {
+const useStore = (initialState) => {
   const store = createStore(
     initialReducers,
     initialState,
-    composeEnhancers(applyMiddleware(thunkMiddleware))
+    composeWithDevTools(applyMiddleware(thunkMiddleware))
   );
   reducerRegistry.setChangeListener((asyncReducers) =>
     store.replaceReducer(combineReducers(asyncReducers))
@@ -50,3 +22,5 @@ export default (initialState) => {
 
   return store;
 };
+
+export default useStore;
