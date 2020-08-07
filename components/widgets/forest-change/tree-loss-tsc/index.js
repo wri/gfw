26190b464f8266d2768e3ce8ad9af801628cceff
Adam J. Pain <aj.pain89@gmail.com>
2020-08-07 17:15:03 +0200

    Hotfix: update loss by driver download query

--- components/widgets/forest-change/tree-loss-tsc/index.js
+++ components/widgets/forest-change/tree-loss-tsc/index.js
@@ -4,12 +4,12 @@ import { getYearsRangeFromMinMax } from 'components/widgets/utils/data';
 
 import {
   POLITICAL_BOUNDARIES_DATASET,
-  TREE_COVER_LOSS_BY_DOMINANT_DRIVER_DATASET
+  TREE_COVER_LOSS_BY_DOMINANT_DRIVER_DATASET,
 } from 'data/layers-datasets';
 import {
   DISPUTED_POLITICAL_BOUNDARIES,
   POLITICAL_BOUNDARIES,
-  TREE_COVER_LOSS_BY_DOMINANT_DRIVER
+  TREE_COVER_LOSS_BY_DOMINANT_DRIVER,
 } from 'data/layers';
 
 import treeLoss from 'components/widgets/forest-change/tree-loss';
@@ -25,7 +25,7 @@ export default {
   widget: 'treeLossTsc',
   title: {
     initial: 'Annual tree cover loss by dominant driver in {location}',
-    global: 'Global annual tree cover loss by dominant driver'
+    global: 'Global annual tree cover loss by dominant driver',
   },
   types: ['global', 'country'],
   admins: ['global', 'adm0'],
@@ -33,7 +33,7 @@ export default {
     {
       key: 'tscDriverGroup',
       label: 'drivers',
-      type: 'select'
+      type: 'select',
     },
     {
       key: 'years',
@@ -41,44 +41,44 @@ export default {
       endKey: 'endYear',
       startKey: 'startYear',
       type: 'range-select',
-      border: true
+      border: true,
     },
     {
       key: 'threshold',
       label: 'canopy density',
       type: 'mini-select',
-      metaKey: 'widget_canopy_density'
-    }
+      metaKey: 'widget_canopy_density',
+    },
   ],
   chartType: 'composedChart',
   datasets: [
     {
       dataset: POLITICAL_BOUNDARIES_DATASET,
       layers: [DISPUTED_POLITICAL_BOUNDARIES, POLITICAL_BOUNDARIES],
-      boundary: true
+      boundary: true,
     },
     {
       dataset: POLITICAL_BOUNDARIES_DATASET,
       layers: [DISPUTED_POLITICAL_BOUNDARIES, POLITICAL_BOUNDARIES],
-      boundary: true
+      boundary: true,
     },
     // loss tsc
     {
       dataset: TREE_COVER_LOSS_BY_DOMINANT_DRIVER_DATASET,
-      layers: [TREE_COVER_LOSS_BY_DOMINANT_DRIVER]
-    }
+      layers: [TREE_COVER_LOSS_BY_DOMINANT_DRIVER],
+    },
   ],
   metaKey: 'widget_tsc_drivers',
   sortOrder: {
     summary: 1,
     forestChange: 1,
-    global: 1
+    global: 1,
   },
   settings: {
     tscDriverGroup: 'all',
     highlighted: false,
     extentYear: 2000,
-    threshold: 30
+    threshold: 30,
   },
   sentences: {
     initial:
@@ -86,24 +86,24 @@ export default {
     noLoss:
       'In {location} from {startYear} to {endYear}, <b>no</b> tree cover loss occurred in areas where the dominant drivers of loss resulted in {deforestation}.',
     globalInitial:
-      '{location} from {startYear} to {endYear}, {permPercent} of tree cover loss occurred in areas where the dominant drivers of loss resulted in {deforestation}.'
+      '{location} from {startYear} to {endYear}, {permPercent} of tree cover loss occurred in areas where the dominant drivers of loss resulted in {deforestation}.',
   },
   whitelists: {
-    checkStatus: true
+    checkStatus: true,
   },
-  getData: params =>
+  getData: (params) =>
     all([
       getLoss({ ...params, landCategory: 'tsc', lossTsc: true }),
-      getExtent({ ...params })
+      getExtent({ ...params }),
     ]).then(
       spread((loss, extent) => {
         let data = {};
         if (loss && loss.data && extent && extent.data) {
           data = {
             loss: loss.data.data.filter(
-              d => d.tsc_tree_cover_loss_drivers__type !== 'Unknown'
+              (d) => d.tsc_tree_cover_loss_drivers__type !== 'Unknown'
             ),
-            extent: (loss.data.data && extent.data.data[0].value) || 0
+            extent: (loss.data.data && extent.data.data[0].value) || 0,
           };
         }
 
@@ -117,39 +117,30 @@ export default {
           settings: {
             startYear,
             endYear,
-            yearsRange: range
+            yearsRange: range,
           },
           options: {
-            years: range
-          }
+            years: range,
+          },
         };
       })
     ),
-  getDataURL: params => [
-    getLoss({ ...params, forestType: 'tsc', tsc: true, download: true }),
-    getExtent({ ...params, download: true })
+  getDataURL: (params) => [
+    getLoss({ ...params, landCategory: 'tsc', lossTsc: true, download: true }),
+    getExtent({ ...params, download: true }),
   ],
   getWidgetProps,
-  parseInteraction: payload => {
+  parseInteraction: (payload) => {
     if (payload) {
-      const year = payload.year;
+      const {year} = payload;
       return {
         updateLayer: true,
         startDate:
-          year &&
-          moment()
-            .year(year)
-            .startOf('year')
-            .format('YYYY-MM-DD'),
-        endDate:
-          year &&
-          moment()
-            .year(year)
-            .endOf('year')
-            .format('YYYY-MM-DD')
+          year && moment().year(year).startOf('year').format('YYYY-MM-DD'),
+        endDate: year && moment().year(year).endOf('year').format('YYYY-MM-DD'),
       };
     }
 
     return {};
-  }
+  },
 };
