c55a249efddc04544dfbee6a479f5dbcc4cc8f45
Adam J. Pain <aj.pain89@gmail.com>
2020-09-22 10:40:35 +0200

    Update decode for confidence value

--- providers/datasets-provider/config.js
+++ providers/datasets-provider/config.js
@@ -147,32 +147,39 @@ const decodes = {
     }
   `,
   RADDs: `
-  float day = (color.r * 255. * 255.) + (color.g * 255.) - 1461.;
-  float confidence = color.b * 255.;
-  if (
-    day > 0. &&
-    day >= startDayIndex &&
-    day <= endDayIndex
-  ) {
-    // get intensity
-    float intensity = mod(confidence, 100.) * 50.;
-    if (intensity > 255.) {
-      intensity = 255.;
+  // values for creating power scale, domain (input), and range (output)
+    float confidenceValue = 0.;
+    if (confirmedOnly > 0.) {
+      confidenceValue = 200.;
     }
-    if (day >= numberOfDays - 7. && day <= numberOfDays) {
-      color.r = 219. / 255.;
-      color.g = 168. / 255.;
-      color.b = 0.;
-      alpha = intensity / 255.;
+    float day = color.r * 255. * 255. + (color.g * 255.) - 1461.;
+    float confidence = color.b * 255.;
+
+    if (
+      day > 0. &&
+      day >= startDayIndex &&
+      day <= endDayIndex &&
+      confidence >= confidenceValue
+    ) {
+      // get intensity
+      float intensity = mod(confidence, 100.) * 50.;
+      if (intensity > 255.) {
+        intensity = 255.;
+      }
+      if (day >= numberOfDays - 7. && day <= numberOfDays) {
+        color.r = 219. / 255.;
+        color.g = 168. / 255.;
+        color.b = 0.;
+        alpha = intensity / 255.;
+      } else {
+        color.r = 220. / 255.;
+        color.g = 102. / 255.;
+        color.b = 153. / 255.;
+        alpha = intensity / 255.;
+      }
     } else {
-      color.r = 220. / 255.;
-      color.g = 102. / 255.;
-      color.b = 153. / 255.;
-      alpha = intensity / 255.;
+      alpha = 0.;
     }
-  } else {
-    alpha = 0.;
-  }
   `,
   biomassLoss: `
     float countBuckets = 5.; // buckets length / 3: three bands
