0701fef26fd14b9c07dde1190686c63191bce758
Ed Brett <e.j.a.brett@gmail.com>
2020-09-23 12:13:06 +0100

    add howto redirects

--- next.config.js
+++ next.config.js
@@ -10,6 +10,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const FilterWarningsPlugin = require('webpack-filter-warnings-plugin');
 const Dotenv = require('dotenv-webpack');
 
+const howToRedirects = require('./static/howto-redirects');
+
 const nextConfig = {
   webpack: (config) => {
     config.plugins = [
@@ -50,20 +52,12 @@ const nextConfig = {
       destination: `/grants-and-fellowships/projects/`,
       permanent: true,
     },
+    ...howToRedirects.map((r) => ({
+      ...r,
+      permanent: true,
+    })),
   ],
   rewrites: async () => [
-    {
-      source: '/howto/',
-      destination: `https://vizzuality.github.io/gfw-howto/`,
-    },
-    {
-      source: '/howto/:path*/',
-      destination: `https://vizzuality.github.io/gfw-howto/:path*/`,
-    },
-    {
-      source: '/howto/:path*',
-      destination: `https://vizzuality.github.io/gfw-howto/:path*`,
-    },
     {
       source: '/help/',
       destination: `https://gfw-help-center.herokuapp.com/help/`,
