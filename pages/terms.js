296857609543cabd6344200e95dc02e36dbbf2e9
Ed Brett <e.j.a.brett@gmail.com>
2020-10-13 10:21:27 +0100

    add noindex meta tags to certain pages

--- pages/terms.js
+++ pages/terms.js
@@ -3,6 +3,7 @@ import Terms from 'pages/terms';
 
 const TermsPage = () => (
   <Layout
+    noIndex
     title="Terms of Service | Global Forest Watch"
     description="Welcome to the WRI family of environmental data platforms. By using the Services, you agree to be bound by these Terms of Service and any future updates."
   >
