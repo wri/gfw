eaa3581ecf9ab3dd158ce89b61520d7dcaf5168b
Ed Brett <e.j.a.brett@gmail.com>
2020-10-20 15:04:03 +0100

    update about partners section to use new grid for logos

--- components/pages/about/section-partners/component.jsx
+++ components/pages/about/section-partners/component.jsx
@@ -1,78 +1,69 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-
 import { Row, Column } from 'gfw-components';
+
+import { foundingPartners, partnersCollaborators, funders } from './partners';
 
 import './styles.scss';
 
-class SectionPartners extends PureComponent {
-  static propTypes = {
-    foundingPartners: PropTypes.array,
-    partnersCollaborators: PropTypes.array,
-    funders: PropTypes.array,
-  };
-
-  render() {
-    const { foundingPartners, partnersCollaborators, funders } = this.props;
-    return (
-      <section className="l-section-partners">
-        <div className="logo-section">
-          <Row>
-            <Column>
-              <h3>Founding partners</h3>
-            </Column>
-            {foundingPartners.map((l) => (
-              <a
-                key={l.title}
-                className="columns small-6 medium-4 large-3"
-                href={l.link}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img alt={l.title} src={l.img} />
-              </a>
-            ))}
-          </Row>
-        </div>
-        <div className="logo-section">
-          <Row>
-            <Column>
-              <h3>Partners</h3>
-            </Column>
-            {partnersCollaborators.map((l) => (
-              <a
-                key={l.title}
-                className="columns small-6 medium-4 large-3"
-                href={l.link}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img alt={l.title} src={l.img} />
-              </a>
-            ))}
-          </Row>
-        </div>
-        <div className="logo-section">
-          <Row>
-            <Column>
-              <h3>Funders</h3>
-            </Column>
-            {funders.map((l) => (
-              <a
-                key={l.title}
-                className="columns small-6 medium-4 large-3"
-                href={l.link}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img alt={l.title} src={l.img} />
-              </a>
-            ))}
-          </Row>
-        </div>
-      </section>
-    );
-  }
-}
+const SectionPartners = () => (
+  <section className="l-section-partners">
+    <div className="logo-section">
+      <Row>
+        <Column>
+          <h3>Founding partners</h3>
+        </Column>
+        {foundingPartners.map((l) => (
+          <Column width={[1 / 2, 1 / 3, 1 / 4]}>
+            <a
+              key={l.title}
+              href={l.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img alt={l.title} src={l.img} />
+            </a>
+          </Column>
+        ))}
+      </Row>
+    </div>
+    <div className="logo-section">
+      <Row>
+        <Column>
+          <h3>Partners</h3>
+        </Column>
+        {partnersCollaborators.map((l) => (
+          <Column width={[1 / 2, 1 / 3, 1 / 4]}>
+            <a
+              key={l.title}
+              href={l.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img alt={l.title} src={l.img} />
+            </a>
+          </Column>
+        ))}
+      </Row>
+    </div>
+    <div className="logo-section">
+      <Row>
+        <Column>
+          <h3>Funders</h3>
+        </Column>
+        {funders.map((l) => (
+          <Column width={[1 / 2, 1 / 3, 1 / 4]}>
+            <a
+              key={l.title}
+              href={l.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img alt={l.title} src={l.img} />
+            </a>
+          </Column>
+        ))}
+      </Row>
+    </div>
+  </section>
+);
 
 export default SectionPartners;
--- components/pages/home/component.jsx
+++ components/pages/home/component.jsx
@@ -4,15 +4,7 @@ import YouTube from 'react-youtube';
 import Link from 'next/link';
 import cx from 'classnames';
 
-import {
-  MediaContextProvider,
-  Desktop,
-  Mobile,
-  Carousel,
-  Button,
-  Row,
-  Column,
-} from 'gfw-components';
+import { Desktop, Mobile, Carousel, Button, Row, Column } from 'gfw-components';
 
 import Cover from 'components/cover';
 import Icon from 'components/ui/icon';
@@ -34,277 +26,275 @@ const HomePage = ({ summary, uses, apps, news }) => {
   const summaryEl = useRef(null);
 
   return (
-    <MediaContextProvider>
-      <div className="l-home-page">
-        <Cover
-          className="home-cover"
-          title="Forest Monitoring Designed for Action"
-          description="Global Forest Watch offers the latest data, technology and tools that empower people everywhere to better protect forests."
-          bgImage={bgImage}
-          large
-        >
-          <>
-            <div className={cx('home-video', { show: showVideo })}>
-              <YouTube
-                videoId="0XsJNU75Si0"
-                opts={{
-                  height: '100%',
-                  width: '100%',
-                  playerVars: {
-                    autoplay: 1,
-                    autohide: 1,
-                    loop: 1,
-                    modestbranding: 1,
-                    rel: 0,
-                    showinfo: 0,
-                    controls: 0,
-                    disablekb: 1,
-                    enablejsapi: 0,
-                    iv_load_policy: 3,
-                  },
-                }}
-                onPlay={() => setTimeout(() => setShowVideo(true), 300)}
-                onEnd={() => setShowVideo(false)}
-              />
-            </div>
-            {showVideo && (
-              <Button
-                className="stop-video-btn"
-                onClick={() => setShowVideo(false)}
-              >
-                STOP VIDEO
+    <div className="l-home-page">
+      <Cover
+        className="home-cover"
+        title="Forest Monitoring Designed for Action"
+        description="Global Forest Watch offers the latest data, technology and tools that empower people everywhere to better protect forests."
+        bgImage={bgImage}
+        large
+      >
+        <>
+          <div className={cx('home-video', { show: showVideo })}>
+            <YouTube
+              videoId="0XsJNU75Si0"
+              opts={{
+                height: '100%',
+                width: '100%',
+                playerVars: {
+                  autoplay: 1,
+                  autohide: 1,
+                  loop: 1,
+                  modestbranding: 1,
+                  rel: 0,
+                  showinfo: 0,
+                  controls: 0,
+                  disablekb: 1,
+                  enablejsapi: 0,
+                  iv_load_policy: 3,
+                },
+              }}
+              onPlay={() => setTimeout(() => setShowVideo(true), 300)}
+              onEnd={() => setShowVideo(false)}
+            />
+          </div>
+          {showVideo && (
+            <Button
+              className="stop-video-btn"
+              onClick={() => setShowVideo(false)}
+            >
+              STOP VIDEO
+            </Button>
+          )}
+          <Link href="/subscribe">
+            <a className="subscribe-btn">
+              <Button round className="subscribe-icon">
+                <Icon icon={mailIcon} />
               </Button>
+              <p className="subscribe-msg">SUBSCRIBE TO THE GFW NEWSLETTER</p>
+            </a>
+          </Link>
+        </>
+      </Cover>
+      <Row>
+        <Column>
+          <div className="section-summary" ref={summaryEl}>
+            <Button
+              className="scroll-to-btn"
+              round
+              onClick={() => {
+                window.scrollTo({
+                  behavior: 'smooth',
+                  left: 0,
+                  top: summaryEl?.current?.offsetTop,
+                });
+              }}
+            >
+              <Icon icon={arrowIcon} />
+            </Button>
+            {summary && (
+              <>
+                <Desktop>
+                  <Carousel settings={{ dots: true }}>
+                    {summary.map((c) => (
+                      <Card
+                        className="summary-card"
+                        key={c.title}
+                        data={{ ...c, fullSummary: true }}
+                      />
+                    ))}
+                  </Carousel>
+                </Desktop>
+                <Mobile>
+                  <Carousel settings={{ dots: true, slidesToShow: 1 }}>
+                    {summary.map((c) => (
+                      <Card
+                        className="summary-card"
+                        key={c.title}
+                        data={{ ...c, fullSummary: true }}
+                      />
+                    ))}
+                  </Carousel>
+                </Mobile>
+              </>
             )}
-            <Link href="/subscribe">
-              <a className="subscribe-btn">
-                <Button round className="subscribe-icon">
-                  <Icon icon={mailIcon} />
-                </Button>
-                <p className="subscribe-msg">SUBSCRIBE TO THE GFW NEWSLETTER</p>
+          </div>
+        </Column>
+      </Row>
+      <div className="section-uses">
+        <h3 className="section-title">
+          What can you do with Global Forest Watch?
+        </h3>
+        {uses && (
+          <Carousel
+            className="uses-carousel"
+            settings={{
+              slidesToShow: 1,
+              dots: true,
+              arrows: false,
+              speed: 0,
+              customPaging: (i) => (
+                <div className="use-user">
+                  <Icon className="icon-user" icon={profileIcon} />
+                  {uses[i].profile}
+                </div>
+              ),
+            }}
+          >
+            {uses.map((c) => (
+              <Row className="uses" key={c.example}>
+                <Column width={[1, 1 / 2]}>
+                  <p className="use-example">
+                    <i>
+                      <span>“</span>
+                      {c.example}
+                      <span>”</span>
+                    </i>
+                  </p>
+                </Column>
+                <Column width={[1, 1 / 2]}>
+                  <div
+                    className="use-image"
+                    style={{ backgroundImage: `url(${c.img})` }}
+                  >
+                    <a
+                      className="use-credit"
+                      href={c.credit.extLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {c.credit.name}
+                    </a>
+                  </div>
+                </Column>
+              </Row>
+            ))}
+          </Carousel>
+        )}
+      </div>
+      <div className="section-apps">
+        <h3 className="section-title">BROWSE APPLICATIONS</h3>
+        {apps && (
+          <Carousel
+            className="apps-carousel"
+            settings={{
+              slidesToShow: 1,
+              infinite: true,
+            }}
+          >
+            {apps.map((app) => (
+              <a
+                key={app.title}
+                href={app.extLink}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div
+                  className="app-slide"
+                  style={{
+                    backgroundColor: app.color,
+                  }}
+                >
+                  <Row className="apps">
+                    <Column>
+                      <div className="app-content">
+                        <Icon
+                          className={cx('app-icon', app.className)}
+                          icon={app.icon}
+                        />
+                        <h4>{app.title}</h4>
+                        <p>{app.description}</p>
+                        <div
+                          className="app-image"
+                          style={{
+                            backgroundImage: `url(${app.background})`,
+                          }}
+                        />
+                      </div>
+                    </Column>
+                  </Row>
+                </div>
               </a>
-            </Link>
-          </>
-        </Cover>
+            ))}
+          </Carousel>
+        )}
+      </div>
+      <div
+        className="section-news"
+        style={{
+          backgroundImage: `url(${newsImage})`,
+        }}
+      >
         <Row>
           <Column>
-            <div className="section-summary" ref={summaryEl}>
-              <Button
-                className="scroll-to-btn"
-                round
-                onClick={() => {
-                  window.scrollTo({
-                    behavior: 'smooth',
-                    left: 0,
-                    top: summaryEl?.current?.offsetTop,
-                  });
-                }}
-              >
-                <Icon icon={arrowIcon} />
-              </Button>
-              {summary && (
+            <h3 className="news-title">New on Global Forest Watch</h3>
+            <div className="news-carousel">
+              {news ? (
                 <>
                   <Desktop>
-                    <Carousel settings={{ dots: true }}>
-                      {summary.map((c) => (
-                        <Card
-                          className="summary-card"
-                          key={c.title}
-                          data={{ ...c, fullSummary: true }}
-                        />
+                    <Carousel
+                      settings={{
+                        slidesToShow: 3,
+                      }}
+                    >
+                      {news.map((item) => (
+                        <a
+                          key={item.name}
+                          href={item.link}
+                          target="_blank"
+                          className="news-card"
+                          rel="noopener noreferrer"
+                        >
+                          <Card
+                            data={{
+                              title: item.name,
+                              summary: item.description,
+                            }}
+                          />
+                        </a>
                       ))}
                     </Carousel>
                   </Desktop>
                   <Mobile>
-                    <Carousel settings={{ dots: true, slidesToShow: 1 }}>
-                      {summary.map((c) => (
-                        <Card
-                          className="summary-card"
-                          key={c.title}
-                          data={{ ...c, fullSummary: true }}
-                        />
+                    <Carousel
+                      settings={{
+                        slidesToShow: 1,
+                      }}
+                    >
+                      {news.map((item) => (
+                        <a
+                          key={item.name}
+                          href={item.link}
+                          target="_blank"
+                          className="news-card"
+                          rel="noopener noreferrer"
+                        >
+                          <Card
+                            data={{
+                              title: item.name,
+                              summary: item.description,
+                            }}
+                          />
+                        </a>
                       ))}
                     </Carousel>
                   </Mobile>
                 </>
+              ) : (
+                <NoContent className="no-news" message="No news available" />
               )}
             </div>
+            <Link href="/my-gfw">
+              <a>
+                <Button className="my-gfw-btn" light>
+                  My GFW
+                </Button>
+              </a>
+            </Link>
           </Column>
         </Row>
-        <div className="section-uses">
-          <h3 className="section-title">
-            What can you do with Global Forest Watch?
-          </h3>
-          {uses && (
-            <Carousel
-              className="uses-carousel"
-              settings={{
-                slidesToShow: 1,
-                dots: true,
-                arrows: false,
-                speed: 0,
-                customPaging: (i) => (
-                  <div className="use-user">
-                    <Icon className="icon-user" icon={profileIcon} />
-                    {uses[i].profile}
-                  </div>
-                ),
-              }}
-            >
-              {uses.map((c) => (
-                <Row className="uses" key={c.example}>
-                  <Column width={[1, 1 / 2]}>
-                    <p className="use-example">
-                      <i>
-                        <span>“</span>
-                        {c.example}
-                        <span>”</span>
-                      </i>
-                    </p>
-                  </Column>
-                  <Column width={[1, 1 / 2]}>
-                    <div
-                      className="use-image"
-                      style={{ backgroundImage: `url(${c.img})` }}
-                    >
-                      <a
-                        className="use-credit"
-                        href={c.credit.extLink}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        {c.credit.name}
-                      </a>
-                    </div>
-                  </Column>
-                </Row>
-              ))}
-            </Carousel>
-          )}
-        </div>
-        <div className="section-apps">
-          <h3 className="section-title">BROWSE APPLICATIONS</h3>
-          {apps && (
-            <Carousel
-              className="apps-carousel"
-              settings={{
-                slidesToShow: 1,
-                infinite: true,
-              }}
-            >
-              {apps.map((app) => (
-                <a
-                  key={app.title}
-                  href={app.extLink}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <div
-                    className="app-slide"
-                    style={{
-                      backgroundColor: app.color,
-                    }}
-                  >
-                    <Row className="apps">
-                      <Column>
-                        <div className="app-content">
-                          <Icon
-                            className={cx('app-icon', app.className)}
-                            icon={app.icon}
-                          />
-                          <h4>{app.title}</h4>
-                          <p>{app.description}</p>
-                          <div
-                            className="app-image"
-                            style={{
-                              backgroundImage: `url(${app.background})`,
-                            }}
-                          />
-                        </div>
-                      </Column>
-                    </Row>
-                  </div>
-                </a>
-              ))}
-            </Carousel>
-          )}
-        </div>
-        <div
-          className="section-news"
-          style={{
-            backgroundImage: `url(${newsImage})`,
-          }}
-        >
-          <Row>
-            <Column>
-              <h3 className="news-title">New on Global Forest Watch</h3>
-              <div className="news-carousel">
-                {news ? (
-                  <>
-                    <Desktop>
-                      <Carousel
-                        settings={{
-                          slidesToShow: 3,
-                        }}
-                      >
-                        {news.map((item) => (
-                          <a
-                            key={item.name}
-                            href={item.link}
-                            target="_blank"
-                            className="news-card"
-                            rel="noopener noreferrer"
-                          >
-                            <Card
-                              data={{
-                                title: item.name,
-                                summary: item.description,
-                              }}
-                            />
-                          </a>
-                        ))}
-                      </Carousel>
-                    </Desktop>
-                    <Mobile>
-                      <Carousel
-                        settings={{
-                          slidesToShow: 1,
-                        }}
-                      >
-                        {news.map((item) => (
-                          <a
-                            key={item.name}
-                            href={item.link}
-                            target="_blank"
-                            className="news-card"
-                            rel="noopener noreferrer"
-                          >
-                            <Card
-                              data={{
-                                title: item.name,
-                                summary: item.description,
-                              }}
-                            />
-                          </a>
-                        ))}
-                      </Carousel>
-                    </Mobile>
-                  </>
-                ) : (
-                  <NoContent className="no-news" message="No news available" />
-                )}
-              </div>
-              <Link href="/my-gfw">
-                <a>
-                  <Button className="my-gfw-btn" light>
-                    My GFW
-                  </Button>
-                </a>
-              </Link>
-            </Column>
-          </Row>
-        </div>
       </div>
-    </MediaContextProvider>
+    </div>
   );
 };
 
