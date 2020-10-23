e709f70d326d8aafcb99bee39c7bb3589a6a52d3
Ed Brett <e.j.a.brett@gmail.com>
2020-10-23 21:07:11 +0100

    move upload shape modal to scoped analysis component

--- components/analysis/components/chose-analysis/component.jsx
+++ components/analysis/components/chose-analysis/component.jsx
@@ -5,6 +5,8 @@ import cx from 'classnames';
 import { track } from 'analytics';
 import { format } from 'd3-format';
 
+import { Modal } from 'gfw-components';
+
 import Button from 'components/ui/button';
 import Icon from 'components/ui/icon';
 import Dropdown from 'components/ui/dropdown';
@@ -27,7 +29,6 @@ class ChoseAnalysis extends PureComponent {
     activeBoundary: PropTypes.object,
     selectBoundaries: PropTypes.func,
     setMenuSettings: PropTypes.func,
-    setModalSources: PropTypes.func,
     error: PropTypes.string,
     errorMessage: PropTypes.string,
     uploadConfig: PropTypes.object,
@@ -38,6 +39,10 @@ class ChoseAnalysis extends PureComponent {
     setMapSettings: PropTypes.func,
     file: PropTypes.object,
   };
+
+  state = {
+    uploadModalOpen: false
+  }
 
   renderLayerOption = () => {
     const {
@@ -94,7 +99,6 @@ class ChoseAnalysis extends PureComponent {
       drawing,
       setMapSettings,
       setMenuSettings,
-      setModalSources,
       errorMessage,
       error,
       onDropAccepted,
@@ -192,7 +196,7 @@ class ChoseAnalysis extends PureComponent {
               onClick={(e) => {
                 e.preventDefault();
                 e.stopPropagation();
-                setModalSources({ open: true, source: 'uploads' });
+                this.setState({ uploadModalOpen: true });
               }}
             >
               <Icon icon={infoIcon} className="info-icon" />
@@ -206,6 +210,14 @@ class ChoseAnalysis extends PureComponent {
             </a>
           </p>
         </div>
+        <Modal
+          open={this.state.uploadModalOpen}
+          title="Upload a custom shape"
+          onRequestClose={() => this.setState({ uploadModalOpen: false })}
+          className="c-upload-shape-modal"
+        >
+          <p>Drop a file in the designated area to analyze or subscribe to it. The recommended <strong>maximum file size is 1MB.</strong> Anything larger than that may not work properly.</p><p>NOTE: This feature counts alerts or hectares inside of polygons; therefore, <strong>only polygon data is supported,</strong> not point and line data. Please ensure that your file only contains polygon data. A maximum of 1,000 feautures can be analysed in a single upload, contained within a single layer. Multiple layers are not supported.</p><p><strong>List of supported file formats</strong>:</p><ul><li>Unzipped: .csv, .json, .geojson, .kml, .kmz (.csv files must contain a geom column of shape data converted to <a href="https://en.wikipedia.org/wiki/Well-known_text">well known text (WKT)</a> format).</li><li>Zipped: .shp (zipped shapefiles must include .shp, .shx, .dbf, and .prj files)</li></ul>
+        </Modal>
       </div>
     );
   };
