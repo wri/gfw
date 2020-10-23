711f771605d6f6ded372906b20e9333799bfc6f9
Ed Brett <e.j.a.brett@gmail.com>
2020-10-23 23:39:38 +0100

    prevent tracking of user data until cookies accepted

--- pages/_document.js
+++ pages/_document.js
@@ -19,10 +19,6 @@ export default class MyDocument extends Document {
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 
           <meta
-            name="google-translate-customization"
-            content="95a31ce28e08eaf2-a31ff49177f3ed6f-g9553d70b9ef10300-15"
-          />
-          <meta
             name="google-site-verification"
             content="xx82D6cZ40Hvf-TT9jkhfsVi11yIeShPcK0zcc7m7ak"
           />
@@ -52,18 +48,7 @@ export default class MyDocument extends Document {
           />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
-          <script
-            type="text/javascript"
-            src="/scripts/hotjar.js"
-            async
-            rel="preconnect"
-          />
-          <script
-            type="text/javascript"
-            src="//script.crazyegg.com/pages/scripts/0027/6897.js"
-            async
-            rel="preconnect"
-          />
+
           <script
             type="text/javascript"
             src="/scripts/transifex.js"
