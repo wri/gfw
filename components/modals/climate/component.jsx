d9eac287e8db71f193d52deab9c98f38c28fe5f2
Ed Brett <e.j.a.brett@gmail.com>
2020-10-24 21:13:19 +0100

    fix errors with fires and climate modal props

--- components/modals/climate/component.jsx
+++ components/modals/climate/component.jsx
@@ -1,5 +1,4 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
+import { useRouter } from 'next/router';
 
 import Modal from 'components/modal';
 
@@ -7,41 +6,37 @@ import { setModalClimateOpen, setContactUsOpen } from './actions';
 
 import './styles.scss';
 
-class ModalGFWClimate extends PureComponent {
-  render() {
-    const { open } = this.props;
+const ModalGFWClimate = () => {
+  const {
+    query: { gfwclimate },
+  } = useRouter();
 
-    return (
-      <Modal
-        open={open}
-        contentLabel="Global Forest Watch Climate"
-        onRequestClose={() => {
-          setModalClimateOpen(false);
-        }}
-        title="Global Forest Watch Climate."
-        className="c-gfw-climate-modal"
-      >
-        <p>
-          {`The Global Forest Watch Climate website is no longer available. We
-          hope you can find the data and information you're looking for
-          here. If not, `}
-          <a
-            href=""
-            onClick={() => {
-              setContactUsOpen();
-            }}
-          >
-            contact us
-          </a>
-          {" and we'll be happy to help."}
-        </p>
-      </Modal>
-    );
-  }
-}
-
-ModalGFWClimate.propTypes = {
-  open: PropTypes.bool,
+  return (
+    <Modal
+      open={!!gfwclimate}
+      contentLabel="Global Forest Watch Climate"
+      onRequestClose={() => {
+        setModalClimateOpen(false);
+      }}
+      title="Global Forest Watch Climate."
+      className="c-gfw-climate-modal"
+    >
+      <p>
+        {`The Global Forest Watch Climate website is no longer available. We
+        hope you can find the data and information you're looking for
+        here. If not, `}
+        <a
+          href=""
+          onClick={() => {
+            setContactUsOpen();
+          }}
+        >
+          contact us
+        </a>
+        {" and we'll be happy to help."}
+      </p>
+    </Modal>
+  );
 };
 
 export default ModalGFWClimate;
