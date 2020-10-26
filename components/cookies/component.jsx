df70eedda84038bb33ec08d0724813f787793ed7
Ed Brett <e.j.a.brett@gmail.com>
2020-10-26 18:28:21 +0000

    update gfw-components, add cookies banner from lib

--- components/cookies/component.jsx
+++ components/cookies/component.jsx
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 
+import { CookiesBanner } from 'gfw-components';
 import { track } from 'analytics';
-import Button from 'components/ui/button';
 
 import './styles.scss';
 
@@ -17,45 +17,15 @@ class Cookies extends PureComponent {
   agreeCookies = () => {
     this.setState({ open: false });
     localStorage.setItem('agreeCookies', true);
+    track('acceptCookies');
   };
 
   render() {
     const { open } = this.state;
 
     return open ? (
-      <div className="c-cookies">
-        <div className="row">
-          <div className="column small-12 medium-8 medium-offset-1">
-            <p className="cookies-text">
-              This website uses cookies to provide you with an improved user
-              experience. By continuing to browse this site, you consent to the
-              use of cookies and similar technologies. Please visit our
-              <a
-                href="/privacy-policy"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {' '}
-                privacy policy
-                {' '}
-              </a>
-              {' '}
-              for further details.
-            </p>
-          </div>
-          <div className="column small-12 medium-2 cookies-button">
-            <Button
-              className="cookies-btn"
-              theme="theme-button-grey theme-button-small"
-              onClick={() => {
-                this.agreeCookies();
-                track('acceptCookies');
-              }}
-            >
-              I agree
-            </Button>
-          </div>
-        </div>
+      <div className="c-cookies-banner">
+        <CookiesBanner onAccept={this.agreeCookies} />
       </div>
     ) : null;
   }
