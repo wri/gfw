127edb8c0317728fdfd529bbaa35ee81a3c8f9b0
Ed Brett <e.j.a.brett@gmail.com>
2020-10-28 14:01:38 +0000

    simplify store using redux toolkit

--- redux/store.js
+++ redux/store.js
@@ -1,42 +1,12 @@
 import { useMemo } from 'react';
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunkMiddleware from 'redux-thunk';
+import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from 'fast-redux';
 
-let store;
+const createStore = () =>
+  configureStore({
+    reducer: rootReducer,
+  });
 
-function initStore(initialState) {
-  return createStore(
-    rootReducer,
-    initialState,
-    composeWithDevTools(applyMiddleware(thunkMiddleware))
-  );
-}
+const makeStore = () => useMemo(() => createStore(), []);
 
-export const initializeStore = (preloadedState) => {
-  let _store = store ?? initStore(preloadedState);
-
-  // After navigating to a page with an initial Redux state, merge that state
-  // with the current state in the store, and create a new store
-  if (preloadedState && store) {
-    _store = initStore({
-      ...store.getState(),
-      ...preloadedState,
-    });
-    // Reset the current store
-    store = undefined;
-  }
-
-  // For SSG and SSR always create a new store
-  if (typeof window === 'undefined') return _store;
-  // Create the store once in the client
-  if (!store) store = _store;
-
-  return _store;
-};
-
-export const useStore = (initialState) =>
-  useMemo(() => initializeStore(initialState), [initialState]);
-
-export default useStore;
+export default makeStore;
