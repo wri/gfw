45eb6f86409b2914de4ebbc2382475fb30ec866c
Ed Brett <e.j.a.brett@gmail.com>
2020-10-29 16:22:35 +0000

    hotfix for url mapping to store on first load

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,11 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useMemo } from 'react';
 import finallyShim from 'promise.prototype.finally';
 import { combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import useStore from 'redux/store';
 import { rootReducer } from 'fast-redux';
 import isEmpty from 'lodash/isEmpty';
-import useDeepCompareEffect from 'use-deep-compare-effect';
 
 import reducerRegistry from 'redux/registry';
 
@@ -27,15 +26,15 @@ if (process.env.NODE_ENV !== 'production' && module.hot) {
 
 const App = ({ Component, pageProps }) => {
   const store = useStore();
-  const reducers = reducerRegistry.getReducers();
 
-  useDeepCompareEffect(() => {
+  useMemo(() => {
+    const reducers = reducerRegistry.getReducers();
     store.replaceReducer(
       isEmpty(reducers)
         ? rootReducer
         : combineReducers(reducerRegistry.getReducers())
     );
-  }, [reducers]);
+  });
 
   return (
     <Provider store={store}>
