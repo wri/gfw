45b1ae067574c9d8d1e946c323d57ccf8d162c18
Ed Brett <e.j.a.brett@gmail.com>
2020-11-01 14:15:21 +0000

    comments for url utils

--- utils/url.js
+++ utils/url.js
@@ -22,6 +22,7 @@ const idToSlugDict = legacyIds.reduce(
   {}
 );
 
+// maps legacy ids for layers and datasets to application level slugs
 const remapDatasets = (datasets) =>
   datasets?.reduce(
     (arr, dataset) => [
@@ -39,44 +40,57 @@ const remapDatasets = (datasets) =>
   );
 
 export const decodeQueryParams = (params) => {
-  const paramsParsed = {};
-  Object.keys(params).forEach((key) => {
+  const decodedParams = Object.keys(params)?.reduce((obj, key) => {
     try {
-      paramsParsed[key] = JSON.parse(
-        Buffer.from(params[key], 'base64').toString('binary')
-      );
+      return {
+        ...obj,
+        // we use Buffer as atob is node native to node for SSR
+        [key]: JSON.parse(
+          Buffer.from(params[key], 'base64').toString('binary')
+        ),
+      };
     } catch (err) {
       try {
-        paramsParsed[key] = JSON.parse(params[key]);
+        return {
+          ...obj,
+          [key]: JSON.parse(params[key]),
+        };
       } catch (error) {
-        paramsParsed[key] = params[key];
+        return {
+          ...obj,
+          [key]: params[key],
+        };
       }
     }
-  });
+  }, {});
 
-  if (paramsParsed?.map?.datasets) {
-    paramsParsed.map = {
-      ...paramsParsed.map,
-      datasets: remapDatasets(paramsParsed.map.datasets),
+  if (decodedParams?.map?.datasets) {
+    decodedParams.map = {
+      ...decodedParams.map,
+      datasets: remapDatasets(decodedParams.map.datasets),
     };
   }
 
-  return paramsParsed;
+  return decodedParams;
 };
 
 export const encodeQueryParams = (params, options) => {
-  const paramsParsed = {};
-  Object.keys(params).forEach((key) => {
+  const encodedParams = Object.keys(params)?.reduce((obj, key) => {
+    // if params in an object encode in base64
     if (
       params[key] &&
       typeof params[key] === 'object' &&
       !isEmpty(params[key])
     ) {
-      paramsParsed[key] = btoa(JSON.stringify(params[key]));
-    } else if (typeof params[key] !== 'object' && params[key]) {
-      paramsParsed[key] = params[key];
+      return { ...obj, [key]: btoa(JSON.stringify(params[key])) };
     }
-  });
+    // if params is a valid key and not falsey
+    if (typeof params[key] !== 'object' && params[key]) {
+      return { ...obj, [key]: params[key] };
+    }
+    // otherwise ingore key
+    return obj;
+  }, {});
 
-  return stringify(paramsParsed, options);
+  return stringify(encodedParams, options);
 };
