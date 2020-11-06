75779c1f8a2729b4fc061ffc1c7f49c1c947311a
Ed Brett <e.j.a.brett@gmail.com>
2020-11-06 13:43:18 +0000

    add error handling for planet tiles endpoint

--- pages/api/planet-tiles/[...params].js
+++ pages/api/planet-tiles/[...params].js
@@ -6,16 +6,20 @@ export default async function userHandler(req, res) {
     method,
   } = req;
   if (method === 'GET') {
-    const tile = await axios.get(
-      `https://tiles.planet.com/basemaps/v1/planet-tiles/${params?.join(
-        '/'
-      )}.png?api_key=${process.env.NEXT_PUBLIC_PLANET_API_KEY}`,
-      {
-        responseType: 'arraybuffer',
-      }
-    );
-    res.setHeader('content-type', 'image/png');
-    res.send(tile?.data);
+    try {
+      const tile = await axios.get(
+        `https://tiles.planet.com/basemaps/v1/planet-tiles/${params?.join(
+          '/'
+        )}.png?api_key=${process.env.NEXT_PUBLIC_PLANET_API_KEY}`,
+        {
+          responseType: 'arraybuffer',
+        }
+      );
+      res.setHeader('content-type', 'image/png');
+      res.send(tile?.data);
+    } catch (err) {
+      res.status(500).end('Error getting tile');
+    }
   } else {
     res.setHeader('Allow', ['GET', 'PUT']);
     res.status(405).end(`Method ${method} Not Allowed`);
