55a819ea8279efdd39585ac4c4dd623749934c5f
teresa-n-schofield <45769948+teresa-n-schofield@users.noreply.github.com>
2021-01-13 17:15:00 -0500

    update of SGF page for 2021

--- layouts/grants-and-fellowships/apply/component.jsx
+++ layouts/grants-and-fellowships/apply/component.jsx
@@ -6,46 +6,20 @@ import techLogo from './assets/gfw-tech-fellowship-logo.png';
 import './styles.scss';
 
 const GrantsApplySection = () => (
-  <section className="l-grants-apply-section">
-    <Row>
-      <Column width={[1, 3 / 4]}>
-        <p className="text -paragraph -color-2 -light -spaced">
-          The specific focus of the Calls for Applications varies from year to
-          year, though successful applicants must clearly articulate how GFW
-          will support and enhance their ongoing work related to:
-        </p>
-        <ul className="list">
-          <li>- Advocacy</li>
-          <li>- Community engagement</li>
-          <li>- Forest monitoring and enforcement</li>
-          <li>- Journalism</li>
-          <li>- Education</li>
-        </ul>
-        <p className="text -paragraph -color-2 -light -spaced">
-          For details on how to apply to each, please see below:
-        </p>
-      </Column>
-    </Row>
     <section className="apply-section">
       <Row>
         <Column width={[1, 3 / 4]} className="logo">
           <h2 className="section-title">Small Grants Fund</h2>
           <ul className="list">
             <p className="text -paragraph -color-2 -light -spaced">
-              The scope of this year’s Small Grants Fund is on using near
-              real-time data to combat deforestation. Winning proposals will
-              clearly demonstrate how they intend to use GLAD deforestation
-              and/or VIIRs fire alerts, made available through GFW tools, to
-              curb deforestation through enforcement or advocacy. Please note
-              that this year’s call also has geographic restrictions. Applicants
-              should carefully read through the Guidelines for Applicants and
-              Frequently Asked Questions for more details.
+              The scope of this year’s Small Grants Fund focuses on three key themes: forest protection, forests and agriculture, and forest policy. The specific objectives for each theme, along with more details on the application process, can be found in the Guidelines for Applicants. Before applying, please carefully review these guidelines along with the Small Grants Fund FAQ.  
+Winning proposals will clearly demonstrate how they intend to use GFW’s suite of tools and data on GFW to curb deforestation within one of these three key themes.
             </p>
             <li>
-              - Read the 2020&nbsp;
+              - Read the 2021&nbsp;
               <a
                 className="text -paragraph -color-4 -bold"
-                href="http://s3.amazonaws.com/gfw.blog/Training%20Guides/SGF/Guidelines%20for%20SGF%20Applicants%202020%20%281%29.pdf"
+                href="http://s3.amazonaws.com/gfw.blog/Training%20Guides/SGF/Guidelines%20for%20SGF%20Applicants%202021.pdf"
                 target="_blank"
                 rel="noopener noreferrer"
               >
@@ -56,7 +30,7 @@ const GrantsApplySection = () => (
               - Review the&nbsp;
               <a
                 className="text -paragraph -color-4 -bold"
-                href="https://www.globalforestwatch.org/howto/categories/faqs/?page=0&filters=[%22small-grants-fund%22]"
+                href="https://www.globalforestwatch.org/help/faqs/grants-fellowships/"
                 target="_blank"
                 rel="noopener noreferrer"
               >
@@ -64,19 +38,29 @@ const GrantsApplySection = () => (
               </a>
             </li>
             <li>
-              - Listen to the&nbsp;
+              - Register for the&nbsp;
               <a
                 className="text -paragraph -color-4 -bold"
-                href="https://youtu.be/hHar_nUYi9k"
+                href="https://wri.zoom.us/webinar/register/WN_8Tu5L0MbRaaZwIvseMS74Q"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                2020 Small Grants Fund Webinar
+                2021 Small Grants Fund Applicant Webinar
               </a>
+              on Wednesday, February 3rd, 2021 at 8:00 am EST
             </li>
           </ul>
           <p className="text -paragraph -color-2 -light -spaced">
-            The 2020 Small Grants Fund application window is now closed.
+            The 2021 Small Grants Fund application window is open from February 1st, 2021 – March 1st, 2021. 
+            <a
+              className="text -paragraph -color-4 -bold"
+              href="https://gfw.smapply.io/prog/small_grants_fund_2021_application_/"
+              target="_blank"
+              rel="noopener noreferrer"
+              >
+              Apply now
+            </a>
+            .
           </p>
         </Column>
         <Column width={[1, 1 / 4]} className="logo">
@@ -88,32 +72,17 @@ const GrantsApplySection = () => (
       <Row>
         <Column width={[1, 3 / 4]} className="logo">
           <h2 className="section-title">Tech Fellowship</h2>
-          <ul className="list">
-            <li>
-              - Review the&nbsp;
-              <a
-                className="text -paragraph -color-4 -bold"
-                href="https://www.globalforestwatch.org/howto/categories/faqs/?page=0&filters=[%22tech-fellowship%22]"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Tech Fellowship Frequently Asked Questions.
-              </a>
-            </li>
-            <li>
-              - Read the 2019&nbsp;
-              <a
-                className="text -paragraph -color-4 -bold"
-                href="https://blog.globalforestwatch.org/wp-content/uploads/2019/01/2019-Fellowship-Guidelines-for-Applicants.pdf"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Guidelines for Applicants
-              </a>
-            </li>
-          </ul>
           <p className="text -paragraph -color-2 -light -spaced">
-            The 2019 Tech Fellowship application window is now closed.
+            We are not currently accepting applications for the GFW Tech Fellowship. For more information about the Tech Fellowship, see our 
+            <a
+              className="text -paragraph -color-4 -bold"
+              href="https://www.globalforestwatch.org/help/faqs/grants-fellowships/"
+              target="_blank"
+              rel="noopener noreferrer"
+              >
+              Frequently Asked Questions
+            </a>
+            .
           </p>
         </Column>
         <Column width={[1, 1 / 4]} className="logo">
