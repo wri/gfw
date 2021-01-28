029923b578fd7273f47428e4cd51446ecdfc4c2c
Tomas Eriksson <tomasmoosee@gmail.com>
2021-01-28 12:14:08 +0100

    simplify hotjar, they deal with most logic

--- utils/hotjar.js
+++ utils/hotjar.js
@@ -1,24 +1,14 @@
-import { hasSeenSurvey, setSurveySeenCookie } from 'utils/cookies';
-
 const isServer = typeof window !== 'undefined';
 const isDebugMode = !isServer && process.env.NODE_ENV !== 'production';
 
 // Hotjar events
 export const TOGGLE_PLANET_BASEMAP = 'planet_toggle_on';
 
-export const triggerEvent = (event, delay = 5) => {
+export const triggerEvent = (event) => {
   if (isDebugMode) {
-    console.info(`hotjar trigger ${event} with delay ${delay}s`); // eslint-disable-line
+    console.info(`hotjar trigger ${event}`); // eslint-disable-line
   }
-  if (typeof hj === 'function' && !hasSeenSurvey(event)) {
-    setTimeout(() => {
-      hj('trigger', event);
-      setSurveySeenCookie(event);
-      if (isDebugMode) {
-        console.info(`Event ${event} triggered after ${delay}s`); // eslint-disable-line
-      }
-    }, delay * 1000);
-  } else if (isDebugMode) {
-    console.error('Hotjar not available'); // eslint-disable-line
+  if (!isServer && typeof hj === 'function') {
+    hj('trigger', event);
   }
 };
