0310761a42a7f73ef1ca3517935712004f3cdb65
Adam J. Pain <aj.pain89@gmail.com>
2021-02-01 17:48:32 +0100

    Add otf tests

--- cypress/integration/otf.js
+++ cypress/integration/otf.js
@@ -18,9 +18,27 @@ const testConfig = [
         description: 'Rio Branco shape Testing OTF tree cover loss',
         visit:
           '/map/geostore/211fca3fbea311473797cc967bc7a073/?analysis=eyJzaG93RHJhdyI6dHJ1ZX0%3D&mainMap=eyJzaG93QW5hbHlzaXMiOnRydWV9&map=eyJjZW50ZXIiOnsibGF0IjotOS43OTk1NDczMDQwNjM5ODMsImxuZyI6LTY3LjgzNjM2NDI5MDkzNDExfSwiem9vbSI6MTEuOTY2ODg2Njk1ODc3MDA2LCJjYW5Cb3VuZCI6ZmFsc2UsImRhdGFzZXRzIjpbeyJkYXRhc2V0IjoicG9saXRpY2FsLWJvdW5kYXJpZXMiLCJsYXllcnMiOlsiZGlzcHV0ZWQtcG9saXRpY2FsLWJvdW5kYXJpZXMiLCJwb2xpdGljYWwtYm91bmRhcmllcyJdLCJvcGFjaXR5IjoxLCJ2aXNpYmlsaXR5Ijp0cnVlfSx7ImRhdGFzZXQiOiJ0cmVlLWNvdmVyLWxvc3MiLCJsYXllcnMiOlsidHJlZS1jb3Zlci1sb3NzIl0sIm9wYWNpdHkiOjEsInZpc2liaWxpdHkiOnRydWV9XX0%3D',
-        selector: '#treeLoss .c-dynamic-sentence',
+        selector: '#treeLoss .c-dynamic-sentence notranslate sentence',
         sentence:
           'From {startYear} to {endYear}, Area in Acre, Brazil lost {loss} of tree cover, equivalent to a {percent} decrease in tree cover since {extentYear}.',
+      },
+      {
+        slug: 'default',
+        description: 'Rio Branco shape Testing OTF tree cover extent 2000',
+        visit:
+          '/map/geostore/211fca3fbea311473797cc967bc7a073/?analysis=eyJzaG93RHJhdyI6dHJ1ZX0%3D&map=eyJjZW50ZXIiOnsibGF0IjotOS43OTk1NDczMDQwNjM5ODMsImxuZyI6LTY3LjgzNjM2NDI5MDkzNDExfSwiem9vbSI6MTEuOTY2ODg2Njk1ODc3MDA2LCJkYXRhc2V0cyI6W3siZGF0YXNldCI6InRyZWUtY292ZXIiLCJvcGFjaXR5IjoxLCJ2aXNpYmlsaXR5Ijp0cnVlLCJsYXllcnMiOlsidHJlZS1jb3Zlci0yMDAwIl19LHsiZGF0YXNldCI6InBvbGl0aWNhbC1ib3VuZGFyaWVzIiwibGF5ZXJzIjpbImRpc3B1dGVkLXBvbGl0aWNhbC1ib3VuZGFyaWVzIiwicG9saXRpY2FsLWJvdW5kYXJpZXMiXSwib3BhY2l0eSI6MSwidmlzaWJpbGl0eSI6dHJ1ZX1dfQ%3D%3D&mapMenu=eyJtZW51U2VjdGlvbiI6ImRhdGFzZXRzIiwiZGF0YXNldENhdGVnb3J5IjoibGFuZENvdmVyIn0%3D',
+        selector: '.c-dynamic-sentence notranslate sentence',
+        sentence:
+          'As of {year}, {percentage} of Area in Acre, Brazil was tree cover.',
+      },
+      {
+        slug: 'default',
+        description: 'Rio Branco shape Testing OTF glad',
+        visit:
+          '/map/geostore/211fca3fbea311473797cc967bc7a073/?analysis=eyJzaG93RHJhdyI6dHJ1ZX0%3D&mainMap=eyJzaG93QW5hbHlzaXMiOnRydWV9&map=eyJjZW50ZXIiOnsibGF0IjotOS43ODk4NTQ0NDk2NjE2NjYsImxuZyI6LTY3LjgxNDg0NzUzNTk3NzM0fSwiem9vbSI6MTMuMzM2NDc1NzEyMzY5Nzc2LCJkYXRhc2V0cyI6W3siZGF0YXNldCI6ImdsYWQtZGVmb3Jlc3RhdGlvbi1hbGVydHMiLCJvcGFjaXR5IjoxLCJ2aXNpYmlsaXR5Ijp0cnVlLCJsYXllcnMiOlsiZGVmb3Jlc3RhdGlvbi1hbGVydHMtZ2xhZCJdLCJ0aW1lbGluZVBhcmFtcyI6eyJzdGFydERhdGUiOiIyMDE1LTAxLTAxIiwiZW5kRGF0ZSI6IjIwMTUtMTItMzEiLCJ0cmltRW5kRGF0ZSI6IjIwMTUtMTItMzEifX0seyJkYXRhc2V0IjoicG9saXRpY2FsLWJvdW5kYXJpZXMiLCJsYXllcnMiOlsiZGlzcHV0ZWQtcG9saXRpY2FsLWJvdW5kYXJpZXMiLCJwb2xpdGljYWwtYm91bmRhcmllcyJdLCJvcGFjaXR5IjoxLCJ2aXNpYmlsaXR5Ijp0cnVlfV19&mapMenu=eyJtZW51U2VjdGlvbiI6ImRhdGFzZXRzIiwiZGF0YXNldENhdGVnb3J5IjoiZm9yZXN0Q2hhbmdlIn0%3D',
+        selector: '.c-dynamic-sentence notranslate sentence',
+        sentence:
+          'There were {count} GLAD alerts reported in the week of the {date}. This was {status} compared to the same week in previous years.',
       },
     ],
     spec: {
