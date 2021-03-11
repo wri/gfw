eb23241606995d47e445debe597fb74f1c6bfcd2
Adam J. Pain <aj.pain89@gmail.com>
2021-03-11 12:54:07 +0100

    Add staging data api

--- utils/apis.js
+++ utils/apis.js
@@ -1,5 +1,7 @@
 export const GFW_API = 'https://production-api.globalforestwatch.org';
 export const GFW_DATA_API = 'https://data-api.globalforestwatch.org';
+export const GFW_STAGING_DATA_API =
+  'https://staging-data-api.globalforestwatch.org';
 export const GFW_TILES_API = 'https://tiles.globalforestwatch.org';
 export const CARTO_API = 'https://wri-01.carto.com/api/v2';
 export const MAPBOX_API = 'https://api.mapbox.com/';
