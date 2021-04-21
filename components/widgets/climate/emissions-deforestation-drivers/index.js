671eb30cd19cd780adc02f2bb9fde7156ac7976e
Adam J. Pain <aj.pain89@gmail.com>
2021-04-21 10:51:44 +0200

    Initial commit, create widget

---
+++ components/widgets/climate/emissions-deforestation-drivers/index.js
@@ -0,0 +1,127 @@
+import { all, spread } from 'axios';
+import { getYearsRangeFromMinMax } from 'components/widgets/utils/data';
+
+import {
+  POLITICAL_BOUNDARIES_DATASET,
+  TREE_COVER_LOSS_BY_DOMINANT_DRIVER_DATASET,
+} from 'data/datasets';
+import {
+  DISPUTED_POLITICAL_BOUNDARIES,
+  POLITICAL_BOUNDARIES,
+  TREE_COVER_LOSS_BY_DOMINANT_DRIVER,
+} from 'data/layers';
+
+import treeLoss from 'components/widgets/forest-change/tree-loss';
+import { getExtent, getLoss } from 'services/analysis-cached';
+
+import getWidgetProps from './selectors';
+
+const MIN_YEAR = 2001;
+const MAX_YEAR = 2019;
+
+export default {
+  ...treeLoss,
+  widget: 'emissionsDeforestationDrivers',
+  title: 'Emissions from biomass loss in {location} by driver',
+  types: ['country', 'geostore', 'aoi', 'use', 'wdpa'],
+  admins: ['adm0', 'adm1', 'adm2'],
+  settingsConfig: [
+    {
+      key: 'tscDriverGroup',
+      label: 'drivers',
+      type: 'select',
+    },
+    {
+      key: 'years',
+      label: 'years',
+      endKey: 'endYear',
+      startKey: 'startYear',
+      type: 'range-select',
+      border: true,
+    },
+    {
+      key: 'threshold',
+      label: 'canopy density',
+      type: 'mini-select',
+      metaKey: 'widget_canopy_density',
+    },
+  ],
+  chartType: 'composedChart',
+  datasets: [
+    {
+      dataset: POLITICAL_BOUNDARIES_DATASET,
+      layers: [DISPUTED_POLITICAL_BOUNDARIES, POLITICAL_BOUNDARIES],
+      boundary: true,
+    },
+    {
+      dataset: POLITICAL_BOUNDARIES_DATASET,
+      layers: [DISPUTED_POLITICAL_BOUNDARIES, POLITICAL_BOUNDARIES],
+      boundary: true,
+    },
+    // loss tsc
+    {
+      dataset: TREE_COVER_LOSS_BY_DOMINANT_DRIVER_DATASET,
+      layers: [TREE_COVER_LOSS_BY_DOMINANT_DRIVER],
+    },
+  ],
+  metaKey: '',
+  colors: 'climate',
+  sortOrder: {
+    climate: 2,
+  },
+  settings: {
+    tscDriverGroup: 'all',
+    highlighted: false,
+    extentYear: 2000,
+    threshold: 30,
+  },
+  sentences: {
+    initial:
+      'In {location} from {startYear} to {endYear}, {permPercent} of tree cover loss occurred in areas where the dominant drivers of loss resulted in {deforestation}.',
+    noLoss:
+      'In {location} from {startYear} to {endYear}, <b>no</b> tree cover loss occurred in areas where the dominant drivers of loss resulted in {deforestation}.',
+    globalInitial:
+      '{location} from {startYear} to {endYear}, {permPercent} of tree cover loss occurred in areas where the dominant drivers of loss resulted in {deforestation}.',
+  },
+  whitelists: {
+    checkStatus: true,
+  },
+  getData: (params) =>
+    all([
+      getLoss({ ...params, landCategory: 'tsc', lossTsc: true }),
+      getExtent({ ...params }),
+    ]).then(
+      spread((loss, extent) => {
+        let data = {};
+        if (loss && loss.data && extent && extent.data) {
+          data = {
+            loss: loss.data.data.filter(
+              (d) => d.tsc_tree_cover_loss_drivers__type !== 'Unknown'
+            ),
+            extent: (loss.data.data && extent.data.data[0].value) || 0,
+          };
+        }
+
+        const { startYear, endYear, range } = getYearsRangeFromMinMax(
+          MIN_YEAR,
+          MAX_YEAR
+        );
+        return {
+          ...data,
+          settings: {
+            startYear,
+            endYear,
+            yearsRange: range,
+          },
+          options: {
+            years: range,
+          },
+        };
+      })
+    ),
+  getDataURL: (params) => [
+    getLoss({ ...params, landCategory: 'tsc', lossTsc: true, download: true }),
+    getExtent({ ...params, download: true }),
+  ],
+  getWidgetProps,
+};
