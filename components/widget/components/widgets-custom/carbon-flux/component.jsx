e41ee8ffcb14a40adf56572e0b3a033a5b09ae6b
mluena <maluenarod@gmail.com>
2021-05-12 13:57:39 +0200

    svg for flux carbon widget

--- components/widget/components/widgets-custom/carbon-flux/component.jsx
+++ components/widget/components/widgets-custom/carbon-flux/component.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
-import Image1 from 'assets/icons/tree-success.png';
+import Emissions from 'assets/icons/widgets/carbon-flux/emissions.svg';
+import Removals from 'assets/icons/widgets/carbon-flux/removals.svg';
 
 import './styles.scss';
 
 export const Component = () => (
   <div className="c-carbon-flux-image-component">
-    <img className="image" src={Image1} alt="success-tree" />
-    <img className="image" src={Image1} alt="success-tree" />
+    <img className="image" src={Removals} alt="Removals" />
+    <img className="image" src={Emissions} alt="Emissions" />
   </div>
 );
 
