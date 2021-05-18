74de019a5c14e5b8b3c6b8287eb03c08aedb65f2
Adam J. Pain <aj.pain89@gmail.com>
2021-05-18 11:50:19 +0200

    Fix global extent query

--- components/widgets/land-cover/intact-tree-cover/index.js
+++ components/widgets/land-cover/intact-tree-cover/index.js
@@ -91,28 +91,22 @@ export default {
     all([
       getExtent({ ...params, forestType: '' }),
       getExtent({ ...params }),
-      getExtent({ ...params, forestType: 'plantations' }),
     ]).then(
-      spread((gadm28Response, iflResponse, plantationsResponse) => {
-        const gadmExtent = gadm28Response.data && gadm28Response.data.data;
+      spread((adminResponse, iflResponse) => {
+        const adminExtent = adminResponse.data && adminResponse.data.data;
         const iflExtent = iflResponse.data && iflResponse.data.data;
         let totalArea = 0;
         let totalExtent = 0;
         let extent = 0;
-        let plantations = 0;
         let data = {};
-        const plantationsData =
-          plantationsResponse.data && plantationsResponse.data.data;
-        plantations = plantationsData.length ? plantationsData[0].extent : 0;
-        if (iflExtent.length && gadmExtent.length) {
-          totalArea = gadmExtent[0].total_area;
-          totalExtent = gadmExtent[0].extent;
+        if (iflExtent.length && adminExtent.length) {
+          totalArea = adminExtent[0].total_area;
+          totalExtent = adminExtent[0].extent;
           extent = iflExtent[0].extent;
           data = {
             totalArea,
             totalExtent,
             extent,
-            plantations,
           };
         }
         return data;
@@ -121,7 +115,6 @@ export default {
   getDataURL: (params) => [
     getExtent({ ...params, forestType: '', download: true }),
     getExtent({ ...params, download: true }),
-    getExtent({ ...params, forestType: 'plantations', download: true }),
   ],
   getWidgetProps,
 };
