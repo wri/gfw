630bb226a693ac92e185b7dc4d5b49986d7104f6
Tomas Eriksson <tomasmoosee@gmail.com>
2021-05-24 13:10:13 +0200

    add widget loader to global sentence

--- layouts/dashboards/components/global-sentence/selectors.js
+++ layouts/dashboards/components/global-sentence/selectors.js
@@ -5,9 +5,11 @@ import { getActiveCategory } from 'components/widgets/selectors';
 import {
   selectLocation,
   getAdminsSelected,
+  selectLoading,
 } from 'layouts/dashboards/components/header/selectors';
 
 export const getGlobalSentenceProps = createStructuredSelector({
+  loading: selectLoading,
   location: selectLocation,
   locationNames: getAdminsSelected,
   category: getActiveCategory,
