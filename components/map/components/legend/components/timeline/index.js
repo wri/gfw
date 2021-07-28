5cd0e4feefcd28f67672c64f67dd4c05e8682768
Tomas Eriksson <tomasmoosee@gmail.com>
2021-07-28 11:57:30 +0200

    add fetch for glad latest within timeline

--- components/map/components/legend/components/timeline/index.js
+++ components/map/components/legend/components/timeline/index.js
@@ -7,6 +7,15 @@ import { dateRange } from 'utils/date-range';
 import TimelineComponent from './component';
 import { getMarks } from './selectors';
 
+const getLatestDate = (url, defaultDate) => {
+  return fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const latest = data[0]?.attributes?.date;
+      return latest || defaultDate;
+    });
+};
+
 const mapStateToProps = (
   state,
   { maxDate, minDate, startDate, endDate, trimEndDate, ...props }
@@ -18,6 +27,7 @@ const mapStateToProps = (
     endDate,
     trimEndDate,
   };
+
   return {
     marks: getMarks({ dates }),
     ...props,
@@ -25,6 +35,29 @@ const mapStateToProps = (
 };
 
 class TimelineContainer extends PureComponent {
+  state = {
+    minDate: '',
+  };
+
+  async fetchMinDate() {
+    const { minDate } = this.props;
+    // if props.url from layer, we should only perform this fetch if present
+    const latest = await getLatestDate(
+      'https://api.resourcewatch.org/glad-alerts/latest',
+      minDate
+    );
+    // else, do nothing
+    if (latest) {
+      this.setState({ minDate: latest });
+    } else {
+      this.setState({ minDate });
+    }
+  }
+
+  componentDidMount() {
+    this.fetchMinDate();
+  }
+
   handleOnDateChange = (date, position, absolute) => {
     const { handleChange } = this.props;
 
@@ -50,6 +83,7 @@ class TimelineContainer extends PureComponent {
 TimelineContainer.propTypes = {
   handleChange: PropTypes.func,
   activeLayer: PropTypes.object,
+  minDate: PropTypes.string,
 };
 
 export default connect(mapStateToProps, null)(TimelineContainer);
