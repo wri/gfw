f275ae0170e473180fc3d6899c6e8d985b515c6a
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2021-08-03 12:10:02 +0100

    Add followRedirects flag to prevent ending up in a AWS LB

--- pages/api/data-api/[...params].js
+++ pages/api/data-api/[...params].js
@@ -7,16 +7,18 @@ const DATA_API_KEY = process.env.NEXT_PUBLIC_DATA_API_KEY;
 const GFW_API_URL =
   ENVIRONMENT === 'staging' ? GFW_STAGING_DATA_API : GFW_DATA_API;
 
-export default (req, res) => httpProxyMiddleware(req, res, {
-  // You can use the `http-proxy` option
-  target: GFW_API_URL,
-  // In addition, you can use the `pathRewrite` option provided by `next-http-proxy`
-  pathRewrite: {
-    '^/api/data-api': '/'
-  },
-  headers: {
-    'x-api-key': DATA_API_KEY,
-  }
-}).catch(async (error) => {
-  res.end(error.message);
-});
+export default (req, res) =>
+  httpProxyMiddleware(req, res, {
+    // You can use the `http-proxy` option
+    target: GFW_API_URL,
+    // In addition, you can use the `pathRewrite` option provided by `next-http-proxy`
+    pathRewrite: {
+      '^/api/data-api': '/',
+    },
+    headers: {
+      'x-api-key': DATA_API_KEY,
+    },
+    followRedirects: true,
+  }).catch(async (error) => {
+    res.end(error.message);
+  });
