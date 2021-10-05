213bffedc97f8361a78f5600bb96ed81a9548bd8
elpamart <elena.palao@vizzuality.com>
2021-10-05 18:08:43 +0200

    first steps in integrated ranked widget

--- components/widgets/manifest.js
+++ components/widgets/manifest.js
@@ -14,6 +14,7 @@ import treeCoverGainSimple from 'components/widgets/forest-change/tree-cover-gai
 import glads from 'components/widgets/forest-change/glads';
 import gladRanked from 'components/widgets/forest-change/glad-ranked';
 import gladAlertsSimple from 'components/widgets/forest-change/glad-alerts-simple';
+import integratedAlertsRanked from 'components/widgets/forest-change/integrated-alerts-ranked';
 
 // fires
 import firesAlerts from 'components/widgets/fires/fires-alerts';
@@ -66,6 +67,7 @@ export default {
   glads,
   gladRanked,
   gladAlertsSimple,
+  integratedAlertsRanked,
 
   // fires
   firesAlerts,
