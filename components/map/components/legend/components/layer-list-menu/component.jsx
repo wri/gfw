e28a6b4bb7c89f57cad2b1fcdb27880b76aa702e
Tomas Eriksson <tomasmoosee@gmail.com>
2021-10-13 12:57:06 +0200

    filter coverage

--- components/map/components/legend/components/layer-list-menu/component.jsx
+++ components/map/components/legend/components/layer-list-menu/component.jsx
@@ -8,42 +8,70 @@ import LayerMoreInfo from '../layer-more-info';
 import './styles.scss';
 
 class LayerListMenu extends PureComponent {
+  filterLayerListMenu() {
+    const { layers } = this.props;
+    const activeLayer = layers && layers.find((l) => l.active);
+
+    return layers
+      .filter((l) => !l.default && !l.isSelector)
+      .filter((l) => {
+        if (l.id.includes('geographic-coverage')) {
+          if (
+            activeLayer.id === 'integrated-deforestation-alerts-8bit' ||
+            activeLayer.id === 'integrated-deforestation-alerts-8bit-gladL'
+          ) {
+            return l.id === 'glad-deforestation-alerts-geographic-coverage';
+          }
+
+          if (activeLayer.id === 'integrated-deforestation-alerts-8bit-radd') {
+            return l.id === 'radd-deforestation-alerts-geographic-coverage';
+          }
+
+          if (activeLayer.id === 'integrated-deforestation-alerts-8bit-gladS') {
+            return (
+              l.id === 'glad-plus-deforestation-alerts-geographic-coverage'
+            );
+          }
+        }
+        return true;
+      });
+  }
+
   render() {
     const { className, layers, onToggle, onInfoClick } = this.props;
     const activeLayer = layers && layers.find((l) => l.active);
+    const filteredLayers = this.filterLayerListMenu();
     return (
       <div className={`c-layer-list-menu ${className || ''}`}>
-        {layers.map((l, i) =>
-          !l.default && !l.isSelector ? (
-            <div className="layer-toggle" key={l.id}>
-              <LayerToggle
-                tabIndex={i}
-                data={{
-                  ...l,
-                  layer: l.id,
-                  ...(l.id === 'confirmedOnly' &&
-                    activeLayer?.highConfInfo && {
-                      description: activeLayer.highConfInfo,
-                    }),
-                }}
-                onToggle={onToggle}
-                onInfoClick={() => onInfoClick(l.metadata)}
-                small
-              />
-              {l.nestedLegend && l.active && (
-                <div className="nested-legend">
-                  <LegendItemTypes activeLayer={l} />
-                </div>
-              )}
-              {l.legendDesc && (
-                <p className="layer-description">{l.legendDesc}</p>
-              )}
-              {l.moreInfo && (
-                <LayerMoreInfo className="more-info" {...l.moreInfo} />
-              )}
-            </div>
-          ) : null
-        )}
+        {filteredLayers.map((l, i) => (
+          <div className="layer-toggle" key={l.id}>
+            <LayerToggle
+              tabIndex={i}
+              data={{
+                ...l,
+                layer: l.id,
+                ...(l.id === 'confirmedOnly' &&
+                  activeLayer?.highConfInfo && {
+                    description: activeLayer.highConfInfo,
+                  }),
+              }}
+              onToggle={onToggle}
+              onInfoClick={() => onInfoClick(l.metadata)}
+              small
+            />
+            {l.nestedLegend && l.active && (
+              <div className="nested-legend">
+                <LegendItemTypes activeLayer={l} />
+              </div>
+            )}
+            {l.legendDesc && (
+              <p className="layer-description">{l.legendDesc}</p>
+            )}
+            {l.moreInfo && (
+              <LayerMoreInfo className="more-info" {...l.moreInfo} />
+            )}
+          </div>
+        ))}
       </div>
     );
   }
