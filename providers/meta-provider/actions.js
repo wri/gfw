6243a41bf527ecf83731717db9774910a90756bc
Tomas Eriksson <tomasmoosee@gmail.com>
2021-11-29 11:56:45 +0100

    fix bug causing map embeds to fail, when id is not present

--- providers/meta-provider/actions.js
+++ providers/meta-provider/actions.js
@@ -8,11 +8,15 @@ export const setGFWMetaLoading = createAction('setGFWMetaLoading');
 export const fetchGfwMeta = createThunkAction(
   'fetchGfwMeta',
   () => async (dispatch) => {
-    dispatch(setGFWMetaLoading({ loading: true, error: false }));
-    const meta = await getGfwMeta();
-    dispatch(setGFWMeta(meta));
-    dispatch(
-      setGFWMetaLoading({ loading: false, error: false, initialized: true })
-    );
+    try {
+      dispatch(setGFWMetaLoading({ loading: true, error: false }));
+      const meta = await getGfwMeta();
+      dispatch(setGFWMeta(meta));
+      dispatch(
+        setGFWMetaLoading({ loading: false, error: false, initialized: true })
+      );
+    } catch (_) {
+      setGFWMetaLoading({ loading: false, error: true, initialized: true });
+    }
   }
 );
