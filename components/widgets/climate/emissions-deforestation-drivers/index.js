5fde32d442a8a6d70fd0a2ea40e634a321e2add6
Pedro Pimenta <pedro@pimenta.co>
2022-05-17 17:32:30 +0200

    Add 2021 to Carbon flux widgets

--- components/widgets/climate/emissions-deforestation-drivers/index.js
+++ components/widgets/climate/emissions-deforestation-drivers/index.js
@@ -19,7 +19,7 @@ import { getEmissions } from 'services/analysis-cached';
 import getWidgetProps from './selectors';
 
 const MIN_YEAR = 2001;
-const MAX_YEAR = 2019;
+const MAX_YEAR = 2021;
 
 export default {
   ...emissionsDeforestation,
