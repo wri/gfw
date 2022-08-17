c79abb9df8d1380c4b365d472e468114db1a977b
Pedro Pimenta <pedro@pimenta.co>
2022-08-17 08:41:11 +0200

    Add redirect to MapBuilderPage

--- pages/mapbuilder.js
+++ pages/mapbuilder.js
@@ -1,41 +1,10 @@
-import PageLayout from 'wrappers/page';
-import MapBuilder from 'layouts/mapbuilder';
-
-import { getPostsByType, getPostByType } from 'services/content';
-
-const MapBuilderPage = (props) => (
-  <PageLayout {...props}>
-    <MapBuilder {...props} />
-  </PageLayout>
-);
+const MapBuilderPage = () => null;
 
 export async function getStaticProps() {
-  const page = await getPostByType({
-    type: 'pages',
-    slug: 'mapbuilder',
-  });
-
-  const apps = await getPostsByType({
-    type: 'apps',
-    params: {
-      per_page: 100,
-    },
-  });
-
-  const tutorials = await getPostsByType({
-    type: 'articles',
-    params: {
-      per_page: 6,
-      help_tools_includes: 1252,
-    },
-  });
-
   return {
-    props: {
-      page: page || {},
-      tutorials: tutorials || [],
-      apps: apps || [],
-      metaTags: page?.yoast_head || '',
+    redirect: {
+      destination: 'https://mapbuilder.wri.org',
+      permanent: false,
     },
   };
 }
