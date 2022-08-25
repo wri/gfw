b22ebc4d0ec51075a1cca58d7b0d5f3ac639d8f9
Pedro Pimenta <pedro@pimenta.co>
2022-08-25 18:00:33 +0200

    Improve decoration positioning

--- components/charts/composed-chart/component.jsx
+++ components/charts/composed-chart/component.jsx
@@ -60,7 +60,7 @@ class CustomComposedChart extends PureComponent {
         <path
           transform={`translate(${
             // TODO: should be more elegant
-            x + 246 / quantity - quantity / 3 + quantity * 0.15
+            x + 246 / quantity - quantity / 3 + quantity * 0.05
           }, ${y - 32 * 0.3 - 6}) scale(0.3)`}
           d="M32 12.408l-11.056-1.607-4.944-10.018-4.944 10.018-11.056 1.607 8 7.798-1.889 11.011 9.889-5.199 9.889 5.199-1.889-11.011 8-7.798z"
         />
