820bb321d478af7eb34c9657605e4e8eba6c12a5
Pedro Pimenta <pedro@pimenta.co>
2022-09-08 15:52:19 +0200

    Fallback noindex changes

--- wrappers/head/component.jsx
+++ wrappers/head/component.jsx
@@ -6,13 +6,6 @@ import ReactHtmlParser from 'react-html-parser';
 // if the metaTags prop is returned from getStaticProps or getServerSide props
 // we parse it and use in favour of page props
 const Head = ({ title, description, noIndex, metaTags }) => {
-  const isServer = typeof window === 'undefined';
-  let isProd = false;
-
-  if (!isServer) isProd =
-    window.location.host === 'globalforestwatch.org' ||
-    window.location.host === 'www.globalforestwatch.org'
-
   return metaTags ? (
     ReactHtmlParser(metaTags)
   ) : (
@@ -27,7 +20,7 @@ const Head = ({ title, description, noIndex, metaTags }) => {
       <meta property="og:description" content={description} />
       <meta property="og:type" content="website" />
       <meta property="og:image" content="/preview.jpg" />
-      {(noIndex || !isProd) && <meta name="robots" content="noindex,follow" />}
+      {noIndex && <meta name="robots" content="noindex,follow" />}
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, maximum-scale=5"
