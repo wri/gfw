fc540ec854a2dfc2b5734524119f29935ace201e
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-10-28 13:12:43 +0100

    Add back selectors to retrieve a list of project categories and filter projects

--- layouts/grants-and-fellowships/projects/selectors.js
+++ layouts/grants-and-fellowships/projects/selectors.js
@@ -1,18 +1,62 @@
 import { createSelector, createStructuredSelector } from 'reselect';
 
+import uniq from 'lodash/uniq';
+import flatten from 'lodash/flatten';
+import compact from 'lodash/compact';
+import sortBy from 'lodash/sortBy';
+
 import { deburrUpper } from 'utils/strings';
 
 const getProjects = (state) => state.projects;
+const getCategory = (state) => state.category;
 const getSearch = (state) => state.search;
 const getCustomFilter = (state) => state.customFilter;
 
-const getProjectsList = createSelector(
-  [getProjects, getSearch, getCustomFilter],
-  (projects, search, filter) => {
+const getCategories = createSelector(getProjects, (projects) => {
+  if (!projects?.length) return null;
+  return [
+    'All',
+    ...sortBy(compact(uniq(flatten(projects.map((p) => p.categories))))),
+  ];
+});
+
+const getProjectsByCategory = createSelector(
+  [getProjects, getCategories],
+  (projects, categories) => {
+    if (!projects || !categories) return null;
+    const projectsByCategory = {};
+    categories.forEach((c) => {
+      projectsByCategory[c] =
+        c === 'All'
+          ? projects
+          : projects.filter((p) => p.categories.indexOf(c) > -1);
+    });
+
+    return projectsByCategory;
+  }
+);
+
+const getCategoriesList = createSelector(
+  [getProjectsByCategory],
+  (projects) => {
     if (!projects) return null;
+
+    return Object.keys(projects).map((c) => ({
+      label: c,
+      count: projects[c].length,
+    }));
+  }
+);
+
+const getProjectsList = createSelector(
+  [getProjects, getProjectsByCategory, getCategory, getSearch, getCustomFilter],
+  (allProjects, groupedProjects, category, search, filter) => {
+    if (!allProjects || !category) return null;
     if (filter && filter.length) {
-      return projects.filter((p) => filter.indexOf(p.id) > -1);
+      return allProjects.filter((p) => filter.indexOf(p.id) > -1);
     }
+    const projects =
+      category === 'All' ? allProjects : groupedProjects[category];
     if (!search) return projects;
     return projects.filter(
       (p) =>
@@ -25,4 +69,5 @@ const getProjectsList = createSelector(
 
 export const getProjectsProps = createStructuredSelector({
   projects: getProjectsList,
+  categories: getCategoriesList,
 });
