b5c31dae1c10276a2560032f957d421ccf29fdac
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-11-04 13:10:14 +0000

    Add new props to the TagsList component to support shallow, replace, scroll next/Link properties

--- components/tags-list/component.jsx
+++ components/tags-list/component.jsx
@@ -9,22 +9,44 @@ const TagsList = ({ title, tags, onClick }) => (
   <div className="c-tags-list">
     {title && <h5>{title}</h5>}
     <div className="c-tags-list__group">
-      {tags.map(({ id, name, link, active = false } = {}) => (
-        <span key={id}>
-          {link ? (
-            <Link href={link}>
-              <a className={cx('tag', { active })}>{name}</a>
-            </Link>
-          ) : (
-            <button
-              className={cx('tag', { active })}
-              onClick={() => onClick(id)}
-            >
-              {name}
-            </button>
-          )}
-        </span>
-      ))}
+      {tags.map(
+        ({
+          id,
+          name,
+          link,
+          active = false,
+          shallow = false,
+          replace = false,
+          scroll = false,
+        } = {}) => (
+          <span key={id}>
+            {link ? (
+              <Link
+                href={link}
+                shallow={shallow}
+                replace={replace}
+                scroll={scroll}
+              >
+                <a
+                  role="button"
+                  tabIndex={0}
+                  className={cx('tag', { active })}
+                  onClick={() => onClick && onClick(id)}
+                >
+                  {name}
+                </a>
+              </Link>
+            ) : (
+              <button
+                className={cx('tag', { active })}
+                onClick={() => onClick && onClick(id)}
+              >
+                {name}
+              </button>
+            )}
+          </span>
+        )
+      )}
     </div>
   </div>
 );
@@ -35,8 +57,12 @@ TagsList.propTypes = {
     PropTypes.shape({
       id: PropTypes.string.isRequired,
       title: PropTypes.string.isRequired,
-      link: PropTypes.string,
       active: PropTypes.bool,
+      // Link specific
+      link: PropTypes.string,
+      shallow: PropTypes.bool,
+      replace: PropTypes.bool,
+      scroll: PropTypes.bool,
     })
   ).isRequired,
   onClick: PropTypes.func,
