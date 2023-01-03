49bdc36a6e4d51eeb7a5b5cc96d79c1df745ef36
Maria Sola <maria.sola@vizzuality.com>
2023-01-03 17:18:43 +0100

    about section finished

--- layouts/grants-and-fellowships/about/component.jsx
+++ layouts/grants-and-fellowships/about/component.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import parse from 'html-react-parser';
 /* eslint-disable react/no-danger */
 import { Row, Column } from 'gfw-components';
 
@@ -25,23 +26,24 @@ const GrantsAboutSection = ({ about }) => {
                 </h2>
                 {section.content.map((c) => (
                   <>
-                    {c.acf_fc_layout === 'paragraph' && (
-                      <p className="text -paragraph -color-2 -light section-paragraph">
-                        {htmlParser(c.text)}
-                      </p>
-                    )}
-                    {c.acf_fc_layout === 'subtitle' && (
-                      <h3
-                        className="section-subtitle"
-                        dangerouslySetInnerHTML={{ __html: c.text }}
-                      />
-                    )}
+                    <Row className="section">
+                      {c.acf_fc_layout === 'paragraph' && <>{parse(c.text)}</>}
+                      {c.acf_fc_layout === 'subtitle' && (
+                        <h3
+                          className="section-subtitle"
+                          dangerouslySetInnerHTML={{ __html: c.text }}
+                        />
+                      )}
+                    </Row>
 
                     {c.acf_fc_layout === 'icons' && (
                       <Row nested className="icon-list">
                         {c.item.map((item) => (
                           <Column width={[1, 1 / 3]} key={item.text}>
-                            <img src={item.image.url} alt="imag" />
+                            <img
+                              src={item.image.url}
+                              alt={htmlParser(item.text)}
+                            />
                             <p
                               dangerouslySetInnerHTML={{ __html: item.text }}
                             />
