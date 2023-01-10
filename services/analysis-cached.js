d4201f710bf2d61f3132743076fed0149cbf12de
Simao Rodrigues <arodrigues.simao@gmail.com>
2023-01-10 10:08:05 +0000

    Fix getLossFiresOTF hardcoded dataset so it can be proxied

--- services/analysis-cached.js
+++ services/analysis-cached.js
@@ -834,7 +834,7 @@ export const getLossFiresOTF = (params) => {
 
   // TODO: IMPORTANT: this is very ugly, will do for now
   url =
-    'https://data-api.globalforestwatch.org/dataset/umd_tree_cover_loss/v1.8/query/json?sql=SELECT%20umd_tree_cover_loss__year,%20sum(umd_tree_cover_loss__ha),%20sum(umd_tree_cover_loss_from_fires__ha)%20FROM%20data%20WHERE%20umd_tree_cover_loss__year%20%3E%3D%202001%20AND%20umd_tree_cover_loss__year%20%3C%3D%202020%20AND%20umd_tree_cover_density_2000__threshold%20%3E%3D%2030%20GROUP%20BY%20umd_tree_cover_loss__year';
+    '/dataset/umd_tree_cover_loss/v1.8/query/json?sql=SELECT%20umd_tree_cover_loss__year,%20sum(umd_tree_cover_loss__ha),%20sum(umd_tree_cover_loss_from_fires__ha)%20FROM%20data%20WHERE%20umd_tree_cover_loss__year%20%3E%3D%202001%20AND%20umd_tree_cover_loss__year%20%3C%3D%202020%20AND%20umd_tree_cover_density_2000__threshold%20%3E%3D%2030%20GROUP%20BY%20umd_tree_cover_loss__year';
   url =
     adm0 === 'river_basins'
       ? `${url}&geostore_id=${geostore.id}`
