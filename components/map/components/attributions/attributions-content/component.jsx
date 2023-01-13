800ac773e0757ddd52d1952f2f7325deff3a63fc
Simao Rodrigues <arodrigues.simao@gmail.com>
2023-01-13 10:37:24 +0000

    Extract map attributions content into separate component for reusability
    But keep the same logic

---
+++ components/map/components/attributions/attributions-content/component.jsx
@@ -0,0 +1,87 @@
+import PropTypes from 'prop-types';
+
+import Icon from 'components/ui/icon';
+
+import mapboxLogo from 'assets/logos/mapbox.svg?sprite';
+import geeLogo from 'assets/logos/gee.png';
+import cartoLogo from 'assets/logos/carto.png';
+import planetLogo from 'assets/logos/planet.png';
+
+const AttributionsContent = ({ narrow = false, isModal = false }) => {
+  return (
+    <>
+      <div className="logos">
+        <a
+          href="https://www.mapbox.com/about/maps/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <Icon className="mapbox-logo" icon={mapboxLogo} />
+        </a>
+        <a
+          href="https://earthengine.google.com/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img
+            className="ee-logo"
+            src={geeLogo}
+            alt="google earth engine"
+            width="115"
+          />
+        </a>
+        <a href="https://carto.com/" rel="noopener noreferrer" target="_blank">
+          <img className="carto-logo" src={cartoLogo} alt="carto" width="60" />
+        </a>
+        <a
+          href="https://www.planet.com"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img
+            className="planet-logo"
+            src={planetLogo}
+            alt="planet"
+            width="80"
+          />
+        </a>
+      </div>
+      {!narrow && (
+        <div className="links">
+          <span>Map data ©2016 Google, INEGI</span>
+          {isModal && (
+            <>
+              <a
+                href="http://www.openstreetmap.org/copyright"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                © OpenStreetMap
+              </a>
+              <a
+                href="https://www.mapbox.com/map-feedback/"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Improve this map
+              </a>
+            </>
+          )}
+          <a href="/terms" rel="noopener noreferrer" target="_blank">
+            Terms of use
+          </a>
+          <a href="/privacy-policy" rel="noopener noreferrer" target="_blank">
+            Privacy policy
+          </a>
+        </div>
+      )}
+    </>
+  );
+};
+
+AttributionsContent.propTypes = {
+  narrow: PropTypes.bool,
+  isModal: PropTypes.bool,
+};
+
+export default AttributionsContent;
