c3bf6afee481069d21918178b5685f6e1bb14f89
Simao Rodrigues <arodrigues.simao@gmail.com>
2023-01-26 15:39:58 +0000

    Do not proxy uploads or a 404 will be returned

--- services/shape.js
+++ services/shape.js
@@ -1,7 +1,12 @@
-import { apiRequest } from 'utils/request';
+import request from 'utils/request';
+
+import { GFW_API, GFW_STAGING_API } from 'utils/apis';
+
+const GFW_API_URL =
+  process.env.NEXT_PUBLIC_FEATURE_ENV === 'staging' ? GFW_STAGING_API : GFW_API;
 
 const QUERIES = {
-  ogrConvert: '/v2/ogr/convert'
+  ogrConvert: '/v2/ogr/convert',
 };
 
 export const uploadShapeFile = (
@@ -10,16 +15,16 @@ export const uploadShapeFile = (
   onDownloadProgress,
   cancelToken
 ) => {
-  const url = `${QUERIES.ogrConvert}`;
+  const url = `${GFW_API_URL}/${QUERIES.ogrConvert}`;
   const formData = new FormData();
   formData.append('file', file);
 
-  return apiRequest({
+  return request({
     method: 'POST',
     data: formData,
     url,
     cancelToken,
     onUploadProgress,
-    onDownloadProgress
+    onDownloadProgress,
   });
 };
