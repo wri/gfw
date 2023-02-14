a537a163fdadba86d5a63db646ca503f035d4d11
Pedro Pimenta <pedro@pimenta.co>
2023-02-14 11:39:38 +0000

    Add an error by wrongly parsing request

--- pages/thisisanerror.js
+++ pages/thisisanerror.js
@@ -1,5 +1,25 @@
-const ThisIsAnError = () => {
-  throw new Error('this is, in fact, an error');
-};
+import React, { PureComponent } from 'react';
+import { apiRequest } from 'utils/request';
+
+class ThisIsAnError extends PureComponent {
+  state = {
+    data: '',
+  };
+
+  componentDidMount = () => {
+    apiRequest.get('/v2/geostore/admin/IDN/?thresh=0.05').then((response) => {
+      this.setState({ data: response.data.data.id });
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <span>This will be an error:</span>
+        <span>{this.state.data.map((d) => d)}</span>
+      </div>
+    );
+  }
+}
 
 export default ThisIsAnError;
