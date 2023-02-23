e8ce3a58dc26b57ee8847648c1f7f9386d91a4e7
Simao Rodrigues <arodrigues.simao@gmail.com>
2023-02-23 15:36:28 +0000

    Fix typo in error message that occurs when a widget cannot load

--- components/ui/refresh-button/component.jsx
+++ components/ui/refresh-button/component.jsx
@@ -8,7 +8,10 @@ import './styles.scss';
 
 const RefreshButton = ({ refetchFn }) => (
   <NoContent className="c-refresh-button">
-    <span>An error occured while fetching data. You can try again, or save the shape and check back tomorrow.</span>
+    <span>
+      An error occurred while fetching data. You can try again, or save the
+      shape and check back tomorrow.
+    </span>
     <Button
       className="refresh-btn"
       onClick={refetchFn}
@@ -20,11 +23,11 @@ const RefreshButton = ({ refetchFn }) => (
 );
 
 RefreshButton.propTypes = {
-  refetchFn: PropTypes.func
+  refetchFn: PropTypes.func,
 };
 
 RefreshButton.defaultProps = {
-  refetchFn: () => {}
+  refetchFn: () => {},
 };
 
 export default RefreshButton;
