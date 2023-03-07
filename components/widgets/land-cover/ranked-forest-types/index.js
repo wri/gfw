a028b1258a3ddefbd63761e8259df4a62defb6ed
Simao Rodrigues <arodrigues.simao@gmail.com>
2023-03-07 14:07:10 +0000

    Add new rankedForestTypes widget

---
+++ components/widgets/land-cover/ranked-forest-types/index.js
@@ -0,0 +1,68 @@
+import { getTreeCoverByLandCoverClass } from 'services/analysis-cached';
+
+import {
+  POLITICAL_BOUNDARIES_DATASET,
+  FOREST_GAIN_DATASET,
+} from 'data/datasets';
+import {
+  DISPUTED_POLITICAL_BOUNDARIES,
+  POLITICAL_BOUNDARIES,
+  FOREST_GAIN,
+} from 'data/layers';
+
+import getWidgetProps from './selectors';
+
+export default {
+  widget: 'rankedForestTypes',
+  title: 'Tree cover by land cover class in {location}',
+  sentence:
+    'In {extentYear}, {location} had {canopyCover} in the following land cover classes:',
+  categories: ['summary', 'land-cover'],
+  large: true,
+  autoHeight: true,
+  types: ['country'],
+  admins: ['adm0', 'adm1', 'adm2'],
+  settingsConfig: [
+    {
+      key: 'forestType',
+      label: 'Forest Type',
+      type: 'select',
+      placeholder: 'All land cover',
+      clearable: true,
+    },
+    {
+      key: 'decile',
+      label: 'tree cover',
+      type: 'mini-select',
+      metaKey: 'widget_canopy_density',
+    },
+  ],
+  refetchKeys: ['forestType', 'decile'],
+  chartType: 'infoList',
+  // TODO: Add metakey when available
+  // metaKey: '',
+  datasets: [
+    {
+      dataset: POLITICAL_BOUNDARIES_DATASET,
+      layers: [DISPUTED_POLITICAL_BOUNDARIES, POLITICAL_BOUNDARIES],
+      boundary: true,
+    },
+    {
+      dataset: FOREST_GAIN_DATASET,
+      layers: [FOREST_GAIN],
+    },
+  ],
+  visible: ['dashboard'],
+  sortOrder: {
+    // TODO: Set correct sortOrder
+    summary: -10,
+    landCover: -10,
+  },
+  settings: {
+    decile: 30,
+  },
+  getData: (params) => {
+    return getTreeCoverByLandCoverClass(params);
+  },
+  getWidgetProps,
+};
---
+++ components/widgets/land-cover/ranked-forest-types/selectors.js
@@ -0,0 +1,98 @@
+import { createSelector, createStructuredSelector } from 'reselect';
+import { format } from 'd3-format';
+import { formatNumber } from 'utils/format';
+import maxBy from 'lodash/maxBy';
+
+import ipccClasses from './ipcc-classes.json';
+
+const getData = (state) => state.data;
+const getSettings = (state) => state.settings;
+const getLocation = (state) => state.locationLabel;
+const getSentence = (state) => state.sentence;
+
+export const parseSentence = createSelector(
+  [getSentence, getLocation, getSettings, getData],
+  (sentence, location, settings, data) => {
+    // We need to do this, or the widget will be empty instead of showing the
+    // "No data in selection for {location}" message
+    if (!data?.length) return null;
+
+    const { extentYear, decile } = settings;
+
+    const params = {
+      location,
+      extentYear,
+      canopyCover: `>${decile}% tree cover`,
+    };
+    return {
+      sentence,
+      params,
+    };
+  }
+);
+
+export const parseData = createSelector([getData], (data) => {
+  if (!data?.length) return null;
+
+  const totalExtent = data.reduce(
+    (acc, { wri_tropical_tree_cover_extent__ha: treeCoverExtent }) =>
+      acc + treeCoverExtent,
+    0
+  );
+
+  const ipccClassesDetails = ipccClasses.map(({ icon, label, ipccClass }) => {
+    const dataEntry = data.find(
+      (entry) => entry.umd_global_land_cover__ipcc_class === ipccClass
+    );
+
+    const extentPercent =
+      (dataEntry?.wri_tropical_tree_cover_extent__ha * 100) / totalExtent;
+
+    const extentHa = dataEntry?.wri_tropical_tree_cover_extent__ha || 0;
+
+    return {
+      icon,
+      label,
+      // extentPercent is not needed for the infoList; we'll use it to mitigate percentage
+      // sum above 100% later on.
+      extentPercent: Number(format('.2r')(extentPercent)),
+      text: `${format('.2r')(extentPercent)}%`,
+      subText: `(${formatNumber({
+        num: extentHa,
+        unit: 'ha',
+        spaceUnit: true,
+      })})`,
+    };
+  });
+
+  // Due to rounding errors, we may end up with a sum of percentages way higher than 100%.
+  // Given our use of 'd3-format' to make numbers more readable, a perfect 100% is neigh impossible.
+  // What we can do, is mitigate it by finding the highest number and take out the extra percentage
+  // from it. We will be below 100% instead, which is less jarring.
+  const percentagesSum = ipccClassesDetails.reduce(
+    (acc, entry) => acc + (entry?.extentPercent || 0),
+    0
+  );
+
+  if (percentagesSum > 100) {
+    const extraPercentage = percentagesSum - 100;
+    const highestPercentEntry = maxBy(ipccClassesDetails, 'extentPercent');
+
+    highestPercentEntry.text = `${format('.2r')(
+      highestPercentEntry.extentPercent - extraPercentage
+    )}%`;
+  }
+
+  // Return only the properties the infoList actually needs, to prevent future confusion.
+  return ipccClassesDetails.map(({ icon, label, text, subText }) => ({
+    icon,
+    label,
+    text,
+    subText,
+  }));
+});
+
+export default createStructuredSelector({
+  sentence: parseSentence,
+  data: parseData,
+});
