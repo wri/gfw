b79e7572542855890a63cde553b5dd761e309809
Willian Viana <willian.batista.viana@gmail.com>
2023-04-20 15:15:18 -0300

    feat(next-config): add new configuration

--- next.config.js
+++ next.config.js
@@ -1,46 +1,24 @@
-// const withPlugins = require('next-compose-plugins');
-// const optimizedImages = require('next-optimized-images');
+const withPlugins = require('next-compose-plugins');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
-// const withBundleAnalyzer = require('@next/bundle-analyzer')({
-//   enabled: process.env.ANALYZE === 'true',
-// });
-// const FilterWarningsPlugin = require('webpack-filter-warnings-plugin');
+const redirects = require('./data/redirects.json');
 
-// const redirects = require('./data/redirects');
+let rewrites;
 
-// let rewrites;
+if (process.env.NEXT_PUBLIC_FEATURE_ENV === 'staging') {
+  // eslint-disable-next-line global-require
+  rewrites = require('./data/rewrites-staging.json');
+} else {
+  // eslint-disable-next-line global-require
+  rewrites = require('./data/rewrites.json');
+}
 
-// if (process.env.NEXT_PUBLIC_FEATURE_ENV === 'staging') {
-//   // eslint-disable-next-line global-require
-//   rewrites = require('./data/rewrites-staging');
-// } else {
-//   // eslint-disable-next-line global-require
-//   rewrites = require('./data/rewrites');
-// }
+const nextConfig = {
+  redirects: async () => redirects,
+  rewrites: async () => rewrites,
+  trailingSlash: true,
+};
 
-// const nextConfig = {
-//   webpack: (config) => {
-//     config.plugins = [
-//       ...config.plugins,
-//       // mini-css-extract-plugin generates a warning when importing css as modules
-//       // https://github.com/zeit/next-plugins/issues/506#issuecomment-589269285
-//       new FilterWarningsPlugin({
-//         exclude: /mini-css-extract-plugin[^]*Conflicting order between:/,
-//       }),
-//     ];
-
-//     config.node = {
-//       fs: 'empty',
-//     };
-
-//     return config;
-//   },
-//   redirects: async () => redirects,
-//   rewrites: async () => rewrites,
-//   trailingSlash: true,
-// };
-
-// module.exports = withPlugins(
-//   [[optimizedImages], [withBundleAnalyzer]],
-//   nextConfig
-// );
+module.exports = withPlugins([withBundleAnalyzer], nextConfig);
