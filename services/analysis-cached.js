19b3b14eb7a9b67fe7626dd39de2f96397156ae1
Simao Rodrigues <arodrigues.simao@gmail.com>
2023-05-22 10:48:42 +0100

    Remove NULL values from treeCoverByLandCoverClass SQL query

--- services/analysis-cached.js
+++ services/analysis-cached.js
@@ -83,7 +83,7 @@ const SQL_QUERIES = {
   netChange:
     'SELECT {select_location}, {select_location}_name, stable, loss, gain, disturb, net, change, gfw_area__ha FROM data {WHERE}',
   treeCoverByLandCoverClass:
-    'SELECT umd_global_land_cover__ipcc_class, SUM(wri_tropical_tree_cover_extent__ha) AS wri_tropical_tree_cover_extent__ha FROM data {WHERE} AND wri_tropical_tree_cover__decile >= {decile} GROUP BY {location}, umd_global_land_cover__ipcc_class ORDER BY {location}, umd_global_land_cover__ipcc_class',
+    'SELECT umd_global_land_cover__ipcc_class, SUM(wri_tropical_tree_cover_extent__ha) AS wri_tropical_tree_cover_extent__ha FROM data {WHERE} AND wri_tropical_tree_cover__decile >= {decile} AND umd_global_land_cover__ipcc_class IS NOT NULL GROUP BY {location}, umd_global_land_cover__ipcc_class ORDER BY {location}, umd_global_land_cover__ipcc_class',
   tropicalExtent:
     'SELECT {select_location}, SUM(CASE WHEN wri_tropical_tree_cover__decile >= {decile} THEN wri_tropical_tree_cover_extent__ha END) AS tropical_tree_cover_extent_2020_ha, SUM(CASE WHEN wri_tropical_tree_cover__decile >= 0 THEN area__ha END) AS area__ha FROM data {WHERE} GROUP BY {location} ORDER BY {location}',
 };
@@ -980,13 +980,7 @@ export const getTreeCoverByLandCoverClass = (params) => {
       .replace('{decile}', params?.decile)
   );
 
-  return dataRequest.get(url).then((response) =>
-    response?.data?.filter(
-      // Unknown values should just be ignored for the widget, they indicate NoData.
-      // See: https://gfw.atlassian.net/wiki/spaces/FLAG/pages/586416133/Widget+Queries+and+Data+Handoff#Schema-Changes
-      ({ umd_global_land_cover__ipcc_class: ipccClass }) => ipccClass !== null
-    )
-  );
+  return dataRequest.get(url).then((response) => response?.data);
 };
 
 export const getTropicalExtent = (params) => {
