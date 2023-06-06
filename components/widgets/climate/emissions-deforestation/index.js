72a8f6b80485337bd93940e96fbbccdd4896c22c
Simao Rodrigues <arodrigues.simao@gmail.com>
2023-06-06 13:47:50 +0100

    Update emissions widgets to use the new TCL tables

--- components/widgets/climate/emissions-deforestation/index.js
+++ components/widgets/climate/emissions-deforestation/index.js
@@ -21,7 +21,7 @@ import treeLoss from 'components/widgets/forest-change/tree-loss';
 import getWidgetProps from './selectors';
 
 const MIN_YEAR = 2001;
-const MAX_YEAR = 2021;
+const MAX_YEAR = 2022;
 
 export default {
   ...treeLoss,
