8a46cb8ae4a78d40364cccb7764dfbc3ba5e37c8
Willian Viana <willian.batista.viana@gmail.com>
2023-06-27 16:05:46 -0300

    feat(space-units: add formatNumber method instead format from d3-format lib in forestryEmployment widget

--- components/widgets/land-use/forestry-employment/selectors.js
+++ components/widgets/land-use/forestry-employment/selectors.js
@@ -1,13 +1,13 @@
 import { createSelector, createStructuredSelector } from 'reselect';
 import isEmpty from 'lodash/isEmpty';
-import { format } from 'd3-format';
+import { formatNumber } from 'utils/format';
 
 // get list data
-const getData = state => state.data;
-const getSettings = state => state.settings;
-const getLocationObject = state => state.location;
-const getColors = state => state.colors;
-const getSentences = state => state.sentences;
+const getData = (state) => state.data;
+const getSettings = (state) => state.settings;
+const getLocationObject = (state) => state.location;
+const getColors = (state) => state.colors;
+const getSentences = (state) => state.sentences;
 
 // get lists selected
 export const getFilteredData = createSelector(
@@ -16,14 +16,14 @@ export const getFilteredData = createSelector(
     if (isEmpty(data) || !locationObject) return null;
     return data
       .filter(
-        item => item.country === locationObject.value && item.year !== 9999
+        (item) => item.country === locationObject.value && item.year !== 9999
       )
-      .map(item => ({
+      .map((item) => ({
         male: item.femempl
           ? (item.forempl - item.femempl) * 1000
           : item.forempl * 1000,
         female: item.femempl ? item.femempl * 1000 : null,
-        year: item.year
+        year: item.year,
       }));
   }
 );
@@ -34,7 +34,7 @@ export const parseData = createSelector(
     if (isEmpty(data)) return null;
 
     const { year } = settings;
-    const selectedFAO = data.filter(item => item.year === year);
+    const selectedFAO = data.filter((item) => item.year === year);
     const { male, female } =
       selectedFAO && selectedFAO.length && selectedFAO[0];
     if (!female) return null;
@@ -45,14 +45,14 @@ export const parseData = createSelector(
         label: 'Male',
         value: male,
         color: colors.male,
-        percentage: male / total * 100
+        percentage: (male / total) * 100,
       },
       {
         label: 'Female',
         value: female,
         color: colors.female,
-        percentage: female / total * 100
-      }
+        percentage: (female / total) * 100,
+      },
     ];
     return formatedData;
   }
@@ -64,7 +64,7 @@ export const parseSentence = createSelector(
     if (!data) return null;
     const { year } = settings;
     const { initial, withFemales } = sentences;
-    const selectedFAO = data.find(item => item.year === year);
+    const selectedFAO = data.find((item) => item.year === year);
     let employees = 0;
     let females = 0;
     if (selectedFAO) {
@@ -73,23 +73,25 @@ export const parseSentence = createSelector(
         : selectedFAO.male;
       females = parseInt(selectedFAO.female, 10);
     }
-    const percentage = 100 * females / employees;
+    const percentage = (100 * females) / employees;
 
     const params = {
       location: `${locationObject && locationObject && locationObject.label}'s`,
-      value: `${employees ? format('.3s')(employees) : 'no'}`,
-      percent: percentage >= 0.1 ? `${format('.2r')(percentage)}%` : '< 0.1%',
-      year
+      value: `${
+        employees ? formatNumber({ num: employees, spaceUnit: true }) : 'no'
+      }`,
+      percent: formatNumber({ num: percentage, unit: '%' }),
+      year,
     };
 
     return {
       sentence: females ? withFemales : initial,
-      params
+      params,
     };
   }
 );
 
 export default createStructuredSelector({
   data: parseData,
-  sentence: parseSentence
+  sentence: parseSentence,
 });
