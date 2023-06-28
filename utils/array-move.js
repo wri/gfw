e5130675ce07656e72ed291d7ebe92744bc5b2fa
Willian Viana <willian.batista.viana@gmail.com>
2023-06-28 17:14:36 -0300

    chore(node): rollback node version and remove array-move dependencies

---
+++ utils/array-move.js
@@ -0,0 +1,16 @@
+export function arrayMoveMutable(array, fromIndex, toIndex) {
+  const startIndex = fromIndex < 0 ? array.length + fromIndex : fromIndex;
+
+  if (startIndex >= 0 && startIndex < array.length) {
+    const endIndex = toIndex < 0 ? array.length + toIndex : toIndex;
+
+    const [item] = array.splice(fromIndex, 1);
+    array.splice(endIndex, 0, item);
+  }
+}
+
+export function arrayMoveImmutable(array, fromIndex, toIndex) {
+  const newArray = [...array];
+  arrayMoveMutable(newArray, fromIndex, toIndex);
+  return newArray;
+}
