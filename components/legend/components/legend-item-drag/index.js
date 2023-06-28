8a8feb5f4102a0ddcb1976f7e8ed8691be9301e3
Willian Viana <willian.batista.viana@gmail.com>
2023-06-28 17:40:27 -0300

    fix(imports): fixing sass and js imports

--- components/legend/components/legend-item-drag/index.js
+++ components/legend/components/legend-item-drag/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SortableHandle } from 'react-sortable-hoc';
-import Icon from 'components/icon';
+import { Icon } from 'vizzuality-components';
 import './styles.scss';
 
 const LegendItemDrag = () => (
