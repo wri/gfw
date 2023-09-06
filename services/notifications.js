ab4787297bcdcff676b00a748eb8b37ca5ce44a0
Luis Zenteno <luis.zenteno.5@wriconsultant.org>
2023-09-05 20:49:55 -0600

    feat(page): fetch notifications and pass them as props to Header component

---
+++ services/notifications.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const mapResponseToNotification = ({
+  id,
+  date,
+  status,
+  title,
+  content,
+  acf,
+}) => {
+  return {
+    id,
+    dateCreated: date,
+    status,
+    title: title.rendered,
+    content: content.rendered,
+    icon: acf.notification_icon,
+    date: acf.notification_date,
+  };
+};
+
+export const getPublishedNotifications = async () => {
+  try {
+    const notificationsData = await axios.get(
+      `${process.env.NEXT_PUBLIC_WORDPRESS_URL}/wp/v2/notice?per_page=100&page=1&orderby=date&order=desc`
+    );
+
+    return notificationsData?.data.map((item) =>
+      mapResponseToNotification(item)
+    );
+  } catch (e) {
+    return null;
+  }
+};
