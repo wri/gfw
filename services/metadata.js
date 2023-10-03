74d94938d4dfd01d7b76a206cdc5d2ebd3aeb105
Solomon Negusse <solomon.negusse@wri.org>
2023-10-02 20:43:03 -0500

    fetch widget metadata from RW API

--- services/metadata.js
+++ services/metadata.js
@@ -1,6 +1,16 @@
-import { metadataRequest } from 'utils/request';
+import { metadataRequest, rwRequest } from 'utils/request';
 
-export const getMetadata = (slug) => metadataRequest.get(slug);
+export const getMetadata = (slug, metaType) => {
+  if (metaType === 'widget') {
+    return rwRequest.get(`widget/${slug}`)
+    .then((resp) => {
+      const dataset = resp.data?.dataset;
+      return rwRequest.get(`dataset/${dataset}/widget/${slug}/metadata`)
+    })
+  }
+  
+  return metadataRequest.get(slug);
+}
 
 export default {
   getMetadata,
