afca48ea48bd8db0411c4aae1925e18c6fbe69b4
Luis Zenteno <luis.zenteno.5@wriconsultant.org>
2023-10-12 10:54:17 -0600

    feat(notice-board): integate notice board in about and search pages

--- pages/search.js
+++ pages/search.js
@@ -1,14 +1,34 @@
 import PageLayout from 'wrappers/page';
 import Search from 'layouts/search';
 
-const SearchPage = () => (
+import PropTypes from 'prop-types';
+
+import { getPublishedNotifications } from 'services/notifications';
+
+const SearchPage = (props) => (
   <PageLayout
     title="Search | Global Forest Watch"
     description="Search forest information, including forest data, news, updates and more."
+    notifications={props.notifications}
     noIndex
   >
     <Search />
   </PageLayout>
 );
+
+// eslint-disable-next-line no-unused-vars
+export const getServerSideProps = async ({ params, query, req }) => {
+  const notifications = await getPublishedNotifications();
+
+  return {
+    props: {
+      notifications: notifications || [],
+    },
+  };
+};
+
+SearchPage.propTypes = {
+  notifications: PropTypes.array,
+};
 
 export default SearchPage;
