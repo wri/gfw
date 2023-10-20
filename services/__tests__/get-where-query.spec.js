fcb2b1456f5de0b2c1d258b51c01517f9a2ec65c
Willian Viana <willian.batista.viana@gmail.com>
2023-10-20 13:31:23 -0300

    chore(jest): add getWhereQuery general test

--- services/__tests__/get-where-query.spec.js
+++ services/__tests__/get-where-query.spec.js
@@ -1,36 +1,39 @@
 import { getWHEREQuery } from '../get-where-query';
 
 describe('getWHEREQuery', () => {
-  describe('Tree Cover Density query', () => {
-    let params;
+  it('should return an string with each parameter separated by AND', () => {
+    const params = {
+      type: 'country',
+      adm0: 'BRA',
+      locationType: 'country',
+      extentYear: 2000,
+      thresh: 30,
+      threshold: 30,
+      forestType: 'plantations',
+      dataset: 'annual',
+    };
+    const query = getWHEREQuery(params);
+    const expected =
+      "WHERE iso = 'BRA' AND umd_tree_cover_density_2000__threshold = 30 AND gfw_planted_forests__type IS NOT NULL ";
 
-    beforeEach(() => {
-      params = {
-        type: 'country',
-        adm0: 'PER',
-        pathname: '/map/[[...location]]',
-        locationType: 'country',
-        extentYear: 2020,
-        url: 'https://tiles.globalforestwatch.org/wri_tropical_tree_cover/v2020/ttcd_{thresh}/{z}/{x}/{y}.png',
-        thresh: '40',
-        threshold: 40,
-        confirmedOnly: 0,
-        gladLOnly: 0,
-        gladSOnly: 0,
-        raddOnly: 0,
-        startDayIndex: 0,
-        endDayIndex: null,
-        numberOfDays: null,
-        dataset: 'treeCoverDensity',
-      };
-    });
+    expect(query).toEqual(expected);
+  });
 
-    // Tree Cover Density has a default threshold
-    it('should return the default threshold even when user set a different threshold', () => {
-      const query = getWHEREQuery(params);
-      const expected = "WHERE iso = 'PER' ";
+  // Tree Cover Density has a default threshold
+  it('should not return threshold for Tree Cover Density', () => {
+    const params = {
+      type: 'country',
+      adm0: 'PER',
+      locationType: 'country',
+      extentYear: 2020,
+      thresh: '40',
+      threshold: 40, // passing threshold as parameter from tropical tree cover layer
+      dataset: 'treeCoverDensity',
+    };
 
-      expect(query).toEqual(expected);
-    });
+    const query = getWHEREQuery(params);
+    const expected = "WHERE iso = 'PER' ";
+
+    expect(query).toEqual(expected);
   });
 });
