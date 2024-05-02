6f49d7d63c127285d6465a13ca3f33d7bd030e99
Willian Viana <willian.batista.viana@gmail.com>
2024-05-02 11:45:25 -0300

    chore(metadata): return logic to set metadata for widgets

--- services/metadata.js
+++ services/metadata.js
@@ -1,16 +1,12 @@
-import { metadataRequest, rwRequest } from 'utils/request';
+import { metadataRequest } from 'utils/request';
 
 export const getMetadata = (slug, metaType) => {
   if (metaType === 'widget') {
-    return rwRequest.get(`widget/${slug}`)
-    .then((resp) => {
-      const dataset = resp.data?.dataset;
-      return rwRequest.get(`dataset/${dataset}/widget/${slug}/metadata`)
-    })
+    return metadataRequest.get(slug);
   }
-  
+
   return metadataRequest.get(slug);
-}
+};
 
 export default {
   getMetadata,
