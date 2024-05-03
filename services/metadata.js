dc307ef4e96acc96ef86ad3c6f19d9cf8d256e93
Willian Viana <willian.batista.viana@gmail.com>
2024-05-03 14:52:00 -0300

    feat(metadata): add new request method to cover metadata for widgets while we update metadata for layers

--- services/metadata.js
+++ services/metadata.js
@@ -1,11 +1,11 @@
-import { metadataRequest } from 'utils/request';
+import { metadataRequest, metadataWidgetRequest } from 'utils/request';
 
 export const getMetadata = (slug, metaType) => {
   if (metaType === 'widget') {
-    return metadataRequest.get(slug);
+    return metadataWidgetRequest.get(slug);
   }
 
-  return metadataRequest.get(slug);
+  return metadataRequest.get(`dataset/${slug}`);
 };
 
 export default {
