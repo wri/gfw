7070b87bba3d30ce80b48ef99b7ad015b4667d05
Willian Viana <willian.batista.viana@gmail.com>
2024-06-10 17:20:54 -0300

    feat(metadata): add requests to get the full metadata object For testing purposes I set the dataset and the version as hardcoded We will remove it as soon as the test get finished

--- pages/api/metadata/[...params].js
+++ pages/api/metadata/[...params].js
@@ -7,13 +7,29 @@ const GFW_METADATA_API_URL =
 
 export default async (req, res) => {
   try {
-    const path = req.query.params.join('/');
-    const url = `${GFW_METADATA_API_URL}/${path}/`;
+    // const path = req.query.params.join('/');
+    // const url = `${GFW_METADATA_API_URL}/${path}`; // ${path}
+    const url = `${GFW_METADATA_API_URL}/dataset/birdlife_endemic_bird_areas`;
+    const datasetMetadata = await axios.get(url);
+    // const datasetVersionMetadata = await axios.get(`${url}/latest/metadata`);
+    const datasetVersionMetadata = await axios.get(
+      `${GFW_METADATA_API_URL}/dataset/gfw_integrated_alerts/v20240605/metadata`
+    );
 
-    const response = await axios.get(url);
+    const response = {
+      ...datasetMetadata.data.data,
+      metadata: {
+        ...datasetMetadata.data.data.metadata,
+        ...datasetVersionMetadata.data.data,
+      },
+    };
 
-    return res.status(200).json(response.data.data);
+    return res.status(200).json(response);
   } catch (error) {
+    if (error.response) {
+      return res.status(400).end(error.response.data.message);
+    }
+
     return res.status(400).end(error.message);
   }
 };
