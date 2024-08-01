d946762999a78d780302ce304a064c8160b8dc5f
Simao Rodrigues <arodrigues.simao@gmail.com>
2024-08-01 11:42:40 +0100

    Minor renaming of geodescriber util variables

--- utils/geodescriber.js
+++ utils/geodescriber.js
@@ -2,21 +2,21 @@ import { translateText } from './lang';
 
 const isGeodescriberLocation = (location) => {
   return !['global', 'country'].includes(location?.type);
-}
+};
 
 const translateSentenceParams = ({ params = {}, excludeKeys = [] }) => {
   const paramsKeys = Object.keys(params);
 
   if (!paramsKeys.length) return {};
 
-  return paramsKeys.reduce((translatedParams, paramKey) => {
+  return paramsKeys.reduce((paramsAccumulator, paramKey) => {
     const paramValue = params[paramKey];
     const shouldTranslateParam = !excludeKeys.includes(paramKey);
 
     return {
-      ...translatedParams,
+      ...paramsAccumulator,
       [paramKey]: shouldTranslateParam ? translateText(paramValue) : paramValue,
-    }
+    };
   }, {});
 };
 
@@ -25,27 +25,31 @@ const formatAreaParams = ({ params, includeKeys = [] }) => {
 
   if (!paramsKeys.length) return {};
 
-  return paramsKeys.reduce((translatedParams, paramKey) => {
+  return paramsKeys.reduce((paramsAccumulator, paramKey) => {
     const paramValue = params[paramKey];
     const shouldFormatParam = includeKeys.includes(paramKey);
 
     return {
-      ...translatedParams,
+      ...paramsAccumulator,
       // We're not using the formatNumber utility here because this comes as a string from the endpoint.
       // It'd require complicated processing to know the actual unit (ha, kha, etc), pull out the number, then
       // use the formatNumber utility when we can just use regex to add the space between number and units.
-      [paramKey]: shouldFormatParam ? params?.[paramKey]?.replace(/([\d|.|,]+)/, '$1 ') : paramValue,
+      [paramKey]: shouldFormatParam
+        ? params?.[paramKey]?.replace(/([\d|.|,]+)/, '$1 ')
+        : paramValue,
     };
   }, {});
 };
 
 const dynamicGeodescriberSentence = (sentence, params) => {
   const translatedSentenceParams = translateSentenceParams({
-    params, excludeKeys: ['area_0'], // we know this is always an area, let's not clutter Transifex
+    params,
+    excludeKeys: ['area_0'], // we know this is always an area, let's not clutter Transifex
   });
 
   const formattedAreaParams = formatAreaParams({
-    params, includeKeys: ['area_0'], // we know this is always an area, we just need to add a space between number and unit
+    params,
+    includeKeys: ['area_0'], // we know this is always an area, we just need to add a space between number and unit
   });
 
   return {
@@ -57,7 +61,4 @@ const dynamicGeodescriberSentence = (sentence, params) => {
   };
 };
 
-export {
-  isGeodescriberLocation,
-  dynamicGeodescriberSentence,
-};
+export { isGeodescriberLocation, dynamicGeodescriberSentence };
