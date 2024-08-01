5dab4d9d42e80c21c56b6482dadc8710fa09196f
Simao Rodrigues <arodrigues.simao@gmail.com>
2024-08-01 15:34:27 +0100

    Encode Base64 URL query params based on MDN web decocs recommendations
    - to get around the "unicode problem"

--- utils/url-param.js
+++ utils/url-param.js
@@ -1,10 +1,25 @@
-const encode = (param) => {
-  return btoa(JSON.stringify(param));
+const base64ToBytes = (base64) => {
+  const binString = Buffer.from(base64, 'base64').toString('binary');
+  return Uint8Array.from(binString, (m) => m.codePointAt(0));
+};
+
+const bytesToBase64 = (bytes) => {
+  const binString = Array.from(bytes, (byte) => String.fromCodePoint(byte)).join('');
+  return btoa(binString);
 };
 
 const decode = (param) => {
-  // we use Buffer as atob is node native to node for SSR
-  return JSON.parse(Buffer.from(param, 'base64').toString('binary'));
+  const base64DecodedParam = base64ToBytes(param);
+  const textDecodedParam = new TextDecoder().decode(base64DecodedParam);
+  const parsedStringifiedParam = JSON.parse(textDecodedParam);
+  return parsedStringifiedParam;
+};
+
+const encode = (param) => {
+  const stringifiedParam = JSON.stringify(param);
+  const textEncodedParam = new TextEncoder().encode(stringifiedParam);
+  const base64EncodedParam = bytesToBase64(textEncodedParam);
+  return base64EncodedParam;
 };
 
 const urlParam = {
