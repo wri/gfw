b224388ef5c67135f9e29ef5ae6a0a0c30830c88
Willian Viana <willian.batista.viana@gmail.com>
2024-09-26 16:51:54 -0300

    feat(metadata): improve safePaths and add more metadata to JSON

--- pages/api/metadata/[...params].js
+++ pages/api/metadata/[...params].js
@@ -7,17 +7,17 @@ import METADATA_EXCEPTION_LIST from '../../../data/metadata-exception.json'; // 
 export default async (req, res) => {
   try {
     const userPath = req.query.params[0];
-    const isSafePath =
-      METADATA_LIST.includes(userPath) ||
-      METADATA_EXCEPTION_LIST.includes(userPath);
     const isExternalMetadata = METADATA_EXCEPTION_LIST.includes(userPath);
+    const safePaths = [...METADATA_LIST, ...METADATA_EXCEPTION_LIST].filter(
+      (path) => path === userPath
+    );
 
-    if (!isSafePath) {
+    if (safePaths.length === 0) {
       return res.status(400).end('Invalid path');
     }
 
     if (isExternalMetadata) {
-      const url = `${GFW_METADATA_API}/${userPath}`;
+      const url = `${GFW_METADATA_API}/${safePaths[0]}`;
       const response = await axios.get(url);
       const transformedResponse = {
         metadata: response.data,
@@ -26,7 +26,7 @@ export default async (req, res) => {
       return res.status(200).json(transformedResponse);
     }
 
-    const url = `${GFW_DATA_API}/dataset/${userPath}`;
+    const url = `${GFW_DATA_API}/dataset/${safePaths[0]}`;
 
     const datasetMetadata = await axios.get(url);
     const datasetVersionMetadata = await axios.get(`${url}/latest/metadata`);
