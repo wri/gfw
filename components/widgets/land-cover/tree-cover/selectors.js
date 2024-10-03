f94522700da657723d56319bc836bc95cb84b54e
Willian Viana <willian.batista.viana@gmail.com>
2024-10-03 12:37:16 -0300

    feat(tree-cover): remove out dated method

--- components/widgets/land-cover/tree-cover/selectors.js
+++ components/widgets/land-cover/tree-cover/selectors.js
@@ -6,26 +6,12 @@ import { formatNumber } from 'utils/format';
 const getData = (state) => state.data;
 const getSettings = (state) => state.settings;
 const getIndicator = (state) => state.indicator;
-const getWhitelist = (state) => state.polynamesWhitelist;
 const getColors = (state) => state.colors;
 const getSentence = (state) => state.sentence;
 const getTitle = (state) => state.title;
 const getLocationName = (state) => state.locationLabel;
 const getMetaKey = (state) => state.metaKey;
 const getAdminLevel = (state) => state.adminLevel;
-
-export const isoHasPlantations = createSelector(
-  [getWhitelist, getLocationName],
-  (whitelist, name) => {
-    const hasPlantations =
-      name === 'global'
-        ? true
-        : whitelist &&
-          whitelist.annual &&
-          whitelist.annual.includes('plantations');
-    return hasPlantations;
-  }
-);
 
 export const parseData = createSelector(
   [getData, getColors, getIndicator],
