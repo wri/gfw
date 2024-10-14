ff96f86bad91a34ec56677d3d142db54ecb244fd
Willian Viana <willian.batista.viana@gmail.com>
2024-10-14 12:58:38 -0300

    TEMPORARY: setting Data API always to production

--- pages/api/data/[...params].js
+++ pages/api/data/[...params].js
@@ -1,12 +1,10 @@
 import httpProxyMiddleware from 'next-http-proxy-middleware';
 
-import { GFW_DATA_API, GFW_STAGING_DATA_API } from 'utils/apis';
+import { GFW_DATA_API } from 'utils/apis';
 import { PROXIES } from 'utils/proxies';
 
-const ENVIRONMENT = process.env.NEXT_PUBLIC_FEATURE_ENV;
 const GFW_API_KEY = process.env.NEXT_PUBLIC_GFW_API_KEY;
-const DATA_API_URL =
-  ENVIRONMENT === 'staging' ? GFW_STAGING_DATA_API : GFW_DATA_API;
+const DATA_API_URL = GFW_DATA_API;
 
 // https://github.com/stegano/next-http-proxy-middleware/issues/32#issuecomment-1031015850
 export const config = {
