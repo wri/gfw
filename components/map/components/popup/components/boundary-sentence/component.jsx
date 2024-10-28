8b68bb0d24835f68f187af255708298f253dc0f3
Willian Viana <willian.batista.viana@gmail.com>
2024-10-28 17:18:05 -0300

    feat(jsdoc): add JSDoc

--- components/map/components/popup/components/boundary-sentence/component.jsx
+++ components/map/components/popup/components/boundary-sentence/component.jsx
@@ -4,6 +4,11 @@ import PropTypes from 'prop-types';
 import DynamicSentence from 'components/ui/dynamic-sentence';
 import Button from 'components/ui/button';
 
+/**
+ * Displays a sentence describing the area clicked by the user on the map.
+ * @param {string} sentence - A string with the area's  description
+ * @param {function} onAnalyze - Function for onClick event.
+ */
 const BoundarySentence = ({ sentence, onAnalyze }) => (
   <div className="c-boundary-sentence">
     <DynamicSentence className="sentence" sentence={sentence} />
