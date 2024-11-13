6092798278852f1804469c46667516f0e00b747d
Luis Zenteno <luis.zenteno.5@wriconsultant.org>
2024-11-13 09:22:05 -0600

    fix(widgets): hide carbon stock and soil organic until we have the correct metadata

--- components/widgets/manifest.js
+++ components/widgets/manifest.js
@@ -49,7 +49,7 @@ import woodyBiomass from 'components/widgets/climate/whrc-biomass/';
 import carbonFlux from 'components/widgets/climate/carbon-flux';
 import emissionsDeforestation from 'components/widgets/climate/emissions-deforestation';
 import emissionsDeforestationDrivers from 'components/widgets/climate/emissions-deforestation-drivers';
-import carbonStock from 'components/widgets/climate/carbon-stock';
+// import carbonStock from 'components/widgets/climate/carbon-stock';
 
 // Land Use
 import economicImpact from 'components/widgets/land-use/economic-impact';
@@ -111,7 +111,7 @@ export default {
   emissionsDeforestationDrivers,
   woodyBiomass,
   // soilBiomass,
-  carbonStock,
+  // carbonStock,
 
   // land use
   economicImpact,
