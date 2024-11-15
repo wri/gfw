34991d0b0a763e5609a191632d64bb455e87a61a
Luis Zenteno <luis.zenteno.5@wriconsultant.org>
2024-11-15 11:46:43 -0600

    feat(sentences): refactor code to support IDN stats with original queries

--- services/sentences.js
+++ services/sentences.js
@@ -1,9 +1,18 @@
+import { all, spread } from 'axios';
 import { formatNumber } from 'utils/format';
+
+import sortBy from 'lodash/sortBy';
+import groupBy from 'lodash/groupBy';
+import sumBy from 'lodash/sumBy';
+import max from 'lodash/max';
+import reverse from 'lodash/reverse';
 import isEmpty from 'lodash/isEmpty';
 
 import {
   getExtentNaturalForest,
   getLossNaturalForest,
+  getExtent,
+  getLoss,
 } from 'services/analysis-cached';
 
 const ADMINS = {
@@ -35,7 +44,107 @@ export const adminSentences = {
   end: '.',
 };
 
-export const getSentenceData = async (params = GLOBAL_LOCATION) => {
+const getSentenceDataForIdn = (params = GLOBAL_LOCATION) =>
+  all([
+    getExtent(params),
+    getExtent({ ...params, forestType: 'plantations' }),
+    getExtent({
+      ...params,
+      forestType: 'primary_forest',
+      extentYear: 2000,
+    }),
+    getLoss(params),
+    getLoss({ ...params, forestType: 'plantations' }),
+    getLoss({ ...params, forestType: 'primary_forest' }),
+  ]).then(
+    spread(
+      (
+        totalExtent,
+        totalPlantationsExtent,
+        totalPrimaryExtent,
+        totalLoss,
+        totalPlantationsLoss,
+        totalPrimaryLoss
+      ) => {
+        const extent = totalExtent.data.data;
+        const loss = totalLoss.data.data;
+        const plantationsLoss = totalPlantationsLoss.data.data;
+        const plantationsExtent = totalPlantationsExtent.data.data;
+        const primaryExtent = totalPrimaryExtent.data.data;
+
+        // group over years
+        const groupedLoss = plantationsLoss && groupBy(loss, 'year');
+        const groupedPlantationsLoss =
+          plantationsLoss && groupBy(plantationsLoss, 'year');
+
+        const primaryLoss = totalPrimaryLoss.data.data;
+        const latestYear = max(Object.keys(groupedLoss));
+
+        const latestPlantationLoss = groupedPlantationsLoss[latestYear] || null;
+        const latestLoss = groupedLoss[latestYear] || null;
+
+        // sum over different bound1 within year
+        const summedPlantationsLoss =
+          latestPlantationLoss &&
+          latestPlantationLoss.length &&
+          latestPlantationLoss[0].area
+            ? sumBy(latestPlantationLoss, 'area')
+            : 0;
+        const summedPlantationsEmissions =
+          latestPlantationLoss &&
+          latestPlantationLoss.length &&
+          latestPlantationLoss[0].emissions
+            ? sumBy(latestPlantationLoss, 'emissions')
+            : 0;
+        const summedLoss =
+          latestLoss && latestLoss.length && latestLoss[0].area
+            ? sumBy(latestLoss, 'area')
+            : 0;
+        const summedEmissions =
+          latestLoss && latestLoss.length && latestLoss[0].emissions
+            ? sumBy(latestLoss, 'emissions')
+            : 0;
+
+        const data = {
+          totalArea:
+            extent && extent.length && extent[0].total_area
+              ? sumBy(extent, 'total_area')
+              : 0,
+          extent:
+            extent && extent.length && extent[0].extent
+              ? sumBy(extent, 'extent')
+              : 0,
+          plantationsExtent:
+            plantationsExtent &&
+            plantationsExtent.length &&
+            plantationsExtent[0].extent
+              ? sumBy(plantationsExtent, 'extent')
+              : 0,
+          primaryExtent:
+            primaryExtent && primaryExtent.length && primaryExtent[0].extent
+              ? sumBy(primaryExtent, 'extent')
+              : 0,
+          totalLoss: {
+            area: summedLoss || 0,
+            year: latestYear || 0,
+            emissions: summedEmissions || 0,
+          },
+          plantationsLoss: {
+            area: summedPlantationsLoss || 0,
+            emissions: summedPlantationsEmissions || 0,
+          },
+          primaryLoss:
+            primaryLoss && primaryLoss.length
+              ? reverse(sortBy(primaryLoss, 'year'))[0]
+              : {},
+        };
+
+        return data;
+      }
+    )
+  );
+
+const getNaturalForestSentenceData = async (params = GLOBAL_LOCATION) => {
   try {
     const extentNaturalForestResponse = await getExtentNaturalForest(params);
     const lossNaturalForestResponse = await getLossNaturalForest({
@@ -99,6 +208,14 @@ export const getSentenceData = async (params = GLOBAL_LOCATION) => {
       primaryLoss: {},
     };
   }
+};
+
+export const getSentenceData = async (params = GLOBAL_LOCATION) => {
+  if (params.adm0 === 'IDN') {
+    return getSentenceDataForIdn(params);
+  }
+
+  return getNaturalForestSentenceData(params);
 };
 
 export const getContextSentence = (location, geodescriber, adminSentence) => {
@@ -248,7 +365,9 @@ export const parseSentence = (
     sentence = countrySpecific[adm0];
   }
 
-  sentence += co2Emissions;
+  if (adm0 !== 'IDN') {
+    sentence += co2Emissions;
+  }
 
   return {
     sentence,
