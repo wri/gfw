3ca905a96f1c0eb55f3a49f46d82561343d1b162
Luis Zenteno <luis.zenteno.5@wriconsultant.org>
2024-12-11 15:04:01 -0600

    TEMP: split gfw and rw keys

--- services/shape.js
+++ services/shape.js
@@ -3,7 +3,9 @@ import request from 'utils/request';
 import { GFW_API, GFW_STAGING_API } from 'utils/apis';
 
 const GFW_API_URL =
-  process.env.NEXT_PUBLIC_FEATURE_ENV === 'staging' ? GFW_STAGING_API : GFW_API;
+  process.env.NEXT_PUBLIC_RW_FEATURE_ENV === 'staging'
+    ? GFW_STAGING_API
+    : GFW_API;
 
 const QUERIES = {
   ogrConvert: '/v2/ogr/convert',
