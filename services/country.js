4d2cce23e2e64f45440e02afe5696cf7103a38d2
Willian Viana <willian.batista.viana@gmail.com>
2024-12-17 12:15:18 -0300

    feat(fao): remove outdated method getRanking

--- services/country.js
+++ services/country.js
@@ -14,8 +14,6 @@ const SQL_QUERIES = {
     "SELECT gid_2 as id, name_2 as name FROM gadm36_adm2 WHERE iso = '{iso}' AND gid_1 = '{adm1}' AND type_2 NOT IN ('Waterbody', 'Water body', 'Water Body') ORDER BY name",
   getCountryLinks:
     'SELECT iso, external_links FROM external_links_gfw WHERE forest_atlas is true',
-  getRanking:
-    "WITH mytable AS (SELECT fao.iso, fao.name, fao.forest_primary, fao.extent forest_extent, a.land as area_ha FROM gfw2_countries as fao INNER JOIN umd_nat_staging as a ON fao.iso = a.iso WHERE fao.forest_primary > 0 AND a.year = 2001 AND a.thresh = 30), rank AS ( SELECT forest_extent * (forest_primary/100)/area_ha * 100 as percent_primary ,iso from mytable ORDER BY percent_primary DESC), item as (select percent_primary from rank where iso = '{adm0}') select count(*) as rank from rank WHERE percent_primary > (select percent_primary from item )",
 };
 
 const convertToOptions = (countries) =>
@@ -45,11 +43,6 @@ export const getSubRegionsProvider = (adm0, adm1, token) => {
 
 export const getCountryLinksProvider = (token) => {
   const url = `/sql?q=${SQL_QUERIES.getCountryLinks}`;
-  return cartoRequest.get(url, { cancelToken: token });
-};
-
-export const getRanking = ({ adm0, token }) => {
-  const url = `/sql?q=${SQL_QUERIES.getRanking}`.replace('{adm0}', adm0);
   return cartoRequest.get(url, { cancelToken: token });
 };
 
