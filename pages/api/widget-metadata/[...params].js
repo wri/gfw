cb113b19b3316ba1d7d887508faed5a06e82dcb1
Willian Viana <willian.batista.viana@gmail.com>
2025-05-22 10:22:57 -0300

    test

--- pages/api/widget-metadata/[...params].js
+++ pages/api/widget-metadata/[...params].js
@@ -9,7 +9,13 @@ export default async (req, res) => {
     const path = req.query.params.join('/');
     const url = `${GFW_METADATA_API_URL}/${path}/`;
 
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Cache-Control': 'no-cache',
+        Pragma: 'no-cache',
+      },
+    });
 
     if (!response.ok) {
       throw new Error(`Request failed with status ${response.status}`);
--- utils/request.js
+++ utils/request.js
@@ -97,6 +97,7 @@ export const metadataWidgetRequest = {
     const baseUrl = getWidgetBaseUrl();
     const config = {
       method: 'GET',
+      cache: 'no-store',
       ...defaultRequestConfig,
       ...options,
     };
