112903e201ff169b644c4ff4efba55aaeca8f5f7
Luis Zenteno <luis.zenteno.5@wriconsultant.org>
2025-07-01 18:05:45 -0600

    feat(legend-item-button-info): add tooltips

---
+++ components/legend/components/legend-item-toolbar/legend-item-button-info/utils.js
@@ -0,0 +1,6 @@
+export const LAYERS_TOOLTIPS = Object.freeze({
+  'intact-forest-landscapes': `Identifies the world's last remaining unfragmented forest landscapes, large enough to retain all native biodiversity and showing no signs of human alteration as of the year 2020. This layer also shows the reduction in the extent of Intact Forest Landscapes from 2000 to 2020.`,
+  'global-mangrove-extent': `Global mangrove coverage`,
+  'tree-plantations': `Shows the coverage of tree plantations globally.`,
+  'key-biodiversity-areas': `Displays Key Biodiversity Areas, defined as "sites contributing significantly to the global persistence of biodiversity"`,
+});
