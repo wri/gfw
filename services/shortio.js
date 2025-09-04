ec10cfd54b16c95b86d03cc9598590e4e0752992
Luis Zenteno <luis.zenteno.5@wriconsultant.org>
2025-09-04 12:21:28 -0600

    feat(FLAG-1351): keep bit.ly request while we migrate fully to short.io

--- services/shortio.js
+++ services/shortio.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-export const getShortenUrl = async (longUrl) => {
+export const getShortenUrl = async ({ longUrl, path }) => {
   return axios.post('/api/shortio', {
     longUrl,
+    path,
   });
 };
