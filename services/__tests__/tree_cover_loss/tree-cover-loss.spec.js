30039b9a247f475661e690dced5a7cdf70b6ec80
Justin Terry <justin.terry@wri.org>
2026-01-21 13:28:21 -0800

    Add test for geostore in tree-cover-loss-spec

--- services/__tests__/tree_cover_loss/tree-cover-loss.spec.js
+++ services/__tests__/tree_cover_loss/tree-cover-loss.spec.js
@@ -64,7 +64,7 @@ describe('getLossNaturalForest', () => {
     expect(originalGetLossNaturalForest).toHaveBeenCalled();
   });
 
-  it.skip("Calls the legacy service for country types that don't need to be downloaded", async () => {
+  it("Calls the legacy service for country types that don't need to be downloaded", async () => {
     // act
     await getLossNaturalForest({
       type: 'country',
@@ -83,12 +83,52 @@ describe('getLossNaturalForest', () => {
       download: false,
     });
 
+    expect(originalGetLossNaturalForest).toHaveBeenCalled();
+  });
+
+  it("Calls the new service for geostores that don't need to be downloaded", async () => {
+    const geostore = {
+      id: '351cfa10a38f86eeacad8a86ab7ce845',
+      geojson: {
+        features: [
+          {
+            type: 'Feature',
+            geometry: {
+              type: 'Polygon',
+              coordinates: [
+                [
+                  [112.371093750044, -1.71406936894705],
+                  [112.54687500004, -2.35087223984772],
+                  [113.475219726588, -2.08739834101191],
+                  [112.371093750044, -1.71406936894705],
+                ],
+              ],
+            },
+          },
+        ],
+        type: 'FeatureCollection',
+      },
+    };
+    await getLossNaturalForest({
+      type: 'geostore',
+      adm0: '351cfa10a38f86eeacad8a86ab7ce845',
+      pathname: '/dashboards/[[...location]]',
+      locationType: 'geostore',
+      geostore,
+      threshold: 30,
+      startYear: 2021,
+      endYear: 2024,
+      extentYear: 2010,
+      token: {
+        promise: {},
+      },
+      dashboard: true,
+      status: 'saved',
+      download: false,
+    });
+
     expect(getTreeCoverLossAnalytics).toHaveBeenCalledWith(
-      expect.objectContaining({
-        adm0: 'BRA',
-        adm1: undefined,
-        adm2: undefined,
-      }),
+      expect.objectContaining({ geostore }),
       { startYear: 2001, endYear: 2024 },
       0
     );
