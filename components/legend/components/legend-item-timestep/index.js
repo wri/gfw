fca2d006aa5e205759e5e8bc830fcbfcdecab94a
Willian Viana <willian.batista.viana@gmail.com>
2026-02-12 17:09:31 -0300

    chore(timestep): add segments file to calculate grouped and linear ranges

--- components/legend/components/legend-item-timestep/index.js
+++ components/legend/components/legend-item-timestep/index.js
@@ -13,6 +13,7 @@ import {
   formatDate,
   getTicks,
 } from './utils';
+import { buildSegments, mapIndexToSegment } from './segments';
 
 export class TimestepContainer extends PureComponent {
   timelineParams = null;
@@ -31,6 +32,7 @@ export class TimestepContainer extends PureComponent {
     const { timelineParams } = activeLayer;
 
     this.timelineParams = timelineParams;
+    this.segments = buildSegments(timelineParams);
   }
 
   componentDidUpdate(prevProps) {
@@ -43,6 +45,7 @@ export class TimestepContainer extends PureComponent {
     // Should we use timelineParams directly from params instead of doing this? I think so
     if (!isEqual(timelineParams, prevTimelineParams)) {
       this.timelineParams = timelineParams;
+      this.segments = buildSegments(timelineParams);
       this.forceUpdate();
     }
   }
@@ -79,8 +82,20 @@ export class TimestepContainer extends PureComponent {
 
   handleOnAfterChange = (range) => {
     const { activeLayer, handleChange } = this.props;
+    const [startOriginal, endOriginal, trimOriginal] = range;
+    let end = endOriginal;
+    let trim = trimOriginal;
 
-    const formattedRange = this.formatRange([range[0], range[1], range[2]]);
+    // For segmented timelines, snap the selection to the nearest segment center
+    if (this.segments && this.segments.length) {
+      const hit = mapIndexToSegment(this.segments, end);
+      if (hit && hit.snappedIndex != null) {
+        end = hit.snappedIndex;
+        trim = hit.snappedIndex;
+      }
+    }
+
+    const formattedRange = this.formatRange([startOriginal, end, trim]);
     handleChange(formattedRange, activeLayer);
   };
 
@@ -97,13 +112,27 @@ export class TimestepContainer extends PureComponent {
   formatValue = (value) => {
     const { minDate, dateFormat, interval } = this.timelineParams;
 
+    // If we have segments, show the segment label instead of the raw date
+    if (this.segments && this.segments.length) {
+      const hit = mapIndexToSegment(this.segments, value);
+      if (hit && hit.segment) {
+        return hit.segment.label;
+      }
+    }
+
+    // Fallback: original linear behavior
     return formatDatePretty(addToDate(minDate, value, interval), dateFormat);
   };
 
   setMarks = ({ marks, customTimelineMarks }) => {
-    if (customTimelineMarks?.length > 0) {
-      return customTimelineMarks.reduce((acc, item) => {
-        acc[item.datasetDate] = item.label;
+    if (
+      customTimelineMarks?.length > 0 &&
+      this.segments &&
+      this.segments.length
+    ) {
+      // Place labels at the segment centers so they align with snapping positions
+      return this.segments.reduce((acc, segment) => {
+        acc[segment.centerIndex] = segment.label;
         return acc;
       }, {});
     }
