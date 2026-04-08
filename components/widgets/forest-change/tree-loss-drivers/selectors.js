25b2dea38a55faa57e43f1ff033514fd35cc380e
Willian Viana <willian.batista.viana@gmail.com>
2026-04-08 13:11:32 -0300

    fix(datamart): copy same structure of _tree-loss-drivers but remove the datamart request

--- components/widgets/forest-change/tree-loss-drivers/selectors.js
+++ components/widgets/forest-change/tree-loss-drivers/selectors.js
@@ -1,258 +1,127 @@
 import { createSelector, createStructuredSelector } from 'reselect';
 import isEmpty from 'lodash/isEmpty';
-import sum from 'lodash/sum';
-import sumBy from 'lodash/sumBy';
-import entries from 'lodash/entries';
-import groupBy from 'lodash/groupBy';
-import findIndex from 'lodash/findIndex';
-import { formatNumber } from 'utils/format';
-import sortBy from 'lodash/sortBy';
-import { yearTicksFormatter } from 'components/widgets/utils/data';
 
 import tscLossCategories from 'data/tsc-loss-categories.json';
+import { formatNumber } from 'utils/format';
 
-// get list data
-const getLoss = (state) => state.data && state.data.loss;
-const getIndicator = (state) => state.indicator;
-const getSettings = (state) => state.settings;
-const getLocationName = (state) => state.locationLabel;
+const getData = (state) => state.data;
 const getColors = (state) => state.colors;
-const getSentences = (state) => state.sentences;
-const getAlerts = (state) => state.alerts;
+const getSettings = (state) => state.settings;
 const getTitle = (state) => state.title;
-const getAdm0 = (state) => state.adm0;
+const getSentences = (state) => state.sentences;
 const getLocationLabel = (state) => state.locationLabel;
+const getSortedCategories = () =>
+  tscLossCategories.sort((a, b) => (a.position > b.position ? 1 : -1));
 
-export const getPermCats = createSelector([], () =>
-  tscLossCategories.filter((x) => x.permanent).map((el) => el.value.toString())
-);
+const groupedLegends = {
+  'Hard commodities': 'Drivers of deforestation',
+  Logging: 'Drivers of temporary disturbances',
+  'Other natural disturbances': 'Drivers of temporary disturbances',
+  'Permanent agriculture': 'Drivers of deforestation',
+  'Settlements & Infrastructure': 'Drivers of deforestation',
+  'Shifting cultivation': 'Drivers of temporary disturbances',
+  Wildfire: 'Drivers of temporary disturbances',
+  Forestry: 'Drivers of deforestation',
+};
 
-export const mergeDataWithCetagories = createSelector(
-  [getLoss, getPermCats],
-  (data, permCats) => {
-    if (isEmpty(data)) return null;
-
-    return data.map((d) => ({
-      ...d,
-      permanent: permCats.includes(d.bound1),
-    }));
-  }
+export const getPermanentCategories = createSelector(
+  [getSortedCategories],
+  (sortedCategories) =>
+    sortedCategories.filter((x) => x.permanent).map((el) => el.value.toString())
 );
 
 export const getFilteredData = createSelector(
-  [mergeDataWithCetagories, getSettings, getPermCats],
-  (data, settings, permCats) => {
-    if (isEmpty(data)) return null;
-    const { startYear, endYear } = settings;
-    const filteredByYear = data.filter(
-      (d) => d.year >= startYear && d.year <= endYear
-    );
-    const permanentData = filteredByYear.filter((d) =>
-      permCats.includes(d.bound1)
-    );
-    return settings.tscDriverGroup === 'permanent'
-      ? permanentData
-      : filteredByYear;
+  [getData, getSortedCategories],
+  (data, sortedCategories) => {
+    console.log('tree-loss-drivers', data);
+    return data && data.length
+      ? sortedCategories
+          .map(({ value }) => data.find((item) => item.driver_type === value))
+          .filter((item) => item)
+      : [];
   }
 );
 
-export const getAllLoss = createSelector(
-  [mergeDataWithCetagories, getSettings],
-  (data, settings) => {
-    if (isEmpty(data)) return null;
-    const { startYear, endYear } = settings;
-    return data.filter((d) => d.year >= startYear && d.year <= endYear);
-  }
-);
+export const parseData = createSelector(
+  [getFilteredData, getColors],
+  (filteredData, colors) => {
+    if (!filteredData || isEmpty(filteredData)) return null;
 
-export const getDrivers = createSelector(
-  [getFilteredData, getSettings, getPermCats],
-  (data, settings, permCats) => {
-    if (isEmpty(data)) return null;
-    const groupedData = groupBy(sortBy(data, 'area').reverse(), 'bound1');
-    const filteredData = Object.keys(groupedData)
-      .filter((key) => permCats.includes(key))
-      .reduce(
-        (obj, key) => ({
-          ...obj,
-          [key]: groupedData[key],
-        }),
-        {}
-      );
-
-    const groupedLoss =
-      settings.tscDriverGroup === 'permanent' ? filteredData : groupedData;
-    const sortedLoss = !isEmpty(groupedLoss)
-      ? sortBy(
-          Object.keys(groupedLoss).map((k) => {
-            const cat = tscLossCategories.find((c) => c.value.toString() === k);
-            return {
-              driver: k,
-              position: cat && cat.position,
-              area: sumBy(groupedLoss[k], 'area'),
-              permanent: permCats.includes(k),
-            };
-          }),
-          'area'
-        )
-      : permCats.map((x) => ({
-          driver: x.toString(),
-          area: 0.0,
-        }));
-    return sortedLoss;
-  }
-);
-
-// get lists selected
-export const parseData = createSelector([getFilteredData], (data) => {
-  if (isEmpty(data)) return null;
-  const groupedData = groupBy(data, 'year');
-  const x = Object.keys(groupedData).map((y) => {
-    const groupedByBound = groupBy(groupedData[y], 'bound1');
-    const datakeys = entries(groupedByBound).reduce((acc, [key, value]) => {
-      const areaSum = sumBy(value, 'area');
-      return {
-        ...acc,
-        [`class_${key}`]: areaSum < 1000 ? Math.round(areaSum) : areaSum,
-      };
-    }, {});
-    return {
-      year: y,
-      total: sum(Object.values(datakeys)),
-      ...datakeys,
-    };
-  });
-  return x;
-});
-
-export const parseConfig = createSelector(
-  [getColors, getFilteredData, getDrivers, getSettings],
-  (colors, data, drivers, settings) => {
-    if (isEmpty(data)) return null;
-    const { highlighted } = settings || {};
-    const yKeys = {};
     const categoryColors = colors.lossDrivers;
-    sortBy(drivers, 'position').forEach((k) => {
-      yKeys[`class_${k.driver}`] = {
-        fill: categoryColors[k.driver],
-        stackId: 1,
-        opacity: !highlighted || (highlighted && k.permanent) ? 1 : 0.3,
-      };
-    });
-    let tooltip = [
-      {
-        key: 'year',
+    const totalLoss = filteredData.reduce(
+      (acc, { loss_area_ha, driver_type }) => {
+        if (driver_type === 'Unknown') {
+          return acc;
+        }
+
+        return acc + loss_area_ha;
       },
-      {
-        key: 'total',
-        label: 'Total',
-        unitFormat: (value) =>
-          formatNumber({ num: value, unit: 'ha', spaceUnit: true }),
-      },
-    ];
-    tooltip = tooltip.concat(
-      sortBy(drivers, 'position')
-        .map((d) => {
-          const tscCat = tscLossCategories.find((c) => c.value === d.driver);
-          const label = tscCat && tscCat.label;
-          return {
-            key: `class_${d.driver}`,
-            label,
-            color: categoryColors[d.driver],
-            unitFormat: (value) =>
-              formatNumber({ num: value, unit: 'ha', spaceUnit: true }),
-          };
-        })
-        .reverse()
+      0
     );
 
-    const insertIndex = findIndex(tooltip, { key: 'class_Urbanization' });
-    if (insertIndex > -1) {
-      tooltip.splice(insertIndex, 0, {
-        key: 'break',
-        label: 'Drivers of permanent deforestation:',
+    return filteredData
+      .filter((item) => item.driver_type !== 'Unknown')
+      .map(({ driver_type, loss_area_ha }) => {
+        return {
+          label: driver_type,
+          value: loss_area_ha,
+          category: groupedLegends[driver_type],
+          color: categoryColors[driver_type],
+          percentage: (loss_area_ha * 100) / totalLoss,
+        };
       });
+  }
+);
+
+export const parseTitle = createSelector(
+  [getTitle, getLocationLabel],
+  (title, location) => {
+    let selectedTitle = title.initial;
+    if (location === 'global') {
+      selectedTitle = title.global;
     }
-
-    const forestryIndex = tooltip.findIndex(
-      (element) => element.key === 'class_Forestry'
-    );
-    const agricultureIndex = tooltip.findIndex(
-      (element) => element.key === 'class_Shifting agriculture'
-    );
-
-    const rearrengedTooltips = [...tooltip];
-
-    delete rearrengedTooltips[forestryIndex];
-    delete rearrengedTooltips[agricultureIndex];
-
-    rearrengedTooltips.splice(2, 0, tooltip[forestryIndex]);
-    rearrengedTooltips.splice(3, 0, tooltip[agricultureIndex]);
-
-    return {
-      height: 250,
-      xKey: 'year',
-      yKeys: {
-        bars: yKeys,
-      },
-      xAxis: {
-        tickFormatter: yearTicksFormatter,
-      },
-      unit: 'ha',
-      tooltip: rearrengedTooltips,
-    };
+    return selectedTitle;
   }
 );
 
 export const parseSentence = createSelector(
-  [
-    getFilteredData,
-    getIndicator,
-    getAllLoss,
-    getSettings,
-    getLocationName,
-    getSentences,
-    getPermCats,
-  ],
-  (data, indicator, allLoss, settings, locationName, sentences, permCats) => {
-    if (isEmpty(data)) return null;
-    const {
-      initial,
-      initialWithIndicator,
-      globalInitial,
-      globalWithIndicator,
-      noLoss,
-    } = sentences;
+  [getFilteredData, getSentences, getSettings, getLocationLabel],
+  (filteredData, sentences, settings, location) => {
+    if (!filteredData) return null;
+    const { globalInitial, initial } = sentences;
     const { startYear, endYear } = settings;
+    const sentence = location === 'global' ? globalInitial : initial;
 
-    const filteredLoss =
-      data && data.filter((x) => permCats.includes(x.bound1));
+    const totalLoss = filteredData.reduce(
+      (acc, { loss_area_ha, driver_type }) => {
+        if (driver_type === 'Unknown') {
+          return acc;
+        }
 
-    const permLoss =
-      (filteredLoss && filteredLoss.length && sumBy(filteredLoss, 'area')) || 0;
-    const totalLoss =
-      (allLoss && allLoss.length && sumBy(allLoss, 'area')) || 0;
-    const permPercent = (permLoss && (permLoss / totalLoss) * 100) || 0;
+        return acc + loss_area_ha;
+      },
+      0
+    );
 
-    let sentence = indicator ? initialWithIndicator : initial;
+    const permanentLoss = filteredData.reduce(
+      (acc, { driver_type, loss_area_ha }) => {
+        if (groupedLegends[driver_type] !== 'Drivers of deforestation') {
+          return acc;
+        }
 
-    if (!permLoss) sentence = noLoss;
-
-    if (locationName === 'global') {
-      sentence = indicator ? globalWithIndicator : globalInitial;
-    }
+        return acc + loss_area_ha;
+      },
+      0
+    );
 
     const params = {
-      location: locationName === 'global' ? 'Globally' : locationName,
-      indicator: indicator && indicator.label,
+      location,
       startYear,
       endYear,
-      permPercent: formatNumber({ num: permPercent, unit: '%' }),
-      component: {
-        key: 'deforestation',
-        tooltip:
-          'The drivers of permanent deforestation are mainly urbanization and commodity-driven deforestation. Shifting agriculture may or may not lead to deforestation, depending upon the impact and permanence of agricultural activities.',
-      },
+      lossPercentage: formatNumber({
+        num: (permanentLoss * 100) / totalLoss,
+        unit: '%',
+      }),
     };
 
     return {
@@ -262,52 +131,8 @@ export const parseSentence = createSelector(
   }
 );
 
-export const parseTitle = createSelector(
-  [getTitle, getLocationName],
-  (title, name) => {
-    let selectedTitle = title.initial;
-    if (name === 'global') {
-      selectedTitle = title.global;
-    }
-    return selectedTitle;
-  }
-);
-
-export const parseAlerts = createSelector(
-  [getAlerts, getAdm0, getLocationLabel],
-  (alerts, adm0, locationLabel) => {
-    const countriesWithNewWarningText = [
-      'CMR',
-      'CIV',
-      'COD',
-      'GNQ',
-      'GAB',
-      'GHA',
-      'GIN',
-      'GNB',
-      'LBR',
-      'MDG',
-      'COG',
-      'SLE',
-    ];
-
-    if (countriesWithNewWarningText.includes(adm0)) {
-      return [
-        {
-          text: `The methods behind this data have changed over time, resulting in an underreporting of tree cover loss in ${locationLabel} prior to 2015. We advise against comparing the data before/after 2015 in ${locationLabel}. [Read more here](https://www.globalforestwatch.org/blog/data-and-research/tree-cover-loss-satellite-data-trend-analysis/).`,
-          visible: ['global', 'country', 'geostore', 'aoi', 'wdpa', 'use'],
-        },
-      ];
-    }
-
-    return adm0 === 'IDN' ? alerts.indonesia : alerts.default;
-  }
-);
-
 export default createStructuredSelector({
   data: parseData,
-  config: parseConfig,
-  sentence: parseSentence,
   title: parseTitle,
-  alerts: parseAlerts,
+  sentence: parseSentence,
 });
--- services/analysis-cached.js
+++ services/analysis-cached.js
@@ -16,9 +16,8 @@ const VIIRS_START_YEAR = 2012;
 const PINNED_GAIN_VERSION = 'v20251209';
 
 const SQL_QUERIES = {
-  // query used by the tree cover loss by dominant driver widget without datamart
   lossTsc:
-    'SELECT tsc_tree_cover_loss_drivers__driver, umd_tree_cover_loss__year, SUM(umd_tree_cover_loss__ha) AS umd_tree_cover_loss__ha, SUM("gfw_gross_emissions_co2e_all_gases__Mg") AS "gfw_gross_emissions_co2e_all_gases__Mg" FROM data {WHERE} GROUP BY tsc_tree_cover_loss_drivers__driver, umd_tree_cover_loss__year',
+    'SELECT wri_google_tree_cover_loss_drivers__driver, umd_tree_cover_loss__year, SUM(umd_tree_cover_loss__ha) AS umd_tree_cover_loss__ha, SUM("gfw_gross_emissions_co2e_all_gases__Mg") AS "gfw_gross_emissions_co2e_all_gases__Mg" FROM data {WHERE} GROUP BY wri_google_tree_cover_loss_drivers__driver, umd_tree_cover_loss__year',
   loss: 'SELECT {select_location}, umd_tree_cover_loss__year, SUM(umd_tree_cover_loss__ha) AS umd_tree_cover_loss__ha, SUM("gfw_gross_emissions_co2e_all_gases__Mg") AS "gfw_gross_emissions_co2e_all_gases__Mg" FROM data {WHERE} GROUP BY umd_tree_cover_loss__year, {location} ORDER BY umd_tree_cover_loss__year, {location}',
   lossNaturalForest: `SELECT {select_location}, sbtn_natural_forests__class, umd_tree_cover_loss__year, SUM(umd_tree_cover_loss__ha) AS umd_tree_cover_loss__ha, SUM("gfw_gross_emissions_co2e_all_gases__Mg") AS gfw_gross_emissions_co2e_all_gases__Mg FROM data {WHERE} GROUP BY sbtn_natural_forests__class, umd_tree_cover_loss__year, {location}`,
   lossFires:
@@ -30,7 +29,7 @@ const SQL_QUERIES = {
   emissionsLossOTF:
     'SELECT umd_tree_cover_loss__year, SUM(area__ha), SUM("gfw_forest_carbon_gross_emissions__Mg_CO2e") FROM data WHERE umd_tree_cover_density_2000__threshold >= {threshold} AND umd_tree_cover_loss__year >= {startYear} AND umd_tree_cover_loss__year <= {endYear} GROUP BY umd_tree_cover_loss__year ORDER BY umd_tree_cover_loss__year&geostore_origin={geostoreOrigin}&geostore_id={geostoreId}',
   emissionsByDriver:
-    'SELECT tsc_tree_cover_loss_drivers__driver, umd_tree_cover_loss__year, SUM("gfw_gross_emissions_co2e_all_gases__Mg") AS "gfw_gross_emissions_co2e_all_gases__Mg", SUM("gfw_full_extent_gross_emissions_non_CO2__Mg_CO2e") AS "gfw_gross_emissions_co2e_non_co2__Mg", SUM("gfw_full_extent_gross_emissions_CO2_only__Mg_CO2") AS "gfw_gross_emissions_co2e_co2_only__Mg" FROM data {WHERE} GROUP BY tsc_tree_cover_loss_drivers__driver, umd_tree_cover_loss__year',
+    'SELECT wri_google_tree_cover_loss_drivers__driver, umd_tree_cover_loss__year, SUM("gfw_gross_emissions_co2e_all_gases__Mg") AS "gfw_gross_emissions_co2e_all_gases__Mg", SUM("gfw_full_extent_gross_emissions_non_CO2__Mg_CO2e") AS "gfw_gross_emissions_co2e_non_co2__Mg", SUM("gfw_full_extent_gross_emissions_CO2_only__Mg_CO2") AS "gfw_gross_emissions_co2e_co2_only__Mg" FROM data {WHERE} GROUP BY wri_google_tree_cover_loss_drivers__driver, umd_tree_cover_loss__year',
   carbonFlux:
     'SELECT SUM("gfw_net_flux_co2e__Mg") AS "gfw_net_flux_co2e__Mg", SUM("gfw_gross_cumulative_aboveground_belowground_co2_removals__Mg") AS "gfw_gross_cumulative_aboveground_belowground_co2_removals__Mg", SUM("gfw_gross_emissions_co2e_all_gases__Mg") AS "gfw_gross_emissions_co2e_all_gases__Mg", TRUE AS "includes_gain_pixels" FROM data {WHERE}',
   carbonFluxOTF: `SELECT SUM("gfw_forest_carbon_net_flux__Mg_CO2e"), SUM("gfw_forest_carbon_gross_removals__Mg_CO2e"), SUM("gfw_forest_carbon_gross_emissions__Mg_CO2e") FROM data WHERE umd_tree_cover_density_2000__threshold >= {threshold} OR is__umd_tree_cover_gain = 'true'&geostore_origin={geostoreOrigin}&geostore_id={geostoreId}`,
@@ -393,7 +392,6 @@ export const getLossNaturalForest = (params) => {
 
 // summed loss for single location
 export const getLoss = (params) => {
-  // function used by the tree cover loss by dominant driver widget without datamart
   const { forestType, landCategory, ifl, download } = params || {};
   const { loss, lossTsc } = SQL_QUERIES;
   const query = params.lossTsc ? lossTsc : loss;
@@ -433,7 +431,7 @@ export const getLoss = (params) => {
     data: {
       data: response?.data?.map((d) => ({
         ...d,
-        bound1: d.tsc_tree_cover_loss_drivers__driver,
+        bound1: d.wri_google_tree_cover_loss_drivers__driver,
         year: d.umd_tree_cover_loss__year,
         area: d.umd_tree_cover_loss__ha,
         emissions: d.gfw_gross_emissions_co2e_all_gases__Mg,
@@ -481,7 +479,7 @@ export const getEmissions = (params) => {
     data: {
       data: response?.data?.map((d) => ({
         ...d,
-        bound1: d.tsc_tree_cover_loss_drivers__driver,
+        bound1: d.wri_google_tree_cover_loss_drivers__driver,
         year: d.umd_tree_cover_loss__year,
         allGases: d.gfw_gross_emissions_co2e_all_gases__Mg,
         co2Only: d.gfw_gross_emissions_co2e_co2_only__Mg,
