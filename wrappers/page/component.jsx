3d4ead2cbe05fc624ab8a31e92186524f4965183
Daniel Mannarino <daniel.mannarino@gmail.com>
2026-08-05 22:03:43 -0400

    Commit 1

--- wrappers/page/component.jsx
+++ wrappers/page/component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useRouter } from 'next/router';
+import dynamic from 'next/dynamic';
 import { Loader, MediaContextProvider } from '@worldresources/gfw-components';
 import cx from 'classnames';
 
@@ -9,10 +10,15 @@ import { useSetLanguage } from 'utils/lang';
 
 import Head from 'wrappers/head';
 
-import Footer from 'components/footer';
 import ContactUsModal from 'components/modals/contact-us';
 import ErrorMessage from 'components/error-message';
 import SiteHeader from 'components/site-header';
+
+// Footer's own MediaContextProvider (nested inside gfw-components' Footer)
+// has the same SSR-vs-client mismatch issue as the Carousel: it can't
+// reliably reproduce server-side markup that matches the client mount.
+// SiteHeader already avoids this for Header via ssr:false; do the same here.
+const Footer = dynamic(() => import('components/footer'), { ssr: false });
 
 const PageWrapper = ({
   children,
