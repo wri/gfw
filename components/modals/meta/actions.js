4ee66f512c33ac0ffddef09f6d667d3a14dd01b5
Daniel Mannarino <daniel.mannarino@gmail.com>
2026-09-01 16:46:48 -0400

    Normalize metadata proxy responses

--- components/modals/meta/actions.js
+++ components/modals/meta/actions.js
@@ -17,11 +17,7 @@ export const getModalMetaData = createThunkAction(
 
         getMetadata(metakey, metaType)
           .then((response) => {
-            if (metaType === 'widget') {
-              dispatch(setModalMetaData(response.data));
-            } else {
-              dispatch(setModalMetaData(response.data.metadata));
-            }
+            dispatch(setModalMetaData(response.data.metadata));
           })
           .catch(() => {
             dispatch(setModalMetaLoading({ loading: false, error: true }));
